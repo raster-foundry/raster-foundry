186c55c0f9593b323ced5d568a63834c71e20b97
Kevin DeLoach <kdeloach@azavea.com>
2015-08-14 17:36:53 -0400

    Create custom admin model form
    Allows CRUD operations on layers in Django admin.

--- src/rf/apps/core/admin.py
+++ src/rf/apps/core/admin.py
@@ -5,4 +5,44 @@ from __future__ import division
 
 from django.contrib import admin
 
-# Register your models here.
+from apps.core import models
+
+
+class LayerTagInline(admin.TabularInline):
+    extra = 0
+    model = models.LayerTag
+
+
+class LayerImageInline(admin.TabularInline):
+    extra = 0
+    model = models.LayerImage
+
+
+class LayerMetaInline(admin.TabularInline):
+    extra = 0
+    model = models.LayerMeta
+
+
+class LayerAdmin(admin.ModelAdmin):
+    inlines = [LayerTagInline, LayerImageInline, LayerMetaInline]
+    readonly_fields = ('created_at', 'updated_at')
+    fieldsets = (
+        (None, {
+            'fields': (
+                'user', 'name', 'description',
+                ('capture_start', 'capture_end'),
+                ('area', 'area_unit'),
+                'projection', 'srid',
+                'created_at', 'updated_at', 'deleted_at'
+            )
+        }),
+        ('Tiles', {
+            'fields': (
+                'tile_srid', 'tile_format', 'tile_origin', 'resampling',
+                'transparency'
+            )
+        }),
+    )
+
+
+admin.site.register(models.Layer, LayerAdmin)
