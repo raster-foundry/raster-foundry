26f691d64040fae479538f004d0a1327d2f9f19d
Kevin DeLoach <kdeloach@azavea.com>
2015-08-18 19:46:55 -0400

    Remove map from Application
    This removes the global map since not all pages in the application will
    need one. Also, it wasn't working anyway.

--- src/rf/js/src/app.js
+++ src/rf/js/src/app.js
@@ -3,24 +3,12 @@
 var $ = require('jquery'),
     Marionette = require('../shim/backbone.marionette'),
     Backbone = require('../shim/backbone'),
-    views = require('./core/views'),
     models = require('./core/models'),
     router = require('./router').router;
 
 var App = Marionette.Application.extend({
     initialize: function() {
-        //this.setupMap();
         this.setupNavigation();
-    },
-
-    setupMap: function() {
-        this.map = new models.MapModel();
-
-        // This view is intentionally not attached to any region.
-        this._mapView = new views.MapView({
-            model: this.map,
-            el: '#map'
-        });
     },
 
     setupNavigation: function() {
