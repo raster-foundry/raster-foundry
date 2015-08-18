88f49c52e18272de92837387427cc220c6a01691
Kevin DeLoach <kdeloach@azavea.com>
2015-08-18 17:46:44 -0400

    Fix styling issues
    Add jQuery dlmenu plugin to primary bundle and add misc. JS from the
    prototype to fix animations, z-index issues, and metadata panels.

--- src/rf/js/src/home/controllers.js
+++ src/rf/js/src/home/controllers.js
@@ -28,11 +28,11 @@ var HomeController = {
         var el = $('#container').get(0);
         React.render(<Library {...props} />, el);
 
-        $('#dl-menu').dlmenu();
-
         this.mapView = new coreViews.MapView({
             el: document.getElementById('map')
         });
+
+        bindEvents();
     }
 };
 
@@ -50,6 +50,52 @@ var UserController = {
         React.render(<Login {...props} />, el);
     }
 };
+
+// Copied from prototype.
+function bindEvents() {
+    $('#dl-menu').dlmenu();
+
+    // Tooltips
+    $('[data-toggle="tooltip"]').tooltip({
+        container: '.sidebar-utility-content',
+        viewport: '.sidebar'
+    });
+
+    // Layer metadata
+    var layerDetail = $('.layer-detail');
+    $('.list-group-item .list-group-link').click(function(evt) {
+        evt.preventDefault();
+        layerDetail.addClass('active');
+    });
+
+    $('.layer-detail .close').click(function(evt) {
+        evt.preventDefault();
+        layerDetail.addClass('slideOutLeft');
+        setTimeout(function() {
+            layerDetail.removeClass('slideOutLeft active');
+        }, 400);
+    });
+
+    // Image metadata
+    var imageMetadata = $('.image-metadata');
+    $('.view-metadata').click(function(evt) {
+        evt.preventDefault();
+        imageMetadata.addClass('active');
+    });
+
+    $('.image-metadata .close').click(function(evt) {
+        evt.preventDefault();
+        imageMetadata.addClass('slideOutLeft');
+        setTimeout(function() {
+            imageMetadata.removeClass('slideOutLeft active');
+        }, 400);
+    });
+
+    // Layer tools
+    $('.select-all').click(function() {
+        $(this).parent('.utility-tools-secondary').toggleClass('active');
+    });
+}
 
 module.exports = {
     HomeController: HomeController,
