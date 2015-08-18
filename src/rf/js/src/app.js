f60b901f2e905b266350c026d97b070304c88cea
Kevin DeLoach <kdeloach@azavea.com>
2015-08-18 12:39:46 -0400

    Refactor Application
    Split up the constructor and use `initialize` method instead of `start`
    to initialize the application. The purpose is to remove unnecessary event
    handlers.

--- src/rf/js/src/app.js
+++ src/rf/js/src/app.js
@@ -1,20 +1,34 @@
 'use strict';
 
-var Marionette = require('../shim/backbone.marionette'),
-    //views = require('./core/views'),
-    models = require('./core/models');
+var $ = require('jquery'),
+    Marionette = require('../shim/backbone.marionette'),
+    Backbone = require('../shim/backbone'),
+    views = require('./core/views'),
+    models = require('./core/models'),
+    router = require('./router').router;
 
-var App = new Marionette.Application({
+var App = Marionette.Application.extend({
     initialize: function() {
+        //this.setupMap();
+        this.setupNavigation();
+    },
+
+    setupMap: function() {
         this.map = new models.MapModel();
 
-        /*
         // This view is intentionally not attached to any region.
         this._mapView = new views.MapView({
             model: this.map,
             el: '#map'
         });
-        */
+    },
+
+    setupNavigation: function() {
+        $('body').on('click', '[data-url]', function(e) {
+            e.preventDefault();
+            router.navigate($(this).data('url'), { trigger: true });
+        });
+        Backbone.history.start({ pushState: true });
     }
 });
 
--- src/rf/js/src/main.js
+++ src/rf/js/src/main.js
@@ -5,21 +5,10 @@ require('./routes');
 
 // Initialize application.
 
-var $ = require('jquery'),
-    Backbone = require('../shim/backbone'),
-    App = require('./app'),
+var App = require('./app'),
     router = require('./router').router;
 
-App.on('start', function() {
-    $('body').on('click', '[data-url]', function(e) {
-        e.preventDefault();
-        router.navigate($(this).data('url'), { trigger: true });
-    });
-    Backbone.history.start({ pushState: true });
-});
-
-App.start();
-
 // Expose application so we can interact with it via JS console.
-window.RF = App;
+window.RF = {};
+window.RF.app = new App();
 window.RF.router = router;
