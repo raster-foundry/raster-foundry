8e47fedbdfe3d71eced1b40455dbd1223a36ca25
Kevin DeLoach <kdeloach@azavea.com>
2015-08-19 12:23:24 -0400

    Lint
    Merged my last PR into develop too hastily and accidentally introduced
    lint.

--- src/rf/js/src/home/controllers.js
+++ src/rf/js/src/home/controllers.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var React = require('react'),
+var $ = require('jquery'),
+    React = require('react'),
     views = require('./views'),
     router = require('../router').router;
 
