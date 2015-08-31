04a767f540deaf614b7d778521008f7faa254d0b
Laurence Liss <lliss@azavea.com>
2015-08-31 11:52:00 -0400

    Ensure user id has been prepended to api request.
    We don't want any user to be able to upload files and we don't want malicious
    users to be able to overwrite files from another user. We'll use uuids for the
    filenames uploaded to S3 and prepend them the user's id number. We don't want
    to rely on JS to tell us the truth so we'll verify here that the user is who
    they say they are before we sign the request.

--- src/rf/apps/uploads/views.py
+++ src/rf/apps/uploads/views.py
@@ -3,31 +3,50 @@ from __future__ import print_function
 from __future__ import unicode_literals
 from __future__ import division
 
-# NEEDED WHEN WE ADD PROPER AUTHORIZATION
-# from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 
-from rest_framework.response import Response
-from rest_framework import decorators
-from rest_framework.permissions import AllowAny
+from rest_framework import status
+from rest_framework.exceptions import APIException
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.renderers import StaticHTMLRenderer
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from django.conf import settings
 
 import boto
 import boto.auth
 import boto.provider
+import uuid
 
 
-# TODO - THIS SHOULD REQUIRE AUTHORIZATION.
-# TODO - WE'LL WANT TO ENSURE THE USER CAN IN FACT ADD NEW CONTENT TO THE
-# BUCKET BEFORE WE SIGN THE REQUEST.
-@decorators.api_view(['GET'])
-@decorators.permission_classes((AllowAny, ))
-@decorators.renderer_classes((StaticHTMLRenderer, ))
-def sign_upload_request(request, format=None):
-    to_sign = str(request.REQUEST.get('to_sign'))
-    provider = boto.provider.Provider('aws', profile_name=settings.AWS_PROFILE)
-    signer = boto.auth.HmacAuthV1Handler(None, None, provider)
-    signature = signer.sign_string(to_sign)
+class SignUploadRequestView(APIView):
+    permission_classes = [IsAuthenticated]
+    renderer_classes = (StaticHTMLRenderer, )
 
-    return Response(signature)
+    # TODO - WE'LL WANT TO ENSURE THE USER CAN IN FACT ADD NEW CONTENT TO THE
+    # BUCKET BEFORE WE SIGN THE REQUEST.
+    def get(self, request, format=None):
+        user_id = request.user.id
+        bucket_name = settings.AWS_BUCKET_NAME + '/'
+        to_sign = str(request.REQUEST.get('to_sign'))
+
+        # Extract file name from the string to sign and then extract the user
+        # id from that.
+        start = len(bucket_name) + to_sign.find(bucket_name)
+        end = to_sign.rfind('?uploads', start)
+        file_name = to_sign[start:end]
+        sent_user_id = int(file_name[0:file_name.find('-')])
+
+        # Safety check. The front end is not safe. Ensure that the user id
+        # that came from front end JS matches the request user id before
+        # signing.
+        if sent_user_id != user_id:
+            return Response('User mismatch', status=status.HTTP_400_BAD_REQUEST)
+
+        provider = boto.provider.Provider('aws',
+                                          profile_name=settings.AWS_PROFILE)
+        signer = boto.auth.HmacAuthV1Handler(None, None, provider)
+        signature = signer.sign_string(to_sign)
+
+        return Response(signature)
