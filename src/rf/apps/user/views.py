5330f3804828020be897a94681bb98b1ec682f89
Kevin DeLoach <kdeloach@azavea.com>
2015-09-05 00:27:26 -0400

    Remove DRF from user endpoints
    This also simplifies some endpoints by restricting how much information
    we reveal.

--- src/rf/apps/user/views.py
+++ src/rf/apps/user/views.py
@@ -3,192 +3,137 @@ from __future__ import print_function
 from __future__ import unicode_literals
 from __future__ import division
 
-from django.contrib.auth import (authenticate,
-                                 logout as auth_logout,
-                                 login as auth_login)
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
-from django.shortcuts import redirect, get_object_or_404
 from django.contrib.auth.forms import PasswordResetForm
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.sites.shortcuts import get_current_site
+from django.shortcuts import redirect, get_object_or_404
 
 from registration.models import RegistrationProfile
 from registration.forms import RegistrationFormUniqueEmail
 from registration.backends.default.views import RegistrationView
 
-from rest_framework import decorators, status
-from rest_framework.response import Response
-from rest_framework.permissions import AllowAny
+from apps.core.exceptions import BadRequest, Forbidden
+from apps.core.decorators import accepts, api_view, login_required
 
 
-@decorators.api_view(['POST', 'GET'])
-@decorators.permission_classes((AllowAny, ))
-def login(request):
-    response_data = {}
-    status_code = status.HTTP_200_OK
+@api_view
+@accepts('GET', 'POST')
+def login_view(request):
+    if request.method == 'GET':
+        return _user_info(request)
+    elif request.method == 'POST':
+        return _authenticate_user(request)
 
-    if request.method == 'POST':
-        user = authenticate(username=request.REQUEST.get('username'),
-                            password=request.REQUEST.get('password'))
 
-        if user is not None:
-            if user.is_active:
-                auth_login(request, user)
-                response_data = {
-                    'result': 'success',
-                    'username': user.username,
-                    'logged_in': True,
-                    'id': user.id
-                }
-            else:
-                response_data = {
-                    'errors': ['Please activate your account'],
-                    'logged_in': False,
-                    'id': 0
-                }
-                status_code = status.HTTP_400_BAD_REQUEST
-        else:
-            response_data = {
-                'errors': ['Invalid username or password'],
-                'logged_in': False,
-                'id': 0
-            }
-            status_code = status.HTTP_400_BAD_REQUEST
-
-    elif request.method == 'GET':
-        user = request.user
-
-        if user.is_authenticated() and user.is_active:
-            response_data = {
-                'result': 'success',
+def _authenticate_user(request):
+    user = authenticate(username=request.POST.get('username'),
+                        password=request.POST.get('password'))
+    if user:
+        if user.is_active:
+            login(request, user)
+            return {
+                'id': user.id,
                 'username': user.username,
-                'logged_in': True,
-                'id': user.id
             }
         else:
-            response_data = {
-                'result': 'success',
-                'logged_in': False,
-                'id': 0
-            }
-
-        status_code = status.HTTP_200_OK
-
-    return Response(data=response_data, status=status_code)
+            raise Forbidden(errors={
+                'all': ['Please activate your account'],
+            })
+    raise Forbidden(errors={
+        'all': ['Invalid username or password'],
+    })
 
 
-@decorators.api_view(['GET'])
-@decorators.permission_classes((AllowAny, ))
-def logout(request):
-    auth_logout(request)
-
-    if request.is_ajax():
-        response_data = {
-            'result': 'success',
-            'logged_in': False,
-            'id': 0
-        }
-        return Response(data=response_data)
-    else:
-        return redirect('/')
+@login_required
+def _user_info(request):
+    return {
+        'id': request.user.id,
+        'username': request.user.username,
+    }
 
 
-@decorators.api_view(['POST'])
-@decorators.permission_classes((AllowAny, ))
+@api_view
+@accepts('GET')
+def logout_view(request):
+    logout(request)
+    return 'OK'
+
+
+@api_view
+@accepts('POST')
 def sign_up(request):
     view = RegistrationView()
     form = RegistrationFormUniqueEmail(request.POST)
-
     if form.is_valid():
         user = view.register(request, form)
-        response_data = {'result': 'success',
-                         'username': user.username}
-        return Response(data=response_data,
-                        status=status.HTTP_200_OK)
+        return {
+            'user_id': user.id
+        }
     else:
         errors = []
+
         if 'username' not in form.cleaned_data:
-            errors.append("Username is invalid or already in use")
+            errors.append('Username is invalid or already in use')
         if 'password1' not in form.cleaned_data:
-            errors.append("Password must be specified")
+            errors.append('Password must be specified')
         if 'password2' not in form.cleaned_data or \
            form.cleaned_data['password1'] != form.cleaned_data['password2']:
-            errors.append("Passwords do not match")
+            errors.append('Passwords do not match')
         if 'email' not in form.cleaned_data:
-            errors.append("Email is invalid or already in use")
+            errors.append('Email is invalid or already in use')
 
         if len(errors) == 0:
-            errors.append("Invalid data submitted")
+            errors.append('Invalid data submitted')
 
-        response_data = {"errors": errors}
-        return Response(data=response_data,
-                        status=status.HTTP_400_BAD_REQUEST)
+        raise BadRequest(errors={
+            'all': errors
+        })
 
 
-@decorators.api_view(['POST'])
-@decorators.permission_classes((AllowAny, ))
+@api_view
+@accepts('POST')
 def resend(request):
-    # Resend activation email if the key hasn't expired.
+    """
+    Resend activation email if the key hasn't expired.
+    """
     form = PasswordResetForm(request.POST)
     if form.is_valid():
         email = form.cleaned_data['email']
         try:
-            registration_profile = RegistrationProfile.objects.get(
-                user__email=email)
-            if registration_profile.activation_key_expired():
-                response_data = {'errors': ["Activation key expired"]}
-                status_code = status.HTTP_400_BAD_REQUEST
-            else:
-                registration_profile.send_activation_email(
-                    get_current_site(request))
-                response_data = {'result': 'success'}
-                status_code = status.HTTP_200_OK
+            profile = RegistrationProfile.objects.get(user__email=email)
+            profile.send_activation_email(get_current_site(request))
         except ObjectDoesNotExist:
-            response_data = {'errors': ["Registration cannot be found"]}
-            status_code = status.HTTP_400_BAD_REQUEST
-    else:
-        response_data = {'errors': ["Email is invalid"]}
-        status_code = status.HTTP_400_BAD_REQUEST
-
-    return Response(data=response_data, status=status_code)
+            pass
+    return 'OK'
 
 
-@decorators.api_view(['POST'])
-@decorators.permission_classes((AllowAny, ))
+@api_view
+@accepts('POST')
 def forgot(request):
     form = PasswordResetForm(request.POST)
-
     if form.is_valid():
-        email = form.cleaned_data['email']
-        try:
-            # If there are active user(s) that match email
-            next(form.get_users(email))
-            form.save(request=request)
-            response_data = {'result': 'success',
-                             'logged_in': False}
-            status_code = status.HTTP_200_OK
-        except StopIteration:
-            response_data = {'errors': ["Email cannot be found"]}
-            status_code = status.HTTP_400_BAD_REQUEST
-    else:
-        response_data = {'errors': ["Email is invalid"]}
-        status_code = status.HTTP_400_BAD_REQUEST
-
-    return Response(data=response_data, status=status_code)
+        form.save(request=request)
+    return 'OK'
 
 
-@decorators.permission_classes((AllowAny, ))
+@api_view
+@accepts('GET')
 def activate(request, activation_key):
     profile = get_object_or_404(RegistrationProfile,
                                 activation_key=activation_key)
     user = get_object_or_404(User, id=profile.user_id)
 
+    if user.is_active:
+        return redirect('login')
+
     user.is_active = True
     user.save()
 
     # Backend needs to be added, for login without a password.
-    # see: http://cheng.logdown.com/posts/2015/06/03/
-    # django-login-users-without-password
+    # http://cheng.logdown.com/posts/2015/06/03/django-login-users-without-password
     user.backend = 'django.contrib.auth.backends.ModelBackend'
-    auth_login(request, user)
-    return redirect('/activate/')
+
+    login(request, user)
+    return redirect('home_page')
