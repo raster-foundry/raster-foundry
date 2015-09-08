73c495f17226808e9709fb6d98ad97deb6494714
Kevin DeLoach <kdeloach@azavea.com>
2015-09-08 16:00:55 -0400

    Fix users unit tests
    Removes `requests` and replaces `LiveServerTestCase` with `TestCase`.
    The CSRF unit tests were not working so they have been removed.

--- src/rf/apps/user/tests.py
+++ src/rf/apps/user/tests.py
@@ -3,97 +3,57 @@ from __future__ import print_function
 from __future__ import unicode_literals
 from __future__ import division
 
-import requests
-
-from django.test import LiveServerTestCase
 from django.contrib.auth.models import User
+from django.test import TestCase
+from django.test.client import Client
 
 
-class UserTestCase(LiveServerTestCase):
-    HOMEPAGE_URL = 'http://localhost:8081/'
-    LOGIN_URL = 'http://localhost:8081/user/login'
+class UserTestCase(TestCase):
+    HOMEPAGE_URL = '/'
+    LOGIN_URL = '/user/login'
 
     def setUp(self):
+        self.client = Client()
+
         User.objects.create_user(username='bob', email='bob@azavea.com',
                                  password='bob')
 
-    def get_token(self):
-        try:
-            init_response = requests.get(self.HOMEPAGE_URL)
-        except requests.RequestException:
-            init_response = {}
-
-        try:
-            csrf = init_response.cookies['csrftoken']
-        except KeyError:
-            csrf = None
-
-        return csrf
-
     def attempt_login(self, username, password):
-        csrf = self.get_token()
-        try:
-            headers = {'HTTP_X_CSRFTOKEN': csrf}
-            payload = {'username': username, 'password': password}
-            response = requests.post(self.LOGIN_URL, params=payload,
-                                     headers=headers)
-        except requests.RequestException:
-            response = {}
-        return response
+        payload = {'username': username, 'password': password}
+        return self.client.post(self.LOGIN_URL, payload)
 
-    def attempt_login_without_token(self, username, password):
-        try:
-            payload = {'username': username, 'password': password}
-            response = requests.post(self.LOGIN_URL, params=payload)
-        except requests.RequestException:
-            response = {}
-        return response
-
-    def test_no_username_returns_400(self):
+    def test_no_username_returns_403(self):
         response = self.attempt_login('', 'bob')
-        self.assertEqual(response.status_code, 400,
-                         'Incorrect server response. Expected 400 found %s'
+        self.assertEqual(response.status_code, 403,
+                         'Incorrect server response. Expected 403 found %s'
                          % response.status_code)
 
-    def test_no_password_returns_400(self):
+    def test_no_password_returns_403(self):
         response = self.attempt_login('bob', '')
-        self.assertEqual(response.status_code, 400,
-                         'Incorrect server response. Expected 400 found %s'
+        self.assertEqual(response.status_code, 403,
+                         'Incorrect server response. Expected 403 found %s'
                          % response.status_code)
 
-    def test_bad_username_returns_400(self):
+    def test_bad_username_returns_403(self):
         response = self.attempt_login('notbob', 'bob')
-        self.assertEqual(response.status_code, 400,
-                         'Incorrect server response. Expected 400 found %s'
+        self.assertEqual(response.status_code, 403,
+                         'Incorrect server response. Expected 403 found %s'
                          % response.status_code)
 
-    def test_bad_password_returns_400(self):
+    def test_bad_password_returns_403(self):
         response = self.attempt_login('bob', 'badpass')
-        self.assertEqual(response.status_code, 400,
-                         'Incorrect server response. Expected 400 found %s'
+        self.assertEqual(response.status_code, 403,
+                         'Incorrect server response. Expected 403 found %s'
                          % response.status_code)
 
-    def test_bad_credentials_returns_400(self):
+    def test_bad_credentials_returns_403(self):
         response = self.attempt_login('bob1', 'bob1')
-        self.assertEqual(response.status_code, 400,
-                         'Incorrect server response. Expected 400 found %s'
+        self.assertEqual(response.status_code, 403,
+                         'Incorrect server response. Expected 403 found %s'
                          % response.status_code)
 
     def test_good_credentials_returns_200(self):
         response = self.attempt_login('bob', 'bob')
         self.assertEqual(response.status_code, 200,
                          'Incorrect server response. Expected 200 found %s'
-                         % response.status_code)
-
-    # TODO: commented out because it fails and we don't know why yet.
-    # def test_no_token_good_credentials_returns_400(self):
-    #    response = self.attempt_login_without_token('bob', 'bob')
-    #    self.assertEqual(response.status_code, 400,
-    #                     'Incorrect server response. Expected 400 found %s'
-    #                     % response.status_code)
-
-    def test_no_token_bad_credentials_returns_400(self):
-        response = self.attempt_login_without_token('badbob', 'badpass')
-        self.assertEqual(response.status_code, 400,
-                         'Incorrect server response. Expected 400 found %s'
                          % response.status_code)
