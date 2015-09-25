e07f3dc242287d2e69466c836cd34c9800729dda
Laurence Liss <lliss@azavea.com>
2015-09-25 13:55:39 -0400

    Make Backbone model pagination aware.
    Update the backbone model to parse the layer data from the new structure.
    Update model pagination data and create functions to retreive the next/prev
    page as well as find out if there are pages before/after the current one.

--- src/rf/js/src/core/utils.js
+++ src/rf/js/src/core/utils.js
@@ -4,6 +4,12 @@ function asset(relPath) {
     return '/static/' + relPath;
 }
 
+function parseQueryData(url) {
+    var data = url.split('?');
+    return JSON.parse('{"' + decodeURI(data[1]).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}');
+}
+
 module.exports = {
-    asset: asset
+    asset: asset,
+    parseQueryData: parseQueryData
 };
