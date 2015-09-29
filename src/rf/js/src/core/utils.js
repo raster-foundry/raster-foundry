f3a495e59bb0131326df1dd46de3e5b81429bea5
Laurence Liss <lliss@azavea.com>
2015-09-29 09:34:24 -0400

    Add comment to url parsing function.

--- src/rf/js/src/core/utils.js
+++ src/rf/js/src/core/utils.js
@@ -4,6 +4,11 @@ function asset(relPath) {
     return '/static/' + relPath;
 }
 
+/**
+ * Given a URL with GET query params, return an object of query parameters
+ * mapped to values.
+ * http://example.com?test=success&ok=true => {test: 'sucecess', ok: 'true'}
+ */
 function parseQueryData(url) {
     var data = url.split('?');
     return JSON.parse('{"' + decodeURI(data[1]).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}');
