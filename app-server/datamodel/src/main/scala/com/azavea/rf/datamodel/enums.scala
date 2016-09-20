8743c9e6b332a3b98f540e9733a030135aafc08f
Chris Brown <cbrown@azavea.com>
2016-09-20 09:33:34 -0400

    Refactor enums so they are easier to interact with
    Previous attempt at using these enums prevented actually serializing
    them correctly

--- app-server/datamodel/src/main/scala/com/azavea/rf/datamodel/enums.scala
+++ app-server/datamodel/src/main/scala/com/azavea/rf/datamodel/enums.scala
@@ -1,31 +1,42 @@
 package com.azavea.rf.datamodel.enums
 
-/** enum trait for easier custom enum column types
-  *
-  */
-trait Enum[A] {
-  trait Value { self: A =>
-    _values :+= this
-  }
-  private var _values = List.empty[A]
-  def values = _values map (v => (v.toString, v)) toMap
-}
 
 // Enum to track visibility of database objects
-object Visibility extends Enum[Visibility]
-sealed trait Visibility extends Visibility.Value
-case object PUBLIC extends Visibility // viewable by all
-case object ORGANIZATION extends Visibility // viewable within organization
-case object PRIVATE extends Visibility // viewable only by creator
+sealed abstract class Visibility(val repr: String)
+case object PUBLIC extends Visibility("PUBLIC")
+case object ORGANIZATION extends Visibility("ORGANIZATION")
+case object PRIVATE extends Visibility("PRIVATE")
+
+
+object Visibility {
+  def fromString(s: String): Visibility = s match {
+    case "PUBLIC" => PUBLIC
+    case "ORGANIZATION" => ORGANIZATION
+    case "PRIVATE" => PRIVATE
+  }
+  def values = Seq(PUBLIC, ORGANIZATION, PRIVATE)
+}
 
 
 // Enum to track status of asynchronous tasks
-object JobStatus extends Enum[JobStatus]
-sealed trait JobStatus extends JobStatus.Value
-case object UPLOADING extends JobStatus
-case object SUCCESS extends JobStatus
-case object FAILURE extends JobStatus
-case object PARTIALFAILURE extends JobStatus
-case object QUEUED extends JobStatus
-case object PROCESSING extends JobStatus
+sealed abstract class JobStatus(val repr: String)
+case object UPLOADING extends JobStatus("UPLOADING")
+case object SUCCESS extends JobStatus("SUCCESS")
+case object FAILURE extends JobStatus("FAILURE")
+case object PARTIALFAILURE extends JobStatus("PARTIALFAILURE")
+case object QUEUED extends JobStatus("QUEUED")
+case object PROCESSING extends JobStatus("PROCESSING")
 
+
+object JobStatus {
+  def fromString(s: String): JobStatus = s match {
+    case "UPLOADING" => UPLOADING
+    case "SUCCESS" => SUCCESS
+    case "FAILURE" => FAILURE
+    case "PARTIALFAILURE" => PARTIALFAILURE
+    case "QUEUED" => QUEUED
+    case "PROCESSING" => PROCESSING
+  }
+
+  def values = Seq(UPLOADING, SUCCESS, PARTIALFAILURE, QUEUED, PROCESSING)
+}
