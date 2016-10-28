e1200184e346215e52285c9f90347fbfd050dea7
Terence Tuhinanshu <ttuhinanshu@azavea.com>
2016-10-28 16:02:56 -0400

    Make buckets.tags NOT NULL, default to empty list

---
+++ app-backend/migrations/src_migrations/main/scala/23.scala
@@ -0,0 +1,15 @@
+import slick.driver.PostgresDriver.api._
+import com.liyaos.forklift.slick.SqlMigration
+
+object M23 {
+  RFMigrations.migrations = RFMigrations.migrations :+ SqlMigration(23)(List(
+    sqlu"""
+UPDATE buckets
+  SET tags = '{}'
+  WHERE tags IS NULL;
+
+ALTER TABLE buckets
+  ALTER COLUMN tags SET NOT NULL;
+    """
+  ))
+}
