6452f855d7969952cff1c0b3614c5076a9d732b9
Alex Kaminsky <akaminsky@cub.local>
2016-12-01 16:41:05 -0500

    Show scene footprint on hover on browse page

--- app-frontend/src/app/components/leafletMap/leafletMap.component.js
+++ app-frontend/src/app/components/leafletMap/leafletMap.component.js
@@ -7,6 +7,7 @@ const leafletMap = {
     bindings: {
         static: '<?',
         footprint: '<?',
+        bypassFitBounds: '<?',
         proposedBounds: '<?',
         onBoundsChange: '&',
         layers: '<?'
