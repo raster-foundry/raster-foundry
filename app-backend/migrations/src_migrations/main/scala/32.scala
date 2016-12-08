32e5150deed18d31820f9bbb567498347d8771ca
Lucien Knechtli <lknechtli@azavea.com>
2016-12-08 10:24:18 -0500

    Fix old footprints which were placed in wrong column

---
+++ app-backend/migrations/src_migrations/main/scala/32.scala
@@ -0,0 +1,12 @@
+import slick.driver.PostgresDriver.api._
+import com.liyaos.forklift.slick.SqlMigration
+
+object M32 {
+  RFMigrations.migrations = RFMigrations.migrations :+ SqlMigration(32)(List(
+    sqlu"""
+ALTER TABLE scenes DROP COLUMN data_footprint;
+ALTER TABLE scenes RENAME COLUMN tile_footprint TO data_footprint;
+ALTER TABLE scenes ADD COLUMN tile_footprint geometry(Multipolygon, 3857);
+""" // your sql code goes here
+  ))
+}
