59d16ee0690996edb5081f84d6faac69b5df9c90
Alex Kaminsky <akaminsky@azavea.com>
2016-12-13 17:05:59 -0500

    Modify ingest classes to better match best practices

--- app-tasks/rf/src/rf/ingest/source.py
+++ app-tasks/rf/src/rf/ingest/source.py
@@ -1,7 +1,9 @@
 """ Python class to represent the sources within an ingest layer """
-class Source():
+
+
+class Source(object):
     def __init__(self, image, extent, starting_target_band, band_maps=None,
-                 crs="epsg:3857", extent_crs="epsg:3857"):
+                 source_crs="epsg:4326", extent_crs="epsg:4326"):
 
         """
             Create a new ingest Source
@@ -14,11 +16,12 @@ class Source():
                 crs (str): CRS of the Source (@TODO: figure out why this is needed)
                 extent_crs (str): CRS of the extent (@TODO: figure out why this is needed)
         """
+
         self.image = image
         self.extent = extent
         self.starting_target_band = starting_target_band
-        self.band_maps = self.get_band_maps()
-        self.crs = crs
+        self.band_maps = band_maps or self.create_band_maps()
+        self.source_crs = source_crs
         self.extent_crs = extent_crs
 
 
@@ -27,19 +30,40 @@ class Source():
         return cls(
             d.get('image'),
             d.get('extent'),
-            d.get('starting_target_band')
+            d.get('starting_target_band'),
+            d.get('band_maps'),
+            d.get('source_crs'),
+            d.get('extent_crs')
         )
 
     def to_dict(self):
         return {
+            'image': self.image.to_dict(),
             'extent': self.extent,
-            'crsExtent': self.extent_crs,
-            'crs': self.crs,
-            'uri': self.image['sourceUri'],
-            'bandMaps': self.get_band_maps()
+            'starting_target_band': self.starting_target_band,
+            'band_maps': self.band_maps,
+            'source_crs': self.source_crs,
+            'extent_crs': self.extent_crs
         }
 
-    def get_band_maps(self):
+    def to_ingest_dict(self):
+        """
+            Return a dict of the source formatted to be serialized and included within an ingest
+            definition
+        """
+        return {
+            'extent': self.extent,
+            'crsExtent': self.extent_crs,
+            'bandMaps': self.band_maps,
+            'crs': self.source_crs,
+            'uri': self.image['sourceUri'],
+        }
+
+    def create_band_maps(self):
+        """
+            Return a list of dicts, each representing the mapping of a band within the source's
+            image to a band of the target output
+        """
         band_maps = []
         source_band_index = 1
         target_band_index = self.starting_target_band
