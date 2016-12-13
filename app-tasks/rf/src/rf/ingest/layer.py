5e1ad698effb34bcbcb13deff9dbb972c6fef3a1
Alex Kaminsky <akaminsky@azavea.com>
2016-12-13 17:05:59 -0500

    Modify ingest classes to better match best practices

--- app-tasks/rf/src/rf/ingest/layer.py
+++ app-tasks/rf/src/rf/ingest/layer.py
@@ -1,7 +1,9 @@
 """ Python class to represent a layer within an ingest """
+
 from .source import Source
 
-class Layer():
+
+class Layer(object):
     def __init__(self, scene,
                  output_uri, output_crs="epsg:3857", output_pyramid=True, output_native=False,
                  output_cell_type="uint16raw", output_histogram_buckets=512, output_tile_size=256,
@@ -41,19 +43,44 @@ class Layer():
     @classmethod
     def from_dict(cls, d):
         return cls(
-            d.get('id'),
-            d.get('output'),
-            d.get('sources')
+            d.get('scene'),
+            d.get('output_uri'),
+            d.get('output_crs'),
+            d.get('output_pyramid'),
+            d.get('output_native'),
+            d.get('output_cell_type'),
+            d.get('output_histogram_buckets'),
+            d.get('output_tile_size'),
+            d.get('output_resample_method'),
+            d.get('output_key_index_method'),
+            d.get('ingest_resolution_meters')
         )
 
     def to_dict(self):
         return {
+            'scene': self.scene.to_dict(),
+            'output_uri': self.output_uri,
+            'output_crs': self.output_crs,
+            'output_pyramid': self.output_pyramid,
+            'output_native': self.output_native,
+            'output_cell_type': self.output_cell_type,
+            'output_histogram_buckets': self.output_histogram_buckets,
+            'output_tile_size': self.output_tile_size,
+            'output_resample_method': self.output_resample_method,
+            'output_key_index_method': self.output_key_index_method,
+            'ingest_resolution_meters': self.ingest_resolution_meters
+        }
+
+    def to_ingest_dict(self):
+        """ Return a dict formatted specifically for serialization to an ingest definition component """
+        return {
             'id': str(self.scene.id),
             'output': self.get_output(),
-            'sources': map(lambda s: s.to_dict(), self.get_sources()),
+            'sources': [s.to_dict() for s in self.get_sources()]
         }
 
     def get_sources(self):
+        """ Return a list of sources created from the images within the layer's scene """
         sources = []
         target_band_index = 1
         for image in self.scene.images:
@@ -63,6 +90,7 @@ class Layer():
         return sources
 
     def get_output(self):
+        """ Return a dict with the layer's output parameters as needed for an ingest definition """
         return {
             'crs': self.output_crs,
             'pyramid': self.output_pyramid,
@@ -76,6 +104,7 @@ class Layer():
         }
 
     def get_extent(self):
+        """ Return a list of floats that represents the extent of the layer's data (not tile) """
         coords = self.scene.dataFootprint['coordinates'][0][0]
         x_coords = map(lambda c: c[0], coords)
         y_coords = map(lambda c: c[1], coords)
@@ -87,6 +116,7 @@ class Layer():
         ]
 
     def get_tile_size(self):
+        """ Return a dict with width and height keys to represent a square tile size """
         return {
             'width': self.output_tile_size,
             'height': self.output_tile_size
