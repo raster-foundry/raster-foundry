bfe4c33db6ab2f396c19c2d718ce63a6bca5cf49
Nathan Zimmerman <npzimmerman@gmail.com>
2016-12-21 17:13:55 -0500

    Use symlink for migration

--- app-backend/migrations/src/main/scala/migrations/33.scala
+++ app-backend/migrations/src/main/scala/migrations/33.scala
@@ -1,12 +1,1 @@
-import slick.driver.PostgresDriver.api._
-import com.liyaos.forklift.slick.SqlMigration
-
-object M33 {
-  RFMigrations.migrations = RFMigrations.migrations :+ SqlMigration(33)(List(
-    sqlu"""
-ALTER TABLE projects ADD COLUMN manual_order boolean DEFAULT true;
-ALTER TABLE scenes_to_projects ADD COLUMN scene_order integer;
-ALTER TABLE scenes_to_projects ADD COLUMN mosaic_definition jsonb;
-"""
-  ))
-}
+../../../../src_migrations/main/scala/33.scala
