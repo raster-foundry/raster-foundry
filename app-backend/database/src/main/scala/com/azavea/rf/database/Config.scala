9df611353a4251f932e5b0a0fa5c62471de225f1
Eugene Cheipesh <echeipesh@gmail.com>
2017-01-06 11:07:11 -0500

    Fix tests to use database configuration trait

--- app-backend/database/src/main/scala/com/azavea/rf/database/Config.scala
+++ app-backend/database/src/main/scala/com/azavea/rf/database/Config.scala
@@ -3,7 +3,7 @@ package com.azavea.rf.database
 import com.typesafe.config.ConfigFactory
 
 trait Config {
-  val config = ConfigFactory.load()
+  private val config = ConfigFactory.load()
   private val databaseConfig = config.getConfig("slick.db")
   val jdbcNoDBUrl = databaseConfig.getString("url")
   val jdbcDBName = databaseConfig.getString("name")
