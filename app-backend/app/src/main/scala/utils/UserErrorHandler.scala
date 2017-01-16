d4948fd9afdc79a4bf5cb2df1f67b47496648e78
James <james.santucci@gmail.com>
2017-01-16 15:32:49 -0500

    Add rollbar to exception handlers

--- app-backend/app/src/main/scala/utils/UserErrorHandler.scala
+++ app-backend/app/src/main/scala/utils/UserErrorHandler.scala
@@ -5,19 +5,28 @@ import akka.http.scaladsl.model.StatusCodes
 import com.typesafe.scalalogging.LazyLogging
 import spray.json.{SerializationException, DeserializationException}
 
-trait UserErrorHandler extends Directives with LazyLogging {
+trait UserErrorHandler extends Directives
+    with RollbarNotifier
+    with LazyLogging {
   val userExceptionHandler = ExceptionHandler {
     case e: IllegalArgumentException =>
       logger.error(RfStackTrace(e))
+      sendError(e)
       complete(StatusCodes.ClientError(400)("Bad Argument", e.getMessage))
     case e: IllegalStateException =>
       logger.error(RfStackTrace(e))
+      sendError(e)
       complete(StatusCodes.ClientError(400)("Bad Request", e.getMessage))
     case e: DeserializationException =>
       logger.error(RfStackTrace(e))
+      sendError(e)
       complete(StatusCodes.ClientError(400)("Decoding Error", e.getMessage))
     case e: SerializationException =>
       logger.error(RfStackTrace(e))
+      sendError(e)
       complete(StatusCodes.ServerError(500)("Encoding Error", e.getMessage))
+    case e: Exception =>
+      sendError(e)
+      complete(StatusCodes.ServerError(501)("An unknown error occurred", ""))
   }
 }
