4a27d8f206038e353e707f4d75970f440e882055
Nathan Zimmerman <npzimmerman@gmail.com>
2017-01-19 14:58:02 -0500

    Use generic 'Tree' trait

--- app-backend/tool/src/main/scala/op/Op.scala
+++ app-backend/tool/src/main/scala/op/Op.scala
@@ -1,9 +1,12 @@
 package geotrellis.raster.op
 
 import geotrellis.raster._
-import spire.syntax.cfor._
 
-trait Op extends TileLike with Grid {
+import spire.syntax.cfor._
+import com.typesafe.scalalogging.LazyLogging
+
+
+sealed trait Op extends TileLike with Grid with LazyLogging {
   // TODO: Move these out into a TypeClass
   def +(x: Int) = this.map((_: Int) + x)
   def +(x: Double) = this.mapDouble(_ + x)
@@ -15,51 +18,58 @@ trait Op extends TileLike with Grid {
   def /(x: Double) = this.mapDouble(_ / x)
   def /(other: Op) = this.dualCombine(other)(_ / _)(_ / _)
 
+  def left: Op
+  def right: Op
   def cols: Int
   def rows: Int
   def get(col: Int, row: Int): Int
   def getDouble(col: Int, row: Int): Double
+  def fullyBound: Boolean
 
-  def map(f: Int => Int): Op.Unary =
+  def map(f: Int => Int): Op.Tree =
     Op.MapInt(this, f)
 
-  def mapDouble(f: Double => Double): Op.Unary =
+  def mapDouble(f: Double => Double): Op.Tree =
     Op.MapDouble(this, f)
 
-  def combine(other: Op)(f: (Int, Int) => Int): Op.Binary =
+  def combine(other: Op)(f: (Int, Int) => Int): Op.Tree =
     Op.CombineInt(this, other, f)
 
-  def combineDouble(other: Op)(f: (Double, Double) => Double): Op.Binary =
+  def combineDouble(other: Op)(f: (Double, Double) => Double): Op.Tree =
     Op.CombineDouble(this, other, f)
 
-  def dualCombine(other: Op)(f: (Int, Int) => Int)(g: (Double, Double) => Double): Op.Binary =
+  def dualCombine(other: Op)(f: (Int, Int) => Int)(g: (Double, Double) => Double): Op.Tree =
     Op.DualCombine(this, other, f, g)
 
-  def mapIntMapper(mapper: IntTileMapper): Op.Unary =
+  def mapIntMapper(mapper: IntTileMapper): Op.Tree =
     Op.IntMapper(this, mapper)
 
-  def mapDoubleMapper(mapper: DoubleTileMapper): Op.Unary =
+  def mapDoubleMapper(mapper: DoubleTileMapper): Op.Tree =
     Op.DoubleMapper(this, mapper)
 
   def bind(args: Map[Op.Var, Op]): Op
 
-  def toTile(ct: CellType): Tile = {
-    val output = ArrayTile.empty(ct, cols, rows)
-    if (ct.isFloatingPoint) {
-      cfor(0)(_ < rows, _ + 1) { row =>
-        cfor(0)(_ < cols, _ + 1) { col =>
-          output.setDouble(col, row, getDouble(col, row))
+  def toTile(ct: CellType): Option[Tile] =
+    this match {
+      case Op.Empty =>
+        None
+      case _ =>
+        val mutableOutput = ArrayTile.empty(ct, cols, rows)
+        if (ct.isFloatingPoint) {
+          cfor(0)(_ < rows, _ + 1) { row =>
+            cfor(0)(_ < cols, _ + 1) { col =>
+              Some(mutableOutput.setDouble(col, row, getDouble(col, row)))
+            }
+          }
+        } else {
+          cfor(0)(_ < rows, _ + 1) { row =>
+            cfor(0)(_ < cols, _ + 1) { col =>
+              Some(mutableOutput.set(col, row, get(col, row)))
+            }
+          }
         }
-      }
-    } else {
-      cfor(0)(_ < rows, _ + 1) { row =>
-        cfor(0)(_ < cols, _ + 1) { col =>
-          output.set(col, row, get(col, row))
-        }
-      }
+        Some(mutableOutput)
     }
-    output
-  }
 }
 
 object Op {
@@ -70,22 +80,29 @@ object Op {
   def apply(identifier: String): Op = Var(identifier)
   def apply(tile: Tile): Op = Bound(tile)
 
-  trait Unary extends Op {
-    def src: Op
-    def cols: Int = src.cols
-    def rows: Int = src.rows
+  /** Nil allows us to have a single trait for both binary and unary ops */
+  case object Nil extends Op {
+    def left = this
+    def right = this
+    def cols = 0
+    def rows = 0
+    def get(col: Int, row: Int): Int = NODATA
+    def getDouble(col: Int, row: Int): Double = NODATA
+    def fullyBound = true
+    def bind(args: Map[Var, Op]): Op = this
   }
 
-  trait Binary extends Op {
-    def left: Op
-    def right: Op
-    def cols: Int = left.cols
-    def rows: Int = left.rows
-
-    // TODO: This makes it hard to have unbound Ops
-    //    require(left.dimensions == right.dimensions, "Cannot combine ops with different dimensions: " +
-    //      s"${left.dimensions} does not match ${right.dimensions}")
+  /** An Op.Tree has a left and right. The terminal node will have Nil on the left and right */
+  trait Tree extends Op {
+    def cols = left.cols
+    def rows = left.rows
+    def fullyBound: Boolean = left.fullyBound && right.fullyBound
   }
+
+  // TODO: Think about assurances we can offer (and how) related to col/row of a tile
+  // This strategy makes it hard to have unbound Ops:
+  //    require(left.dimensions == right.dimensions, "Cannot combine ops with different dimensions: " +
+  //      s"${left.dimensions} does not match ${right.dimensions}")
 
   object Var {
     val VarRx = """([a-zA-Z_\d-]+)(\[(\d+)\])?""".r
@@ -102,72 +119,110 @@ object Op {
     def apply(name: Symbol, band: Int): Var = Var(name, Some(band))
   }
 
-  case class Var(name: Symbol, band: Option[Int] = None) extends Op {
-    def cols: Int = ???
-    def rows: Int = ???
-    def get(col: Int, row: Int): Int = ???
-    def getDouble(col: Int, row: Int): Double = ???
+  case class Var(name: Symbol, band: Option[Int] = None) extends Tree {
+    def get(col: Int, row: Int): Int = NODATA
+    def getDouble(col: Int, row: Int): Double = NODATA
+    def left = Op.Nil
+    def right = Op.Nil
+    override def fullyBound: Boolean = false
     def bind(args: Map[Var, Op]): Op = {
-      assert(args.keys.map(_.name).toSeq.contains(this.name))
-      args.get(this) match {
-        case Some(replacement) =>
-          replacement
-        case _ => this
+      logger.debug(s"Attempting to bind a variable: (${this}) with these args: ($args)")
+      args.find(_._1.name == this.name).map(_._2) match {
+        case Some(Op.Var(symbol, band)) =>
+          Op.Var(symbol, band)
+        case Some(Unbound(Some(mbtile))) =>
+          Bound(mbtile.bands(band.getOrElse(0)))
+        case Some(Unbound(None)) =>
+          Op.Empty
+        case None =>
+          logger.debug(s"Unable to find match for variable")
+          this
+        case _ =>
+          logger.info(s"Unexpected match found while binding variable ($this) with args: ($args)")
+          this
       }
     }
   }
 
-  case class Bound(tile: Tile) extends Op {
-    def cols: Int = tile.cols
-    def rows: Int = tile.rows
-    def get(col: Int, row: Int): Int = tile.get(col, row)
-    def getDouble(col: Int, row: Int): Double = tile.getDouble(col, row)
+  /** This object represents a possibly returned tile which is to be bound as a parameter to an
+    *  [[Op]] expression tree. It can either resolve (and provide a tile) or not (and provide NODATA)
+    */
+  case class Unbound(maybeTile: Option[MultibandTile]) extends Tree {
+    def get(col: Int, row: Int): Int = NODATA
+    def getDouble(col: Int, row: Int): Double = NODATA
+    def left = Op.Nil
+    def right = Op.Nil
+    override def fullyBound: Boolean = false
     def bind(args: Map[Var, Op]): Op = this
   }
 
-  case class MapInt(src: Op, f: Int => Int) extends Unary {
-    def get(col: Int, row: Int) = f(src.get(col, row))
-    def getDouble(col: Int, row: Int) = i2d(f(src.get(col, row)))
-    def bind(args: Map[Var, Op]): Op =
-      MapInt(src.bind(args), f)
+  /** This object represents cases in which the catalog returns no tiles at the given coords */
+  case object Empty extends Tree {
+    def get(col: Int, row: Int): Int = NODATA
+    def getDouble(col: Int, row: Int): Double = NODATA
+    def left = Op.Nil
+    def right = Op.Nil
+    def bind(args: Map[Var, Op]): Op = this
   }
 
-  case class MapDouble(src: Op, f: Double => Double) extends Unary {
-    def get(col: Int, row: Int) = d2i(f(src.getDouble(col, row)))
-    def getDouble(col: Int, row: Int) = f(src.getDouble(col, row))
-    def bind(args: Map[Var, Op]): Op =
-      MapDouble(src.bind(args), f)
+  /** This object represents tile data sources */
+  case class Bound(tile: Tile) extends Tree {
+    override def cols: Int = tile.cols
+    override def rows: Int = tile.rows
+    def get(col: Int, row: Int): Int = tile.get(col, row)
+    def getDouble(col: Int, row: Int): Double = tile.getDouble(col, row)
+    def left = Op.Nil
+    def right = Op.Nil
+    def bind(args: Map[Var, Op]): Op = this
   }
 
-  case class IntMapper(src: Op, mapper: IntTileMapper) extends Unary {
-    def get(col: Int, row: Int) = mapper(col, row, src.get(col, row))
-    def getDouble(col: Int, row: Int) = i2d(mapper(col, row, src.get(col, row)))
+  case class MapInt(left: Op, f: Int => Int) extends Tree {
+    def get(col: Int, row: Int) = f(left.get(col, row))
+    def getDouble(col: Int, row: Int) = i2d(f(left.get(col, row)))
+    def right = Op.Nil
     def bind(args: Map[Var, Op]): Op =
-      IntMapper(src.bind(args), mapper)
+      MapInt(left.bind(args), f)
   }
 
-  case class DoubleMapper(src: Op, mapper: DoubleTileMapper) extends Unary {
-    def get(col: Int, row: Int) = d2i(mapper(col, row, src.getDouble(col, row)))
-    def getDouble(col: Int, row: Int) = mapper(col, row, src.getDouble(col, row))
+  case class MapDouble(left: Op, f: Double => Double) extends Tree {
+    def get(col: Int, row: Int) = d2i(f(left.getDouble(col, row)))
+    def getDouble(col: Int, row: Int) = f(left.getDouble(col, row))
+    def right = Op.Nil
     def bind(args: Map[Var, Op]): Op =
-      DoubleMapper(src.bind(args), mapper)
+      MapDouble(left.bind(args), f)
   }
 
-  case class CombineInt(left: Op, right: Op, f: (Int, Int) => Int) extends Binary {
+  case class IntMapper(left: Op, mapper: IntTileMapper) extends Tree {
+    def get(col: Int, row: Int) = mapper(col, row, left.get(col, row))
+    def getDouble(col: Int, row: Int) = i2d(mapper(col, row, left.get(col, row)))
+    def right = Op.Nil
+    def bind(args: Map[Var, Op]): Op =
+      IntMapper(left.bind(args), mapper)
+  }
+
+  case class DoubleMapper(left: Op, mapper: DoubleTileMapper) extends Tree {
+    def get(col: Int, row: Int) = d2i(mapper(col, row, left.getDouble(col, row)))
+    def getDouble(col: Int, row: Int) = mapper(col, row, left.getDouble(col, row))
+    def right = Op.Nil
+    def bind(args: Map[Var, Op]): Op =
+      DoubleMapper(left.bind(args), mapper)
+  }
+
+  case class CombineInt(left: Op, right: Op, f: (Int, Int) => Int) extends Tree {
     def get(col: Int, row: Int) = f(left.get(col, row), right.get(col, row))
     def getDouble(col: Int, row: Int) = i2d(f(left.get(col, row), right.get(col, row)))
     def bind(args: Map[Var, Op]): Op =
       CombineInt(left.bind(args), right.bind(args), f)
   }
 
-  case class CombineDouble(left: Op, right: Op, f: (Double, Double) => Double) extends Binary {
+  case class CombineDouble(left: Op, right: Op, f: (Double, Double) => Double) extends Tree {
     def get(col: Int, row: Int) = d2i(f(left.getDouble(col, row), right.getDouble(col, row)))
     def getDouble(col: Int, row: Int) = f(left.getDouble(col, row), right.getDouble(col, row))
     def bind(args: Map[Var, Op]): Op =
       CombineDouble(left.bind(args), right.bind(args), f)
   }
 
-  case class DualCombine(left: Op, right: Op, f: (Int, Int) => Int, g: (Double, Double) => Double) extends Binary {
+  case class DualCombine(left: Op, right: Op, f: (Int, Int) => Int, g: (Double, Double) => Double) extends Tree {
     def get(col: Int, row: Int) = f(left.get(col, row), right.get(col, row))
     def getDouble(col: Int, row: Int) = g(left.getDouble(col, row), right.getDouble(col, row))
     def bind(args: Map[Var, Op]): Op =
