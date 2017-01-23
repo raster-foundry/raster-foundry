478640512b39e2600dbfba4feab0c4e74fd4250a
Eugene Cheipesh <echeipesh@gmail.com>
2017-01-22 20:35:22 -0500

    Add reclassify endpoints to NDVI demo

--- app-backend/tile/src/main/scala/routes/ToolRoutes.scala
+++ app-backend/tile/src/main/scala/routes/ToolRoutes.scala
@@ -4,6 +4,7 @@ import com.azavea.rf.tile.image._
 import com.azavea.rf.database.Database
 
 import geotrellis.raster._
+import geotrellis.raster.render._
 import geotrellis.raster.io._
 import geotrellis.raster.op._
 import geotrellis.raster.render.{Png, ColorRamp, ColorMap}
@@ -19,59 +20,61 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent._
 import java.util.UUID
 
+// TODO: I need a better way to bind shit ... Right now I just swap out Var for Op ...
+
 object ToolRoutes extends LazyLogging {
   val userId: String = "rf_airflow-user"
 
-  def ndviDiff: Op.Tree = {
-    val red = Op('red)
-    val nir = Op('nir)
-    val ndvi = (nir - red) / (nir + red)
+  val red = Op('red)
+  val nir = Op('nir)
+  val ndvi = (nir - red) / (nir + red)
 
+  def ndvi0 = {
     val params0 = Map(
-      Op.Var('red) -> Op("LC8_1[3]"),
-      Op.Var('nir) -> Op("LC8_1[4]")
-    )
-
-    val params1 = Map(
       Op.Var('red) -> Op("LC8_0[3]"),
-      Op.Var('nir) -> Op("LC8_0[4]")
-    )
-
-    val maskThreshold = 0.01
-    val mask: Double => Double = { d: Double => if (d > maskThreshold) 1.0 else 0.0 }
-    Op.MapDouble(ndvi.bind(params0) - ndvi.bind(params1), mask)
+      Op.Var('nir) -> Op("LC8_0[4]"))
+    ndvi.bind(params0)
   }
 
-  type TMS = (Int, Int, Int) => Future[Option[MultibandTile]]
+  def ndvi1 = {
+    val params1 = Map(
+      Op.Var('red) -> Op("LC8_1[3]"),
+      Op.Var('nir) -> Op("LC8_1[4]"))
+    ndvi.bind(params1)
+  }
 
-  def varParams(orgId: UUID)(implicit db: Database): Directive1[Map[Symbol, TMS]] =
-    parameters('LC8_0, 'LC8_1).as { (p0: String, p1: String) =>
-      Map(
-        'LC8_0 -> { (z: Int, x: Int, y: Int) =>
-          Mosaic(orgId, userId, UUID.fromString(p0), z, x, y, None, false)},
-        'LC8_1 -> { (z: Int, x: Int, y: Int) =>
-          Mosaic(orgId, userId, UUID.fromString(p1), z, x, y, None, false)})
+  def reclass0(breaks: Map[Double, Double]) = {
+    Reclassify(ndvi0, BreakMap(breaks))
+  }
+
+  def reclass1(breaks: Map[Double, Double]) = {
+    Reclassify(ndvi1, BreakMap(breaks))
+  }
+
+  def ndviDiff(breaks0: Map[Double, Double], breaks1: Map[Double, Double]) = {
+    reclass1(breaks1) - reclass0(breaks0)
+  }
+
+  def lookupColorMap(str: Option[String]): ColorMap = {
+    str match {
+      case Some(s) if str.contains(':') =>
+        ColorMap.fromString(s).get
+      case None =>
+        val colorRamp = ColorRamp(Array[Int](0xffffe5aa, 0xf7fcb9ff, 0xd9f0a3ff, 0xaddd8eff, 0x78c679ff, 0x41ab5dff, 0x238443ff, 0x006837ff, 0x004529ff))
+        val breaks = Array[Double](0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 1.0)
+        ColorMap(breaks, colorRamp)
     }
-
-  // TODO: refactor; there should an awesome way to navigate the op tree without having to match
-  def lookupModel: PartialFunction[Option[String], Op] = {
-    case None =>
-      ndviDiff
-    case Some("ndvi0") =>
-      ndviDiff.left.left
-    case Some("ndvi1") =>
-      ndviDiff.left.right
-  }
-
-  def lookupColorMap: PartialFunction[Option[String], ColorMap] = {
-    case _ =>
-      val colorRamp = ColorRamp(Array[Int](0xffffe5aa, 0xf7fcb9ff, 0xd9f0a3ff, 0xaddd8eff, 0x78c679ff, 0x41ab5dff, 0x238443ff, 0x006837ff, 0x004529ff))
-      val breaks = Array[Double](0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 1.0)
-      ColorMap(breaks, colorRamp)
   }
 
   def pngAsHttpResponse(png: Png): HttpResponse =
     HttpResponse(entity = HttpEntity(ContentType(MediaTypes.`image/png`), png.bytes))
+
+  def parseBreakMap(str: String): Map[Double,Double] = {
+    str.split(';').map { c: String =>
+      val Array(a, b) = c.trim.split(':').map(_.toDouble)
+      (a, b)
+    }.toMap
+  }
 
   def root(implicit db: Database) =
     pathPrefix(Segment / "ndvi-diff-tool"){ organizationId =>
@@ -79,10 +82,32 @@ object ToolRoutes extends LazyLogging {
         complete("model JSON")
       } ~
       pathPrefix(IntNumber / IntNumber / IntNumber){ (z, x, y) =>
-        (varParams(UUID.fromString(organizationId)) & parameter('part.?)) { (varMap, partId) =>
+        parameter(
+          'part.?,
+          'LC8_0, 'LC8_1,
+          'class0.?("-1:0;0.01:1.0"),
+          'class1.?("-1:0;0.01:1.0"),
+          'cm.?
+        )
+        { (partId, p0, p1, class0, class1, colorMap) =>
           complete {
-            val model: Op = lookupModel(partId)
+            val orgId = UUID.fromString(organizationId)
+            val varMap = Map(
+              'LC8_0 -> { (z: Int, x: Int, y: Int) =>
+                Mosaic(orgId, userId, UUID.fromString(p0), z, x, y, None, false)},
+              'LC8_1 -> { (z: Int, x: Int, y: Int) =>
+                Mosaic(orgId, userId, UUID.fromString(p1), z, x, y, None, false)})
+
+            val model: Op = partId match {
+              case Some("ndvi0") => ndvi0
+              case Some("ndvi1") => ndvi1
+              case Some("class0") => reclass0(parseBreakMap(class0))
+              case Some("class1") => reclass1(parseBreakMap(class1))
+              case _ => ndviDiff(parseBreakMap(class0), parseBreakMap(class1))
+            }
+
             logger.debug(s"model prior to param binding: $model")
+
             // TODO: Inspect what variables are actually required, only fetch those tiles
             val futureTiles = varMap.map { case (sym, tms) =>
               tms(z, x, y).map { maybeTile => sym -> maybeTile }
@@ -93,6 +118,7 @@ object ToolRoutes extends LazyLogging {
               val params = tiles.map { case (name, maybeTile) =>
                 Op.Var(name) -> Op.Unbound(maybeTile)
               }.toMap
+
               logger.debug(s"Params to bind to model: $params")
 
               val assignedModel = model.bind(params)
@@ -102,7 +128,7 @@ object ToolRoutes extends LazyLogging {
               val calculation = assignedModel.toTile(FloatCellType)
 
               calculation.map { tile =>
-                val png = tile.renderPng(lookupColorMap(partId))
+                val png = tile.renderPng(lookupColorMap(colorMap))
                 pngAsHttpResponse(png)
               }
             }
