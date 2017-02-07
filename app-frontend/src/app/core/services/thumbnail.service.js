5b5e0266905a160ff378f40550513c9ce9c13c12
Lucien Knechtli <lknechtli@azavea.com>
2017-02-07 15:27:40 -0500

    Change thumbnail service to return Url, not thumbnail
    Use regex to check if url is relative or absolute
    Add request token to relative urls

--- app-frontend/src/app/core/services/thumbnail.service.js
+++ app-frontend/src/app/core/services/thumbnail.service.js
@@ -1,12 +1,21 @@
 export default (app) => {
     class ThumbnailService {
-        getBestFit(thumbnails, size) {
-            return thumbnails.reduce((thumb, next) => {
+        constructor(authService) {
+            'ngInject';
+            this.authService = authService;
+        }
+
+        getBestFitUrl(thumbnails, size) {
+            let url = thumbnails.reduce((thumb, next) => {
                 if (Math.abs(size - next.widthPx) < Math.abs(size - thumb.widthPx)) {
                     return next;
                 }
                 return thumb;
-            });
+            }).url;
+            if (url.startsWith('/')) {
+                return `${url}?token=${this.authService.token()}`;
+            }
+            return url;
         }
     }
 
