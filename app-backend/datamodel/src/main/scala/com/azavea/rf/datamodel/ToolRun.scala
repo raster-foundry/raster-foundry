37d78669325030e9345c12435bb7834d3321f6ae
James <james.santucci@gmail.com>
2017-02-08 09:43:56 -0500

    Add organization and visibility fields

--- app-backend/datamodel/src/main/scala/com/azavea/rf/datamodel/ToolRun.scala
+++ app-backend/datamodel/src/main/scala/com/azavea/rf/datamodel/ToolRun.scala
@@ -11,6 +11,8 @@ case class ToolRun(
   createdBy: String,
   modifiedAt: Timestamp,
   modifiedBy: String,
+  visibility: Visibility,
+  organizationId: UUID,
   project: UUID,
   tool: UUID,
   execution_parameters: Map[String, Any]
@@ -20,9 +22,11 @@ object ToolRun {
   def create = Create.apply _
   def tupled = (ToolRun.apply _).tupled
 
-  implicit def defaultToolRunFormat = jsonFormat8(ToolRun.apply _)
+  implicit def defaultToolRunFormat = jsonFormat10(ToolRun.apply _)
 
   case class Create(
+    visibility: Visibility,
+    organizationId: UUID,
     project: UUID,
     tool: UUID,
     execution_parameters: Map[String, Any]
@@ -35,6 +39,8 @@ object ToolRun {
         userId,
         now,
         userId,
+        visibility,
+        organizationId,
         project,
         tool,
         execution_parameters
@@ -43,6 +49,6 @@ object ToolRun {
   }
 
   object Create {
-    implicit val defaultToolRunCreateFormat = jsonFormat3(Create.apply _)
+    implicit val defaultToolRunCreateFormat = jsonFormat5(Create.apply _)
   }
 }
