f9b1bd4bebbaf47f927bef3667bf7a43817fb100
Alex Kaminsky <akaminsky@azavea.com>
2017-02-09 15:17:57 -0500

    Add custom tile URL format to publish

--- app-frontend/src/app/components/publishModal/publishModal.controller.js
+++ app-frontend/src/app/components/publishModal/publishModal.controller.js
@@ -1,4 +1,49 @@
 export default class PublishModalController {
     constructor() {
+        'ngInject';
+    }
+
+    $onInit() {
+        this.urlMappings = [
+            {
+                label: 'Standard',
+                z: 'z',
+                x: 'x',
+                y: 'y',
+                active: true
+            },
+            {
+                label: 'ArcGIS',
+                z: 'level',
+                x: 'col',
+                y: 'row',
+                active: false
+            }
+        ];
+
+        this.mappedTileUrl =
+            this.hydrateTileUrl(this.getActiveMapping());
+    }
+
+    onUrlMappingChange(mapping) {
+        this.setActiveMappingByLabel(mapping.label);
+        this.mappedTileUrl = this.hydrateTileUrl(mapping);
+    }
+
+    hydrateTileUrl(mapping) {
+        return this.resolve.tileUrl
+            .replace('{z}', `{${mapping.z}}`)
+            .replace('{x}', `{${mapping.x}}`)
+            .replace('{y}', `{${mapping.y}}`);
+    }
+
+    getActiveMapping() {
+        return this.urlMappings.find(m => m.active);
+    }
+
+    setActiveMappingByLabel(label) {
+        this.urlMappings.forEach(m => {
+            m.active = m.label === label;
+        });
     }
 }
