c33d621f9c73458ea73e10cc67619a4d5209a2ba
Alex Kaminsky <akaminsky@azavea.com>
2017-02-20 11:22:54 -0500

    Upgrade akka-http

--- app-backend/project/Version.scala
+++ app-backend/project/Version.scala
@@ -1,6 +1,7 @@
 object Version {
   val rasterFoundry      = "0.1"
   val akka               = "2.4.3"
+  val akkaHttp           = "10.0.3"
   val geotrellis         = "1.0.0"
   val hikariCP           = "3.1.1"
   val postgres           = "9.4-1201-jdbc41"
@@ -16,7 +17,7 @@ object Version {
   val jwtCore            = "0.10.0"
   val jwtJson            = "0.10.0"
   val akkaHttpExtensions = "0.4.8"
-  val akkaHttpCors       = "0.1.6"
+  val akkaHttpCors       = "0.1.11"
   val akkaSlf4j          = "2.4.11"
   val ammoniteOps        = "0.7.0"
   val spark              = "2.0.0"
