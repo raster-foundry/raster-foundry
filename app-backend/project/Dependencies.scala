4f6d1b2e6ab4fc48c261b324df87c04bd0c6bdcd
Nathan Zimmerman <npzimmerman@gmail.com>
2017-02-21 16:23:32 -0500

    Cache render by extent result

--- app-backend/project/Dependencies.scala
+++ app-backend/project/Dependencies.scala
@@ -4,10 +4,10 @@ import sbt._
 
 object Dependencies {
   val akka                    = "com.typesafe.akka"           %% "akka-actor"                        % Version.akka
-  val akkahttp                = "com.typesafe.akka"           %% "akka-http-experimental"            % Version.akka
-  val akkajson                = "com.typesafe.akka"           %% "akka-http-spray-json-experimental" % Version.akka
+  val akkahttp                = "com.typesafe.akka"           %% "akka-http"                         % Version.akkaHttp
+  val akkajson                = "com.typesafe.akka"           %% "akka-http-spray-json"              % Version.akkaHttp
+  val akkatestkit             = "com.typesafe.akka"           %% "akka-http-testkit"                 % Version.akkaHttp
   val akkastream              = "com.typesafe.akka"           %% "akka-stream"                       % Version.akka
-  val akkatestkit             = "com.typesafe.akka"           %% "akka-http-testkit"                 % Version.akka
   val akkaSlf4j               = "com.typesafe.akka"           %% "akka-slf4j"                        % Version.akkaSlf4j
   val geotrellisSparkEtl      = "org.locationtech.geotrellis" %% "geotrellis-spark-etl"              % Version.geotrellis
   val geotrellisSpark         = "org.locationtech.geotrellis" %% "geotrellis-spark"                  % Version.geotrellis
