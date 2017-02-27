0573cae0fb6d48d540bf28cef22ded72c4f3bdad
Nathan Zimmerman <npzimmerman@gmail.com>
2017-02-27 12:42:26 -0500

    Cache layer prefix lookups

--- app-backend/tile/src/main/scala/LayerCache.scala
+++ app-backend/tile/src/main/scala/LayerCache.scala
@@ -3,7 +3,10 @@ package com.azavea.rf.tile
 import com.azavea.rf.database.Database
 import com.azavea.rf.common.cache._
 import com.azavea.rf.common.cache.kryo.KryoMemcachedClient
-import com.github.benmanes.caffeine.cache.Caffeine
+import com.azavea.rf.ingest.util.S3.S3UrlRx
+import com.azavea.rf.database.Database
+import com.azavea.rf.database.tables.Scenes
+
 import geotrellis.raster._
 import geotrellis.raster.histogram.Histogram
 import geotrellis.spark._
@@ -13,16 +16,16 @@ import geotrellis.spark.io.s3.{S3AttributeStore, S3ValueReader, S3CollectionLaye
 import com.github.blemale.scaffeine.{ Cache => ScaffeineCache, Scaffeine }
 import geotrellis.vector.Extent
 import com.github.benmanes.caffeine.cache._
-import com.github.benmanes.caffeine.cache.Caffeine
 import spray.json.DefaultJsonProtocol._
 import net.spy.memcached._
 import java.net.InetSocketAddress
 
+import java.util.concurrent.{Executors, TimeUnit}
+import java.util.UUID
 import scala.concurrent._
 import scala.collection.JavaConverters._
 import scala.concurrent.duration._
 import scala.util._
-import java.util.concurrent.{Executors, TimeUnit}
 
 
 /**
@@ -65,15 +68,15 @@ object LayerCache extends Config {
     attributeStore(defaultBucket, prefix)
 
   val tileCache = HeapBackedMemcachedClient[Option[MultibandTile]](memcachedClient)
-  def maybeRenderExtent(id: RfLayerId, zoom: Int, extent: Extent): Future[Option[MultibandTile]] =
+  def maybeRenderExtent(id: UUID, zoom: Int, extent: Extent): Future[Option[MultibandTile]] =
     tileCache.caching(s"rendered-extent-$id-$zoom-$extent") { implicit ec =>
       for {
-        prefix <- id.prefix
+        prefix <- prefixFromLayerId(id)
         store <- attributeStore(defaultBucket, prefix)
       } yield {
-        val reader = new S3ValueReader(store).reader[SpatialKey, MultibandTile](id.catalogId(zoom))
+        val reader = new S3ValueReader(store).reader[SpatialKey, MultibandTile](LayerId(id.toString, zoom))
         val query = S3CollectionLayerReader(store)
-          .query[SpatialKey, MultibandTile, TileLayerMetadata[SpatialKey]](id.catalogId(zoom))
+          .query[SpatialKey, MultibandTile, TileLayerMetadata[SpatialKey]](LayerId(id.toString, zoom))
         val tile = query.where(Intersects(extent))
           .result
           .stitch
@@ -86,13 +89,13 @@ object LayerCache extends Config {
       }
     }
 
-  def maybeTile(id: RfLayerId, zoom: Int, key: SpatialKey): Future[Option[MultibandTile]] =
+  def maybeTile(id: UUID, zoom: Int, key: SpatialKey): Future[Option[MultibandTile]] =
     tileCache.caching(s"tile-$id-$zoom-$key") { implicit ec =>
       for {
-        prefix <- id.prefix
+        prefix <- prefixFromLayerId(id)
         store <- attributeStore(defaultBucket, prefix)
       } yield {
-        val reader = new S3ValueReader(store).reader[SpatialKey, MultibandTile](id.catalogId(zoom))
+        val reader = new S3ValueReader(store).reader[SpatialKey, MultibandTile](LayerId(id.toString, zoom))
         Try(reader.read(key)) match {
           // Only cache failures through failed query
           case Success(tile) => Some(tile)
@@ -104,11 +107,24 @@ object LayerCache extends Config {
 
 
   val histogramCache = HeapBackedMemcachedClient[Array[Histogram[Double]]](memcachedClient)
-  def bandHistogram(id: RfLayerId, zoom: Int): Future[Array[Histogram[Double]]] =
+  def bandHistogram(id: UUID, zoom: Int): Future[Array[Histogram[Double]]] =
     histogramCache.caching(s"histogram-$id-$zoom") { implicit ec =>
       for {
-        prefix <- id.prefix
+        prefix <- prefixFromLayerId(id)
         store <- attributeStore(defaultBucket, prefix)
-      } yield store.read[Array[Histogram[Double]]](id.catalogId(0), "histogram")
+      } yield store.read[Array[Histogram[Double]]](LayerId(id.toString, 0), "histogram")
+    }
+
+  val layerPrefixCache = HeapBackedMemcachedClient[Option[String]](memcachedClient)
+  def prefixFromLayerId(layerId: UUID): Future[Option[String]] =
+    layerPrefixCache.caching(s"") { implicit ec =>
+      val sceneQuery = Scenes.getScene(layerId)
+      sceneQuery.map { sceneOption =>
+        for {
+          scene <- sceneOption
+          ingestLocation <- scene.ingestLocation
+          result <- S3UrlRx.findFirstMatchIn(ingestLocation)
+        } yield result.group("prefix")
+      }
     }
 }
