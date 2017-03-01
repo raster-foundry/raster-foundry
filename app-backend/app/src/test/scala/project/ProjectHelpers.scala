fca1b7c6fa1ba58a7227b68d1211274c8a9fd38c
Alex Kaminsky <akaminsky@azavea.com>
2017-03-01 12:45:50 -0500

    Add tests for /project/{id}/scenes endpoint auth

--- app-backend/app/src/test/scala/project/ProjectHelpers.scala
+++ app-backend/app/src/test/scala/project/ProjectHelpers.scala
@@ -25,10 +25,26 @@ trait ProjectSpecHelper {
     publicOrgId, "Test Three", "This is the third test project", Visibility.Public, List("testing")
   )
 
+  val newProject4 = Project.Create(
+    publicOrgId, "Test Three", "This is the third test project", Visibility.Private, List("testing")
+  )
+
   val landsatId = UUID.fromString("697a0b91-b7a8-446e-842c-97cda155554d")
 
   def newScene(name: String, cloudCover: Option[Float] = None) = Scene.Create(
     None, publicOrgId, 0, Visibility.Public, List("Test", "Public", "Low Resolution"), landsatId,
+    Map("instrument type" -> "satellite", "splines reticulated" -> 0):Map[String, Any],
+    name, None, None, List.empty[String], List.empty[Image.Banded],
+    List.empty[Thumbnail.Identified], None,
+    SceneFilterFields(cloudCover,
+                      Some(Timestamp.from(Instant.parse("2016-09-19T14:41:58.408544Z"))),
+                      None,
+                      None),
+    SceneStatusFields(JobStatus.Processing, JobStatus.Processing, IngestStatus.NotIngested)
+  )
+
+  def newPrivateScene(name: String, cloudCover: Option[Float] = None) = Scene.Create(
+    None, publicOrgId, 0, Visibility.Private, List("Test", "Public", "Low Resolution"), landsatId,
     Map("instrument type" -> "satellite", "splines reticulated" -> 0):Map[String, Any],
     name, None, None, List.empty[String], List.empty[Image.Banded],
     List.empty[Thumbnail.Identified], None,
