32df56bdc6f311493e1407fb0fee6fded0023b77
Eugene Cheipesh <echeipesh@gmail.com>
2017-03-01 17:50:03 -0500

    Add debug log messages to memcached wrapper

--- app-backend/common/src/main/scala/cache/package.scala
+++ app-backend/common/src/main/scala/cache/package.scala
@@ -1,24 +1,32 @@
 package com.azavea.rf.common
 
 import net.spy.memcached._
-
-import java.util.concurrent.{Executors, TimeUnit}
+import com.typesafe.scalalogging.LazyLogging
 import scala.concurrent._
 import scala.concurrent.duration._
-import scala.util._
 import scala.concurrent.ExecutionContext.Implicits.global
 
 
-package object cache {
+package object cache extends LazyLogging {
   implicit class MemcachedClientMethods(client: MemcachedClient) {
     def getOrElseUpdate[CachedType](cacheKey: String, expensiveOperation: Future[CachedType], ttl: Duration)(implicit ec: ExecutionContext): Future[CachedType] = {
       val futureCached = Future { client.asyncGet(cacheKey).get() }
       futureCached.flatMap({ value =>
         if (value != null) { // cache hit
+          logger.debug(s"Cache Hit: $cacheKey - $value")
           Future.successful(value.asInstanceOf[CachedType])
         } else { // cache miss
+          logger.debug(s"Cache Miss: $cacheKey" )
           val futureCached: Future[CachedType] = expensiveOperation
-          futureCached.foreach({ cachedValue => client.set(cacheKey, ttl.toSeconds.toInt, cachedValue) })
+          futureCached.foreach{ cachedValue =>
+            try {
+              client.set(cacheKey, ttl.toSeconds.toInt, cachedValue)
+              logger.debug(s"Cache Set: $cacheKey - $cachedValue")
+            } catch {
+              case e: Exception => logger.debug(s"Cache Set Error: ${e.getMessage}")
+            }
+          }
+
           futureCached
         }
       })
