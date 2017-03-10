edfd3d3a38155276395ac2f09c3be120a1d2b5d6
Lucien Knechtli <lknechtli@azavea.com>
2017-03-10 17:19:28 -0500

    Add share policy to project share modal

--- app-frontend/src/app/components/publishModal/publishModal.controller.js
+++ app-frontend/src/app/components/publishModal/publishModal.controller.js
@@ -1,6 +1,9 @@
 export default class PublishModalController {
-    constructor() {
+    constructor(projectService, $log) {
         'ngInject';
+
+        this.projectService = projectService;
+        this.$log = $log;
     }
 
     $onInit() {
@@ -21,8 +24,71 @@ export default class PublishModalController {
             }
         ];
 
+        let sharePolicies = [
+            {
+                label: 'Private',
+                description:
+                `Only you and those you create tokens for
+                 will be able to view tiles for this project`,
+                enum: 'PRIVATE',
+                active: false
+            },
+            {
+                label: 'Organization',
+                description:
+                `Users in your organization will be able to use
+                 their own tokens to view tiles for this project`,
+                enum: 'ORGANIZATION',
+                active: false
+            },
+            {
+                label: 'Public',
+                description: 'Anyone can view tiles for this project without a token',
+                enum: 'PUBLIC',
+                active: false
+            }
+        ];
+
+        this.sharePolicies = sharePolicies.map(
+            (policy) => {
+                let isActive = policy.enum === this.resolve.project.tileVisibility;
+                policy.active = isActive;
+                return policy;
+            }
+        );
+
+        this.activePolicy = this.sharePolicies.find((policy) => policy.active);
+
         this.mappedTileUrl =
             this.hydrateTileUrl(this.getActiveMapping());
+    }
+
+    onPolicyChange(policy) {
+        let project = this.resolve.project;
+        let shouldUpdate = true;
+
+        let oldPolicy = this.activePolicy;
+        if (this.activePolicy) {
+            this.activePolicy.active = false;
+        } else {
+            shouldUpdate = false;
+        }
+
+        this.activePolicy = policy;
+        policy.active = true;
+
+        project.tileVisibility = policy.enum;
+        if (shouldUpdate) {
+            this.projectService.updateProject(project).then((res) => {
+                this.$log.log(res);
+            }, (err) => {
+                // TODO: Toast this
+                this.$log.debug('Error while updating project share policy', err);
+                this.activePolicy.active = false;
+                oldPolicy.active = true;
+                this.activePolicy = oldPolicy;
+            });
+        }
     }
 
     onUrlMappingChange(mapping) {
