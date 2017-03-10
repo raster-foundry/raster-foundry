35d6b7f0ccf89b54c5a3efc6458bef4290cd608c
Derek Dohler <ddohler@azavea.com>
2017-03-10 11:24:22 -0500

    Update NavBar links

--- app-frontend/src/app/components/navBar/navBar.controller.js
+++ app-frontend/src/app/components/navBar/navBar.controller.js
@@ -2,13 +2,12 @@ import assetLogo from '../../../assets/images/logo-raster-foundry.png';
 
 export default class NavBarController {
     constructor( // eslint-disable-line max-params
-        $log, $state, $uibModal, $scope, APP_CONFIG, authService, localStorage
+        $log, $state, $scope, APP_CONFIG, authService, localStorage
     ) {
         'ngInject';
 
         this.$log = $log;
         this.$state = $state;
-        this.$uibModal = $uibModal;
         this.localStorage = localStorage;
         this.authService = authService;
 
@@ -18,41 +17,6 @@ export default class NavBarController {
 
         this.optionsOpen = false;
         this.assetLogo = assetLogo;
-    }
-
-    gotoEditor() {
-        if (!this.$state.$current.name.includes('editor')) {
-            if (this.activeModal) {
-                this.activeModal.dismiss();
-            }
-
-            this.activeModal = this.$uibModal.open({
-                component: 'rfSelectProjectModal'
-            });
-
-            this.activeModal.result.then(p => {
-                this.$state.go('editor.project.color.scenes', {projectid: p.id});
-            });
-        }
-        return this.activeModal;
-    }
-
-    gotoLab() {
-        if (!this.$state.$current.name.includes('lab')) {
-            if (this.activeModal) {
-                this.activeModal.dismiss();
-            }
-
-            this.activeModal = this.$uibModal.open({
-                component: 'rfSelectToolModal'
-            });
-
-            this.activeModal.result.then(t => {
-                this.$state.go('lab.run', {toolid: t.id});
-            });
-        }
-
-        return this.activeModal;
     }
 
     signin() {
