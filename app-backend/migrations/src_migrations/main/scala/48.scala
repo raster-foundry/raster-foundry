36f24dfb962118586dc6f852040084fc807f7bc0
Terence Tuhinanshu <ttuhinanshu@azavea.com>
2017-03-16 15:44:30 -0400

    Add migration to remove users_to_organizations
    We add organization_id and organization_role to users table.
    Each user now corresponds to exactly one organization. All
    users are added to the Public organization by default.

---
+++ app-backend/migrations/src_migrations/main/scala/48.scala
@@ -0,0 +1,34 @@
+import slick.driver.PostgresDriver.api._
+import com.liyaos.forklift.slick.SqlMigration
+
+object M48 {
+  RFMigrations.migrations = RFMigrations.migrations :+ SqlMigration(48)(List(
+    sqlu"""
+ALTER TABLE users
+  ADD COLUMN
+    organization_id
+      UUID
+      DEFAULT 'dfac6307-b5ef-43f7-beda-b9f208bb7726'
+      NOT NULL
+      REFERENCES organizations(id),
+  ADD COLUMN
+    role
+      VARCHAR(255)
+      DEFAULT 'VIEWER'
+      NOT NULL,
+  ADD COLUMN
+    created_at
+      TIMESTAMP
+      DEFAULT (now() AT TIME ZONE 'utc')
+      NOT NULL,
+  ADD COLUMN
+    modified_at
+      TIMESTAMP
+      DEFAULT (now() AT TIME ZONE 'utc')
+      NOT NULL;
+
+
+DROP TABLE users_to_organizations;
+"""
+  ))
+}
