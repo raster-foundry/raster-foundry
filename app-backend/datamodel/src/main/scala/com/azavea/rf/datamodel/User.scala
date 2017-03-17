413ee9ca107db4f766495b4c253726077dd7d7e3
Terence Tuhinanshu <ttuhinanshu@azavea.com>
2017-03-17 15:46:52 -0400

    Update datamodel
    All case classes assisting Role creation are removed. User class
    is updated to include an organizationId and role, in addition to
    createdAt and modifiedAt fields.
    
    User.Role is moved to a top level case class, UserRole, since now
    User needs to refer it in its own declaration.

--- app-backend/datamodel/src/main/scala/com/azavea/rf/datamodel/User.scala
+++ app-backend/datamodel/src/main/scala/com/azavea/rf/datamodel/User.scala
@@ -5,102 +5,58 @@ import spray.json.DefaultJsonProtocol._
 import java.sql.Timestamp
 import java.util.UUID
 
-case class User(id: String)
+sealed abstract class UserRole(val repr: String) extends Product with Serializable
+case object UserRoleRole extends UserRole("USER")
+case object Viewer extends UserRole("VIEWER")
+case object Admin extends UserRole("ADMIN")
+
+object UserRole {
+  def fromString(s: String) = s.toUpperCase match {
+    case "USER" => UserRoleRole  // TODO Think of a better name than UserRoleRole
+    case "VIEWER" => Viewer
+    case "ADMIN" => Admin
+    case _ => throw new Exception(s"Unsupported user role string: $s")
+  }
+
+  implicit object DefaultJobStatusJsonFormat extends RootJsonFormat[UserRole] {
+    def write(role: UserRole): JsValue = JsString(role.toString)
+    def read(js: JsValue): UserRole = js match {
+      case JsString(js) => fromString(js)
+      case _ =>
+        deserializationError("Failed to parse thumbnail size string representation (${js}) to JobStatus")
+    }
+  }
+}
+
+case class User(
+  id: String,
+  organizationId: UUID,
+  role: UserRole,
+  createdAt: Timestamp,
+  modifiedAt: Timestamp
+)
 
 
 object User {
 
+  def tupled = (User.apply _).tupled
+
   def create = Create.apply _
 
-  implicit val defaultUserFormat = jsonFormat1(User.apply _)
-
-  sealed abstract class Role(val repr: String) extends Product with Serializable
-  case object UserRole extends Role("USER")
-  case object Viewer extends Role("VIEWER")
-  case object Admin extends Role("ADMIN")
-
-  object Role {
-    def fromString(s: String) = s.toUpperCase match {
-      case "USER" => UserRole
-      case "VIEWER" => Viewer
-      case "ADMIN" => Admin
-      case _ => throw new Exception(s"Unsupported user role string: $s")
-    }
-
-    implicit object DefaultJobStatusJsonFormat extends RootJsonFormat[Role] {
-      def write(role: Role): JsValue = JsString(role.toString)
-      def read(js: JsValue): Role = js match {
-        case JsString(js) => fromString(js)
-        case _ =>
-          deserializationError("Failed to parse thumbnail size string representation (${js}) to JobStatus")
-      }
-    }
-  }
+  implicit val defaultUserFormat = jsonFormat5(User.apply _)
 
   case class Create(
     id: String,
     organizationId: UUID,
-    role: Role = Viewer
+    role: UserRole = Viewer
   ) {
-    def toUsersOrgTuple(): (User, User.ToOrganization)= {
+    def toUser(): User = {
       val now = new Timestamp((new java.util.Date()).getTime())
-      val newUUID = java.util.UUID.randomUUID
-      val user = User(id)
-
-      val userToOrg = User.ToOrganization(
-        userId=id,
-        organizationId=organizationId,
-        role,
-        now,
-        now
-      )
-      (user, userToOrg)
+      User(id, organizationId, role, now, now)
     }
   }
-
   object Create {
     implicit val defaultUserCreateFormat = jsonFormat3(Create.apply _)
-  }
-
-  case class ToOrganization(
-    userId: String,
-    organizationId: UUID,
-    role: User.Role,
-    createdAt: Timestamp,
-    modifiedAt: Timestamp
-  )
-  object ToOrganization {
-    def tupled = (ToOrganization.apply _).tupled
-    implicit val defaultUserToOrgFormat = jsonFormat5(ToOrganization.apply _)
-  }
-
-  case class WithRole(id: String, role: User.Role, createdAt: Timestamp, modifiedAt: Timestamp)
-  object WithRole {
-    def tupled = (WithRole.apply _).tupled
-    implicit val defaultUserWithRoleFormat = jsonFormat4(WithRole.apply _)
-  }
-
-  case class WithRoleCreate(id: String, role: User.Role) {
-    def toUserWithRole(): User.WithRole = {
-      val now = new Timestamp((new java.util.Date()).getTime())
-      User.WithRole(id, role, now, now)
-    }
-  }
-  object WithRoleCreate {
-    def tupled = (WithRoleCreate.apply _).tupled
-    implicit val defaultUserWithRoleCreateFormat = jsonFormat2(WithRoleCreate.apply _)
-  }
-
-  // join between role and org
-  case class RoleOrgJoin(userId: String, orgId: java.util.UUID, orgName: String, userRole: User.Role)
-  object RoleOrgJoin {
-    def tupled = (RoleOrgJoin.apply _).tupled
-    implicit val defaultUserWithRoleFormat = jsonFormat4(RoleOrgJoin.apply _)
-  }
-
-  case class WithOrgs(id: String, organizations: Seq[Organization.WithRole])
-  object WithOrgs {
-    implicit val defaultUserWithOrgsFormat = jsonFormat2(WithOrgs.apply _)
   }
 }
 
