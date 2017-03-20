1205d32be8c35031454bd2c289ffe7ad13ed280c
Terence Tuhinanshu <ttuhinanshu@azavea.com>
2017-03-20 14:17:45 -0400

    Load data bucket in configuration
    This is where all user uploads will be stored. Depends on the
    environment. Actual uploads will be stored inside a `user-uploads`
    directory in the bucket, since it is also used for ingests.

--- app-backend/api/src/main/scala/utils/Config.scala
+++ app-backend/api/src/main/scala/utils/Config.scala
@@ -27,5 +27,6 @@ trait Config {
 
   val featureFlags = featureFlagConfig.getConfigList("features")
 
+  val dataBucket = s3Config.getString("dataBucket")
   val thumbnailBucket = s3Config.getString("thumbnailBucket")
 }
