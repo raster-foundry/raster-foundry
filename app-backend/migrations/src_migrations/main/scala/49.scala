57cd3fd3c38cf8ca5c8b1832f13eb0d75f525812
Chris Brown <cbrown@azavea.com>
2017-03-27 10:41:12 -0400

    Add API/Models for Uploads
    This commit adds an API endpoint for uploads and updates the swagger
    specification to include a metadata field for optional metadata that
    will be used during scene creation

---
+++ app-backend/migrations/src_migrations/main/scala/49.scala
@@ -0,0 +1,29 @@
+import slick.driver.PostgresDriver.api._
+import com.liyaos.forklift.slick.SqlMigration
+
+object M49 {
+  RFMigrations.migrations = RFMigrations.migrations :+ SqlMigration(49)(List(
+    sqlu"""
+CREATE TYPE upload_type AS ENUM ('DROPBOX', 'S3', 'LOCAL');
+
+CREATE TYPE upload_status AS ENUM ('CREATED', 'UPLOADING', 'UPLOADED', 'QUEUED', 'PROCESSING', 'COMPLETE');
+
+CREATE TYPE file_type as ENUM ('GEOTIFF', 'GEOTIFF_WITH_METADATA');
+
+CREATE TABLE uploads (
+  id UUID PRIMARY KEY NOT NULL,
+  created_at TIMESTAMP NOT NULL,
+  created_by VARCHAR(255) REFERENCES users(id) NOT NULL,
+  modified_at TIMESTAMP NOT NULL,
+  modified_by VARCHAR(255) REFERENCES users(id) NOT NULL,
+  organization_id UUID REFERENCES organizations(id) NOT NULL,
+  upload_status upload_status NOT NULL,
+  file_type file_type NOT NULL,
+  upload_type upload_type NOT NULL,
+  files text[],
+  datasource UUID REFERENCES datasources(id) NOT NULL,
+  metadata jsonb NOT NULL
+);
+"""
+  ))
+}
