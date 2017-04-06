02b068fd2abeebf84dbc2db04ddb96eeff2fd980
Nathan Zimmerman <npzimmerman@gmail.com>
2017-04-06 10:15:12 -0400

    Solidify initial interpreter tests

--- app-backend/tool/src/main/scala/op/InterpreterError.scala
+++ app-backend/tool/src/main/scala/op/InterpreterError.scala
@@ -1,10 +1,29 @@
 package com.azavea.rf.tool.op
 
+import io.circe._
+import io.circe.syntax._
+import io.circe.generic.auto._
+
 import java.net.URI
 import java.util.UUID
 
 
-sealed trait InterpreterError { val id: UUID }
-case class MissingParameter(id: UUID) extends InterpreterError
-case class TileRetrievalError(id: UUID, attempted: URI) extends InterpreterError
+sealed trait InterpreterError {
+  val id: UUID
+  def repr: String
+}
+case class MissingParameter(id: UUID) extends InterpreterError {
+  def repr = s"Unbound parameter encountered, unable to evaluate"
+}
+case class RasterRetrievalError(id: UUID) extends InterpreterError {
+  def repr = s"Unable to retrieve raster for $id"
+}
 
+object InterpreterError {
+  implicit val encodeInterpreterErrors: Encoder[InterpreterError] =
+    new Encoder[InterpreterError] {
+      final def apply(err: InterpreterError): Json = JsonObject.fromMap {
+        Map("node" -> err.id.asJson, "reason" -> err.repr.asJson)
+      }.asJson
+    }
+}
