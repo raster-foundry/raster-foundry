37fbbc0b247846eea065bc75cf3d2d60a7c89194
James Santucci <james.santucci@gmail.com>
2017-04-10 13:08:22 -0400

    Add a custom rejection handler for malformed requests (#1458)
    * Add a custom rejection handler for malformed requests
    
    * Include helpful error message for type mismatches

---
+++ app-backend/common/src/main/scala/MalformedContentRejectionHandler.scala
@@ -0,0 +1,26 @@
+package com.azavea.rf.common
+
+import akka.http.scaladsl.server._
+import akka.http.scaladsl.model._
+import StatusCodes._
+import Directives._
+
+import scala.util.matching.Regex
+
+object RFRejectionHandler {
+  implicit val rfRejectionHandler:RejectionHandler = RejectionHandler.newBuilder()
+    .handle {
+    case e: MalformedRequestContentRejection => {
+      val missingFieldPattern = """(.*decode value.*)(?:DownField\()(.*)(?:\))""".r
+      val badTypePattern = """(?:.*DownField\()(.*)(?:\))""".r
+      e.getCause.getMessage match {
+        case missingFieldPattern(missing, field) =>
+          complete(ClientError(400)("Bad Request", s"Missing field: $field"))
+        case badTypePattern(field) =>
+          complete(ClientError(400)("Bad Request", s"Field cannot be parsed to expected type: $field"))
+        case _ => complete(ClientError(400)("Bad Request", e.getCause.getMessage))
+      }
+    }
+  }
+  .result()
+}
