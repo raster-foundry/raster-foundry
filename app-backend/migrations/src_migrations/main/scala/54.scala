23e3750a131a2ea4d6eaeca427c1d3fec8dfe6b8
jtarricone <jtarricone@users.noreply.github.com>
2017-04-11 14:07:55 -0400

    Add org->feature mapping table (#1474)

---
+++ app-backend/migrations/src_migrations/main/scala/54.scala
@@ -0,0 +1,15 @@
+import slick.driver.PostgresDriver.api._
+import com.liyaos.forklift.slick.SqlMigration
+
+object M54 {
+  RFMigrations.migrations = RFMigrations.migrations :+ SqlMigration(54)(List(
+    sqlu"""
+    CREATE TABLE organization_features (
+      organization UUID REFERENCES organizations (id),
+      feature_flag UUID REFERENCES feature_flags (id),
+      active BOOLEAN NOT NULL DEFAULT FALSE,
+      PRIMARY KEY (organization, feature_flag)
+    );
+    """
+  ))
+}
