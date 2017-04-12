422c9d73cd52e3a8ea83d001c2d8b417d78b66cc
Joe Tarricone <jtarricone@azavea.com>
2017-04-12 15:05:27 -0400

    Expose organization feature-flag override endpoint

--- app-backend/api/src/main/scala/config/Routes.scala
+++ app-backend/api/src/main/scala/config/Routes.scala
@@ -2,20 +2,39 @@ package com.azavea.rf.api.config
 
 import akka.http.scaladsl.server.Route
 
-import com.azavea.rf.common.Authentication
+import com.azavea.rf.common.{Authentication}
 import com.azavea.rf.database.Database
 import io.circe._
+import com.azavea.rf.database.tables.OrgFeatureFlags
+import io.circe.generic.auto._
 import de.heikoseeberger.akkahttpcirce.CirceSupport._
-
 
 trait ConfigRoutes extends Authentication {
   implicit def database: Database
 
-  def configRoutes: Route = {
+  val configRoutes: Route = {
     pathEndOrSingleSlash {
       get {
         complete {
           AngularConfigService.getConfig()
+        }
+      }
+    } ~
+    pathPrefix("organization") {
+      pathEndOrSingleSlash {
+        get {
+          complete {
+            AngularConfigService.getConfig()
+          }
+        }
+      } ~
+      path(JavaUUID) { orgId =>
+        pathEndOrSingleSlash {
+          get {
+            complete {
+              OrgFeatureFlags.getFeatures(orgId)
+            }
+          }
         }
       }
     }
