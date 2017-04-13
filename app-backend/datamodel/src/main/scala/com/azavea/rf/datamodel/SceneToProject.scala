72273c644c797838d6b0c37d4bb4ec694e594074
Colin Woodbury <colingw@gmail.com>
2017-04-13 08:27:41 -0700

    Give `ScenesToProjects` an `accepted` field
    - Any Scene marked `true` has either been added by the user directly,
      or has been accepted by them after it was flagged for addition by an
      AOI check.
    
    - Any row marked `false` should be removed.

--- app-backend/datamodel/src/main/scala/com/azavea/rf/datamodel/SceneToProject.scala
+++ app-backend/datamodel/src/main/scala/com/azavea/rf/datamodel/SceneToProject.scala
@@ -7,6 +7,7 @@ import io.circe.generic.JsonCodec
 case class SceneToProject(
   sceneId: UUID,
   projectId: UUID,
+  accepted: Boolean, /* Has a Scene been accepted from an AOI check? */
   sceneOrder: Option[Int] = None,
   colorCorrectParams: Option[ColorCorrect.Params] = None
 )
