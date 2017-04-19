12a17ab77ff7d4343546ad8097f187a74d37b6e4
Derek Dohler <ddohler@azavea.com>
2017-04-19 12:26:23 -0400

    Use per-user feature flags on the frontend

--- app-frontend/src/app/index.run.js
+++ app-frontend/src/app/index.run.js
@@ -1,37 +1,63 @@
 function runBlock(
     $rootScope, jwtHelper, $state, $location, APP_CONFIG,
-    authService, localStorage, rollbarWrapperService, intercomService
+    authService, localStorage, rollbarWrapperService, intercomService,
+    featureFlags, perUserFeatureFlags
 ) {
     'ngInject';
+    let flagsPromise;
+
+    if (authService.verifyAuthCache()) {
+        flagsPromise = perUserFeatureFlags.load();
+        featureFlags.set(flagsPromise);
+    }
 
     $rootScope.$on('$stateChangeStart', function (e, toState) {
-        if (APP_CONFIG.error && toState.name !== 'error') {
-            e.preventDefault();
-            $state.go('error');
-        } else if (toState.name !== 'login' && !authService.verifyAuthCache()) {
-            e.preventDefault();
-            rollbarWrapperService.init();
-            intercomService.shutdown();
-            $state.go('login');
-        } else if (toState.name !== 'login') {
-            rollbarWrapperService.init(authService.profile());
-            intercomService.bootWithUser(authService.profile());
+        function setupState() {
+            if (APP_CONFIG.error && toState.name !== 'error') {
+                e.preventDefault();
+                $state.go('error');
+            } else if (toState.name !== 'login' && !authService.verifyAuthCache()) {
+                e.preventDefault();
+                rollbarWrapperService.init();
+                intercomService.shutdown();
+                $state.go('login');
+            } else if (toState.name !== 'login') {
+                rollbarWrapperService.init(authService.profile());
+                intercomService.bootWithUser(authService.profile());
+            }
+        }
+        // TODO: I'm not sure exactly where this lies on the continuum between awful and the worst
+        // thing ever, but it's pretty bad. We should either refactor our app initialization so this
+        // is easier, or refactor FeatureFlags to deal in promises.
+        // Note that on initial login, the feature flags get populated in the AuthService.
+        if (flagsPromise) {
+            flagsPromise.then(setupState);
+        } else {
+            setupState();
         }
     });
 
     $rootScope.$on('$locationChangeStart', function () {
-        let token = localStorage.get('id_token');
-        if (token) {
-            if (!authService.verifyAuthCache()) {
+        function setupState() {
+            let token = localStorage.get('id_token');
+            if (token) {
+                if (!authService.verifyAuthCache()) {
+                    rollbarWrapperService.init();
+                    authService.login(token);
+                }
+            } else {
+                intercomService.shutdown();
                 rollbarWrapperService.init();
-                authService.login(token);
+                $state.go('login');
             }
+        }
+        if (flagsPromise) {
+            flagsPromise.then(setupState);
         } else {
-            intercomService.shutdown();
-            rollbarWrapperService.init();
-            $state.go('login');
+            setupState();
         }
     });
+
 }
 
 export default runBlock;
