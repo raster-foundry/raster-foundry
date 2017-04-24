5c362ec6498d990a624ff036177996f841441cb1
Nathan Zimmerman <npzimmerman@gmail.com>
2017-04-24 17:17:53 -0400

    Use toolrun to provide sources

---
+++ app-backend/tool/src/main/scala/params/EvalParams.scala
@@ -0,0 +1,12 @@
+package com.azavea.rf.tool.params
+
+import com.azavea.rf.tool.ast._
+
+import io.circe.generic.JsonCodec
+
+import java.util.UUID
+
+
+@JsonCodec
+case class EvalParams(sources: Map[UUID, RFMLRaster])
+
