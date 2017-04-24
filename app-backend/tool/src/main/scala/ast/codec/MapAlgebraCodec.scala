518dea76c0c27aaf80ce94a427fa74126dd38951
Nathan Zimmerman <npzimmerman@gmail.com>
2017-04-24 17:17:53 -0400

    Use toolrun to provide sources

--- app-backend/tool/src/main/scala/ast/codec/MapAlgebraCodec.scala
+++ app-backend/tool/src/main/scala/ast/codec/MapAlgebraCodec.scala
@@ -8,21 +8,16 @@ import io.circe.syntax._
 import java.security.InvalidParameterException
 
 trait MapAlgebraCodec
-    extends MapAlgebraSourceCodecs
-       with MapAlgebraOperationCodecs
+    extends MapAlgebraOperationCodecs
        with MapAlgebraUtilityCodecs {
 
   /** TODO: Add codec paths besides `raster source` and `operation` when supported */
   implicit def mapAlgebraDecoder = Decoder.instance[MapAlgebraAST] { ma =>
-    (ma._type, ma._symbol) match {
-      case (Some(_), None) =>
-        ma.as[MapAlgebraAST.Source[_]]
-      case (None, Some(_)) =>
+    ma._symbol match {
+      case Some(_) =>
         ma.as[MapAlgebraAST.Operation]
-      case (Some(_), Some(_)) =>
-        throw new InvalidParameterException(s"The JSON, '$ma.value.noSpaces', has both a 'type' and an 'apply' symbol")
-      case (None, None) =>
-        throw new InvalidParameterException(s"The JSON, '$ma.value.noSpaces', is not a recognized map algebra data type")
+      case None =>
+        ma.as[MapAlgebraAST.Source]
     }
   }
 
@@ -30,8 +25,8 @@ trait MapAlgebraCodec
     final def apply(ast: MapAlgebraAST): Json = ast match {
       case operation: MapAlgebraAST.Operation =>
         operation.asJson
-      case source: MapAlgebraAST.Source[_] =>
-        implicitly[Encoder[MapAlgebraAST.Source[_]]].apply(source)
+      case source: MapAlgebraAST.Source =>
+        source.asJson
       case _ =>
         throw new InvalidParameterException(s"Unrecognized AST: $ast")
     }
