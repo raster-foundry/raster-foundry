53f137b38f9f7e92a389efce4979d6b07e454908
Nathan Zimmerman <npzimmerman@gmail.com>
2017-04-25 12:14:29 -0400

    Fix tests for paramater binding

--- app-backend/tool/src/test/scala/eval/LazyTileSpec.scala
+++ app-backend/tool/src/test/scala/eval/LazyTileSpec.scala
@@ -2,6 +2,7 @@ package com.azavea.rf.tool.eval
 
 import com.azavea.rf.tool.ast._
 import com.azavea.rf.tool.eval._
+import com.azavea.rf.tool.params._
 import com.azavea.rf.tool.ast.codec.MapAlgebraCodec._
 import com.azavea.rf.tool.ast.MapAlgebraAST._
 
@@ -25,27 +26,14 @@ class LazyTileSpec
        with Matchers
        with TileBuilders
        with RasterMatchers {
-  import MapAlgebraAST._
 
-  val scene = SceneRaster(UUID.randomUUID, None)
+  def randomSourceAST = MapAlgebraAST.Source(UUID.randomUUID, None)
 
-  val red = RFMLRasterSource(
-    id = UUID.randomUUID,
-    label = Some("RED"),
-    value = Some(scene.copy(band = Some(4)))
-  )
+  val redTileSource = SceneRaster(UUID.randomUUID, Some(4))
 
-  val nir = RFMLRasterSource(
-    id = UUID.randomUUID,
-    label = Some("NIR"),
-    value = Some(scene.copy(band = Some(5)))
-  )
+  val nirTileSource = SceneRaster(UUID.randomUUID, Some(5))
 
-  val blue = RFMLRasterSource(
-    id = UUID.randomUUID,
-    label = Some("BLUE"),
-    value = Some(scene.copy(band = Some(1)))
-  )
+  val blueTileSource = SceneRaster(UUID.randomUUID, Some(1))
 
   val tile: MultibandTile = ArrayMultibandTile(
     createValueTile(d = 4, v = 0),
@@ -70,8 +58,11 @@ class LazyTileSpec
     requests = Nil
     // This breakmap should convert all cells (which are set to a value of 5) to 77
     val breakmap = ClassBreaks(Map(6.0 -> 77), ClassBreaks.Options(LessThanOrEqualTo, 123))
+    val srcAST = randomSourceAST
     val tms = Interpreter.interpretTMS(
-      ast = red.classify(breakmap), source = goodSource
+      ast = srcAST.classify(breakmap),
+      params = EvalParams(Map(srcAST.id -> redTileSource)),
+      source = goodSource
     )
 
     val ret = tms(0, 1, 1)
@@ -87,8 +78,12 @@ class LazyTileSpec
 
   it("should evaluate subtraction") {
     requests = Nil
+    val src1 = randomSourceAST
+    val src2 = randomSourceAST
     val tms = Interpreter.interpretTMS(
-      ast = blue - nir, source = goodSource
+      ast = src1 - src2,
+      params = EvalParams(Map(src1.id -> blueTileSource, src2.id -> nirTileSource)),
+      source = goodSource
     )
 
     val ret = tms(0, 1, 1)
@@ -105,9 +100,14 @@ class LazyTileSpec
   it("should preserve order of operations while evaluating subtraction") {
     requests = Nil
     // 4, 5, 1
-    val ast = Subtraction(List(nir, red, blue), UUID.randomUUID(), None)
+    val src1 = randomSourceAST
+    val src2 = randomSourceAST
+    val src3 = randomSourceAST
+    val ast = Subtraction(List(src1, src2, src3), UUID.randomUUID(), None)
     val tms = Interpreter.interpretTMS(
-      ast = ast, source = goodSource
+      ast = ast,
+      params = EvalParams(Map(src1.id -> nirTileSource, src2.id -> redTileSource, src3.id -> blueTileSource)),
+      source = goodSource
     )
 
     val ret = tms(0, 1, 1)
@@ -123,8 +123,12 @@ class LazyTileSpec
 
   it("should evaluate division") {
     requests = Nil
+    val src1 = randomSourceAST
+    val src2 = randomSourceAST
     val tms = Interpreter.interpretTMS(
-      ast = red / nir, source = goodSource
+      ast = src1 / src2,
+      params = EvalParams(Map(src1.id -> redTileSource, src2.id -> nirTileSource)),
+      source = goodSource
     )
 
     val ret = tms(0, 1, 1)
@@ -140,9 +144,14 @@ class LazyTileSpec
 
   it("should preserve order of operations while evaluating division") {
     requests = Nil
-    val ast = Division(List(blue, nir, red), UUID.randomUUID(), None)
+    val src1 = randomSourceAST
+    val src2 = randomSourceAST
+    val src3 = randomSourceAST
+    val ast = Division(List(src1, src2, src3), UUID.randomUUID(), None)
     val tms = Interpreter.interpretTMS(
-      ast = ast, source = goodSource
+      ast = ast,
+      params = EvalParams(Map(src1.id -> blueTileSource, src2.id -> nirTileSource, src3.id -> redTileSource)),
+      source = goodSource
     )
 
     val ret = tms(0, 1, 1)
@@ -158,8 +167,12 @@ class LazyTileSpec
 
   it("should evaluate multiplication") {
     requests = Nil
+    val src1 = randomSourceAST
+    val src2 = randomSourceAST
     val tms = Interpreter.interpretTMS(
-      ast = red * nir, source = goodSource
+      ast = src1 * src2,
+      params = EvalParams(Map(src1.id -> redTileSource, src2.id -> nirTileSource)),
+      source = goodSource
     )
 
     val ret = tms(0, 1, 1)
@@ -174,24 +187,14 @@ class LazyTileSpec
   }
 
   it("should evaluate addition") {
-
-    val r = RFMLRasterSource(
-      id = UUID.randomUUID,
-      label = Some("red"),
-      value = Some(SceneRaster(UUID.fromString("63b15316-2b28-4280-8eb5-c98de4adffab"), Some(4)))
-    )
-    val n = RFMLRasterSource(
-      id = UUID.randomUUID,
-      label = Some("nir"),
-      value = Some(SceneRaster(UUID.fromString("63b15316-2b28-4280-8eb5-c98de4adffab"), Some(5)))
-    )
-    val testAST = (n - r) / (n + r)
-
     requests = Nil
+    val src1 = randomSourceAST
+    val src2 = randomSourceAST
     val tms = Interpreter.interpretTMS(
-      ast = red + nir, source = goodSource
+      ast = src1 + src2,
+      params = EvalParams(Map(src1.id -> redTileSource, src2.id -> nirTileSource)),
+      source = goodSource
     )
-    val whatever = (red + nir).asJson.noSpaces
 
     val ret = tms(0, 1, 1)
     val op = Await.result(ret, 10.seconds) match {
