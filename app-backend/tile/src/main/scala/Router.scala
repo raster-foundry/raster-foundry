b4fdeef21008878b458d6be7aae95c6e343833de
Colin Woodbury <colingw@gmail.com>
2017-04-25 10:42:33 -0700

    First pass at AST `/validate/` endpoint

--- app-backend/tile/src/main/scala/Router.scala
+++ app-backend/tile/src/main/scala/Router.scala
@@ -1,31 +1,11 @@
 package com.azavea.rf.tile
 
-import com.azavea.rf.database.Database
-import com.azavea.rf.tile.image._
-import com.azavea.rf.tile.tool._
-import com.azavea.rf.tile.tool.ToolParams._
-import com.azavea.rf.tile.routes._
-import com.azavea.rf.tile.tool.TileSources
-import com.azavea.rf.common.UserErrorHandler
-import com.azavea.rf.datamodel.ColorCorrect
-import com.azavea.rf.datamodel.ColorCorrect.Params.colorCorrectParams
-
-
-import geotrellis.raster._
-import geotrellis.raster.io._
-import geotrellis.raster.histogram.Histogram
-import geotrellis.raster.render.{Png, ColorRamp, ColorMap}
-import geotrellis.spark._
-import geotrellis.spark.io._
+import akka.http.scaladsl.model.{HttpResponse, StatusCodes}
 import akka.http.scaladsl.server._
-import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.model.{ContentType, HttpEntity, HttpResponse, MediaTypes, StatusCodes}
-import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import com.azavea.rf.database.Database
+import com.azavea.rf.tile.routes._
+import com.azavea.rf.tile.tool._
 import com.typesafe.scalalogging.LazyLogging
-import spray.json._
-
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent._
 
 class Router extends LazyLogging
     with TileAuthentication
