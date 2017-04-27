24d0a91f3d043f5de2a1c253227cb7006a49accf
Colin Woodbury <colingw@gmail.com>
2017-04-27 09:31:16 -0700

    Remove project column from toolruns

--- app-backend/migrations/src_migrations/main/scala/40.scala
+++ app-backend/migrations/src_migrations/main/scala/40.scala
@@ -1,1 +1,13 @@
-../../../src/main/scala/migrations/40.scala
+
+import slick.driver.PostgresDriver.api._
+import com.liyaos.forklift.slick.SqlMigration
+
+object M40 {
+  RFMigrations.migrations = RFMigrations.migrations :+ SqlMigration(40)(List(
+    sqlu"""
+  CREATE TYPE ingest_status AS ENUM ('NOTINGESTED', 'TOBEINGESTED', 'INGESTING', 'INGESTED', 'FAILED');
+
+  ALTER TABLE scenes ADD COLUMN ingest_status ingest_status NOT NULL DEFAULT 'NOTINGESTED';
+"""
+  ))
+}
