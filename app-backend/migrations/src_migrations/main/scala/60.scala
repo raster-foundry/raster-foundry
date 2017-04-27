404895583aaf70df8281e479fdd6e5b053a23063
Colin Woodbury <colingw@gmail.com>
2017-04-27 09:31:16 -0700

    Remove project column from toolruns

---
+++ app-backend/migrations/src_migrations/main/scala/60.scala
@@ -0,0 +1,10 @@
+import slick.driver.PostgresDriver.api._
+import com.liyaos.forklift.slick.SqlMigration
+
+object M60 {
+  RFMigrations.migrations = RFMigrations.migrations :+ SqlMigration(60)(List(
+    sqlu"""
+ALTER TABLE tool_runs DROP COLUMN project
+"""
+  ))
+}
