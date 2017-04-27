d40a7e4bc7be939c3d203ec111f735054b4cb1fe
Alex Kaminsky <akaminsky@azavea.com>
2017-04-27 11:10:34 -0400

    Improve reference project deletion handling

--- app-backend/migrations/src_migrations/main/scala/58.scala
+++ app-backend/migrations/src_migrations/main/scala/58.scala
@@ -5,7 +5,7 @@ object M58 {
   RFMigrations.migrations = RFMigrations.migrations :+ SqlMigration(58)(List(
     sqlu"""
 ALTER TABLE uploads ADD COLUMN project_id UUID DEFAULT NULL;
-ALTER TABLE uploads ADD CONSTRAINT upload_project_fkey FOREIGN KEY (project_id) REFERENCES projects(id);
+ALTER TABLE uploads ADD CONSTRAINT upload_project_fkey FOREIGN KEY (project_id) REFERENCES projects(id) ON DELETE SET NULL;
 """
   ))
 }
