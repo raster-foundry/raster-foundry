fe9a6ab447fc914f1ebd4c4ef5d4ad628ce3a0ae
Grisha Pomadchin <gr.pomadchin@gmail.com>
2017-05-01 15:30:06 +0300

    Sentinel2 import

--- app-backend/batch/src/main/scala/com/azavea/rf/batch/util/conf/Config.scala
+++ app-backend/batch/src/main/scala/com/azavea/rf/batch/util/conf/Config.scala
@@ -2,6 +2,7 @@ package com.azavea.rf.batch.util.conf
 
 import com.azavea.rf.datamodel.Band
 
+import geotrellis.proj4.CRS
 import com.typesafe.config.ConfigFactory
 import net.ceedubs.ficus.Ficus
 import net.ceedubs.ficus.readers.ArbitraryTypeReader
@@ -40,8 +41,47 @@ trait Config {
     sparkMemory: String
   )
 
+  protected case class Sentinel2Bands(
+    // 10m
+    B02: Band.Create,
+    B03: Band.Create,
+    B04: Band.Create,
+    B08: Band.Create,
+    // 20m
+    B05: Band.Create,
+    B06: Band.Create,
+    B07: Band.Create,
+    B8A: Band.Create,
+    B11: Band.Create,
+    B12: Band.Create,
+    // 60m
+    B01: Band.Create,
+    B09: Band.Create,
+    B10: Band.Create
+  )
+
+  protected case class Sentinel2(
+    organization: String,
+    bandLookup: Sentinel2Bands,
+    datasourceId: String,
+    baseHttpPath: String,
+    awsRegion: Option[String],
+    bucketName: String,
+    targetProj: String
+  ) {
+    def organizationUUID = UUID.fromString(organization)
+    def datasourceUUID = UUID.fromString(datasourceId)
+    def targetProjCRS = CRS.fromName(targetProj)
+    def bandByName(key: String): Option[Band.Create] =
+      bandLookup.getClass.getDeclaredFields.toList.filter(_.getName == key).map { field =>
+        field.setAccessible(true)
+        field.get(bandLookup).asInstanceOf[Band.Create]
+      }.headOption
+  }
+
   private lazy val config = ConfigFactory.load()
   protected lazy val landsat8Config = config.as[Landsat8]("landsat8")
+  protected lazy val sentinel2Config = config.as[Sentinel2]("sentinel2")
   protected lazy val airflowUser = config.as[String]("airflow.user")
   protected lazy val exportDefConfig = config.as[ExportDef]("export-def")
   val jarPath = "s3://us-east-1.elasticmapreduce/libs/script-runner/script-runner.jar"
