c00279e0b9984a100d99c10be286d2db6efa4790
Hector Castro <hcastro@azavea.com>
2017-05-04 13:37:50 -0400

    Remove Python API client from main repository
    The Python API client have been migrated to:
    
      https://github.com/azavea/raster-foundry-python-client

--- app-clients/python/rf/__init__.py
+++
@@ -1,5 +0,0 @@
-"""rf - A python client for Raster Foundry -- a web platform for combining, analyzing, and publishing raster data."""
-
-__version__ = '0.0.1'
-__author__ = 'Raster Foundry <info@rasterfoundry.com>'
-__all__ = []
