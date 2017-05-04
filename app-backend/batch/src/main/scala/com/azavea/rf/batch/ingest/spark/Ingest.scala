050b02988c883dddfb4b69eb4d7bda516c275a72
Chris Brown <cbrown@azavea.com>
2017-05-04 19:39:26 -0400

    URL decode prefix from S3 URI
    Fixes a bug where a properly encoded URI is interpreted
    literally and does not exist as a prefix.

--- app-backend/batch/src/main/scala/com/azavea/rf/batch/ingest/spark/Ingest.scala
+++ app-backend/batch/src/main/scala/com/azavea/rf/batch/ingest/spark/Ingest.scala
@@ -145,7 +145,8 @@ object Ingest extends SparkJob with LazyLogging {
         FileRangeReader(new File(uri))
       case "s3" | "s3a" | "s3n" =>
         val (bucket, prefix) = S3.parse(uri)
-        S3RangeReader(bucket, prefix, S3Client.DEFAULT)
+        val decodedPrefix = java.net.URLDecoder.decode(prefix, "UTF-8")
+        S3RangeReader(bucket, decodedPrefix, S3Client.DEFAULT)
       case "http" | "https"
           if uri.getAuthority == "s3.amazonaws.com"
           && uri.getQuery.contains("AWSAccessKeyId") =>
