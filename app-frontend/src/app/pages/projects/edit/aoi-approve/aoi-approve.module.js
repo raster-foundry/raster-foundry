3b52b1de2691a63fee693095b184f82a0c6ba668
Alex Kaminsky <akaminsky@azavea.com>
2017-05-10 17:10:45 -0400

    Add AoI interface and new sidepanel styles

---
+++ app-frontend/src/app/pages/projects/edit/aoi-approve/aoi-approve.module.js
@@ -0,0 +1,8 @@
+import angular from 'angular';
+import AOIApproveController from './aoi-approve.controller.js';
+
+const AOIApproveModule = angular.module('page.projects.edit.aoi-approve', []);
+
+AOIApproveModule.controller('AOIApproveController', AOIApproveController);
+
+export default AOIApproveModule;
