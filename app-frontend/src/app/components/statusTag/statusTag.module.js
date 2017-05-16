dfcfc62abb66611d11b4dabad2fedd03a0ae065d
mwilliams <mwilliams@azavea.com>
2017-05-16 07:30:42 -0400

    cleansup status tags styling

--- app-frontend/src/app/components/statusTag/statusTag.module.js
+++ app-frontend/src/app/components/statusTag/statusTag.module.js
@@ -2,8 +2,6 @@ import angular from 'angular';
 import StatusTagComponent from './statusTag.component.js';
 import StatusTagController from './statusTag.controller.js';
 
-require('./statusTag.scss');
-
 const StatusTagModule = angular.module('components.statusTag', []);
 
 StatusTagModule.component('rfStatusTag', StatusTagComponent);
