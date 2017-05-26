658d51d26f4c7deab62586096cf4db61abd4782e
Colin Woodbury <colingw@gmail.com>
2017-05-26 10:09:36 -0700

    AST-based Exports possible
    - Note that `interpretRDD` produces an RDD of `Tile`, not `MultibandTile`.
    - Still need to reinstate Grisha's code which does simple
      colour-correcting exports on MultibandTiles.

--- app-backend/datamodel/src/main/scala/com/azavea/rf/datamodel/InputDefinition.scala
+++ app-backend/datamodel/src/main/scala/com/azavea/rf/datamodel/InputDefinition.scala
@@ -1,17 +1,16 @@
-package com.azavea.rf.datamodel
-
-import com.azavea.rf.datamodel._
-import io.circe.generic.JsonCodec
-
-import geotrellis.vector._
-import geotrellis.vector.io._
+ package com.azavea.rf.datamodel
 
 import java.util.UUID
 
+import io.circe.generic.JsonCodec
+import com.azavea.rf.tool.ast.MapAlgebraAST
+import com.azavea.rf.tool.params.EvalParams
+
 @JsonCodec
 case class InputDefinition(
-  projectId: UUID,
+  projectId: UUID,  // TODO: Might not be necessary.
   resolution: Int,
-  layers: Array[ExportLayerDefinition],
-  mask: Option[MultiPolygon]
+  ast: MapAlgebraAST,
+  params: EvalParams,
+  colorCorrections: Option[ColorCorrect.Params]
 )
