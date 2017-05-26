78447bbcc328f18e560934cb6f7e0ab3e2654d78
Alex Kaminsky <akaminsky@azavea.com>
2017-05-26 14:44:27 -0400

    Remove stub data

--- app-frontend/src/app/pages/projects/edit/colormode/colormode.controller.js
+++ app-frontend/src/app/pages/projects/edit/colormode/colormode.controller.js
@@ -5,51 +5,11 @@ export default class ProjectsEditColormode {
         this.$parent = $scope.$parent.$ctrl;
         this.colorCorrectService = colorCorrectService;
         this.projectService = projectService;
-
-        this.bands = {
-            natural: {
-                label: 'Natural Color',
-                value: {redBand: 3, greenBand: 2, blueBand: 1}
-            },
-            cir: {
-                label: 'Color infrared',
-                value: {redBand: 4, greenBand: 3, blueBand: 2}
-            },
-            urban: {
-                label: 'Urban',
-                value: {redBand: 6, greenBand: 5, blueBand: 4}
-            },
-            water: {
-                label: 'Water',
-                value: {redBand: 4, greenBand: 5, blueBand: 3}
-            },
-            atmosphere: {
-                label: 'Atmosphere penetration',
-                value: {redBand: 6, greenBand: 4, blueBand: 2}
-            },
-            agriculture: {
-                label: 'Agriculture',
-                value: {redBand: 5, greenBand: 4, blueBand: 1}
-            },
-            forestfire: {
-                label: 'Forest Fire',
-                value: {redBand: 6, greenBand: 4, blueBand: 1}
-            },
-            bareearth: {
-                label: 'Bare Earth change detection',
-                value: {redBand: 5, greenBand: 2, blueBand: 1}
-            },
-            vegwater: {
-                label: 'Vegetation & water',
-                value: {redBand: 4, greenBand: 6, blueBand: 0}
-            }
-        };
-
-        this.currentBands = null;
-        this.correction = null;
     }
 
     $onInit() {
+        this.currentBands = null;
+        this.correction = null;
         this.$parent.sceneListQuery.then(() => {
             let layer = this.$parent.sceneLayers.values().next();
             if (layer && layer.value) {
@@ -66,24 +26,32 @@ export default class ProjectsEditColormode {
     }
 
     isActiveColorMode(key) {
-        let keyBands = this.bands[key].value;
+        const unifiedComposites = this.$parent.unifiedComposites;
+        if (unifiedComposites) {
+            let keyBands = unifiedComposites[key].value;
 
-        let isActive = this.currentBands &&
-            keyBands.redBand === this.currentBands.redBand &&
-            keyBands.greenBand === this.currentBands.greenBand &&
-            keyBands.blueBand === this.currentBands.blueBand;
-        return isActive;
+            let isActive = this.currentBands &&
+                keyBands.redBand === this.currentBands.redBand &&
+                keyBands.greenBand === this.currentBands.greenBand &&
+                keyBands.blueBand === this.currentBands.blueBand;
+
+            return isActive;
+        }
+        return false;
     }
 
     setBands(bandName) {
-        this.currentBands = this.bands[bandName].value;
-        this.correction = Object.assign(this.correction, this.currentBands);
-        const promise = this.colorCorrectService.bulkUpdate(
-            this.projectService.currentProject.id,
-            Array.from(this.$parent.sceneLayers.keys()),
-            this.correction
-        );
-        this.redrawMosaic(promise);
+        const unifiedComposites = this.$parent.unifiedComposites;
+        if (unifiedComposites) {
+            this.currentBands = unifiedComposites[bandName].value;
+            this.correction = Object.assign(this.correction, this.currentBands);
+            const promise = this.colorCorrectService.bulkUpdate(
+                this.projectService.currentProject.id,
+                Array.from(this.$parent.sceneLayers.keys()),
+                this.correction
+            );
+            this.redrawMosaic(promise);
+        }
     }
 
     /**
