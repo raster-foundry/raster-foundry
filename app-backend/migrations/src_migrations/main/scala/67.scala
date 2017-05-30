b46843d7e72b32ccfc8801e5e40f245a3363d151
Lucien Knechtli <lknechtli@azavea.com>
2017-05-30 15:49:09 -0400

    Add migration to remove scene_name_org_datasource constraint

---
+++ app-backend/migrations/src_migrations/main/scala/67.scala
@@ -0,0 +1,10 @@
+import slick.driver.PostgresDriver.api._
+import com.liyaos.forklift.slick.SqlMigration
+
+object M67 {
+  RFMigrations.migrations = RFMigrations.migrations :+ SqlMigration(67)(List(
+sqlu"""
+ALTER TABLE scenes DROP CONSTRAINT scene_name_org_datasource;
+"""
+))
+}
