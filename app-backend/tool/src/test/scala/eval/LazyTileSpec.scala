3c6a133040b7b923aab23f3ef32c48ad7661a46e
Nathan Zimmerman <npzimmerman@gmail.com>
2017-06-05 16:47:06 -0400

    Add some AST documentation

--- app-backend/tool/src/test/scala/eval/LazyTileSpec.scala
+++ app-backend/tool/src/test/scala/eval/LazyTileSpec.scala
@@ -29,39 +29,52 @@ class LazyTileSpec
 
   def randomSourceAST = MapAlgebraAST.Source(UUID.randomUUID, None)
 
-  val redTileSource = SceneRaster(UUID.randomUUID, Some(4))
+  def tileSource(band: Int) = SceneRaster(UUID.randomUUID, Some(band))
 
-  val nirTileSource = SceneRaster(UUID.randomUUID, Some(5))
-
-  val blueTileSource = SceneRaster(UUID.randomUUID, Some(1))
-
-  val tile: MultibandTile = ArrayMultibandTile(
-    createValueTile(d = 4, v = 0),
-    createValueTile(d = 4, v = 1),
-    createValueTile(d = 4, v = 2),
-    createValueTile(d = 4, v = 3),
-    createValueTile(d = 4, v = 4),
-    createValueTile(d = 4, v = 5)
-  )
+  def tile(value: Int): Tile = createValueTile(d = 4, v = value)
 
   var requests = List.empty[RFMLRaster]
+
   val goodSource = (raster: RFMLRaster, z: Int, x: Int, y: Int) => {
     raster match {
       case r@SceneRaster(id, Some(band)) =>
         requests = raster :: requests
-        Future { Some(tile.bands(band)) }
+        Future { Some(tile(band)) }
       case _ => Future.failed(new Exception("can't find that"))
     }
   }
 
-  it("should evaluate classification") {
+  it("should evaluate classification in (case hit)") {
     requests = Nil
     // This breakmap should convert all cells (which are set to a value of 5) to 77
     val breakmap = ClassMap(Map(6.0 -> 77), ClassMap.Options(LessThanOrEqualTo, 123))
     val srcAST = randomSourceAST
     val tms = Interpreter.interpretTMS(
       ast = srcAST.classify(breakmap),
-      sourceMapping = Map(srcAST.id -> redTileSource),
+      sourceMapping = Map(srcAST.id -> tileSource(4)),
+      source = goodSource
+    )
+    println("Classification node: ", srcAST.classify(breakmap).asJson.noSpaces)
+
+    val ret = tms(0, 1, 1)
+    val op = Await.result(ret, 10.seconds) match {
+      case Valid(lazyTile) =>
+        val maybeTile = lazyTile.evaluate
+        requests.length should be (1)
+        assertEqual(maybeTile.get, createValueTile(4, 77))
+      case i@Invalid(_) =>
+        fail(s"$i")
+    }
+  }
+
+  it("should evaluate classification (case miss)") {
+    requests = Nil
+    // This breakmap should convert all cells (which are set to a value of 5) to 77
+    val breakmap = ClassMap(Map(2.0 -> 77), ClassMap.Options(LessThanOrEqualTo, 123))
+    val srcAST = randomSourceAST
+    val tms = Interpreter.interpretTMS(
+      ast = srcAST.classify(breakmap),
+      sourceMapping = Map(srcAST.id -> tileSource(4)),
       source = goodSource
     )
 
@@ -70,7 +83,7 @@ class LazyTileSpec
       case Valid(lazyTile) =>
         val maybeTile = lazyTile.evaluate
         requests.length should be (1)
-        assertEqual(maybeTile.get, createValueTile(4, 77).toArray)
+        assertEqual(maybeTile.get, createValueTile(d = 4, v = NODATA))
       case i@Invalid(_) =>
         fail(s"$i")
     }
@@ -82,9 +95,10 @@ class LazyTileSpec
     val src2 = randomSourceAST
     val tms = Interpreter.interpretTMS(
       ast = src1 - src2,
-      sourceMapping = Map(src1.id -> blueTileSource, src2.id -> nirTileSource),
+      sourceMapping = Map(src1.id -> tileSource(1), src2.id -> tileSource(5)),
       source = goodSource
     )
+    println("Subtraction node: ", (src1 - src2).asJson.noSpaces)
 
     val ret = tms(0, 1, 1)
     val op = Await.result(ret, 10.seconds) match {
@@ -106,7 +120,7 @@ class LazyTileSpec
     val ast = Subtraction(List(src1, src2, src3), UUID.randomUUID(), None)
     val tms = Interpreter.interpretTMS(
       ast = ast,
-      sourceMapping = Map(src1.id -> nirTileSource, src2.id -> redTileSource, src3.id -> blueTileSource),
+      sourceMapping = Map(src1.id -> tileSource(5), src2.id -> tileSource(4), src3.id -> tileSource(1)),
       source = goodSource
     )
 
@@ -127,9 +141,10 @@ class LazyTileSpec
     val src2 = randomSourceAST
     val tms = Interpreter.interpretTMS(
       ast = src1 / src2,
-      sourceMapping = Map(src1.id -> redTileSource, src2.id -> nirTileSource),
+      sourceMapping = Map(src1.id -> tileSource(4), src2.id -> tileSource(5)),
       source = goodSource
     )
+    println("Division node: ", (src1 / src2).asJson.noSpaces)
 
     val ret = tms(0, 1, 1)
     val op = Await.result(ret, 10.seconds) match {
@@ -150,7 +165,7 @@ class LazyTileSpec
     val ast = Division(List(src1, src2, src3), UUID.randomUUID(), None)
     val tms = Interpreter.interpretTMS(
       ast = ast,
-      sourceMapping = Map(src1.id -> blueTileSource, src2.id -> nirTileSource, src3.id -> redTileSource),
+      sourceMapping = Map(src1.id -> tileSource(1), src2.id -> tileSource(5), src3.id -> tileSource(4)),
       source = goodSource
     )
 
@@ -171,9 +186,10 @@ class LazyTileSpec
     val src2 = randomSourceAST
     val tms = Interpreter.interpretTMS(
       ast = src1 * src2,
-      sourceMapping = Map(src1.id -> redTileSource, src2.id -> nirTileSource),
+      sourceMapping = Map(src1.id -> tileSource(4), src2.id -> tileSource(5)),
       source = goodSource
     )
+    println("Multiplication node: ", (src1 * src2).asJson.noSpaces)
 
     val ret = tms(0, 1, 1)
     val op = Await.result(ret, 10.seconds) match {
@@ -192,9 +208,10 @@ class LazyTileSpec
     val src2 = randomSourceAST
     val tms = Interpreter.interpretTMS(
       ast = src1 + src2,
-      sourceMapping = Map(src1.id -> redTileSource, src2.id -> nirTileSource),
+      sourceMapping = Map(src1.id -> tileSource(4), src2.id -> tileSource(5)),
       source = goodSource
     )
+    println("Addition node: ", (src1 + src2).asJson.noSpaces)
 
     val ret = tms(0, 1, 1)
     val op = Await.result(ret, 10.seconds) match {
@@ -213,9 +230,10 @@ class LazyTileSpec
     val src2 = randomSourceAST
     val tms = Interpreter.interpretTMS(
       ast = src1.max(src2),
-      sourceMapping = Map(src1.id -> blueTileSource, src2.id -> nirTileSource),
+      sourceMapping = Map(src1.id -> tileSource(1), src2.id -> tileSource(5)),
       source = goodSource
     )
+    println("LocalMax node: ", (src1.max(src2)).asJson.noSpaces)
 
     val ret = tms(0, 1, 1)
     val op = Await.result(ret, 10.seconds) match {
@@ -234,9 +252,10 @@ class LazyTileSpec
     val src2 = randomSourceAST
     val tms = Interpreter.interpretTMS(
       ast = src1.min(src2),
-      sourceMapping = Map(src1.id -> blueTileSource, src2.id -> nirTileSource),
+      sourceMapping = Map(src1.id -> tileSource(1), src2.id -> tileSource(5)),
       source = goodSource
     )
+    println("LocalMin node: ", (src1.min(src2)).asJson.noSpaces)
 
     val ret = tms(0, 1, 1)
     val op = Await.result(ret, 10.seconds) match {
@@ -244,6 +263,29 @@ class LazyTileSpec
         val maybeTile = lazytile.evaluateDouble
         requests.length should be (2)
         maybeTile.get.get(0, 0) should be (1)
+      case i@Invalid(_) =>
+        fail(s"$i")
+    }
+  }
+
+  it("should evaluate multiple operations (ndvi)") {
+    requests = Nil
+    val nir = randomSourceAST
+    val red = randomSourceAST
+    val ndvi = (nir - red) / (nir + red)
+    val tms = Interpreter.interpretTMS(
+      ast = ndvi,
+      sourceMapping = Map(nir.id -> tileSource(1), red.id -> tileSource(5)),
+      source = goodSource
+    )
+    println("Simple NDVI calculation: ", ndvi.asJson.noSpaces)
+
+    val ret = tms(0, 1, 1)
+    val op = Await.result(ret, 10.seconds) match {
+      case Valid(lazytile) =>
+        val maybeTile = lazytile.evaluateDouble
+        requests.length should be (4)
+        maybeTile.get.getDouble(0, 0) should be (-4.0/6.0)
       case i@Invalid(_) =>
         fail(s"$i")
     }
