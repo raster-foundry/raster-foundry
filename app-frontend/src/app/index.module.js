b22530f63af3b37fa2ff5e4e1c767a7d41106819
Lucien Knechtli <lknechtli@azavea.com>
2017-06-06 14:34:02 -0400

    Add Dropbox Frontend views

--- app-frontend/src/app/index.module.js
+++ app-frontend/src/app/index.module.js
@@ -77,10 +77,10 @@ const App = angular.module(
 
         require('./pages/settings/settings.module.js').name,
         require('./pages/settings/profile/profile.module.js').name,
-        require('./pages/settings/account/account.module.js').name,
         require('./pages/settings/tokens/tokens.module.js').name,
         require('./pages/settings/tokens/api/api.module.js').name,
-        require('./pages/settings/tokens/map/map.module.js').name
+        require('./pages/settings/tokens/map/map.module.js').name,
+        require('./pages/settings/connections/connections.module.js').name
     ]
 );
 
