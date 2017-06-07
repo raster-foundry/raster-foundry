5150ff39b3ca5fb81cd77caa1a556d4e6a680fbe
Colin Woodbury <colingw@gmail.com>
2017-06-07 10:51:22 -0700

    Make project_id in exports table nullable

---
+++ app-backend/migrations/src_migrations/main/scala/68.scala
@@ -0,0 +1,10 @@
+import slick.driver.PostgresDriver.api._
+import com.liyaos.forklift.slick.SqlMigration
+
+object M68 {
+  RFMigrations.migrations = RFMigrations.migrations :+ SqlMigration(68)(List(
+    sqlu"""
+ALTER TABLE exports ALTER COLUMN project_id DROP NOT NULL;
+"""
+  ))
+}
