ccdd104259f464f71375dc9ce844f96f3ba0f096
Lucien Knechtli <lknechtli@azavea.com>
2017-06-07 12:24:26 -0400

    Remove old mosaic components

--- app-frontend/src/app/components/mosaicParameters/mosaicScenesParameters.component.js
+++
@@ -1,8 +0,0 @@
-import mosaicScenesParametersTpl from './mosaicScenesParameters.html';
-
-const mosaicScenesParameters = {
-    templateUrl: mosaicScenesParametersTpl,
-    controller: 'MosaicScenesParametersController'
-};
-
-export default mosaicScenesParameters;
