a70ade32e088def89f68fd8f0531e41a5d2861bc
Lucien Knechtli <lknechtli@azavea.com>
2017-06-07 11:36:27 -0400

    Remove prototypes and old editor pages

--- app-frontend/src/app/pages/editor/editor.controller.js
+++
@@ -1,9 +0,0 @@
-class EditorController {
-    constructor() {
-        'ngInject';
-
-        // This is an abstract view so it's just a container for its children
-    }
-}
-
-export default EditorController;
