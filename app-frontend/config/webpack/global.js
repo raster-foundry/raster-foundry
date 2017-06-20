3fd5a02b6374591eb60207037afd0ca90b419f82
Chris Brown <cbrown@azavea.com>
2017-06-20 05:03:38 -0400

    Use HTTPs for aerial map tiles

--- app-frontend/config/webpack/global.js
+++ app-frontend/config/webpack/global.js
@@ -39,7 +39,7 @@ const basemaps = JSON.stringify({
             }
         },
         Aerial: {
-            url: 'http://{s}.{base}.maps.cit.api.here.com/maptile/2.1/{type}/{mapID}/hybrid.day/{z}/{x}/{y}/{size}/{format}?app_id={app_id}&app_code={app_code}&lg={language}',
+            url: 'https://{s}.{base}.maps.cit.api.here.com/maptile/2.1/{type}/{mapID}/hybrid.day/{z}/{x}/{y}/{size}/{format}?app_id={app_id}&app_code={app_code}&lg={language}',
             properties: {
                 attribution: 'Map &copy; 1987-2014 <a href="http://developer.here.com">HERE</a>',
                 subdomains: '1234',
