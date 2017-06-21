a3b614db856c6dd0dae0c0e38e9131d9262e66f8
Aaron Su <xunzesu@outlook.com>
2017-06-21 11:44:27 -0400

    Enable front end color auto correction.

--- app-frontend/src/app/pages/projects/edit/color/color.controller.js
+++ app-frontend/src/app/pages/projects/edit/color/color.controller.js
@@ -1,18 +1,52 @@
 export default class ProjectsEditColorController {
     constructor( // eslint-disable-line max-params
-        $log, $scope, $q, projectService, layerService, sceneService, $state, mapService,
-        datasourceService
+        $scope, projectService, colorCorrectService
     ) {
         'ngInject';
-        this.projectService = projectService;
-        this.layerService = layerService;
-        this.sceneService = sceneService;
-        this.datasourceService = datasourceService;
-        this.$state = $state;
         this.$scope = $scope;
         this.$parent = $scope.$parent.$ctrl;
-        this.$q = $q;
-        this.$log = $log;
-        this.getMap = () => mapService.getMap('edit');
+        this.projectService = projectService;
+        this.colorCorrectService = colorCorrectService;
+    }
+
+    $onInit() {
+        this.currentBands = null;
+        this.correction = null;
+        this.$parent.sceneListQuery.then(() => {
+            let layer = this.$parent.sceneLayers.values().next();
+            if (layer && layer.value) {
+                layer.value.getColorCorrection().then((correction) => {
+                    this.currentBands = {
+                        redBand: correction.redBand,
+                        greenBand: correction.greenBand,
+                        blueBand: correction.blueBand
+                    };
+                    this.correction = correction;
+                });
+            }
+        });
+    }
+
+    isActiveAutoColorCorrection(correctionType) {
+        if (this.correction) {
+            return this.correction[correctionType].enabled;
+        }
+        return false;
+    }
+
+    setAutoColorCorrection(correctionType) {
+        this.correction[correctionType].enabled = !this.correction[correctionType].enabled;
+        const promise = this.colorCorrectService.bulkUpdate(
+            this.projectService.currentProject.id,
+            Array.from(this.$parent.sceneLayers.keys()),
+            this.correction
+        );
+        this.redrawMosaic(promise);
+    }
+
+    redrawMosaic(promise) {
+        promise.then(() => {
+            this.$parent.layerFromProject();
+        });
     }
 }
