726868aaea9acb01a9aaa6612af155c9aea0c516
Colin Woodbury <colingw@gmail.com>
2017-06-27 13:58:03 -0700

    Improve how Tiles are sized for Global Interpretation
    - This is the result of a long meeting with Nathan this morning
      regarding the new Masking operation and how Tile operations might
      not be defined for non-overlapping Scenes. This ensures that *all*
      Tiles produced for feeding into `interpretGlobal` (for calculating a
      histogram) overlap and are of the same size.

--- app-backend/tile/src/main/scala/tool/TileSources.scala
+++ app-backend/tile/src/main/scala/tool/TileSources.scala
@@ -7,13 +7,16 @@ import com.azavea.rf.tool.ast._
 import com.azavea.rf.tool.params._
 
 import com.typesafe.scalalogging.LazyLogging
+import cats.data._
 import cats.implicits._
 import geotrellis.raster._
 import geotrellis.slick.Projected
 import geotrellis.spark._
 import geotrellis.spark.io.s3._
 import geotrellis.spark.io._
+import geotrellis.vector.Extent
 
+import java.util.UUID
 import scala.util._
 import scala.concurrent._
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -26,50 +29,80 @@ import scala.concurrent.ExecutionContext.Implicits.global
   */
 object TileSources extends LazyLogging {
 
+  /** Given the data sources for some AST, determine the "data window" for
+    * the entire data set. This ensures that global AST interpretation will behave
+    * correctly, so that valid  histograms can be generated.
+    */
+  def fullDataWindow(
+    rs: Map[UUID, RFMLRaster]
+  )(implicit database: Database): OptionT[Future, (Extent, Map[UUID, Int])] = {
+    rs
+      .values
+      .toStream
+      .map(r => dataWindow(r).map({ case (e, z) => (e, (r.id, z)) }))
+      .sequence
+      .map({ pairs =>
+        val (extents, zooms) = pairs.unzip
+        val extent = extents.reduce(_ combine _)
+
+        (extent, zooms.toMap)
+      })
+  }
+
+  /** Given a reference to a source of data, computes the "data window" of
+    * the entire dataset. The `Int` is the minimally acceptable zoom level at
+    * which one could read a Layer for the purpose of calculating a representative
+    * histogram.
+    */
+  def dataWindow(r: RFMLRaster)(implicit database: Database): OptionT[Future, (Extent, Int)] = r match {
+    case SceneRaster(id, Some(_), _) => {
+      LayerCache.attributeStoreForLayer(id).mapFilter({ case (store, _) =>
+        GlobalSummary.minAcceptableSceneZoom(id, store, 256)  // TODO: 512?
+      })
+    }
+    case ProjectRaster(id, Some(_), _) => GlobalSummary.minAcceptableProjectZoom(id, 256) // TODO: 512?
+
+    /* Don't attempt work for a RFMLRaster which will fail AST validation anyway */
+    case _ => OptionT.none
+  }
+
   /** This source will return the raster for all of zoom level 1 and is
     *  useful for generating a histogram which allows binning values into
     *  quantiles.
     */
-  def globalSource(r: RFMLRaster)(implicit database: Database): Future[Option[Tile]] =
-    r match {
-      case scene @ SceneRaster(sceneId, Some(band), maybeND) =>
-        LayerCache.attributeStoreForLayer(sceneId).mapFilter { case (store, _) =>
-          GlobalSummary.minAcceptableSceneZoom(sceneId, store).flatMap { case (extent, zoom) =>
-            blocking {
-              Try {
-                val layerId = LayerId(sceneId.toString, zoom)
-                S3CollectionLayerReader(store)
-                  .query[SpatialKey, MultibandTile, TileLayerMetadata[SpatialKey]](layerId)
-                  .result
-                  .stitch
-                  .crop(extent)
-                  .tile
-              } match {
-                case Success(tile) =>
-                  Some(tile.interpretAs(maybeND.getOrElse(tile.cellType)))
-                case Failure(e) =>
-                  logger.error(s"Query layer $sceneId at zoom $zoom for $extent: ${e.getMessage}")
-                  None
-              }
-            }
+  def globalSource(
+    extent: Extent,
+    zooms: Map[UUID, Int],
+    r: RFMLRaster
+  )(implicit database: Database): Future[Option[Tile]] = r match {
+    case SceneRaster(id, Some(band), maybeND) =>
+      LayerCache.attributeStoreForLayer(id).mapFilter({ case (store, _) =>
+        blocking {
+          Try {
+            val layerId = LayerId(id.toString, zooms(id))
+
+            S3CollectionLayerReader(store)
+              .query[SpatialKey, MultibandTile, TileLayerMetadata[SpatialKey]](layerId)
+              .result
+              .stitch
+              .crop(extent)
+              .tile
+          } match {
+            case Success(tile) => Option(tile.band(band).interpretAs(maybeND.getOrElse(tile.cellType)))
+            case Failure(e) =>
+              logger.error(s"Query layer $id at zoom ${zooms(id)} for $extent: ${e.getMessage}")
+              None
           }
-        }.map({ mbtile => mbtile.band(band) }).value
+        }
+      }).value
 
-      case scene @ SceneRaster(sceneId, None, _) =>
-        logger.warn(s"Request for $scene does not contain band index")
-        Future.successful(None)
-
-      case project @ ProjectRaster(projId, Some(band), maybeND) =>
-        GlobalSummary.minAcceptableProjectZoom(projId).flatMap { case (extent, zoom) =>
-          Mosaic.rawForExtent(projId, zoom, Some(Projected(extent.toPolygon, 3857)))
-            .map({ tile => tile.band(band).interpretAs(maybeND.getOrElse(tile.cellType)) })
-        }.value
-
-      case project @ ProjectRaster(projId, None, _) =>
-        logger.warn(s"Request for $project does not contain band index")
-        Future.successful(None)
-
+    case ProjectRaster(projId, Some(band), maybeND) => {
+      Mosaic.rawForExtent(projId, zooms(projId), Some(Projected(extent.toPolygon, 3857)))
+        .map({ tile => tile.band(band).interpretAs(maybeND.getOrElse(tile.cellType)) }).value
     }
+
+    case _ => Future.successful(None)
+  }
 
   /** This source provides support for z/x/y TMS tiles */
   def cachedTmsSource(r: RFMLRaster, z: Int, x: Int, y: Int)(implicit database: Database): Future[Option[Tile]] =
