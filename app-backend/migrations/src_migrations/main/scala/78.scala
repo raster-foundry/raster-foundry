4067aea2ca302acde67c7ec2e16cc733363d6c82
James <james.santucci@gmail.com>
2017-07-13 17:39:11 -0400

    Include migration for uploads table

---
+++ app-backend/migrations/src_migrations/main/scala/78.scala
@@ -0,0 +1,11 @@
+import slick.driver.PostgresDriver.api._
+import com.liyaos.forklift.slick.SqlMigration
+
+object M78 {
+  RFMigrations.migrations = RFMigrations.migrations :+ SqlMigration(77)(List(
+    sqlu"""
+ALTER TABLE uploads ADD COLUMN scene_cloud_cover REAL;
+ALTER TABLE uploads ADD COLUMN scene_acquisition_date TIMESTAMP;
+"""
+  ))
+}
