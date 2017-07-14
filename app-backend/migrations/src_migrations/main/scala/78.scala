6449785f6e013a2fe1d2fc92eaf4480ec7b47530
James <james.santucci@gmail.com>
2017-07-14 11:00:05 -0400

    Don't have two migration 77s like a big dummy

--- app-backend/migrations/src_migrations/main/scala/78.scala
+++ app-backend/migrations/src_migrations/main/scala/78.scala
@@ -2,7 +2,7 @@ import slick.driver.PostgresDriver.api._
 import com.liyaos.forklift.slick.SqlMigration
 
 object M78 {
-  RFMigrations.migrations = RFMigrations.migrations :+ SqlMigration(77)(List(
+  RFMigrations.migrations = RFMigrations.migrations :+ SqlMigration(78)(List(
     sqlu"""
 ALTER TABLE uploads ADD COLUMN scene_cloud_cover REAL;
 ALTER TABLE uploads ADD COLUMN scene_acquisition_date TIMESTAMP;
