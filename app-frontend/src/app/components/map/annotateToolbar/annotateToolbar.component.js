3c5c09d38f054e976255ae4b4daa37a4b346dc42
Aaron Su <xunzesu@outlook.com>
2017-07-25 11:20:34 -0400

    List annotations on sidebar

--- app-frontend/src/app/components/map/annotateToolbar/annotateToolbar.component.js
+++ app-frontend/src/app/components/map/annotateToolbar/annotateToolbar.component.js
@@ -3,7 +3,7 @@ import annotateToolbarTpl from './annotateToolbar.html';
 /*
   @param {string} mapId id of map to use
   @param {object?} geom geometry containing FeatureCollection
-  @param {function(geometry: Object)} onSave function to call when the save button is clicked.\
+  @param {function(geometry: Object)} onSave function to call when the save button is clicked.
  */
 const annotateToolbar = {
     templateUrl: annotateToolbarTpl,
