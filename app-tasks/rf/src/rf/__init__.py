f3e846b7b67e80233735da9a00c809bbeec40db4
James <james.santucci@gmail.com>
2017-08-14 10:22:41 -0400

    Decrease boto logging verbosity

--- app-tasks/rf/src/rf/__init__.py
+++ app-tasks/rf/src/rf/__init__.py
@@ -4,3 +4,6 @@ import logging
 from logging import NullHandler
 
 logging.getLogger(__name__).addHandler(NullHandler())
+logging.getLogger('boto3').setLevel(logging.WARNING)
+logging.getLogger('botocore').setLevel(logging.WARNING)
+logging.getLogger('nose').setLevel(logging.WARNING)
