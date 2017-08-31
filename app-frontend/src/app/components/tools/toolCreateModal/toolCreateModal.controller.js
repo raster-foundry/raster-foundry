365222bfe1d8e0706a12533e42b7ffb3109b53ed
Alex Kaminsky <akaminsky@azavea.com>
2017-08-31 09:47:13 -0400

    Add parsing of `classify` function in mamlscript

--- app-frontend/src/app/components/tools/toolCreateModal/toolCreateModal.controller.js
+++ app-frontend/src/app/components/tools/toolCreateModal/toolCreateModal.controller.js
@@ -46,12 +46,13 @@ export default class ToolCreateModalController {
         let expressionTree = null;
         try {
             expressionTree = mathjs.parse(this.definitionExpression);
-            this.toolBuffer.definition = this.transformNode(expressionTree);
+            this.toolBuffer.definition = this.expressionTreeToMAML(expressionTree);
             this.toolService.createTool(this.toolBuffer).then(tool => {
                 this.dismiss();
                 this.$state.go('lab.run', { toolid: tool.id });
             });
         } catch (e) {
+            console.log(e);
             this.currentError = 'The tool definition is not valid';
             this.isProcessing = false;
         }
@@ -125,6 +126,85 @@ export default class ToolCreateModalController {
 
         return builtNode;
     }
+
+    expressionTreeToMAML(tree, value, allowCollapse = true) {
+        let mamlNode = {
+          id: this.uuid4.generate(),
+          metadata: {}
+        };
+
+        switch (tree.type) {
+          case "AssignmentNode":
+            return this.expressionTreeToMAML(tree.object, tree.value.value);
+          case "ConstantNode":
+            Object.assign(mamlNode, {
+              type: 'const',
+              constant: tree.value,
+              metadata: {
+                label: tree.value
+              }
+            });
+            break;
+          case "OperatorNode":
+            mamlNode.apply = tree.op;
+            mamlNode.metadata.label = tree.fn;
+            mamlNode.metadata.collapsable = allowCollapse;
+            break;
+          case "FunctionNode":
+            mamlNode.apply = tree.fn.name;
+            mamlNode.metadata.label = tree.fn.name;
+            if (mamlNode.apply === 'classify') {
+              const classifications = tree.args[1];
+              tree.args = [ tree.args[0] ];
+              mamlNode.classMap = {
+                classifications: this.objectNodeToClassifications(classifications)
+              };
+            }
+            break;
+          case "ParenthesisNode":
+            return this.expressionTreeToMAML(tree.content, null, false);
+          case "SymbolNode":
+            if (tree.name.startsWith('_')) {
+              mamlNode.type = 'const';
+              mamlNode.metadata.label = tree.name.substring(1);
+              if (value) {
+                mamlNode.constant = parseFloat(value);
+              }
+            } else {
+              mamlNode.type = 'src';
+              mamlNode.metadata.label = tree.name;
+            }
+            mamlNode.id = this.getSymbolId(mamlNode.metadata.label);
+            break;
+          default:
+            return false;
+        };
+
+        if (tree.args && tree.args.length) {
+          mamlNode.args = tree.args.map(a => this.expressionTreeToMAML(a));
+        }
+
+        if (tree.type === 'OperatorNode' && tree.args) {
+            mamlNode.args.forEach((c, i) => {
+                if (c.apply && c.apply === mamlNode.apply && c.metadata.collapsable) {
+                    mamlNode.args = [
+                        ...mamlNode.args.slice(0, i),
+                        ...c.args,
+                        ...mamlNode.args.slice(i + 1, mamlNode.args.length)
+                    ];
+                }
+            });
+        }
+
+        return mamlNode;
+      }
+
+      objectNodeToClassifications(node) {
+        return Object.keys(node.properties).reduce((acc, p) => {
+          acc[`${parseFloat(p)}`] = parseInt(node.properties[p].value, 10);
+          return acc;
+        }, {});
+      }
 
     isValid() {
         this.currentParsingError = '';
