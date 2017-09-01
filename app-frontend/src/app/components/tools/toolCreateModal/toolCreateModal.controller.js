7a580f8d3b628107fab09208009eba171acd731b
Alex Kaminsky <alexander.s.kaminsky@gmail.com>
2017-09-01 12:39:44 -0400

    Add parsing of `classify` function in mamlscript (#2474)

--- app-frontend/src/app/components/tools/toolCreateModal/toolCreateModal.controller.js
+++ app-frontend/src/app/components/tools/toolCreateModal/toolCreateModal.controller.js
@@ -46,7 +46,7 @@ export default class ToolCreateModalController {
         let expressionTree = null;
         try {
             expressionTree = mathjs.parse(this.definitionExpression);
-            this.toolBuffer.definition = this.transformNode(expressionTree);
+            this.toolBuffer.definition = this.expressionTreeToMAML(expressionTree);
             this.toolService.createTool(this.toolBuffer).then(tool => {
                 this.dismiss();
                 this.$state.go('lab.run', { toolid: tool.id });
@@ -76,61 +76,95 @@ export default class ToolCreateModalController {
         return id;
     }
 
-    transformNode(node, value) {
-        let builtNode = {
+    expressionTreeToMAML(tree, value, allowCollapse = true) {
+        let mamlNode = {
+            id: this.uuid4.generate(),
             metadata: {}
         };
 
-        if (node.op && allowedOps.indexOf(node.op) >= 0) {
-            // Op node
-            builtNode.id = this.uuid4.generate();
-            builtNode.apply = node.op;
-            builtNode.metadata.label = node.fn;
-            builtNode.args = node.args.map(n => this.transformNode(n));
-        } else if (node.fn && node.fn.name && allowedOps.indexOf(node.fn.name) >= 0) {
-            // Function node; handle nearly the same as op node, for now
-            builtNode.id = this.uuid4.generate();
-            builtNode.apply = node.fn.name;
-            builtNode.metadata.label = node.fn.name;
-            builtNode.args = node.args.map(n => this.transformNode(n));
-        } else if (node.name) {
-            // Symbol node
-            if (node.object && node.value) {
-                // Symbol with a default (constant)
-                return this.transformNode(node.object, node.value.value);
+        switch (tree.type) {
+        case 'AssignmentNode':
+            return this.expressionTreeToMAML(tree.object, tree.value.value);
+        case 'ConstantNode':
+            Object.assign(mamlNode, {
+                type: 'const',
+                constant: tree.value,
+                metadata: {
+                    label: tree.value
+                }
+            });
+            break;
+        case 'OperatorNode':
+            if (allowedOps.indexOf(tree.op) >= 0) {
+                mamlNode.apply = tree.op;
+                mamlNode.metadata.label = tree.fn;
+                mamlNode.metadata.collapsable = allowCollapse;
+                break;
             }
-            // Symbol without a default yet (constant or not)
-            let isConst = node.name.startsWith('_');
-            if (isConst) {
-                // Constant symbols without a default yet
-                builtNode.type = 'const';
-                builtNode.metadata.label = node.name.substring(1);
+            return false;
+        case 'FunctionNode':
+            mamlNode.apply = tree.fn.name;
+            mamlNode.metadata.label = tree.fn.name;
+            if (mamlNode.apply === 'classify') {
+                const classifications = tree.args[1];
+                tree.args = [ tree.args[0] ];
+                mamlNode.classMap = {
+                    classifications: this.objectNodeToClassifications(classifications)
+                };
+            }
+            break;
+        case 'ParenthesisNode':
+            return this.expressionTreeToMAML(tree.content, null, false);
+        case 'SymbolNode':
+            if (tree.name.startsWith('_')) {
+                mamlNode.type = 'const';
+                mamlNode.metadata.label = tree.name.substring(1);
                 if (value) {
-                    builtNode.constant = parseFloat(value);
+                    mamlNode.constant = parseFloat(value);
                 }
             } else {
-                // Raster symbolds
-                builtNode.type = 'src';
-                builtNode.metadata.label = node.name;
+                mamlNode.type = 'src';
+                mamlNode.metadata.label = tree.name;
             }
-            builtNode.id = this.getSymbolId(builtNode.metadata.label);
-        } else if (node.value) {
-            builtNode.id = this.uuid4.generate();
-            builtNode.type = 'const';
-            builtNode.constant = node.value;
-            builtNode.metadata.label = node.value;
-        } else if (node.content) {
-            return this.transformNode(node.content);
+            mamlNode.id = this.getSymbolId(mamlNode.metadata.label);
+            break;
+        default:
+            return false;
         }
 
-        return builtNode;
+        if (tree.args && tree.args.length) {
+            mamlNode.args = tree.args.map(a => this.expressionTreeToMAML(a));
+        }
+
+        if (tree.type === 'OperatorNode' && tree.args) {
+            mamlNode.args.forEach((c, i) => {
+                if (c.apply && c.apply === mamlNode.apply && c.metadata.collapsable) {
+                    mamlNode.args = [
+                        ...mamlNode.args.slice(0, i),
+                        ...c.args,
+                        ...mamlNode.args.slice(i + 1, mamlNode.args.length)
+                    ];
+                }
+            });
+        }
+        return mamlNode;
+    }
+
+    objectNodeToClassifications(node) {
+        return Object.keys(node.properties).reduce((acc, p) => {
+            acc[`${parseFloat(p)}`] = parseInt(node.properties[p].value, 10);
+            return acc;
+        }, {});
     }
 
     isValid() {
         this.currentParsingError = '';
         const parensBalanced = this.validateParenDepth();
+        const bracesBalanced = this.validateBraceDepth();
         if (!parensBalanced) {
             this.currentParsingError = 'The parentheses in this expression are not balanced';
+        } else if (!bracesBalanced) {
+            this.currentParsingError = 'The braces in this expression are not balanced';
         }
         return this.toolBuffer.title && this.definitionExpression && parensBalanced;
     }
@@ -143,6 +177,23 @@ export default class ToolCreateModalController {
                 if (c === '(') {
                     d += 1;
                 } else if (c === ')') {
+                    d -= 1;
+                }
+                return { depth: d, continue: d >= 0};
+            }
+            return acc;
+        }, { depth: 0, continue: true });
+        return result.continue && result.depth === 0;
+    }
+
+    validateBraceDepth() {
+        const expressionArray = this.definitionExpression.split('');
+        const result = expressionArray.reduce((acc, c) => {
+            if (acc.continue) {
+                let d = acc.depth;
+                if (c === '{') {
+                    d += 1;
+                } else if (c === '}') {
                     d -= 1;
                 }
                 return { depth: d, continue: d >= 0};
