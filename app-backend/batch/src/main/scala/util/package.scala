58a85a3dfabecc0a3060d438b0ff71b93aea0f7c
Hector Castro <hectcastro@gmail.com>
2017-09-25 21:45:25 -0400

    Fix IOUtils.toString deprecation warning
    Need to supply `Charset` as second argument.

--- app-backend/batch/src/main/scala/util/package.scala
+++ app-backend/batch/src/main/scala/util/package.scala
@@ -2,7 +2,7 @@ package com.rasterfoundry.batch
 
 import java.io._
 import java.net._
-import java.util.Scanner
+import java.nio.charset.Charset
 
 import cats.implicits._
 import com.amazonaws.services.s3.{AmazonS3ClientBuilder, AmazonS3URI}
@@ -104,10 +104,7 @@ package object util extends LazyLogging {
   def readString(fileUri: URI): String = {
     val is = getStream(fileUri)
     try {
-      // A jdk [input stream -> string] method which avoids the IOUtils' 'toString' deprecation:
-      // https://community.oracle.com/blogs/pat/2004/10/23/stupid-scanner-tricks
-      val scan: Scanner = new Scanner(is).useDelimiter("\\A")
-      if (scan.hasNext()) scan.next() else ""
+      IOUtils.toString(is, Charset.defaultCharset())
     } finally {
       is.close()
     }
