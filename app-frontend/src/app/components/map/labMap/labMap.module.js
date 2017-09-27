ed68edf0b65f30fe4989b03afa2cde4c2d6e8828
Aaron Su <xunzesu@outlook.com>
2017-09-26 21:14:33 -0400

    Add measurement tool to lab map.

--- app-frontend/src/app/components/map/labMap/labMap.module.js
+++ app-frontend/src/app/components/map/labMap/labMap.module.js
@@ -3,6 +3,11 @@ import LabMapComponent from './labMap.component.js';
 import LabMapController from './labMap.controller.js';
 require('./frame.module.js');
 
+require('./labMap.scss');
+
+require('leaflet-draw/dist/leaflet.draw.css');
+require('leaflet-draw/dist/leaflet.draw.js');
+
 const LabMapModule = angular.module('components.map.labMap', []);
 
 LabMapModule.component('rfLabMap', LabMapComponent);
