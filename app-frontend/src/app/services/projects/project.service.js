9e66a390f18d3ecec8fa2a8a9cf033ff4638d3a7
Alex Kaminsky <akaminsky@azavea.com>
2017-10-05 15:43:45 -0400

    Add search for projects list

--- app-frontend/src/app/services/projects/project.service.js
+++ app-frontend/src/app/services/projects/project.service.js
@@ -148,6 +148,55 @@ export default (app) => {
             return this.Project.query(params).$promise;
         }
 
+        searchQuery() {
+            let deferred = this.$q.defer();
+            let pageSize = 1000;
+            let firstPageParams = {
+                pageSize: pageSize,
+                page: 0,
+                sort: 'createdAt,desc'
+            };
+
+            let firstRequest = this.query(firstPageParams);
+
+            firstRequest.then((page) => {
+                let self = this;
+                let num = page.count;
+                let requests = [firstRequest];
+                if (page.count > pageSize) {
+                    let requestMaker = function *(totalResults) {
+                        let pageNum = 1;
+                        while (pageNum * pageSize <= totalResults) {
+                            let pageParams = {
+                                pageSize: pageSize,
+                                page: pageNum,
+                                sort: 'createdAt,desc'
+                            };
+                            yield self.query(pageParams);
+                            pageNum += 1;
+                        }
+                    };
+
+                    requests = requests.concat(Array.from(requestMaker(num)));
+                }
+
+                this.$q.all(requests).then(
+                    (allResponses) => {
+                        deferred.resolve(
+                            allResponses.reduce((res, resp) => res.concat(resp.results), [])
+                        );
+                    },
+                    () => {
+                        deferred.reject('Error loading projects.');
+                    }
+                );
+            }, () => {
+                deferred.reject('Error loading projects.');
+            });
+
+            return deferred.promise;
+        }
+
         get(id) {
             return this.Project.get({id}).$promise;
         }
