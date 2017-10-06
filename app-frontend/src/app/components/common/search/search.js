77611d8574871f9fa9ba52d294fdf60b9c9a0e03
Alex Kaminsky <akaminsky@azavea.com>
2017-10-06 14:15:32 -0400

    Change format of component

--- app-frontend/src/app/components/common/search/search.js
+++ app-frontend/src/app/components/common/search/search.js
@@ -1,39 +1,43 @@
 import angular from 'angular';
 import searchTpl from './search.html';
 
+const searchComponent = {
+    templateUrl: searchTpl,
+    controller: 'SearchController',
+    bindings: {
+        autoFocus: '<',
+        placeholder: '@',
+        onSearch: '&'
+    }
+};
+
+class SearchController {
+    constructor(
+        $element, $timeout
+    ) {
+        'ngInject';
+        this.$element = $element;
+        this.$timeout = $timeout;
+    }
+
+    $postLink() {
+        if (this.autoFocus) {
+            this.$timeout(() => {
+                const el = $(this.$element[0]).find('input').get(0);
+                el.focus();
+            }, 0);
+        }
+    }
+
+    onSearchAction() {
+        this.onSearch({value: this.searchText});
+    }
+
+    clearSearch() {
+        this.searchText = '';
+    }
+}
+
 export default angular.module('components.common.search', [])
-    .component('rfSearch', {
-        templateUrl: searchTpl,
-        controller: 'SearchController',
-        bindings: {
-            autoFocus: '<',
-            placeholder: '@',
-            onSearch: '&'
-        }
-    })
-    .controller('SearchController', class SearchController {
-        constructor(
-            $element, $timeout
-        ) {
-            'ngInject';
-            this.$element = $element;
-            this.$timeout = $timeout;
-        }
-
-        $postLink() {
-            if (this.autoFocus) {
-                this.$timeout(() => {
-                    const el = $(this.$element[0]).find('input').get(0);
-                    el.focus();
-                }, 0);
-            }
-        }
-
-        onSearchAction() {
-            this.onSearch({value: this.searchText});
-        }
-
-        clearSearch() {
-            this.searchText = '';
-        }
-    });
+    .component('rfSearch', searchComponent)
+    .controller('SearchController', SearchController);
