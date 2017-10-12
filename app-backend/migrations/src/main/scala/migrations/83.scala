773212bc5124dcc354e92f014b26843933d7e620
Lucien Knechtli <lknechtli@azavea.com>
2017-10-12 12:01:11 -0400

    Add name to tool_runs
    Remove tool from tool_runs
    Fix tests & update swagger spec

---
+++ app-backend/migrations/src/main/scala/migrations/83.scala
@@ -0,0 +1,1 @@
+../../../../src_migrations/main/scala/83.scala
---
+++ app-backend/migrations/src_migrations/main/scala/83.scala
@@ -0,0 +1,17 @@
+import slick.driver.PostgresDriver.api._
+import com.liyaos.forklift.slick.SqlMigration
+
+object M83 {
+  RFMigrations.migrations = RFMigrations.migrations :+ SqlMigration(83)(List(
+    sqlu"""
+ALTER TABLE tool_runs ADD COLUMN name TEXT DEFAULT '';
+
+UPDATE tool_runs
+SET name = tools.title
+FROM tools
+WHERE tool_runs.tool = tools.id;
+
+ALTER TABLE tool_runs DROP COLUMN tool;
+""" // your sql code goes here
+  ))
+}
