8e23c98214a005c329259658c09ebe4cb07f477d
Alex Kaminsky <akaminsky@azavea.com>
2017-10-20 12:02:53 -0400

    Add `emailNotifications` field to Users

---
+++ app-backend/migrations/src_migrations/main/scala/84.scala
@@ -0,0 +1,10 @@
+import slick.driver.PostgresDriver.api._
+import com.liyaos.forklift.slick.SqlMigration
+
+object M84 {
+  RFMigrations.migrations = RFMigrations.migrations :+ SqlMigration(84)(List(
+    sqlu"""
+      ALTER TABLE users ADD COLUMN email_notifications BOOLEAN DEFAULT FALSE;
+    """
+  ))
+}
