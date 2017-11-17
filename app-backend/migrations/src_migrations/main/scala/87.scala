3ff2316baaafa4d9f0a35f7836375c6f39b6e01f
Lucien Knechtli <lknechtli@gmail.com>
2017-11-17 11:09:08 -0500

    Add migration to delete duplicate unused landsat scenes

---
+++ app-backend/migrations/src_migrations/main/scala/87.scala
@@ -0,0 +1,25 @@
+import slick.driver.PostgresDriver.api._
+import com.liyaos.forklift.slick.SqlMigration
+
+object M87 {
+  RFMigrations.migrations = RFMigrations.migrations :+ SqlMigration(87)(List(
+    sqlu"""
+delete from scenes
+where id in (
+  select id from (
+    select id, owner, datasource,  ROW_NUMBER() over (
+      partition by name order by created_at
+    ) as rnum
+    from scenes
+    where datasource = '697a0b91-b7a8-446e-842c-97cda155554d'
+    and owner = 'rf|airflow-user'
+  ) t
+  where t.rnum > 1
+  and id not in (
+    select scenes.id
+    from scenes join scenes_to_projects on scenes.id = scenes_to_projects.scene_id
+  )
+);
+"""
+  ))
+}
