4b753e4f692cd6272e40b0e611bc00901b3ff70d
Lucien Knechtli <lknechtli@gmail.com>
2017-11-21 13:57:00 -0500

    Remove old unused projects/detail/... files

--- app-frontend/src/app/pages/projects/detail/detail.module.js
+++
@@ -1,9 +0,0 @@
-import pagination from 'angular-ui-bootstrap/src/pagination';
-
-import ProjectsDetailController from './detail.controller.js';
-
-const ProjectsDetailModule = angular.module('pages.projects.detail', [pagination]);
-
-ProjectsDetailModule.controller('ProjectsDetailController', ProjectsDetailController);
-
-export default ProjectsDetailModule;
