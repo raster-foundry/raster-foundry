7b01ca503824ba5df4daf4dfe88ba06867eeccfa
Aaron Su <xunzesu@outlook.com>
2017-12-14 17:16:50 -0500

    Allow dragging map when creating polygon without adding new nodes on annotation map.

--- app-frontend/src/app/components/map/annotateToolbar/annotateToolbar.module.js
+++ app-frontend/src/app/components/map/annotateToolbar/annotateToolbar.module.js
@@ -1,4 +1,4 @@
-/* globals L */
+/* globals L, _ */
 import angular from 'angular';
 import annotateToolbarTpl from './annotateToolbar.html';
 require('./annotateToolbar.scss');
@@ -16,11 +16,10 @@ const AnnotateToolbarComponent = {
 
 class AnnotateToolbarController {
     constructor(
-        $log, $scope, $ngRedux,
+        $scope, $ngRedux,
         mapService
     ) {
         'ngInject';
-        this.$log = $log;
         this.$scope = $scope;
 
         let unsubscribe = $ngRedux.connect(
@@ -47,7 +46,9 @@ class AnnotateToolbarController {
         this.getMap().then((mapWrapper) => {
             this.listeners = [
                 mapWrapper.on(L.Draw.Event.CREATED, this.createShape.bind(this)),
-                mapWrapper.on('click', this.onMapClick.bind(this))
+                mapWrapper.on('click', this.onMapClick.bind(this)),
+                mapWrapper.on('mousedown', this.onMapMousedown.bind(this)),
+                mapWrapper.on('mouseup', this.onMapMouseup.bind(this))
             ];
             this.setDrawHandlers(mapWrapper);
         });
@@ -80,6 +81,25 @@ class AnnotateToolbarController {
         }
     }
 
+    onMapMousedown() {
+        if (this.isDrawCancel && this.isDrawingPolygon) {
+            this.getMap().then((mapWrapper) => {
+                this.mapCenter = mapWrapper.map.getCenter();
+            });
+        }
+    }
+
+    onMapMouseup() {
+        if (this.isDrawCancel && this.isDrawingPolygon) {
+            this.getMap().then((mapWrapper) => {
+                let center = mapWrapper.map.getCenter();
+                if (!_.isEqual(this.mapCenter, center)) {
+                    this.drawPolygonHandler.deleteLastVertex();
+                }
+            });
+        }
+    }
+
     setDrawHandlers(mapWrapper) {
         this.drawRectangleHandler = new L.Draw.Rectangle(mapWrapper.map, {
             shapeOptions: {
@@ -108,6 +128,7 @@ class AnnotateToolbarController {
             this.drawPolygonHandler.disable();
             this.drawMarkerHandler.disable();
         } else if (shapeType === 'polygon') {
+            this.isDrawingPolygon = true;
             this.drawPolygonHandler.enable();
             this.lastHandler = this.drawPolygonHandler;
             this.drawRectangleHandler.disable();
@@ -149,6 +170,10 @@ class AnnotateToolbarController {
 
         if (this.isDrawingRectangle && e.layerType === 'rectangle') {
             this.isDrawingRectangle = false;
+        }
+
+        if (this.isDrawingPolygon && e.layerType === 'polygon') {
+            this.isDrawingPolygon = false;
         }
 
         this.onShapeCreated({
