d0009c7594fa0400932e2a68ba2b9c1d0efbacc3
Lucien Knechtli <lknechtli@gmail.com>
2018-01-08 17:55:53 -0500

    Move color composites controller to module file

--- app-frontend/src/app/pages/imports/datasources/detail/colorComposites/colorComposites.module.js
+++ app-frontend/src/app/pages/imports/datasources/detail/colorComposites/colorComposites.module.js
@@ -1,4 +1,37 @@
-import ColorCompositesController from './colorComposites.controller.js';
+class ColorCompositesController {
+    constructor($scope, $state) {
+        'ngInject';
+        this.$scope = $scope;
+        this.$parent = $scope.$parent.$ctrl;
+        this.$state = $state;
+    }
+
+    $onInit() {
+        this.initListeners();
+    }
+
+    initListeners() {
+        this.$scope.$on('$locationChangeStart', () => {
+            this.$parent.cancel();
+        });
+    }
+
+    updateBuffer(preset, key, val) {
+        this.$parent.colorCompositesBuffer[preset].value[key] = +val;
+    }
+
+    deleteFromBuffer(preset) {
+        delete this.$parent.colorCompositesBuffer[preset];
+    }
+
+    save() {
+        this.$parent.saveColorComposites();
+    }
+
+    cancel() {
+        this.$state.go('imports.datasources.detail');
+    }
+}
 
 const ColorCompositesModule =
   angular.module('pages.imports.datasources.details.colorComposites', []);
