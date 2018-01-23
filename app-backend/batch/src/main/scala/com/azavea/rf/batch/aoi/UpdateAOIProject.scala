40f43ed596adfc94ff9fac795528375076db1376
Lucien Knechtli <lknechtli@gmail.com>
2018-01-23 11:17:46 -0500

    Add notification stub to project AOI updates

--- app-backend/batch/src/main/scala/com/azavea/rf/batch/aoi/UpdateAOIProject.scala
+++ app-backend/batch/src/main/scala/com/azavea/rf/batch/aoi/UpdateAOIProject.scala
@@ -1,5 +1,10 @@
 package com.azavea.rf.batch.aoi
 
+import com.auth0.client.mgmt._
+import com.auth0.client.mgmt.filter.UserFilter
+import com.auth0.client.auth._
+import com.auth0.json.auth.TokenHolder
+
 import com.azavea.rf.batch.Job
 import com.azavea.rf.database.tables._
 import com.azavea.rf.database.{Database => DB}
@@ -22,40 +27,65 @@ case class UpdateAOIProject(projectId: UUID)(implicit val database: DB) extends 
   import database.driver.api._
 
   def run: Unit = {
+    val authApi = new AuthAPI(auth0Config.domain, auth0Config.clientId, auth0Config.clientSecret)
+    val mgmtTokenRequest = authApi.requestToken(s"https://${auth0Config.domain}/api/v2/")
+    val mgmtToken = mgmtTokenRequest.execute
+    val mgmtApi = new ManagementAPI(auth0Config.domain, mgmtToken.getAccessToken)
+
     logger.info(s"Updating Project $projectId AOI...")
     val result = for {
       user: User                   <- OptionT(Users.getUserById(systemUser))
       (project: Project, aoi: AOI) <- OptionT(Projects.getAOIProject(projectId, user))
-      scenes: Iterable[UUID]       <- OptionT({
-        val area = aoi.area
-        val queryParams =
-          aoi
-            .filters.as[CombinedSceneQueryParams]
-            .getOrElse(throw new Exception(s"No valid queryParams passed: ${aoi.filters}"))
+      scenes: Iterable[UUID]       <- OptionT(
+        {
+          val area = aoi.area
+          val queryParams =
+            aoi
+              .filters.as[CombinedSceneQueryParams]
+              .getOrElse(throw new Exception(s"No valid queryParams passed: ${aoi.filters}"))
 
-        val scenes: Future[Iterable[UUID]] = (for {
-          sceneIds <- OptionT.liftF(
-            database.db.run {
-              Scenes
-                .filterScenes(queryParams)
-                .filterUserVisibility(user)
-                .filter { s => s.dataFootprint.intersects(area) && s.createdAt > project.aoisLastChecked }
-                .map { _.id }
-                .result
+          val scenes: Future[Iterable[UUID]] = (
+            for {
+              sceneIds <- OptionT.liftF(
+                database.db.run {
+                  Scenes
+                    .filterScenes(queryParams)
+                    .filterUserVisibility(user)
+                    .filter { s => s.dataFootprint.intersects(area) && s.createdAt > project.aoisLastChecked }
+                    .map { _.id }
+                    .result
+                }
+              )
+              projects <- OptionT.liftF(Projects.addScenesToProject(sceneIds, projectId, user))
+            } yield projects.map(_.id)).value.map(_.getOrElse(Seq()))
+
+
+          scenes.map {
+            scenesSeq => {
+              logger.info(s"Project $projectId is updated.")
+              Projects.updateProject(
+                project.copy(aoisLastChecked = Timestamp.from(ZonedDateTime.now.toInstant)),
+                projectId,
+                user
+              )
+              scenesSeq.some
             }
-          )
-          projects <- OptionT.liftF(Projects.addScenesToProject(sceneIds, projectId, user))
-        } yield projects.map(_.id)).value.map(_.getOrElse(Seq()))
+          }
+        }
+      )
+    } yield (scenes, project)
 
-        scenes.map { scenesSeq => {
-          logger.info(s"Project $projectId is updated.")
-          Projects.updateProject(project.copy(aoisLastChecked = Timestamp.from(ZonedDateTime.now.toInstant)), projectId, user)
-          scenesSeq.some
-        } }
-      })
-    } yield scenes
-
-    val future: Future[Iterable[UUID]] = result.value.map { _.getOrElse(Seq()) }
+    val future: Future[Iterable[UUID]] = result.value.map {
+      (option: Option[(Iterable[UUID], Project)]) =>
+      option match {
+        case Some((scenes: Iterable[UUID], project: Project)) =>
+          val user = mgmtApi.users().get(project.owner, new UserFilter()).execute()
+          logger.info(s"Notifications stub - ${project.owner} -> ${user.getEmail}")
+          scenes
+        case _ =>
+          Seq()
+      }
+    }
 
     future onComplete {
       case Success(seq) => {
