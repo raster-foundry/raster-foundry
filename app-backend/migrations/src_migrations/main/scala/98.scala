f121b054e7ed4003e78791fbd894933e79c680ce
Aaron Su <xunzesu@outlook.com>
2018-02-06 15:29:22 -0500

    -Add a new licenses table. -Add a new license name column in the datasources table to reference the licenses table.

---
+++ app-backend/migrations/src_migrations/main/scala/98.scala
@@ -0,0 +1,18 @@
+import slick.jdbc.PostgresProfile.api._
+import com.liyaos.forklift.slick.SqlMigration
+
+object M98 {
+  RFMigrations.migrations = RFMigrations.migrations :+ SqlMigration(98)(List(
+    sqlu"""
+    CREATE TABLE licenses (
+      short_name VARCHAR(255) PRIMARY KEY NOT NULL,
+      name VARCHAR(255) NOT NULL,
+      url TEXT NOT NULL,
+      osi_approved BOOLEAN NOT NULL
+    );
+
+    ALTER TABLE datasources
+    ADD COLUMN license_name VARCHAR(255) REFERENCES licenses (short_name) NULL;
+    """
+  ))
+}
