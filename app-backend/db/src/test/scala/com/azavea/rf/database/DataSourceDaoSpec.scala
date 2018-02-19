51a471219c849d77ec989e4cb8df0d8936ec3a0d
Chris Brown <cbrown@azavea.com>
2018-02-19 12:37:15 -0500

    Fix DatasourceDAO tests

--- app-backend/db/src/test/scala/com/azavea/rf/database/DataSourceDaoSpec.scala
+++ app-backend/db/src/test/scala/com/azavea/rf/database/DataSourceDaoSpec.scala
@@ -1,17 +1,21 @@
 package com.azavea.rf.database
 
+import java.sql.Timestamp
+
 import com.azavea.rf.datamodel._
 import com.azavea.rf.database.Implicits._
-
-import doobie._, doobie.implicits._
-import cats._, cats.data._, cats.effect.IO
+import doobie._
+import doobie.implicits._
+import cats._
+import cats.data._
+import cats.effect.IO
 import cats.syntax.either._
-import doobie.postgres._, doobie.postgres.implicits._
+import doobie.postgres._
+import doobie.postgres.implicits._
 import doobie.scalatest.imports._
 import org.scalatest._
 import io.circe._
 import io.circe.syntax._
-
 import java.util.UUID
 
 
@@ -20,11 +24,17 @@ class DatasourceDaoSpec extends FunSuite with Matchers with IOChecker with DBTes
   test("insertion") {
     val testName = "some test Name"
     val dummyJson = List(1, 2).asJson
+    val now = new Timestamp((new java.util.Date()).getTime())
 
     val transaction = for {
       usr <- defaultUserQ
       org <- rootOrgQ
-      dsIn <- DatasourceDao.create(usr, org.id, testName, Visibility.Public, Some(usr.id), dummyJson, dummyJson, dummyJson)
+      dsIn <- {
+        val ds = Datasource(
+          UUID.randomUUID(), now, usr.id, now, usr.id, usr.id,
+          org.id, testName, Visibility.Public, dummyJson, dummyJson, dummyJson)
+        DatasourceDao.create(ds, usr)
+      }
       dsOut <- DatasourceDao.query.filter(fr"id = ${dsIn.id}").selectQ.unique
     } yield dsOut
 
