d5c5a286c369fa3463aaf0430f75fdc485a1b639
Chris Brown <cbrown@azavea.com>
2018-02-20 15:43:25 -0500

    Add project scene insertion

--- app-backend/db/src/main/scala/com/azavea/rf/database/ProjectDao.scala
+++ app-backend/db/src/main/scala/com/azavea/rf/database/ProjectDao.scala
@@ -6,18 +6,31 @@ import doobie._
 import doobie.implicits._
 import doobie.postgres._
 import doobie.postgres.implicits._
+import geotrellis.slick.Projected
+import geotrellis.vector.Polygon
+import java.util.UUID
+import java.sql.{Date, Timestamp}
+
+import com.lonelyplanet.akka.http.extensions.PageRequest
+import com.azavea.rf.database.filter.Filterables._
+import scala.concurrent.Future
+
+import com.azavea.rf.datamodel._
+import com.azavea.rf.datamodel.color._
+import com.lonelyplanet.akka.http.extensions._
+import com.typesafe.scalalogging.LazyLogging
+import geotrellis.slick.Projected
+import geotrellis.vector.{Extent, Geometry}
+import io.circe._
+import io.circe.optics.JsonPath._
+import io.circe.generic.JsonCodec
+import io.circe.syntax._
+
 import cats._
 import cats.data._
 import cats.effect.IO
 import cats.implicits._
-import geotrellis.slick.Projected
-import geotrellis.vector.Polygon
-import java.util.UUID
-import java.sql.Timestamp
 
-import com.lonelyplanet.akka.http.extensions.PageRequest
-
-import scala.concurrent.Future
 
 
 object ProjectDao extends Dao[Project] {
@@ -34,27 +47,224 @@ object ProjectDao extends Dao[Project] {
     FROM
   """ ++ tableF
 
-  def insertProject(newProject: Project.Create, user: User): Future[Project] = ???
+  def insertProject(newProject: Project.Create, user: User): ConnectionIO[Project] = {
+    val id = UUID.randomUUID()
+    val now = new Timestamp((new java.util.Date()).getTime())
+    val ownerId = util.Ownership.checkOwner(user, newProject.owner)
+    val slug = Project.slugify(newProject.name)
+    (fr"INSERT INTO" ++ tableF ++ fr"""
+        (id, created_at, modified_at, organization_id, created_by,
+        modified_by, owner, name, slug_label, description,
+        visibility, tile_visibility, is_aoi_project,
+        aoi_cadence_millis, aois_last_checked, tags, extent,
+        manual_order, is_single_band, single_band_options)
+      VALUES
+        ($id, $now, $now, ${newProject.organizationId}, ${user.id},
+        ${user.id}, $ownerId, ${newProject.name}, $slug, ${newProject.description},
+        ${newProject.visibility}, ${newProject.tileVisibility}, ${newProject.isAOIProject},
+        ${newProject.aoiCadenceMillis}, $now, ${newProject.tags}, null,
+        TRUE, ${newProject.isSingleBand}, ${newProject.singleBandOptions})
+    """).update.withUniqueGeneratedKeys[Project](
+      "id", "created_at", "modified_at", "organization_id", "created_by",
+      "modified_by", "owner", "name", "slug_label", "description",
+      "visibility", "tile_visibility", "is_aoi_project",
+      "aoi_cadence_millis", "aois_last_checked", "tags", "extent",
+      "manual_order", "is_single_band", "single_band_options"
+    )
+  }
 
-  def updateProject(project: Project, id: UUID, user: User): Future[Int] = ???
+  def ownerEditFilter(user: User): Option[Fragment] = {
+    user.isInRootOrganization match {
+      case true => None
+      case _ => Some(fr"(organization_id = ${user.organizationId} OR owner = ${user.id})")
+    }
+  }
 
-  def deleteProject(id: UUID, user: User): Future[Int] = ???
+  def filterUserVisibility(user: User) = {
+    user.isInRootOrganization match {
+      case true => None
+      case _ => Some(fr"(organization_id = ${user.organizationId} OR owner = ${user.id} OR visibility = 'PUBLIC')")
+    }
+  }
 
-  def listAOIs(projectId: UUID, page: PageRequest, user: User): Future[PaginatedResponse[Project]] = ???
+  def updateProjectQ(project: Project, id: UUID, user: User): Update0 = {
+    val updateTime = new Timestamp((new java.util.Date()).getTime)
+    val idFilter = fr"id = ${id}"
 
-  def listProjectScenes(projectId: UUID, page: PageRequest, sceneParams: CombinedSceneQueryParams, user: User): Future[PaginatedResponse[Scene]] = ???
+    val query = (fr"UPDATE" ++ tableF ++ fr"""SET
+       modified_at = ${updateTime},
+       modified_by = ${user.id},
+       owner = ${project.owner},
+       name = ${project.name},
+       description = ${project.description},
+       visibility = ${project.visibility},
+       tile_visibility = ${project.tileVisibility},
+       is_aoi_project = ${project.isAOIProject},
+       aoi_cadence_millis = ${project.aoiCadenceMillis},
+       aois_last_checked = ${project.aoisLastChecked},
+       tags = ${project.tags},
+       extent = ${project.extent},
+       manual_order = ${project.manualOrder},
+       is_single_band = ${project.isSingleBand},
+       single_band_options = ${project.singleBandOptions}
+    """ ++ Fragments.whereAndOpt(ownerEditFilter(user), Some(idFilter))).update
+    query
+  }
 
-  def listProjectSceneOrder(projectId: UUID, page: PageRequest, user: User): Future[PaginatedResponse[UUID]] = ???
+  def updateProject(project: Project, id: UUID, user: User): ConnectionIO[Int] = {
+    updateProjectQ(project, id, user).run
+  }
 
-  // Check swagger spec for proper return type
-  def replaceScenesInProject(sceneIds: Seq[UUID], projectId: UUID): Future[PaginatedResponse[Scene]] = ???
+  def deleteProject(id: UUID, user: User): ConnectionIO[Int] = {
 
-  // Check swagger spec for proper return type
-  def deleteScenesFromProject(sceneIds: Seq[UUID], projectId: UUID): Future[Int] = ???
+    val aoiDeleteQuery = sql"DELETE FROM aois USING aois_to_projects WHERE aois.id = aois_to_projects.aoi_id AND aois_to_projects.project_id = ${id}"
+    val aoiToProjectDelete = sql"DELETE FROM aois_to_projects WHERE project_id = ${id}"
+    for {
+      _ <- aoiDeleteQuery.update.run
+      _ <- aoiToProjectDelete.update.run
+      projectDeleteCount <- query.filter(fr"id = ${id}").delete
+    } yield projectDeleteCount
+  }
+//
+//  def listAOIs(projectId: UUID, page: PageRequest, user: User): Future[PaginatedResponse[Project]] = ???
+//
+//  def listProjectScenes(projectId: UUID, page: PageRequest, sceneParams: CombinedSceneQueryParams, user: User): Future[PaginatedResponse[Scene]] = ???
 
-  def addScenesToProjectFromQuery(combinedSceneQueryParams: CombinedSceneQueryParams, projectId: UUID, user: User): Future[PaginatedResponse[Scene]] = ???
+  type stp = (UUID, UUID, Boolean, Option[Int], Option[Json])
 
-  def addScenesToProject(sceneIds: Seq[UUID], projectId: UUID, user: User): Future[Seq[Scene]] = ???
+  def updateSceneIngestStatus(projectId: UUID): ConnectionIO[Int] = {
+    val updateStatusQuery =
+      sql"""
+           UPDATE scenes
+           SET ingest_status = 'TOBEINGESTED'
+           FROM
+             (SELECT scene_id
+              FROM scenes
+              INNER JOIN scenes_to_projects ON scene_id = scenes.id
+              WHERE project_id = '30ee749c-7bf3-4d28-838a-d4aeeb451911') sub
+           WHERE scenes.ingest_status IN ('NOTINGESTED',
+                                          'FAILED')
+             AND sub.scene_id = scenes.id;
+         """
+    updateStatusQuery.update.run
+  }
+
+  def addScenesToProjects(sceneIds: NonEmptyList[UUID], projectId: UUID): ConnectionIO[Int] = {
+    // Filter duplicates
+    //
+
+    val inClause = Fragments.in(fr"scenes.id", sceneIds)
+    val x = sql"""
+         SELECT scenes.id,
+                datasources.id,
+                datasources.created_at,
+                datasources.created_by,
+                datasources.modified_at,
+                datasources.modified_by,
+                datasources.owner,
+                datasources.organization_id,
+                datasources.name,
+                datasources.visibility,
+                datasources.composites,
+                datasources.extras,
+                datasources.bands
+         FROM scenes
+         INNER JOIN datasources ON scenes.datasource = datasources.id
+         WHERE NOT (scenes.id = ANY(ARRAY(SELECT scene_id FROM scenes_to_projects WHERE project_id = ${projectId})::UUID[])) AND """ ++ inClause
+    for {
+      sceneQueryResult <- x.query[(UUID, Datasource)].list
+      sceneToProjectInserts <- {
+        val stps: List[stp] = sceneQueryResult.map { case (sceneId, datasource) =>
+            createScenesToProject(sceneId, projectId, datasource)
+        }
+        val inserts = "INSERT INTO scenes_to_projects (scene_id, project_id, accepted, scene_order, mosaic_definition) VALUES (?, ?, ?, ?, ?)"
+        Update[stp](inserts).updateMany(stps)
+      }
+      _ <- {(sql"""
+               UPDATE projects
+               SET extent = subquery.extent
+               FROM
+                 (SELECT ST_SETSRID(ST_EXTENT(scenes.data_footprint), 3857) AS extent
+                  FROM projects
+                  INNER JOIN scenes_to_projects ON project_id = projects.id
+                  INNER JOIN scenes ON scenes.id = scene_id
+                  WHERE projects.id = ${projectId}
+                  GROUP BY projects.id) AS subquery
+               WHERE projects.id = ${projectId};
+              """).update.run
+      }
+      _ <- updateSceneIngestStatus(projectId)
+    } yield sceneToProjectInserts
+  }
+
+  def createScenesToProject(sceneId: UUID, projectId: UUID, datasource: Datasource): stp = {
+    val composites = datasource.composites
+    val redBandPath = root.natural.selectDynamic("value").redBand.int
+    val greenBandPath = root.natural.selectDynamic("value").greenBand.int
+    val blueBandPath = root.natural.selectDynamic("value").blueBand.int
+
+    val redBand = redBandPath.getOption(composites).getOrElse(0)
+    val greenBand = greenBandPath.getOption(composites).getOrElse(1)
+    val blueBand = blueBandPath.getOption(composites).getOrElse(2)
+
+    (
+      sceneId, projectId, true, None, Some(
+        ColorCorrect.Params(
+          redBand, greenBand, blueBand,             // Bands
+          // Color corrections; everything starts out disabled (false) and null for now
+          BandGamma(false, None, None, None),       // Gamma
+          PerBandClipping(false, None, None, None,  // Clipping Max: R,G,B
+            None, None, None), // Clipping Min: R,G,B
+          MultiBandClipping(false, None, None),     // Min, Max
+          SigmoidalContrast(false, None, None),     // Alpha, Beta
+          Saturation(false, None),                  // Saturation
+          Equalization(false),                      // Equalize
+          AutoWhiteBalance(false)                    // Auto White Balance
+        ).asJson
+      )
+    )
+  }
+
+  def listProjectSceneOrder(projectId: UUID, page: PageRequest, user: User): ConnectionIO[PaginatedResponse[UUID]] = {
+    val projectQuery = query.filter(fr"id = ${projectId}").filter(ownerEditFilter(user)).select
+    val selectClause = fr"SELECT scene_id "
+    val countClause = fr"SELECT count(*) "
+    val sceneQuery = fr"FROM scenes_to_projects INNER JOIN scenes ON scene_id = scenes.id WHERE project_id = ${projectId}"
+    val orderFragment = projectQuery.map{ project =>
+      project.manualOrder match {
+        case true => sceneQuery ++ fr"ORDER BY scene_order DESC, scene_id ASC"
+        case _ => sceneQuery ++ fr"ORDER BY acquisition_date DESC, cloud_cover ASC"
+      }
+    }
+    for {
+      orderedQuery <- orderFragment
+      pageResponse <- (selectClause ++ orderedQuery ++ fr"LIMIT ${page.limit} OFFSET ${page.offset * page.limit}").query[UUID].list
+      countResponse <- (countClause ++ sceneQuery).query[Int].unique
+    } yield {
+      val hasPrevious = page.offset > 0
+      val hasNext = (page.offset * page.limit) + 1 < countResponse
+      PaginatedResponse[UUID](countResponse, hasPrevious, hasNext, page.offset, page.limit, pageResponse)
+    }
+  }
+
+  // Not Sure if This is Used Anywhere
+  def replaceScenesInProject(sceneIds: Seq[UUID], projectId: UUID): ConnectionIO[Iterable[Scene]] = {
+    val deleteQuery = sql"DELETE FROM scenes_to_projects WHERE project_id = ${projectId}"
+    val insertQuery = sql"INSERT INTO "
+    SceneToProject
+    ???
+  }
+
+  def deleteScenesFromProject(sceneIds: List[UUID], projectId: UUID): ConnectionIO[Int] = {
+    val f:Option[Fragment] = sceneIds.toNel.map(Fragments.in(fr"scene_id", _))
+    val deleteQuery = sql"DELETE FROM scenes_to_projects" ++ Fragments.whereAndOpt(f) ++ fr"project_id = ${projectId}"
+    deleteQuery.update.run
+  }
+
+  def addScenesToProjectFromQuery(combinedSceneQueryParams: CombinedSceneQueryParams, projectId: UUID, user: User): ConnectionIO[List[Scene]] = {
+//    SceneDao.query.filter(combinedSceneQueryParams)
+    ???
+  }
 
   def create(
     user: User,
@@ -96,4 +306,3 @@ object ProjectDao extends Dao[Project] {
     )
   }
 }
-
--- app-backend/db/src/test/scala/com/azavea/rf/database/ProjectDaoSpec.scala
+++ app-backend/db/src/test/scala/com/azavea/rf/database/ProjectDaoSpec.scala
@@ -1,35 +1,81 @@
 package com.azavea.rf.database
 
+import java.sql.Timestamp
+
 import com.azavea.rf.datamodel._
 import com.azavea.rf.database.Implicits._
-
-import doobie._, doobie.implicits._
-import cats._, cats.data._, cats.effect.IO
+import doobie._
+import doobie.implicits._
+import cats._
+import cats.data._
+import cats.effect.IO
 import cats.syntax.either._
-import doobie.postgres._, doobie.postgres.implicits._
+import doobie.postgres._
+import doobie.postgres.implicits._
 import doobie.scalatest.imports._
 import org.scalatest._
-
+import io.circe._
+import io.circe.syntax._
+import java.util.UUID
+import scala.concurrent.ExecutionContext.Implicits.global
 
 class ProjectDaoSpec extends FunSuite with Matchers with IOChecker with DBTestConfig {
 
-  test("insertion") {
+  def createProject: ConnectionIO[Project] = {
     val testName = "test project name"
 
-    val transaction = for {
+    for {
       usr <- defaultUserQ
       org <- rootOrgQ
-      projectIn <- ProjectDao.create(
-        usr, Some(usr.id), org.id, testName, "description", Visibility.Public,
-        Visibility.Public, true, 123L, List("tags"), false, None
-      )
+      projectIn <- {
+        val projectCreate = Project.Create(org.id, testName, "description",
+          Visibility.Public, Visibility.Public, true, 123L, Some(usr.id), List("tags"), false, None
+        )
+        ProjectDao.insertProject(projectCreate, usr)
+      }
+    } yield {
+      println(s"Created Project with ID: ${projectIn.id}")
+      projectIn
+    }
+  }
+
+  test("insertion") {
+    val transaction = for {
+      projectIn <- createProject
       projectOut <- ProjectDao.query.filter(fr"id = ${projectIn.id}").selectQ.unique
     } yield projectOut
 
     val result = transaction.transact(xa).unsafeRunSync
-    result.name shouldBe testName
+    result.name shouldBe "test project name"
   }
 
-  test("types") { check(ProjectDao.selectF.query[Project]) }
+  test("insertion types") { check(ProjectDao.selectF.query[Project]) }
+
+  test("update") {
+    val transaction = for {
+      user <- defaultUserQ
+      projectIn <- createProject
+      projectUpdate <- {
+        val updateQuery = ProjectDao.updateProjectQ(projectIn, projectIn.id, user)
+        check(updateQuery)
+        updateQuery.run
+      }
+    } yield projectUpdate
+
+    val result = transaction.transact(xa).unsafeRunSync
+    result shouldBe 1
+  }
+
+  test("delete") {
+    val transaction = for {
+      user <- defaultUserQ
+      projectIn <- createProject
+      projectDelete <- ProjectDao.deleteProject(projectIn.id, user)
+    } yield projectDelete
+
+    val result = transaction.transact(xa).unsafeRunSync
+    result shouldBe 1
+  }
+
 }
 
