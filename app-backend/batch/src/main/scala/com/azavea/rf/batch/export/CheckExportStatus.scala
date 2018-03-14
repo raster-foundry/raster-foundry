a6f00b6bd606951a632ebaae7d3bbf1a6e3c0cec
James Santucci <james.santucci@gmail.com>
2018-03-14 12:47:54 -0400

    Switch getExportStatus to doobie

--- app-backend/batch/src/main/scala/com/azavea/rf/batch/export/CheckExportStatus.scala
+++ app-backend/batch/src/main/scala/com/azavea/rf/batch/export/CheckExportStatus.scala
@@ -11,9 +11,15 @@ import com.azavea.rf.batch.util._
 import com.azavea.rf.datamodel._
 import io.circe.parser.decode
 
+import doobie._
+import doobie.implicits._
+import doobie.postgres._
+import doobie.postgres.implicits._
 import doobie.util.transactor.Transactor
 
-import scala.concurrent.Future
+import com.azavea.rf.database.Implicits._
+import com.azavea.rf.database.util.RFTransactor
+
 import scala.concurrent.duration._
 import scala.io.Source
 import scala.util._
@@ -28,92 +34,68 @@ case class CheckExportStatus(exportId: UUID, statusBucket: String = "rasterfound
     export.copy(exportStatus = status)
 
   def run: Unit = {
-  //   logger.info(s"Checking export ${exportId} process...")
-  //   val json =
-  //     try {
-  //       retry(time, 30.seconds) {
-  //         Source
-  //           .fromInputStream(s3Client.getObject(statusBucket, exportId.toString).getObjectContent)
-  //           .getLines
-  //           .mkString(" ")
-  //       }
-  //     } catch {
-  //       case e: Throwable =>
-  //         logger.error(e.stackTraceString)
-  //         sendError(e.stackTraceString)
-  //         stop
-  //         sys.exit(1)
-  //     }
+    logger.info(s"Checking export ${exportId} process...")
+    val json =
+      try {
+        retry(time, 30.seconds) {
+          Source
+            .fromInputStream(s3Client.getObject(statusBucket, exportId.toString).getObjectContent)
+            .getLines
+            .mkString(" ")
+        }
+      } catch {
+        case e: Throwable =>
+          logger.error(e.stackTraceString)
+          sendError(e.stackTraceString)
+          stop
+          sys.exit(1)
+      }
 
-  //   val s3ExportStatus =
-  //     decode[S3ExportStatus](json) match {
-  //       case Right(r) => r
-  //       case _ => {
-  //         logger.error("Incorrect S3ExportStatus JSON")
-  //         sys.exit(1)
-  //       }
-  //     }
+    val s3ExportStatus =
+      decode[S3ExportStatus](json) match {
+        case Right(r) => r
+        case _ => {
+          logger.error("Incorrect S3ExportStatus JSON")
+          sys.exit(1)
+        }
+      }
 
-  //   // This changed to OptionT because of strange compilation behavior after bumping the cats version
-  //   // I'd like to investigate further, but it looks like it might be a bug
-  //   val result = for {
-  //     user <- OptionT[Future, User](Users.getUserById(systemUser))
-  //     export <- OptionT[Future, Export](database.db.run(Exports.getExport(exportId, user)))
-  //     exportStatus <- OptionT.liftF({
-  //       database.db.run(
-  //         Exports.updateExport(
-  //           updateExportStatus(export, s3ExportStatus.exportStatus),
-  //           exportId,
-  //           user
-  //         )
-  //       )
-  //     })
-  //   } yield exportStatus
+    def updateIo(exportId: UUID, exportStatus: ExportStatus): ConnectionIO[ExportStatus] =
+      sql"update exports set export_status = ${exportStatus} where id = ${exportId}"
+        .update.withUniqueGeneratedKeys("export_status")
 
-  //   result.value.onComplete {
-  //     case Success(None) => {
-  //       logger.error("DB: Failed to fetch user or export")
-  //       sendError("DB: Failed to fetch user or export")
-  //       stop
-  //       sys.exit(1)
-  //     }
-  //     case Success(_) if s3ExportStatus.exportStatus == ExportStatus.Failed => {
-  //       val msg = s"Export finished with ${ExportStatus.Failed}"
-  //       logger.info(msg)
-  //       sendError(msg)
-  //       stop
-  //       sys.exit(1)
-  //     }
-  //     case Success(_) => {
-  //       logger.info("Export updated successfully")
-  //       stop
-  //     }
-  //     case Failure(e) => {
-  //       logger.info("Export job status set to Failed")
-  //       logger.error(e.stackTraceString)
-  //       sendError(e)
-  //       stop
-  //       sys.exit(1)
-  //     }
-  //   }
-  // }
+    val withLoggingUpdateExportStatus: ConnectionIO[Unit] =
+      updateIo(exportId, s3ExportStatus.exportStatus) map {
+        case ExportStatus.Failed => {
+          logger.info(s"Export finished with ${s3ExportStatus.exportStatus}")
+          sendError(s"Export status update failed for ${exportId}")
+        }
+        case ExportStatus.Exported => {
+          logger.info(s"Export updated successfully")
+        }
+        case _ =>
+          logger.info(s"Export ${exportId} has not yet completed: ${s3ExportStatus.exportStatus}")
+      }
+
+    withLoggingUpdateExportStatus.transact(xa).unsafeRunSync
   }
 }
 
 object CheckExportStatus {
   val name = "check_export_status"
+  implicit val xa = RFTransactor.xa
 
-  def main(args: Array[String]): Unit = {}
+  def main(args: Array[String]): Unit = {
 
-  //   val job = args.toList match {
-  //     case List(exportId, statusBucket, duration, region) => CheckExportStatus(UUID.fromString(exportId), statusBucket, Duration(duration), Some(region))
-  //     case List(exportId, statusBucket, duration) => CheckExportStatus(UUID.fromString(exportId), statusBucket, Duration(duration))
-  //     case List(exportId, statusBucket) => CheckExportStatus(UUID.fromString(exportId), statusBucket)
-  //     case List(exportId) => CheckExportStatus(UUID.fromString(exportId))
-  //     case _ =>
-  //       throw new IllegalArgumentException("Argument could not be parsed to UUID")
-  //   }
+    val job = args.toList match {
+      case List(exportId, statusBucket, duration, region) => CheckExportStatus(UUID.fromString(exportId), statusBucket, Duration(duration), Some(region))
+      case List(exportId, statusBucket, duration) => CheckExportStatus(UUID.fromString(exportId), statusBucket, Duration(duration))
+      case List(exportId, statusBucket) => CheckExportStatus(UUID.fromString(exportId), statusBucket)
+      case List(exportId) => CheckExportStatus(UUID.fromString(exportId))
+      case _ =>
+        throw new IllegalArgumentException("Argument could not be parsed to UUID")
+    }
 
-  //   job.run
-  // }
+    job.run
+  }
 }
