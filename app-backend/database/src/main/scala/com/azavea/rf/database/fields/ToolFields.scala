215d54dc836512b21d2c581bdc878bcbafcf1def
James Santucci <james.santucci@gmail.com>
2018-03-19 14:03:58 -0400

    Remove (slick, old) database subproject

--- app-backend/database/src/main/scala/com/azavea/rf/database/fields/ToolFields.scala
+++
@@ -1,7 +0,0 @@
-package com.azavea.rf.database.fields
-
-import com.azavea.rf.database.ExtendedPostgresDriver.api._
-
-trait ToolFields { self: Table[_] =>
-  def title: Rep[String]
-}
--- app-backend/database/src/main/scala/com/azavea/rf/database/fields/UserFields.scala
+++
@@ -1,9 +0,0 @@
-package com.azavea.rf.database.fields
-
-import com.azavea.rf.database.ExtendedPostgresDriver.api._
-import com.azavea.rf.datamodel.UserRole
-
-trait UserFields { self: Table[_] =>
-  def id: Rep[String]
-  def role: Rep[UserRole]
-}
