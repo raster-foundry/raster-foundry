8f9c44462916ca44fe415855f5380a9233849ed7
Aaron Su <xunzesu@outlook.com>
2018-03-27 15:30:21 -0400

    Add a migration to update sentinel-2 in datasource and in scenes.

---
+++ app-backend/migrations/src_migrations/main/scala/102.scala
@@ -0,0 +1,19 @@
+import slick.jdbc.PostgresProfile.api._
+import com.liyaos.forklift.slick.SqlMigration
+
+object M102 {
+  RFMigrations.migrations = RFMigrations.migrations :+ SqlMigration(102)(List(
+    sqlu"""
+    UPDATE datasources
+    SET name = 'Sentinel-2'
+    WHERE id = '4a50cb75-815d-4fe5-8bc1-144729ce5b42';
+
+    DELETE FROM datasources
+    WHERE id = 'c33db82d-afdb-43cb-a6ac-ba899e48638d';
+
+    UPDATE scenes
+    SET datasource = '4a50cb75-815d-4fe5-8bc1-144729ce5b42'
+    WHERE datasource = 'c33db82d-afdb-43cb-a6ac-ba899e48638d';
+    """
+  ))
+}
