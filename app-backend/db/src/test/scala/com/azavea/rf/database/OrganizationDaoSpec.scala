c3f28271b33273a34908defa9d1cebd57b255e16
Alex Kaminsky <akaminsky@azavea.com>
2018-04-10 16:15:42 -0400

    Connect organizations and platforms

--- app-backend/db/src/test/scala/com/azavea/rf/database/OrganizationDaoSpec.scala
+++ app-backend/db/src/test/scala/com/azavea/rf/database/OrganizationDaoSpec.scala
@@ -1,11 +1,11 @@
 package com.azavea.rf.database
 
-import com.azavea.rf.datamodel.Organization
+import com.azavea.rf.datamodel.{Organization, Platform}
 import com.azavea.rf.database.Implicits._
 
-import doobie._, doobie.implicits._
 import cats._, cats.data._, cats.effect.IO
 import cats.syntax.either._
+import doobie._, doobie.implicits._
 import doobie.postgres._, doobie.postgres.implicits._
 import doobie.scalatest.imports._
 import org.scalatest._
@@ -14,15 +14,18 @@ import org.scalatest._
 class OrganizationDaoSpec extends FunSuite with Matchers with IOChecker with DBTestConfig {
 
   test("insertion") {
-    val testName = "test org"
+    val testPlatformName = "test platform"
+    val testOrgName = "test org"
 
     val transaction = for {
-      orgIn <- OrganizationDao.create(testName)
+      userIn <- defaultUserQ
+      platformIn <- PlatformDao.create(Platform.Create(testPlatformName, userIn).toPlatform)
+      orgIn <- OrganizationDao.create(Organization.Create(testOrgName, platformIn.id).toOrganization)
       orgOut <- OrganizationDao.query.filter(fr"id = ${orgIn.id}").selectQ.unique
     } yield orgOut
 
     val result = transaction.transact(xa).unsafeRunSync
-    result.name shouldBe testName
+    result.name shouldBe testOrgName
   }
 
   test("types") { check(OrganizationDao.selectF.query[Organization]) }
