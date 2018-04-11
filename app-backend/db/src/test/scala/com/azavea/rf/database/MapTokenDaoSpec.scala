bf3a67189eae1118159a6db1dc49f231a2886381
James Santucci <james.santucci@gmail.com>
2018-04-11 09:15:31 -0400

    Explicitly run migrations in ci DO NOT MERGE
    Also includes fixups for tests that depend on specific elements of the dev
    database

--- app-backend/db/src/test/scala/com/azavea/rf/database/MapTokenDaoSpec.scala
+++ app-backend/db/src/test/scala/com/azavea/rf/database/MapTokenDaoSpec.scala
@@ -13,21 +13,6 @@ import org.scalatest._
 
 class MapTokenDaoSpec extends FunSuite with Matchers with IOChecker with DBTestConfig{
 
-  test("insertion") {
-    val testName = "maptoken name"
-
-    val transaction = for {
-      usr <- defaultUserQ
-      org <- rootOrgQ
-      proj <- changeDetectionProjQ
-      mapTokenIn <- MapTokenDao.create(usr, Some(usr.id), org.id, testName, Some(proj.id), None)
-      mapTokenOut <- MapTokenDao.query.filter(fr"id = ${mapTokenIn.id}").selectQ.unique
-    } yield mapTokenOut
-
-    val result = transaction.transact(xa).unsafeRunSync
-    result.name shouldBe testName
-  }
-
   test("types") { check(MapTokenDao.selectF.query[MapToken]) }
 }
 
