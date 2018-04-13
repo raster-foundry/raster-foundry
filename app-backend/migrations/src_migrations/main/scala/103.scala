f22ed922dfb3d2f76018325381ebe94952b34c4b
James Santucci <james.santucci@gmail.com>
2018-04-13 10:12:37 -0400

    Fixup delete to rely on a cascaded delete from fk
    with user safety

--- app-backend/migrations/src_migrations/main/scala/103.scala
+++ app-backend/migrations/src_migrations/main/scala/103.scala
@@ -11,6 +11,9 @@ ALTER TABLE users ALTER COLUMN dropbox_credential SET NOT NULL;
 ALTER TABLE users ALTER COLUMN planet_credential SET DEFAULT '';
 UPDATE users SET planet_credential = '' WHERE planet_credential IS NULL;
 ALTER TABLE users ALTER COLUMN planet_credential SET NOT NULL;
+
+ALTER TABLE aois_to_projects DROP CONSTRAINT aois_to_projects_aoi_id_fkey;
+ALTER TABLE aois_to_projects ADD CONSTRAINT aois_to_projects_aoi_id_fkey FOREIGN KEY (aoi_id) REFERENCES aois(id) ON DELETE CASCADE;
 """
   ))
 }
