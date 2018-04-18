496b260c675a3faecba74b3e70c8002d12c83cad
James Santucci <james.santucci@gmail.com>
2018-04-18 13:32:49 -0400

    Set selectF and countF in page in datasource routes

--- app-backend/api/src/main/scala/datasource/Routes.scala
+++ app-backend/api/src/main/scala/datasource/Routes.scala
@@ -44,7 +44,7 @@ trait DatasourceRoutes extends Authentication
   def listDatasources: Route = authenticate { user =>
     (withPagination & datasourceQueryParams) { (page: PageRequest, datasourceParams: DatasourceQueryParameters) =>
       complete {
-        DatasourceDao.query.filter(datasourceParams).filter(user).page(page).transact(xa).unsafeToFuture
+        DatasourceDao.query.filter(datasourceParams).filter(user).page(page, DatasourceDao.query.selectF, DatasourceDao.query.countF).transact(xa).unsafeToFuture
       }
     }
   }
