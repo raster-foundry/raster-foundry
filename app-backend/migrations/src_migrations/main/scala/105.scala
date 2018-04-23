a2d16d843e1757fc510442b7284dd98d2ced78b0
Aaron Su <xunzesu@outlook.com>
2018-04-23 14:34:01 -0400

    Add a platform and organization management related migration.

---
+++ app-backend/migrations/src_migrations/main/scala/105.scala
@@ -0,0 +1,75 @@
+import slick.jdbc.PostgresProfile.api._
+import com.liyaos.forklift.slick.SqlMigration
+
+object M105 {
+  RFMigrations.migrations = RFMigrations.migrations :+ SqlMigration(105)(List(
+    sqlu"""
+    CREATE TABLE platforms (
+        id UUID PRIMARY KEY NOT NULL,
+        created_at TIMESTAMP NOT NULL,
+        created_by VARCHAR(255) REFERENCES users(id) NOT NULL,
+        modified_at TIMESTAMP NOT NULL,
+        modified_by VARCHAR(255) REFERENCES users(id) NOT NULL,
+        name TEXT NOT NULL,
+        settings JSONB NOT NULL default '{}'
+    );
+
+    INSERT INTO platforms (id, created_at, created_by, modified_at, modified_by, name)
+    VALUES (
+        '31277626-968b-4e40-840b-559d9c67863c',
+        NOW(),
+        'default',
+        NOW(),
+        'default',
+        'Raster Foundry'
+    );
+
+    ALTER TABLE organizations ADD COLUMN platform_id UUID references platforms(id) NOT NULL default '31277626-968b-4e40-840b-559d9c67863c';
+
+    CREATE TABLE teams (
+        id UUID PRIMARY KEY NOT NULL,
+        created_at TIMESTAMP NOT NULL,
+        created_by VARCHAR(255) REFERENCES users(id) NOT NULL,
+        modified_at TIMESTAMP NOT NULL,
+        modified_by VARCHAR(255) REFERENCES users(id) NOT NULL,
+        organization_id UUID REFERENCES organizations(id) NOT NULL,
+        name text NOT NULL,
+        settings JSONB NOT NULL default '{}'
+    );
+
+    CREATE TYPE group_type AS ENUM ('PLATFORM', 'ORGANIZATION', 'TEAM');
+    CREATE TYPE group_role AS ENUM ('ADMIN', 'MEMBER');
+
+    CREATE TABLE user_group_roles (
+        id UUID PRIMARY KEY NOT NULL,
+        created_at TIMESTAMP NOT NULL,
+        created_by VARCHAR(255) REFERENCES users(id) NOT NULL,
+        modified_at TIMESTAMP NOT NULL,
+        modified_by VARCHAR(255) REFERENCES users(id) NOT NULL,
+        is_active BOOLEAN DEFAULT true NOT NULL,
+        user_id VARCHAR(255) REFERENCES users(id) NOT NULL,
+        group_type group_type NOT NULL,
+        group_id UUID NOT NULL,
+        group_role group_role NOT NULL
+    );
+
+    CREATE TYPE object_type AS ENUM ('PROJECT', 'SCENE', 'DATASOURCE', 'SHAPE', 'WORKSPACE', 'TEMPLATE', 'ANALYSIS');
+    CREATE TYPE subject_type AS ENUM ('ALL', 'PLATFORM', 'ORGANIZATION', 'TEAM', 'USER');
+    CREATE TYPE action_type AS ENUM ('VIEW', 'EDIT', 'DEACTIVATE', 'DELETE');
+
+    CREATE TABLE access_control_rules (
+        id UUID PRIMARY KEY NOT NULL,
+        created_at TIMESTAMP NOT NULL,
+        created_by VARCHAR(255) REFERENCES users(id) NOT NULL,
+        modified_at TIMESTAMP NOT NULL,
+        modified_by VARCHAR(255) REFERENCES users(id) NOT NULL,
+        is_active BOOLEAN DEFAULT true NOT NULL,
+        object_type object_type NOT NULL,
+        object_id UUID NOT NULL,
+        subject_type subject_type NOT NULL,
+        subject_id text,
+        action_type action_type NOT NULL
+    );
+    """
+  ))
+}
