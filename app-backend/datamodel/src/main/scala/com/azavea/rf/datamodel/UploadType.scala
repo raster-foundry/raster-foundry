b6d2dbb2941d0968396039672f8c64d6fa1e7a12
James Santucci <james.santucci@gmail.com>
2018-04-23 17:03:55 -0400

    Extend upload type enum in datamodel and database

--- app-backend/datamodel/src/main/scala/com/azavea/rf/datamodel/UploadType.scala
+++ app-backend/datamodel/src/main/scala/com/azavea/rf/datamodel/UploadType.scala
@@ -12,6 +12,7 @@ object UploadType {
   case object S3 extends UploadType("S3")
   case object Local extends UploadType("LOCAL")
   case object Planet extends UploadType("PLANET")
+  case object Modis extends UploadType("MODIS_USGS")
 
   def fromString(s: String): UploadType = s.toUpperCase match {
     case "DROPBOX" => Dropbox
---
+++ app-backend/migrations/src_migrations/main/scala/105.scala
@@ -0,0 +1,13 @@
+import slick.jdbc.PostgresProfile.api._
+import com.liyaos.forklift.slick.SqlMigration
+
+object M105 {
+  RFMigrations.migrations = RFMigrations.migrations :+ SqlMigration(105)(List(
+    sqlu"""
+ALTER TABLE uploads ALTER COLUMN upload_type TYPE varchar(255);
+DROP TYPE upload_type;
+CREATE TYPE upload_type AS ENUM ('DROPBOX', 'S3', 'LOCAL', 'PLANET', 'MODIS_USGS');
+ALTER TABLE uploads ALTER COLUMN upload_type TYPE upload_type using upload_type::upload_type;
+"""
+  ))
+}
