e0f4bc4ab933cde7cee77a44de46050af234d2d1
James Santucci <james.santucci@gmail.com>
2018-04-24 10:23:18 -0400

    Two spaces for consistency

--- app-backend/datamodel/src/main/scala/com/azavea/rf/datamodel/Platform.scala
+++ app-backend/datamodel/src/main/scala/com/azavea/rf/datamodel/Platform.scala
@@ -9,32 +9,32 @@ import io.circe.syntax._
 
 @JsonCodec
 case class Platform(
-    id: UUID,
-    createdAt: Timestamp,
-    createdBy: String,
-    modifiedAt: Timestamp,
-    modifiedBy: String,
-    name: String,
-    settings: Json
+  id: UUID,
+  createdAt: Timestamp,
+  createdBy: String,
+  modifiedAt: Timestamp,
+  modifiedBy: String,
+  name: String,
+  settings: Json
 )
 
 object Platform {
-    def create = Create.apply _
-    def tupled = (Platform.apply _).tupled
+  def create = Create.apply _
+  def tupled = (Platform.apply _).tupled
 
-    @JsonCodec
-    case class Create(name: String, user: User, settings: Json = "{}".asJson) {
-        def toPlatform: Platform = {
-            val now = new Timestamp((new java.util.Date()).getTime())
-            Platform(
-                UUID.randomUUID(),
-                now, // createdAt
-                user.id, // createdBy
-                now, // modifiedAt
-                user.id, // modifiedBy
-                name,
-                settings
-            )
-        }
+  @JsonCodec
+  case class Create(name: String, user: User, settings: Json = "{}".asJson) {
+    def toPlatform: Platform = {
+      val now = new Timestamp((new java.util.Date()).getTime())
+      Platform(
+        UUID.randomUUID(),
+        now, // createdAt
+        user.id, // createdBy
+        now, // modifiedAt
+        user.id, // modifiedBy
+        name,
+        settings
+      )
     }
+  }
 }
