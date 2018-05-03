172bf3b71bbca1a96f1e59ee0ecf956acbb5e325
Aaron Su <xunzesu@outlook.com>
2018-05-03 14:38:56 -0400

    Add annotate to action_type and update related routes

---
+++ app-backend/migrations/src_migrations/main/scala/109.scala
@@ -0,0 +1,16 @@
+import slick.jdbc.PostgresProfile.api._
+import com.liyaos.forklift.slick.SqlMigration
+
+object M109 {
+  RFMigrations.migrations = RFMigrations.migrations :+ SqlMigration(109)(List(
+    sqlu"""
+      ALTER TABLE access_control_rules ALTER COLUMN action_type TYPE varchar(255);
+
+      DROP TYPE action_type;
+
+      CREATE TYPE action_type AS ENUM ('VIEW', 'EDIT', 'DEACTIVATE', 'DELETE', 'ANNOTATE');
+
+      ALTER TABLE access_control_rules ALTER COLUMN action_type TYPE action_type using action_type::action_type;
+    """
+  ))
+}
