0c7b85692f1585fcac176ea4bf9a912eb7b94171
Eugene Cheipesh <echeipesh@gmail.com>
2018-05-11 17:25:24 -0400

    Add header caching to COG reading from URI

--- app-backend/common/src/main/scala/utils/CogUtils.scala
+++ app-backend/common/src/main/scala/utils/CogUtils.scala
@@ -1,5 +1,8 @@
 package com.azavea.rf.common.utils
 
+import com.azavea.rf.common.cache._
+import com.azavea.rf.common.cache.kryo._
+import com.azavea.rf.common.{Config => CommonConfig}
 import geotrellis.vector._
 import geotrellis.raster._
 import geotrellis.raster.crop._
@@ -12,13 +15,38 @@ import geotrellis.spark.tiling._
 import scala.util.Properties
 import scala.math
 import scala.util.Try
-import scala.concurrent.blocking
+import scala.concurrent._
+import cats.data._
+import cats.implicits._
 
 object CogUtils {
+  lazy val cacheConfig = CommonConfig.memcached
+  lazy val memcachedClient = KryoMemcachedClient.DEFAULT
+  lazy val rfCache = new CacheClient(memcachedClient)
+  
   private val TmsLevels: Array[LayoutDefinition] = {
     val scheme = ZoomedLayoutScheme(WebMercator, 256)
     for (zoom <- 0 to 64) yield scheme.levelForZoom(zoom).layout
   }.toArray
+
+  /** Read GeoTiff from URI while caching the header bytes in memcache */
+  def fromUri(uri: String)(implicit ec: ExecutionContext): OptionT[Future, GeoTiff[MultibandTile]] = {
+    val cacheKey = s"cog-header-${uri}"
+    val cacheSize = 1<<18
+
+    rfCache.cachingOptionT(cacheKey, doCache = cacheConfig.tool.enabled) {
+      OptionT{ 
+        Future { 
+          RangeReaderUtils.fromUri(uri).map(_.readRange(0, cacheSize)) 
+        } 
+      }
+    }.mapFilter { headerBytes =>
+      RangeReaderUtils.fromUri(uri).map { rr =>
+        val crr = CacheRangeReader(rr, headerBytes)
+        GeoTiffReader.readMultiband(crr, decompress = false, streaming = true)
+      }
+    }
+  }
 
   def fetch(uri: String, zoom: Int, x: Int, y: Int): Option[MultibandTile] =
     RangeReaderUtils.fromUri(uri).flatMap { rr =>
