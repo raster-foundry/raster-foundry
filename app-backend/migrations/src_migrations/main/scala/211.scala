e481fea3381b1b33f4112d3713cb80e287b2cc6c
Alex Kaminsky <akaminsky@azavea.com>
2018-05-18 12:49:44 -0400

    Move migration up

--- app-backend/migrations/src_migrations/main/scala/211.scala
+++ app-backend/migrations/src_migrations/main/scala/211.scala
@@ -1,8 +1,8 @@
 import slick.jdbc.PostgresProfile.api._
 import com.liyaos.forklift.slick.SqlMigration
 
-object M210 {
-  RFMigrations.migrations = RFMigrations.migrations :+ SqlMigration(210)(List(
+object M211 {
+  RFMigrations.migrations = RFMigrations.migrations :+ SqlMigration(211)(List(
     sqlu"""
       ALTER TABLE platforms ADD COLUMN default_organization_id UUID REFERENCES organizations(id);
     """
