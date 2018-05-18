6d964221b7775cb090bcbcde567e26d911224a4e
Chris Brown <cbrown@azavea.com>
2018-05-18 07:25:49 -0400

    Add MODIS upload processing
    This commit does the following:
     - Adds MODIS scene creation to upload processing
     - Creates COG from MODIS HDF file
     - Uploads geotiff to user's S3 directory

---
+++ app-tasks/rf/src/rf/uploads/modis/create_geotiff.py
@@ -0,0 +1,73 @@
+import logging
+import os
+import subprocess
+
+logger = logging.getLogger(__name__)
+
+
+def create_geotiffs(modis_path, output_directory):
+    """Create geotiffs from MODIS HDF
+
+    1. Create separate tifs for each band
+    2. Combine tifs into a single tif
+    3. Warp tif to web mercator (for quicker access)
+    4. Generate COG
+
+    Args:
+        modis_path (str): path to modis HDF file
+        output_directory (str): directory to output tiffs to
+    """
+    logger.info('Preparing to create geotiffs')
+
+    # Set up directories/paths for created files
+    pre_warp_directory = os.path.join(output_directory, 'modis-separated')
+    os.mkdir(pre_warp_directory)
+    pre_warp_output_path = os.path.join(pre_warp_directory, 'B.tif')
+    combined_tif_filepath = os.path.join(output_directory, 'combined-tif.tif')
+    warped_tif_path = os.path.join(output_directory, 'warped-combined.tif')
+    cog_filename = '.'.join(os.path.basename(modis_path).split('.')[:-1]) + '.tif'
+    cog_tif_filepath = os.path.join(output_directory, cog_filename)
+
+    # Separate out into tifs for each band
+    translate_command = ['gdal_translate', '-sds', modis_path, pre_warp_output_path]
+
+    # Combine into single tif (assign nodata value)
+    merge_command = ['gdal_merge.py',
+                     '-o', combined_tif_filepath,
+                     '-a_nodata', '-28672',
+                     '-separate']
+
+    warp_command = [
+        'gdalwarp', '-co', 'COMPRESS=LZW',
+        '-co', 'TILED=YES',
+        '-t_srs', 'epsg:3857',
+        combined_tif_filepath,
+        warped_tif_path
+    ]
+
+    # Generate overviews
+    overview_command = ['gdaladdo', warped_tif_path, '2', '4', '8', '16', '32']
+
+    # Create final tif with overviews
+    translate_cog_command = ['gdal_translate', warped_tif_path, '-co', 'TILED=YES',
+                             '-co', 'COMPRESS=LZW', '-co', 'COPY_SRC_OVERVIEWS=YES', cog_tif_filepath]
+
+    logger.info('Creating tifs for Subdatasets: %s', ' '.join(translate_command))
+    subprocess.check_call(translate_command)
+
+    tifs = [os.path.join(pre_warp_directory, f) for f in os.listdir(pre_warp_directory)]
+    # Sort so that band order is correct
+    tifs.sort()
+
+    logger.info('Running Merge Command: %s', ' '.join(merge_command + tifs))
+    subprocess.check_call(merge_command + tifs)
+
+    # Warp combined tif
+    logger.info('Warping tif %s => %s', combined_tif_filepath, warped_tif_path)
+    subprocess.check_call(warp_command)
+    logger.info('Running Overview Command: %s', ' '.join(overview_command))
+    subprocess.check_call(overview_command)
+    logger.info('Running COG translate Command: %s', ' '.join(translate_cog_command))
+    subprocess.check_call(translate_cog_command)
+
+    return [cog_tif_filepath]
