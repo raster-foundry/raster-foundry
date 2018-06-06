5cc8124941e64d89b4f686de3d3d18c388caf473
Lucien Knechtli <lknechtli@gmail.com>
2018-06-06 15:05:40 -0400

    Place all users in the RF platform

---
+++ app-backend/migrations/src_migrations/main/scala/121.scala
@@ -0,0 +1,12 @@
+import slick.jdbc.PostgresProfile.api._
+import com.liyaos.forklift.slick.SqlMigration
+
+object M121 {
+  RFMigrations.migrations = RFMigrations.migrations :+ SqlMigration(121)(List(
+    sqlu"""
+INSERT INTO user_group_roles (
+id, created_at, created_by, modified_at, modified_by, user_id, group_type, group_id, group_role
+) select uuid_generate_v4(), now(), 'default', now(), 'default', id, 'PLATFORM', '31277626-968b-4e40-840b-559d9c67863c', 'MEMBER' from users;
+"""
+  ))
+}
