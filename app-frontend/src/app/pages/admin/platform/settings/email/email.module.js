3f73de271fe31b61d748f0d8e90d40163892bdb0
Aaron Su <xunzesu@outlook.com>
2018-06-11 15:50:24 -0400

    Finish functionalities.

--- app-frontend/src/app/pages/admin/platform/settings/email/email.module.js
+++ app-frontend/src/app/pages/admin/platform/settings/email/email.module.js
@@ -1,8 +1,53 @@
+/* globals _ */
+
 import angular from 'angular';
 
 class PlatformEmailController {
-    constructor() {
+    constructor(
+        $stateParams, $log, $timeout,
+        platformService
+    ) {
         'ngInject';
+        this.$stateParams = $stateParams;
+        this.$log = $log;
+        this.$timeout = $timeout;
+
+        this.platformService = platformService;
+    }
+
+    $onInit() {
+        this.platformService
+            .getPlatform(this.$stateParams.platformId)
+            .then((platform) => {
+                this.platform = platform;
+                this.platformBuffer = _.cloneDeep(this.platform);
+                this.platformBuffer.privateSettings = {emailPassword: ''};
+            });
+    }
+
+    onToggleChange(type) {
+        if (type === 'ingest') {
+            this.platformBuffer.publicSettings.emailIngestNotification =
+                !this.platformBuffer.publicSettings.emailIngestNotification;
+        } else if (type === 'aoi') {
+            this.platformBuffer.publicSettings.emailAoiNotification =
+                !this.platformBuffer.publicSettings.emailAoiNotification;
+        }
+    }
+
+    onSubmit() {
+        this.saved = false;
+        this.platformService.updatePlatform(this.platformBuffer).then(
+            () => {
+                this.saved = true;
+                this.$timeout(() => {
+                    this.saved = false;
+                }, 500);
+            },
+            err => {
+                this.$log.error(err);
+            }
+        );
     }
 }
 
