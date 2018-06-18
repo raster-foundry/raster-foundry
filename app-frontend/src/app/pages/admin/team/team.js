cc2a0ab3449fb9ba2d259e1fc331be2d528cf2e8
Aaron Su <xunzesu@outlook.com>
2018-06-18 14:06:33 -0400

    Add name change feature for a team on team management page.

--- app-frontend/src/app/pages/admin/team/team.js
+++ app-frontend/src/app/pages/admin/team/team.js
@@ -1,13 +1,14 @@
 import angular from 'angular';
 
 class TeamController {
-    constructor($q, $stateParams, teamService, organizationService) {
+    constructor($q, $stateParams, teamService, organizationService, authService) {
         'ngInject';
         this.$q = $q;
         this.$stateParams = $stateParams;
         this.teamService = teamService;
         this.organizationService = organizationService;
         this.fetching = true;
+        this.authService = authService;
     }
 
     $onInit() {
@@ -26,6 +27,42 @@ class TeamController {
                         }, reject);
                 }, reject);
         });
+
+        this.isSuperOrAdmin = this.isUserSuperOrAdmin();
+    }
+
+    isUserSuperOrAdmin() {
+        return this.teamPromise.then(resp => {
+            this.platformId = resp.organization.platformId;
+            this.organizationId = resp.organization.id;
+            this.isSuperOrAdmin = this.authService.isSuperOrAdmin(
+                [this.platformId,
+                this.organizationId,
+                this.$stateParams.teamId]
+            );
+        });
+    }
+
+    toggleTeamNameEdit() {
+        this.isEditTeamName = !this.isEditTeamName;
+        delete this.nameBuffer;
+    }
+
+    finishTeamNameEdit() {
+        if (this.nameBuffer && this.nameBuffer.length
+            && this.nameBuffer !== this.team.name) {
+            let teamUpdated = Object.assign({}, this.team, {name: this.nameBuffer});
+            this.teamService.updateTeam(
+                this.platformId,
+                this.organizationId,
+                this.$stateParams.teamId,
+                teamUpdated)
+            .then(resp => {
+                this.team = resp;
+            });
+        }
+        delete this.nameBuffer;
+        delete this.isEditTeamName;
     }
 }
 
