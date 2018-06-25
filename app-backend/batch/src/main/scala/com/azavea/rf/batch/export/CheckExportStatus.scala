3a946f64a7c03f18c0bbe47cf694ad652c75dd33
Aaron Su <xunzesu@outlook.com>
2018-06-25 15:27:40 -0400

    Finish export notification.

--- app-backend/batch/src/main/scala/com/azavea/rf/batch/export/CheckExportStatus.scala
+++ app-backend/batch/src/main/scala/com/azavea/rf/batch/export/CheckExportStatus.scala
@@ -18,7 +18,7 @@ import doobie.postgres._
 import doobie.postgres.implicits._
 import Fragments._
 import doobie.util.transactor.Transactor
-import com.azavea.rf.database.{ExportDao, UserGroupRoleDao, ProjectDao, UserDao, PlatformDao}
+import com.azavea.rf.database.{ExportDao, UserGroupRoleDao, ProjectDao, UserDao, PlatformDao, ToolRunDao}
 import com.azavea.rf.database.Implicits._
 import com.azavea.rf.database.util.RFTransactor
 
@@ -35,34 +35,48 @@ case class CheckExportStatus(exportId: UUID, statusURI: URI, time: Duration = 60
   def updateExportStatus(export: Export, status: ExportStatus): Export =
     export.copy(exportStatus = status)
 
-  def exportEmailContent(status: String, user: User, platform: Platform, project: Project): (String, String, String) = {
+  def exportEmailContent(status: String, user: User, platform: Platform, nameO: Option[String], id: UUID, exportType: String): (String, String, String) = {
   // TODO: domain needs to be parameterized
-    val (subject, content) = status match {
+    val (subject, content): (String, String) = status match {
       case status: String if status == "EXPORTED" => ("is ready", "is ready! You")
       case status: String if status == "FAILED" => ("has failed", "has failed. But you")
     }
+    val targetName: String = nameO match {
+      case Some(name) => name
+      case _ => id.toString
+    }
+    val (targetLink, listLink): (String, String) = exportType match {
+      case eType: String if eType == "project" => (
+        s"https://app.rasterfoundry.com/projects/edit/${id}/exports",
+        "https://app.rasterfoundry.com/projects/list")
+      case eType: String if eType == "analysis" => (
+        s"https://app.rasterfoundry.com/lab/analysis/${id}",
+        "https://app.rasterfoundry.com/lab/browse/analyses")
+    }
+
     (
       s"${platform.name}: Your export ${subject}",
       s"""
       <html>
         <p>${user.name},</p><br>
-        <p>Your export in project "${project.name}" ${content} can access
-        this project <a href="https://app.rasterfoundry.com/projects/edit/${project.id}/exports">here</a> or any past
-        projects you've created at any time <a href="https://app.rasterfoundry.com/projects/">here</a>.</p>
+        <p>Your export in ${exportType} "${targetName}" ${content} can access
+        this ${exportType} <a href="${targetLink}" target="_blank">here</a> or any past
+        projects you've created at any time <a href="${listLink}">here</a>.</p>
         <p>If you have questions, please feel free to reach out any time at ${platform.publicSettings.emailUser}.</p>
         <p>- The ${platform.name} Team</p>
       </html>
       """,
       s"""
-      ${user.name}: Your export in project "${project.name}" ${content} can access this project here:
-      https://app.rasterfoundry.com/projects/edit/${project.id}/exports or any past projects you've created
-      at any time here: https://app.rasterfoundry.com/projects/ . If you have questions, please feel free to
-      reach out any time at ${platform.publicSettings.emailUser}. - The ${platform.name} Team
+      ${user.name}:
+      Your export in ${exportType} "${targetName}" ${content} can access this ${exportType} here: ${targetLink},
+      or any past projects you've created at any time here: ${listLink}.
+      If you have questions, please feel free to reach out any time at ${platform.publicSettings.emailUser}.
+      - The ${platform.name} Team
       """
     )
   }
 
-  def sendExportNotificationEmail(status: String, user: User, platform: Platform, projectO: Option[Project] = None) = {
+  def sendExportNotification(status: String, user: User, platform: Platform, name: Option[String], id: UUID, exportType: String) = {
     val email = new NotificationEmail
     (user.emailNotifications, platform.publicSettings.emailExportNotification) match {
       case (true, true) =>
@@ -70,13 +84,9 @@ case class CheckExportStatus(exportId: UUID, statusURI: URI, time: Duration = 60
         (pub.emailSmtpHost, pub.emailSmtpPort, pub.emailSmtpEncryption, pub.emailUser, pri.emailPassword, user.email) match {
           case (host: String, port: Int, encryption: String, platUserEmail: String, pw: String, userEmail: String) if
              email.isValidEmailSettings(host, port, encryption, platUserEmail, pw, userEmail) =>
-             projectO match {
-               case Some(project) =>
-                 val (subject, html, plain) = exportEmailContent(status, user, platform, project)
-                 email.setEmail(host, port, encryption, platUserEmail, pw, userEmail, subject, html, plain).send()
-                 logger.info(s"Notified project owner ${user.id} about export ${exportId}.")
-               case None => ???
-             }
+             val (subject, html, plain) = exportEmailContent(status, user, platform, name, id, exportType)
+             email.setEmail(host, port, encryption, platUserEmail, pw, userEmail, subject, html, plain).send()
+             logger.info(s"Notified owner ${user.id} about export ${exportId}.")
           case _ => logger.warn(email.insufficientSettingsWarning(platform.id.toString(), user.id))
         }
       case (false, true) => logger.warn(email.userEmailNotificationDisabledWarning(user.id))
@@ -86,23 +96,27 @@ case class CheckExportStatus(exportId: UUID, statusURI: URI, time: Duration = 60
     }
   }
 
-  def notificationInfo(status: String) = {
+  def notifyExportOwner(status: String) = {
     val projectIdAndPlatformIO = for {
       export <- ExportDao.query.filter(fr"id = ${exportId}").select
       projectIdO = export.projectId
+      analysisIdO = export.toolRunId
       ugr <- UserGroupRoleDao.query.filter(fr"user_id = ${export.owner}")
         .filter(fr"group_type = 'PLATFORM'").filter(fr"is_active = true").select
       platform <- PlatformDao.query.filter(ugr.groupId).select
       user <- UserDao.query.filter(fr"id = ${export.owner}").select
-    } yield (projectIdO, platform, user)
+    } yield (projectIdO, analysisIdO, platform, user)
 
-    val (projectIdO, platform, user) = projectIdAndPlatformIO.transact(xa).unsafeRunSync
+    val (projectIdO, analysisIdO, platform, user) = projectIdAndPlatformIO.transact(xa).unsafeRunSync
 
-    projectIdO match {
-      case Some(projectId) =>
-        val projectO = ProjectDao.query.filter(projectId).selectOption.transact(xa).unsafeRunSync
-        sendExportNotificationEmail(status, user, platform, projectO)
-      case None => sendExportNotificationEmail(status, user, platform)
+    (projectIdO, analysisIdO) match {
+      case (Some(projectId), None) =>
+        val project = ProjectDao.query.filter(projectId).select.transact(xa).unsafeRunSync
+        sendExportNotification(status, user, platform, Some(project.name), project.id, "project")
+      case (None, Some(analysisId)) =>
+        val analysis = ToolRunDao.query.filter(analysisId).select.transact(xa).unsafeRunSync
+        sendExportNotification(status, user, platform, analysis.name, analysis.id, "analysis")
+      case _ => logger.warn(s"No project or analysis found for export ${exportId}")
     }
   }
 
@@ -142,11 +156,11 @@ case class CheckExportStatus(exportId: UUID, statusURI: URI, time: Duration = 60
         case ExportStatus.Failed => {
           logger.info(s"Export finished with ${s3ExportStatus.exportStatus}")
           sendError(s"Export status update failed for ${exportId}")
-          notificationInfo("FAILED")
+          notifyExportOwner("FAILED")
         }
         case ExportStatus.Exported => {
           logger.info(s"Export updated successfully")
-          notificationInfo("EXPORTED")
+          notifyExportOwner("EXPORTED")
         }
         case _ =>
           logger.info(s"Export ${exportId} has not yet completed: ${s3ExportStatus.exportStatus}")
