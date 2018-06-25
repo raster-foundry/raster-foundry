5f596bd28da6892204ed482e8b6b77888c3602ed
Alex Kaminsky <akaminsky@azavea.com>
2018-06-25 15:08:00 -0400

    working commit -- squash

--- app-frontend/src/app/pages/admin/organization/settings/settings.module.js
+++ app-frontend/src/app/pages/admin/organization/settings/settings.module.js
@@ -2,17 +2,28 @@ import angular from 'angular';
 
 class OrganizationSettingsController {
     constructor(
-        $stateParams, authService,
+        $stateParams, authService, organizationService,
         platform, organization, user, userRoles
     ) {
         'ngInject';
 
         this.$stateParams = $stateParams;
         this.authService = authService;
+        this.organizationService = organizationService;
         this.platform = platform;
         this.organization = organization;
         this.user = user;
         this.userRoles = userRoles;
+    }
+
+    updateOrgPrivacy(privacy) {
+        const previousValue = this.organization.visibility;
+        this.organization.visibility = privacy;
+        this.organizationService.updateOrganization(
+            this.platform.id, this.organization.id, this.organization
+        ).catch(() => {
+            this.organization.visibility = previousValue;
+        });
     }
 }
 
