cab164665446eb82616218a882d414d0264a3712
Aaron Su <xunzesu@outlook.com>
2018-07-03 11:59:26 -0400

    Add optional org logo element on team list... and display on user pages not on org pages.

--- app-frontend/src/app/components/admin/sidebarTeamList/sidebarTeamList.js
+++ app-frontend/src/app/components/admin/sidebarTeamList/sidebarTeamList.js
@@ -1,13 +1,55 @@
 import tpl from './sidebarTeamList.html';
 
 const SidebarTeamListComponent = {
+    controller: 'SidebarTeamListController',
     bindings: {
         paginatedResponse: '<',
+        showOrgLogo: '<?',
         sref: '@'
     },
     templateUrl: tpl
 };
 
-export default angular
-    .module('components.sidebarTeamList', [])
-    .component('rfSidebarTeamList', SidebarTeamListComponent);
+const displayLimit = 3;
+
+class SidebarTeamListController {
+    constructor(organizationService) {
+        'ngInject';
+        this.organizationService = organizationService;
+        this.displayLimit = displayLimit;
+    }
+
+    $onInit() {
+        if (this.showOrgLogo) {
+            this.diplayTeams = this.paginatedResponse.results.length <= this.displayLimit ?
+                this.paginatedResponse.results : this.paginatedResponse.results.slice(2);
+            this.getOrganizations();
+        }
+    }
+
+    getOrganizations() {
+        this.orgURIs = this.diplayTeams.map(team => team.organizationId)
+            .filter((val, idx, self) => self.indexOf(val) === idx)
+            .reduce((obj, orgId) => {
+                obj[orgId] = '';
+                return obj;
+            }, {});
+
+        Object.keys(this.orgURIs).forEach(orgId => {
+            this.organizationService.getOrganization(orgId).then(resp => {
+                this.orgURIs[orgId] = resp.logoUri;
+            });
+        });
+    }
+
+    cacheBustUri(uri) {
+        return `${uri}?${new Date().getTime()}`;
+    }
+  }
+
+const SidebarTeamListModule = angular.module('components.sidebarTeamList', []);
+
+SidebarTeamListModule.component('rfSidebarTeamList', SidebarTeamListComponent);
+SidebarTeamListModule.controller('SidebarTeamListController', SidebarTeamListController);
+
+export default SidebarTeamListModule;
