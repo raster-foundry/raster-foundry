ba067c21c99bcbe9b345db126ff7a9e7d5b23f90
Aaron Su <xunzesu@outlook.com>
2018-07-12 13:29:48 -0400

    Create feature flag migration.

---
+++ app-backend/migrations/src_migrations/main/scala/139.scala
@@ -0,0 +1,18 @@
+import slick.jdbc.PostgresProfile.api._
+import com.liyaos.forklift.slick.SqlMigration
+
+object M139 {
+  RFMigrations.migrations = RFMigrations.migrations :+ SqlMigration(139)(List(
+    sqlu"""
+      INSERT INTO feature_flags (
+        id, key, active, name, description
+      ) VALUES (
+        'dd486e2d-de6c-4ea6-afbe-cd0a2a2fa698',
+        'project-preview-mini-map',
+        false,
+        'Project preview mini leaflet map',
+        'Make Project Previews mini Leaflet Maps'
+      );
+    """
+  ))
+}
