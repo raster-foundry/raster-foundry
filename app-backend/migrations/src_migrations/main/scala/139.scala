bea5584b45a7c8752545c21f59acfa30e8a09da3
James Santucci <james.santucci@gmail.com>
2018-07-13 11:26:45 -0400

    Add migration to fix existing scenes

---
+++ app-backend/migrations/src_migrations/main/scala/139.scala
@@ -0,0 +1,15 @@
+import slick.jdbc.PostgresProfile.api._
+import com.liyaos.forklift.slick.SqlMigration
+
+object M139 {
+  RFMigrations.migrations = RFMigrations.migrations :+ SqlMigration(139)(List(
+    sqlu"""
+UPDATE images
+SET sourceuri = replace(sourceuri, '%7C', '|')
+WHERE scene IN (
+  SELECT id FROM scenes WHERE
+  datasource IN ('a11b768b-d869-476e-a1ed-0ac3205ed761', '55735945-9da5-47c3-8ae4-572b5e11205b')
+);
+"""
+  ))
+}
