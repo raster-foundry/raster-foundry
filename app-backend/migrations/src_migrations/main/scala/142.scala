f24d896ca1690c480b53d885decbe1dd577a877c
James Santucci <james.santucci@gmail.com>
2018-07-13 13:46:36 -0400

    Remove Landsat 8 scenes with < 0 cloud cover from database

---
+++ app-backend/migrations/src_migrations/main/scala/142.scala
@@ -0,0 +1,12 @@
+import slick.jdbc.PostgresProfile.api._
+import com.liyaos.forklift.slick.SqlMigration
+
+object M142 {
+  RFMigrations.migrations = RFMigrations.migrations :+ SqlMigration(142)(List(
+    sqlu"""
+DELETE FROM scenes
+WHERE cloud_cover < 0
+AND datasource = '697a0b91-b7a8-446e-842c-97cda155554d';
+"""
+  ))
+}
