28da92f23847ecd0f4a7f4cd9650a1de8e06b2d2
James Santucci <james.santucci@gmail.com>
2018-07-16 13:42:20 -0400

    Don't die if tile footprint index already exists on scenes

--- app-backend/migrations/src_migrations/main/scala/140.scala
+++ app-backend/migrations/src_migrations/main/scala/140.scala
@@ -3,6 +3,6 @@ import com.liyaos.forklift.slick.SqlMigration
 
 object M140 {
   RFMigrations.migrations = RFMigrations.migrations :+ SqlMigration(140)(List(
-    sqlu"CREATE INDEX tile_footprint_idx ON scenes USING GIST (tile_footprint);"
+    sqlu"CREATE INDEX IF NOT EXISTS tile_footprint_idx ON scenes USING GIST (tile_footprint);"
   ))
 }
