8ed434403d68a07f7ec06b58c972582aacd5b4bb
Aaron Su <xunzesu@outlook.com>
2018-07-17 12:38:13 -0400

    Update migration

--- app-backend/migrations/src_migrations/main/scala/144.scala
+++ app-backend/migrations/src_migrations/main/scala/144.scala
@@ -4,6 +4,8 @@ import com.liyaos.forklift.slick.SqlMigration
 object M144 {
   RFMigrations.migrations = RFMigrations.migrations :+ SqlMigration(144)(List(
     sqlu"""
+    CREATE TYPE organization_type AS ENUM ('COMMERCIAL', 'GOVERNMENT', 'NON-PROFIT', 'ACADEMIC', 'MILITARY', 'OTHER');
+
     ALTER TABLE users
     ADD COLUMN personal_info JSONB NOT NULL default
       JSONB '{
@@ -13,7 +15,7 @@ object M144 {
         "emailNotifications": false,
         "phoneNumber": "",
         "organizationName": "",
-        "organizationType": "",
+        "organizationType": "OTHER",
         "organizationWebsite": "",
         "profileWebsite": "",
         "profileBio": "",
