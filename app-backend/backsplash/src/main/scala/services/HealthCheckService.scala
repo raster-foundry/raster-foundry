cd5a70325176272366173145f0d9d7e833f14e6c
James Santucci <james.santucci@gmail.com>
2018-08-13 16:26:25 -0400

    Create backsplash subproject

---
+++ app-backend/backsplash/src/main/scala/services/HealthCheckService.scala
@@ -0,0 +1,17 @@
+package com.azavea.rf.backsplash.services
+
+import cats.effect.{Effect, IO}
+import io.circe.Json
+import org.http4s.HttpService
+import org.http4s.circe._
+import org.http4s.dsl.Http4sDsl
+
+class HealthCheckService[F[_]: Effect] extends Http4sDsl[F] {
+  val service: HttpService[F] = {
+    HttpService[F] {
+      case GET -> Root / "healthcheck" => {
+        Ok(Json.obj("message" -> Json.fromString("Healthy"), "reason" -> Json.fromString("A-ok")))
+      }
+    }
+  }
+}
