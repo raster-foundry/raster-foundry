36ae6a7eb6c23c238429901f302c034da0cbb212
James Santucci <james.santucci@gmail.com>
2018-08-27 19:25:04 -0400

    Create histograms for COG scenes when they're created (#3953)
    * Create histograms for COG scenes when they're created
    
    * Remove duplicate import

--- app-backend/api/src/main/scala/scene/Routes.scala
+++ app-backend/api/src/main/scala/scene/Routes.scala
@@ -4,6 +4,7 @@ import java.util.UUID
 
 import akka.http.scaladsl.model.{HttpEntity, MediaTypes, StatusCodes}
 import akka.http.scaladsl.server.Route
+import cats.data._
 import cats.effect.IO
 import cats.implicits._
 import com.amazonaws.services.s3.AmazonS3URI
@@ -21,20 +22,27 @@ import doobie.implicits._
 import doobie.postgres.implicits._
 import doobie.util.transactor.Transactor
 import geotrellis.raster.{IntArrayTile, MultibandTile}
+import geotrellis.raster.histogram.Histogram
+import geotrellis.raster.io.json.HistogramJsonFormats
+import io.circe.parser._
 import kamon.akka.http.KamonTraceDirectives
 
-import scala.concurrent.ExecutionContext.Implicits.global
+import spray.json._
+import spray.json.DefaultJsonProtocol._
+
 import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
 
 trait SceneRoutes extends Authentication
-  with Config
-  with SceneQueryParameterDirective
-  with ThumbnailQueryParameterDirective
-  with PaginationDirectives
-  with CommonHandlers
-  with AWSBatch
-  with UserErrorHandler
-  with KamonTraceDirectives {
+    with Config
+    with SceneQueryParameterDirective
+    with ThumbnailQueryParameterDirective
+    with PaginationDirectives
+    with CommonHandlers
+    with AWSBatch
+    with UserErrorHandler
+    with HistogramJsonFormats
+    with KamonTraceDirectives {
 
   val xa: Transactor[IO]
 
@@ -122,7 +130,6 @@ trait SceneRoutes extends Authentication
 
   def createScene: Route = authenticate { user =>
     entity(as[Scene.Create]) { newScene =>
-
       val tileFootprint = (newScene.sceneType, newScene.ingestLocation, newScene.tileFootprint) match {
         case (Some(SceneType.COG), Some(ingestLocation), None) => {
           logger.debug(s"Ingest location is: $ingestLocation")
@@ -136,6 +143,14 @@ trait SceneRoutes extends Authentication
         case _ => None
       }
 
+      val histogram: OptionT[Future, Array[Histogram[Double]]] = (newScene.sceneType, newScene.ingestLocation) match {
+        case (Some(SceneType.COG), Some(ingestLocation)) =>
+          CogUtils.fromUri(ingestLocation) map { geoTiff =>
+            CogUtils.geoTiffDoubleHistogram(geoTiff)
+          }
+        case _ => OptionT.fromOption(None)
+      }
+
       val dataFootprint = (tileFootprint, newScene.dataFootprint) match {
         case (Some(tf), None) => tileFootprint
         case _ => newScene.dataFootprint
@@ -143,9 +158,26 @@ trait SceneRoutes extends Authentication
 
       val updatedScene = newScene.copy(dataFootprint = dataFootprint, tileFootprint = tileFootprint)
 
-      onSuccess(SceneDao.insert(updatedScene, user).transact(xa).unsafeToFuture) { scene =>
-        if (scene.statusFields.ingestStatus == IngestStatus.ToBeIngested) kickoffSceneIngest(scene.id)
-        complete((StatusCodes.Created, scene))
+      val histogramAndInsertFut = for {
+        hist <- histogram
+        insertedScene <- OptionT.liftF(SceneDao.insert(updatedScene, user).transact(xa).unsafeToFuture)
+        _ <- OptionT.liftF(
+          // We consider the geotrellis upstring json format to be infallible, and "guarantee" presence
+          // of the right projection
+          parse(hist.toJson.toString) traverse {
+            parsed => LayerAttributeDao.insertLayerAttribute(
+              LayerAttribute(insertedScene.id.toString, 0, "histogram", parsed)
+            ).transact(xa).unsafeToFuture
+          } map { _.toOption }
+        )
+      } yield insertedScene
+
+      onSuccess(histogramAndInsertFut.value) {
+        case Some(scene) => 
+          if (scene.statusFields.ingestStatus == IngestStatus.ToBeIngested) kickoffSceneIngest(scene.id)
+          complete((StatusCodes.Created, scene))
+        case None =>
+          complete(StatusCodes.BadRequest)
       }
     }
   }
