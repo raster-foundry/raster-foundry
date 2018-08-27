6c756dc21e4e3b64ea38514f9f6ef1b09eec41b3
James Santucci <james.santucci@gmail.com>
2018-08-27 19:25:04 -0400

    Create histograms for COG scenes when they're created (#3953)
    * Create histograms for COG scenes when they're created
    
    * Remove duplicate import

--- app-backend/api/src/main/scala/scene/Routes.scala
+++ app-backend/api/src/main/scala/scene/Routes.scala
@@ -4,6 +4,7 @@ import java.util.UUID
 
 import akka.http.scaladsl.model.{HttpEntity, MediaTypes, StatusCodes}
 import akka.http.scaladsl.server.Route
+import cats.data._
 import cats.effect.IO
 import cats.implicits._
 import com.amazonaws.services.s3.AmazonS3URI
@@ -21,10 +22,16 @@ import doobie.implicits._
 import doobie.postgres.implicits._
 import doobie.util.transactor.Transactor
 import geotrellis.raster.{IntArrayTile, MultibandTile}
+import geotrellis.raster.histogram.Histogram
+import geotrellis.raster.io.json.HistogramJsonFormats
+import io.circe.parser._
 import kamon.akka.http.KamonTraceDirectives
 
-import scala.concurrent.ExecutionContext.Implicits.global
+import spray.json._
+import spray.json.DefaultJsonProtocol._
+
 import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
 
 trait SceneRoutes
     extends Authentication
@@ -35,6 +42,7 @@ trait SceneRoutes
     with CommonHandlers
     with AWSBatch
     with UserErrorHandler
+    with HistogramJsonFormats
     with KamonTraceDirectives {
 
   val xa: Transactor[IO]
@@ -147,6 +155,14 @@ trait SceneRoutes
         case _ => None
       }
 
+      val histogram: OptionT[Future, Array[Histogram[Double]]] = (newScene.sceneType, newScene.ingestLocation) match {
+        case (Some(SceneType.COG), Some(ingestLocation)) =>
+          CogUtils.fromUri(ingestLocation) map { geoTiff =>
+            CogUtils.geoTiffDoubleHistogram(geoTiff)
+          }
+        case _ => OptionT.fromOption(None)
+      }
+
       val dataFootprint = (tileFootprint, newScene.dataFootprint) match {
         case (Some(tf), None) => tileFootprint
         case _                => newScene.dataFootprint
@@ -155,11 +171,26 @@ trait SceneRoutes
       val updatedScene = newScene.copy(dataFootprint = dataFootprint,
                                        tileFootprint = tileFootprint)
 
-      onSuccess(SceneDao.insert(updatedScene, user).transact(xa).unsafeToFuture) {
-        scene =>
-          if (scene.statusFields.ingestStatus == IngestStatus.ToBeIngested)
-            kickoffSceneIngest(scene.id)
+      val histogramAndInsertFut = for {
+        hist <- histogram
+        insertedScene <- OptionT.liftF(SceneDao.insert(updatedScene, user).transact(xa).unsafeToFuture)
+        _ <- OptionT.liftF(
+          // We consider the geotrellis upstring json format to be infallible, and "guarantee" presence
+          // of the right projection
+          parse(hist.toJson.toString) traverse {
+            parsed => LayerAttributeDao.insertLayerAttribute(
+              LayerAttribute(insertedScene.id.toString, 0, "histogram", parsed)
+            ).transact(xa).unsafeToFuture
+          } map { _.toOption }
+        )
+      } yield insertedScene
+
+      onSuccess(histogramAndInsertFut.value) {
+        case Some(scene) =>
+          if (scene.statusFields.ingestStatus == IngestStatus.ToBeIngested) kickoffSceneIngest(scene.id)
           complete((StatusCodes.Created, scene))
+        case None =>
+          complete(StatusCodes.BadRequest)
       }
     }
   }
