4cf3fe95a6ab47858bf4c7b69a9a3e18aec93763
Aaron Su <xunzesu@outlook.com>
2018-08-29 16:27:53 -0400

    Fix bug.

--- app-frontend/src/app/pages/projects/edit/exports/new/new.controller.js
+++ app-frontend/src/app/pages/projects/edit/exports/new/new.controller.js
@@ -162,6 +162,7 @@ export default class NewExportController {
     }
 
     onOutputProcessingChange(option) {
+        this.exportProcessingOption = option;
         if (!option.default) {
             this.exportOptions = Object.assign(this.exportOptions, option.exportOptions);
         }
