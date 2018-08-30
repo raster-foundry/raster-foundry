e16b95837df6b3828b57b2a2790bfe2442bf34f4
Aaron Su <xunzesu@outlook.com>
2018-08-30 15:34:51 -0400

    Add object acr data model and meta

--- app-backend/datamodel/src/main/scala/com/azavea/rf/datamodel/AccessControlRule.scala
+++ app-backend/datamodel/src/main/scala/com/azavea/rf/datamodel/AccessControlRule.scala
@@ -47,3 +47,36 @@ object AccessControlRule {
     }
   }
 }
+
+@JsonCodec
+case class ObjectAccessControlRule(
+  subjectType: SubjectType,
+  subjectIdO: Option[String],
+  actionType: ActionType
+) {
+  def toObjAcrString: String = (subjectType, subjectIdO) match {
+    case (SubjectType.All, None) => s"ALL;;${actionType}"
+    case (SubjectType.All, Some(subjectId)) => throw new Exception(s"Invalid subjectId for subjectType All: ${subjectId}")
+    case (_, None) => throw new Exception(s"No subject Id provided for subject type: ${subjectType}")
+    case (_, Some(subjectId)) => s"${subjectType};${subjectId};${actionType}"
+  }
+}
+
+object ObjectAccessControlRule {
+  def fromObjAcrString(objAcrString: String): Option[ObjectAccessControlRule] = objAcrString.split(";") match {
+    case Array(subjectType, subjectId, actionType) => (subjectType, subjectId.length) match {
+      case ("ALL", 0) => Some(ObjectAccessControlRule(SubjectType.All, None, ActionType.fromString(actionType)))
+      case ("ALL", _) => throw new Exception(s"Invalid subjectId for subjectType All: ${subjectId}")
+      case (_, 0) => throw new Exception(s"No subject Id provided for subject type: ${subjectType}")
+      case (_, _) => Some(ObjectAccessControlRule(
+        SubjectType.fromString(subjectType),
+        Some(subjectId),
+        ActionType.fromString(actionType)
+      ))
+    }
+    case _ => None
+  }
+
+  def unsafeFromObjAcrString(objAcrString: String): ObjectAccessControlRule =
+    fromObjAcrString(objAcrString).getOrElse(throw new Exception("Invalid format: " + objAcrString))
+}
