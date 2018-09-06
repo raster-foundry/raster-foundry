4501e0e39422b4fc4e28cfafd3790d35834cfed4
James Santucci <james.santucci@gmail.com>
2018-09-06 18:45:10 +0000

    styling

--- app-backend/batch/src/main/scala/com/azavea/rf/batch/cogMetadata/HistogramBackfill.scala
+++ app-backend/batch/src/main/scala/com/azavea/rf/batch/cogMetadata/HistogramBackfill.scala
@@ -43,8 +43,8 @@ object HistogramBackfill extends RollbarNotifier with HistogramJsonFormats {
            scene_type = 'COG' and
            ingest_location is not null and
            cast(id as varchar(255)) not in (select layer_name from layer_attributes);
-       """.query[CogTuple].to[List].transact(xa) map {
-      tuples => {
+       """.query[CogTuple].to[List].transact(xa) map { tuples =>
+      {
         logger.info(s"Found ${tuples.length} scenes to create histograms for")
         tuples.grouped(8).toList
       }
@@ -53,60 +53,71 @@ object HistogramBackfill extends RollbarNotifier with HistogramJsonFormats {
 
   // presence of the ingest location is guaranteed by the filter in the sql string
   @SuppressWarnings(Array("OptionGet"))
-  def insertHistogramLayerAttribute(cogTuple: CogTuple): OptionT[IO, LayerAttribute] = (
+  def insertHistogramLayerAttribute(
+      cogTuple: CogTuple): OptionT[IO, LayerAttribute] = (
     for {
       histogram <- getSceneHistogram(cogTuple._2.get)
-      inserted  <- OptionT(
-        parse(histogram.toJson.toString).toOption traverse {
-          parsed => LayerAttributeDao.insertLayerAttribute(
-            LayerAttribute(
-              cogTuple._1.toString, 0, "histogram", parsed
+      inserted <- OptionT(
+        parse(histogram.toJson.toString).toOption traverse { parsed =>
+          LayerAttributeDao
+            .insertLayerAttribute(
+              LayerAttribute(
+                cogTuple._1.toString,
+                0,
+                "histogram",
+                parsed
+              )
             )
-          ).transact(xa)
+            .transact(xa)
         }
       )
     } yield inserted
   )
 
-  def getSceneHistogram(ingestLocation: String): OptionT[IO, List[Histogram[Double]]] = {
+  def getSceneHistogram(
+      ingestLocation: String): OptionT[IO, List[Histogram[Double]]] = {
     logger.info(s"Fetching histogram for scene at $ingestLocation")
     OptionT(
       IO.fromFuture {
         IO(
           (CogUtils.fromUri(ingestLocation) map {
-             CogUtils.geoTiffDoubleHistogram(_).toList
-           }).value
+            CogUtils.geoTiffDoubleHistogram(_).toList
+          }).value
         )
       }
     )
   }
 
-  def run: IO[Unit] = for {
-    chunkedTuples <- getScenesToBackfill
-    inserts       <- (chunkedTuples parTraverse { sceneList =>
-      sceneList traverse { scene => insertHistogramLayerAttribute(scene) }
-    }).value
-  } yield {
-    val allResults = inserts reduce { _ ++ _ }
-    val errors = allResults filter { _.isEmpty }
-    val successes = allResults filter { !_.isEmpty }
-    logger.info(
-      s"Created histograms for ${successes.length} scenes"
-    )
-    logger.warn(
-      s"Failed to create histograms for ${errors.length} scenes"
-    )
-  }
+  def run: IO[Unit] =
+    for {
+      chunkedTuples <- getScenesToBackfill
+      inserts <- (chunkedTuples parTraverse { sceneList =>
+        sceneList traverse { scene =>
+          insertHistogramLayerAttribute(scene)
+        }
+      }).value
+    } yield {
+      val allResults = inserts reduce { _ ++ _ }
+      val errors = allResults filter { _.isEmpty }
+      val successes = allResults filter { !_.isEmpty }
+      logger.info(
+        s"Created histograms for ${successes.length} scenes"
+      )
+      logger.warn(
+        s"Failed to create histograms for ${errors.length} scenes"
+      )
+    }
 
   def main(args: Array[String]): Unit =
-    run.recoverWith(
-      {
-        case t: Throwable =>
-          IO {
-            sendError(t)
-            logger.error(t.getMessage, t)
-          }
-      }
-    ).unsafeRunSync
+    run
+      .recoverWith(
+        {
+          case t: Throwable =>
+            IO {
+              sendError(t)
+              logger.error(t.getMessage, t)
+            }
+        }
+      )
+      .unsafeRunSync
 }
-
