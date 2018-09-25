29703292eca32e6e97331e1fcc5726039a2337bd
James Santucci <james.santucci@gmail.com>
2018-09-25 15:32:47 +0000

    Scalafmt some things

--- app-backend/migrations/src_migrations/main/scala/151.scala
+++ app-backend/migrations/src_migrations/main/scala/151.scala
@@ -2,11 +2,12 @@ import slick.jdbc.PostgresProfile.api._
 import com.liyaos.forklift.slick.SqlMigration
 
 object M151 {
-  RFMigrations.migrations = RFMigrations.migrations :+ SqlMigration(151)(List(
-    sqlu"""
+  RFMigrations.migrations = RFMigrations.migrations :+ SqlMigration(151)(
+    List(
+      sqlu"""
 UPDATE projects
 SET single_band_options = single_band_options || '{"extraNoData": []}'
 WHERE single_band_options is not null;
 """
-  ))
+    ))
 }
