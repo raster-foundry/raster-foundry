e9fa2525dcdbf3a406ad60738d6cf75111ee1a48
Aaron Su <xunzes@upenn.edu>
2018-10-09 08:19:48 -0400

    Display user info on template items (#4172)

--- app-frontend/src/app/components/lab/templateItem/templateItem.module.js
+++ app-frontend/src/app/components/lab/templateItem/templateItem.module.js
@@ -10,9 +10,34 @@ const TemplateItemComponent = {
 };
 
 class TemplateItemController {
-    constructor() {
+    constructor($rootScope, $log, userService) {
         'ngInject';
+        $rootScope.autoInject(this, arguments);
         this.BUILDCONFIG = BUILDCONFIG;
+    }
+
+    $onInit() {
+        this.getTemplateOwner();
+    }
+
+    getTemplateOwner() {
+        if (this.templateData.owner === 'default') {
+            this.templateOwner = this.BUILDCONFIG.APP_NAME;
+        } else {
+            this.userService.getUserById(this.templateData.owner).then(user => {
+                this.setTemplateOwnerDisplay(user);
+            }, err => {
+                this.$log.error(err);
+                this.templateOwner = this.BUILDCONFIG.APP_NAME;
+            });
+        }
+    }
+
+    setTemplateOwnerDisplay(user) {
+        this.templateOwner = user.personalInfo.firstName.trim() &&
+            user.personalInfo.lastName.trim() ?
+            `${user.personalInfo.firstName.trim()} ${user.personalInfo.lastName.trim()}` :
+            user.name || 'Anonymous';
     }
 }
 
