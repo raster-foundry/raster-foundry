21a0ef9a21cd2039666884556a932fd03af895ab
Chris Brown <cbrown@azavea.com>
2018-10-16 10:14:38 -0400

    Add test for Annotation Group Summary

--- app-backend/db/src/test/scala/com/azavea/rf/database/AnnotationGroupDaoSpec.scala
+++ app-backend/db/src/test/scala/com/azavea/rf/database/AnnotationGroupDaoSpec.scala
@@ -159,6 +159,47 @@ class AnnotationGroupDaoSpec
     }
   }
 
+  test("retrieve annotation summary for a group") {
+    check {
+      forAll {
+        (user: User.Create,
+         org: Organization.Create,
+         project: Project.Create,
+         ag: AnnotationGroup.Create,
+         agAnnotations: List[Annotation.Create]) =>
+          {
+            val annotationGroupIO = for {
+              orgUserProject <- insertUserOrgProject(user, org, project)
+              (dbOrg, dbUser, dbProject) = orgUserProject
+              annotationGroupDB <- AnnotationGroupDao.createAnnotationGroup(
+                dbProject.id,
+                ag,
+                dbUser)
+              annotationsDB <- AnnotationDao.insertAnnotations(
+                agAnnotations.map(
+                  _.copy(annotationGroup = Some(annotationGroupDB.id))),
+                dbProject.id,
+                dbUser)
+              projectAnnotations <- AnnotationDao.query
+                .filter(fr"project_id=${dbProject.id}")
+                .list
+              projectAnnotationGroups <- AnnotationGroupDao
+                .listAnnotationGroupsForProject(dbProject.id)
+              annotationGroupSummary <- AnnotationGroupDao
+                .getAnnotationGroupSummary(annotationGroupDB.id)
+            } yield annotationGroupSummary
+
+            val annotationGroupSummary =
+              annotationGroupIO.transact(xa).unsafeRunSync()
+
+            assert(annotationGroupSummary.length > 0,
+                   "; No summary produced for annotation group")
+            true
+          }
+      }
+    }
+  }
+
   test("Creating annotations on a project with no annotation groups") {
     check {
       forAll {
