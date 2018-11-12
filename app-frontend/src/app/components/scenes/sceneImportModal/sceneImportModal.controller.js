040c4cb37ff8f44dc9b0591cb8db248c106ff69c
Aaron Su <xunzes@upenn.edu>
2018-11-12 15:00:24 -0500

    Let import modal suggest and use correct S3 path (#4290)
    Support submitting either an absolute path to a .tiff or .tif file, or a path as bucket/prefix
    to upload scenes from S3 on frontend using the scene import modal.

--- app-frontend/src/app/components/scenes/sceneImportModal/sceneImportModal.controller.js
+++ app-frontend/src/app/components/scenes/sceneImportModal/sceneImportModal.controller.js
@@ -103,7 +103,7 @@ export default class SceneImportModalController {
                 if (this.importType === 'local') {
                     return true;
                 } else if (this.importType === 'S3') {
-                    return this.validateS3Config();
+                    return this.validateS3Config() && this.isValidS3Path;
                 } else if (this.importType === 'Planet') {
                     return this.validatePlanetConfig();
                 }
@@ -157,7 +157,12 @@ export default class SceneImportModalController {
             previous: () => 'IMPORT',
             next: () => 'IMPORT_SUCCESS',
             onEnter: () => this.startS3Upload(),
-            onExit: () => this.finishUpload()
+            onExit: () => {
+                if (!this.isProcessS3Failed) {
+                    this.finishUpload();
+                }
+                this.isProcessS3Failed = !this.isProcessS3Failed;
+            }
         }, {
             name: 'IMPORT_SUCCESS',
             allowDone: () => true
@@ -329,11 +334,13 @@ export default class SceneImportModalController {
             .getCurrentUser()
             .then(this.createUpload.bind(this))
             .then(upload => {
+                this.isProcessS3Failed = false;
+                delete this.error;
                 this.upload = upload;
                 this.uploadsDone();
                 return upload;
             }, err => {
-                this.handleNext();
+                this.isProcessS3Failed = true;
                 this.uploadError(err);
             }).finally(() => {
                 this.allowInterruptions();
@@ -398,8 +405,20 @@ export default class SceneImportModalController {
             uploadObject.files = this.selectedFileDatasets.map(f => f.file.name);
             uploadObject.uploadType = 'LOCAL';
         } else if (this.importType === 'S3') {
-            uploadObject.uploadType = 'S3';
-            uploadObject.source = encodeURI(this.s3Config.bucket);
+            const segs = this.s3Config.bucket.split('/');
+            if (segs[segs.length - 1].includes('.tif')) {
+                this.importType = 'local';
+                uploadObject = Object.assign(uploadObject, {
+                    files: [encodeURI(this.s3Config.bucket)],
+                    uploadType: 'LOCAL',
+                    uploadStatus: 'UPLOADED'
+                });
+            } else {
+                uploadObject = Object.assign(uploadObject, {
+                    uploadType: 'S3',
+                    source: encodeURI(this.s3Config.bucket)
+                });
+            }
         } else if (this.importType === 'Planet') {
             uploadObject.uploadType = 'PLANET';
             uploadObject.metadata.planetKey = this.planetCredential;
@@ -668,6 +687,46 @@ export default class SceneImportModalController {
         }
         if (this.resolve.origin === 'datasource') {
             this.$state.go('imports.rasters');
+        }
+    }
+
+    backToImport() {
+        this.setCurrentStep(this.getStep('IMPORT'));
+    }
+
+    usePath(path) {
+        this.s3Config.bucket = path;
+        this.isValidS3Path = true;
+    }
+
+    generateSuggestedPaths(path, protocol) {
+        this.isValidS3Path = false;
+        let segments = path.replace(protocol, '').split('/');
+
+        if (_.get(segments, 'length') === 1) {
+            this.suggestedPaths = [path];
+        }
+
+        if (_.get(segments, 'length') > 1) {
+            let cleanSegs = segments.filter(seg => !seg.includes('s3.amazonaws.com'));
+            this.suggestedPaths = cleanSegs.map((seg, idx) =>
+                `s3://${cleanSegs.slice(0, idx + 1).join('/')}`
+            );
+        }
+    }
+
+    onPathChange(path) {
+        this.suggestedPaths = [];
+        if (path && path.includes('s3://')) {
+            this.isValidS3Path = true;
+        }
+
+        if (path && path.includes('https://')) {
+            this.generateSuggestedPaths(path, 'https://');
+        }
+
+        if (path && path.includes('http://')) {
+            this.generateSuggestedPaths(path, 'http://');
         }
     }
 }
