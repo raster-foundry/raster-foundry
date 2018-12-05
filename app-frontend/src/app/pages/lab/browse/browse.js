7cd9b35acae8e9c3c498213a63f4c4484f5ad442
Alex Kaminsky <akaminsky@azavea.com>
2018-12-05 11:12:42 -0500

    Update lab browse navbar styles

--- app-frontend/src/app/pages/lab/browse/browse.js
+++ app-frontend/src/app/pages/lab/browse/browse.js
@@ -1,8 +1,34 @@
 class LabBrowseController {
-    constructor() {
+    constructor($scope, modalService) {
         'ngInject';
+        $scope.autoInject(this, arguments);
+    }
+
+    openTemplateCreateModal() {
+        this.modalService.open({
+            component: 'rfTemplateCreateModal'
+        });
     }
 }
 
-export default angular.module('pages.lab.browse', [])
-    .controller('LabBrowseController', LabBrowseController);
+const LabBrowseModule = angular
+  .module('pages.lab.browse', [])
+  .controller('LabBrowseController', LabBrowseController);
+
+LabBrowseModule.resolve = {
+    user: ($stateParams, authService) => {
+        return authService.getCurrentUser();
+    },
+    userRoles: (authService) => {
+        return authService.fetchUserRoles();
+    },
+    platform: (userRoles, platformService) => {
+        const platformRole = userRoles.find(r =>
+            r.groupType === 'PLATFORM'
+        );
+
+        return platformService.getPlatform(platformRole.groupId);
+    }
+};
+
+export default LabBrowseModule;
