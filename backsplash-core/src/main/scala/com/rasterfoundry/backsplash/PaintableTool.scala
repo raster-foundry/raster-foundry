7cb36eff07fac2b85cc2b7e232bedf92aa74248f
James Santucci <james.santucci@gmail.com>
2018-12-10 16:38:48 -0500

    Move typeclass constraints into PaintableTool default constructor

--- backsplash-core/src/main/scala/com/rasterfoundry/backsplash/PaintableTool.scala
+++ backsplash-core/src/main/scala/com/rasterfoundry/backsplash/PaintableTool.scala
@@ -5,20 +5,10 @@ import com.azavea.maml.eval.BufferingInterpreter
 import geotrellis.raster.MultibandTile
 import geotrellis.server.{TmsReification, ExtentReification, HasRasterExtents}
 
-case class PaintableTool[Param](
+case class PaintableTool[Param: TmsReification: ExtentReification: HasRasterExtents](
     expr: Expression,
     painter: MultibandTile => MultibandTile,
     paramMap: Map[String, Param],
     interpreter: BufferingInterpreter = BufferingInterpreter.DEFAULT,
     paint: Boolean // whether to paint the tile or return raw values
 )
-
-object PaintableTool {
-  def safely[Param: TmsReification: ExtentReification: HasRasterExtents](
-      expr: Expression,
-      painter: MultibandTile => MultibandTile,
-      paramMap: Map[String, Param],
-      interpreter: BufferingInterpreter = BufferingInterpreter.DEFAULT,
-      paint: Boolean): PaintableTool[Param] =
-    PaintableTool(expr, painter, paramMap, interpreter, paint)
-}
