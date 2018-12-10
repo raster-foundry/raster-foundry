a126d1aeadcee822534d4f0030918e59a1df0ed0
James Santucci <james.santucci@gmail.com>
2018-12-10 16:37:03 -0500

    Remove unused imports

--- backsplash-core/src/main/scala/com/rasterfoundry/backsplash/package.scala
+++ backsplash-core/src/main/scala/com/rasterfoundry/backsplash/package.scala
@@ -1,10 +1,6 @@
 package com.rasterfoundry
 
 import cats.effect._
-import com.azavea.maml.ast.Expression
-import geotrellis.raster.Tile
-import geotrellis.raster.render.Png
-import geotrellis.server.TmsReification
 import io.circe.KeyEncoder
 
 package object backsplash {
