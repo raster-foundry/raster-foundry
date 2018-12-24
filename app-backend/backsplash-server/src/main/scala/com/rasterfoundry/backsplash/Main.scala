20aa82d477f5cba9bb3b38ec1d02f19cba6537a1
James Santucci <james.santucci@gmail.com>
2018-12-24 11:52:32 -0500

    Bump to gt-server 0.14.0-RC2

--- app-backend/backsplash-server/src/main/scala/com/rasterfoundry/backsplash/Main.scala
+++ app-backend/backsplash-server/src/main/scala/com/rasterfoundry/backsplash/Main.scala
@@ -38,8 +38,6 @@ import java.util.UUID
 object Main extends IOApp with ProjectStoreImplicits {
 
   sgdal.setConfigOption("GDAL_DISABLE_READDIR_ON_OPEN", "YES")
-  sgdal.setConfigOption("GDAL_MAX_DATASET_POOL_SIZE", "1")
-  sgdal.setConfigOption("VRT_SHARED_SOURCE", "0")
 
   override protected implicit def contextShift: ContextShift[IO] =
     IO.contextShift(
