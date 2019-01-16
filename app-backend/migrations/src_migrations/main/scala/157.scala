f94a34286ce7435c63ea90b351e1a0e449b22b22
Aaron Su <xunzesu@outlook.com>
2019-01-16 16:12:21 -0500

    Insert a default project layer upon project creation
    * Update generator for property tests

--- app-backend/migrations/src_migrations/main/scala/157.scala
+++ app-backend/migrations/src_migrations/main/scala/157.scala
@@ -15,24 +15,24 @@ object M157 {
     DROP COLUMN smart_layer_id,
     ADD COLUMN smart_layer_id UUID;
 
-    -- Add a default_layer column to projects table and populate with uuid
+    -- Add a default_layer_id column to projects table and populate with uuid
     -- these default uuid values are temporary
     -- this field is nullable due to similiar reason as default_annotation_group
     ALTER TABLE projects
-    ADD COLUMN default_layer UUID DEFAULT uuid_generate_v4();
+    ADD COLUMN default_layer_id UUID DEFAULT uuid_generate_v4();
 
     -- Insert default project layers based on project id and project default layer from projects table
     INSERT INTO project_layers (
       SELECT
-        default_layer AS id, now() AS created_at, now() AS modified_at, 'default_layer' AS name, id AS project_id
+        default_layer_id AS id, now() AS created_at, now() AS modified_at, 'Project default layer' AS name, id AS project_id
       FROM projects
     );
 
-    -- default_layer of projects table should refer to id of project_layers
-    -- remove the default value constraint on default_layer
+    -- default_layer_id of projects table should refer to id of project_layers
+    -- remove the default value constraint on default_layer_id
     ALTER TABLE projects
-    ADD CONSTRAINT projects_default_project_layer_id_fkey FOREIGN KEY (default_layer) REFERENCES project_layers(id),
-    ALTER COLUMN default_layer DROP DEFAULT;
+    ADD CONSTRAINT projects_default_project_layer_id_fkey FOREIGN KEY (default_layer_id) REFERENCES project_layers(id),
+    ALTER COLUMN default_layer_id DROP DEFAULT;
 
     -- Create a scenes_to_layers table similar to scenes_to_projects table
     CREATE TABLE scenes_to_layers (
@@ -42,14 +42,6 @@ object M157 {
       mosaic_definition     JSONB NOT NULL DEFAULT '{}'::json,
       accepted              BOOLEAN NOT NULL DEFAULT true,
       CONSTRAINT scenes_to_layers_pkey PRIMARY KEY (scene_id, project_layer_id)
-    );
-
-    -- Populate scenes_to_layers table based on the joined result of scenes_to_projects and projects tables
-    INSERT INTO scenes_to_layers (
-      SELECT
-        stp.scene_id, p.default_layer AS project_layer_id, stp.scene_order, stp.mosaic_definition, stp.accepted
-      FROM scenes_to_projects AS stp
-      JOIN projects p ON stp.project_id = p.id
     );
     """
     ))
