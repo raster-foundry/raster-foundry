f1d8e8ac8bf51db84d68b0cb563774b546ddc898
Aaron Su <xunzesu@outlook.com>
2019-01-16 16:10:11 -0500

    Update migration
    * Remove the deletion command of scenes_to_projects table
    * Alter data type of smart_layer_id in project_layers table
    * Remove uuid default value constraint of default_layer of projects table
    * Make default_layer of projects table nullable

--- app-backend/migrations/src_migrations/main/scala/157.scala
+++ app-backend/migrations/src_migrations/main/scala/157.scala
@@ -10,9 +10,16 @@ object M157 {
     DROP CONSTRAINT project_layers_project_id_fkey,
     ADD CONSTRAINT project_layers_project_id_fkey FOREIGN KEY (project_id) REFERENCES projects(id) ON DELETE CASCADE;
 
+    -- Change smart_layer_id column type to nullable uuid
+    ALTER TABLE project_layers
+    DROP COLUMN smart_layer_id,
+    ADD COLUMN smart_layer_id UUID;
+
     -- Add a default_layer column to projects table and populate with uuid
+    -- these default uuid values are temporary
+    -- this field is nullable due to similiar reason as default_annotation_group
     ALTER TABLE projects
-    ADD COLUMN default_layer UUID NOT NULL DEFAULT uuid_generate_v4();
+    ADD COLUMN default_layer UUID DEFAULT uuid_generate_v4();
 
     -- Insert default project layers based on project id and project default layer from projects table
     INSERT INTO project_layers (
@@ -22,8 +29,10 @@ object M157 {
     );
 
     -- default_layer of projects table should refer to id of project_layers
+    -- remove the default value constraint on default_layer
     ALTER TABLE projects
-    ADD CONSTRAINT projects_default_project_layer_id_fkey FOREIGN KEY (default_layer) REFERENCES project_layers(id);
+    ADD CONSTRAINT projects_default_project_layer_id_fkey FOREIGN KEY (default_layer) REFERENCES project_layers(id),
+    ALTER COLUMN default_layer DROP DEFAULT;
 
     -- Create a scenes_to_layers table similar to scenes_to_projects table
     CREATE TABLE scenes_to_layers (
@@ -42,9 +51,6 @@ object M157 {
       FROM scenes_to_projects AS stp
       JOIN projects p ON stp.project_id = p.id
     );
-
-    -- drop scenes_to_projects table after backend is compatible
-    -- DROP TABLE IF EXISTS scenes_to_projects;
     """
     ))
 }
