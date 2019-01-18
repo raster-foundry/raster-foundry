04cfa90f4ab98e0829f7201e9a4a277ff89f5e18
Aaron Su <xunzesu@outlook.com>
2019-01-18 15:34:29 -0500

    Add more function to scene to layer dao

--- app-backend/db/src/main/scala/SceneToLayerDao.scala
+++ app-backend/db/src/main/scala/SceneToLayerDao.scala
@@ -39,7 +39,10 @@ object SceneToLayerDao extends Dao[SceneToLayer] with LazyLogging {
 
   def acceptScene(projectLayerId: UUID, sceneId: UUID): ConnectionIO[Int] = {
     fr"""
-      UPDATE scenes_to_layers SET accepted = true WHERE project_layer_id = ${projectLayerId} AND scene_id = ${sceneId}
+      UPDATE scenes_to_layers
+      SET accepted = true
+      WHERE project_layer_id = ${projectLayerId}
+        AND scene_id = ${sceneId}
     """.update.run
   }
 
@@ -50,15 +53,106 @@ object SceneToLayerDao extends Dao[SceneToLayer] with LazyLogging {
     }
   }
 
-  def addSceneOrdering(projectLayerId: UUID): ConnectionIO[Int] = {
+  def addSceneOrdering(projectId: UUID): ConnectionIO[Int] = {
     (fr"""
-    UPDATE scenes_to_layers
-    SET scene_order = rnum
-    (
-    SELECT id, row_number() over (ORDER BY scene_order ASC, acquisition_date ASC, cloud_cover ASC) as rnum,
-    FROM scenes_to_layers join scenes on scenes.id = scene_id where project_layer_id = $projectLayerId
-    ) s
-    WHERE id = s.id
+      UPDATE scenes_to_layers
+      SET scene_order = s.rnum
+      FROM (
+        SELECT scene_id, project_layer_id, row_number() over (ORDER BY stl.scene_order ASC, scenes.acquisition_date ASC, scenes.cloud_cover ASC) as rnum
+        FROM scenes_to_layers stl
+          JOIN scenes ON scenes.id = stl.scene_id
+          JOIN project_layers pl ON pl.id = stl.project_layer_id
+          WHERE pl.project_id = ${projectId}
+      ) s
+      WHERE scenes_to_layers.project_layer_id = s.project_layer_id
+        AND scenes_to_layers.scene_id = s.scene_id
     """).update.run
+  }
+
+  def setManualOrder(projectLayerId: UUID,
+                     sceneIds: Seq[UUID],): ConnectionIO[Seq[UUID]] = {
+    val updates = for {
+      i <- sceneIds.indices
+    } yield {
+      fr"""
+      UPDATE scenes_to_layers
+      SET scene_order = ${i}
+      WHERE project_layer_id = ${projectLayerId}
+        AND scene_id = ${sceneIds(i)}
+    """.update.run
+    }
+    for {
+      _ <- updates.toList.sequence
+    } yield sceneIds
+  }
+
+  def getMosaicDefinition(projectId: UUID,
+                          polygonOption: Option[Projected[Polygon]],
+                          redBand: Option[Int] = None,
+                          greenBand: Option[Int] = None,
+                          blueBand: Option[Int] = None,
+                          sceneIdSubset: List[UUID] = List.empty)
+    : Stream[ConnectionIO, MosaicDefinition] = {
+    val filters = List(
+      polygonOption.map(polygon =>
+        fr"ST_Intersects(scenes_stp.tile_footprint, ${polygon})"),
+      Some(fr"scenes_stp.project_id = ${projectId}"),
+      Some(fr"scenes_stp.accepted = true"),
+      Some(fr"scenes_stp.ingest_status = 'INGESTED'"),
+      sceneIdSubset.toNel map {
+        Fragments.in(fr"scene_id", _)
+      }
+    )
+    val select = fr"""
+    SELECT
+      scene_id, project_layer_id, accepted, scene_order, mosaic_definition, scene_type, ingest_location,
+      data_footprint, is_single_band, single_band_options
+    FROM (
+      scenes_to_layers
+    LEFT JOIN
+      scenes
+    ON scenes.id = scenes_to_projects.scene_id
+    ) scenes_stp
+    LEFT JOIN
+      project_layers
+    ON
+      scenes_stp.project_layer_id = project_layers.id
+    LEFT JOIN
+      projects
+    ON
+      scenes_stp.project_id = projects.id
+      """
+    (select ++ whereAndOpt(filters: _*) ++ fr"ORDER BY scenes_stp.scene_order ASC")
+      .query[SceneToProjectwithSceneType]
+      .stream map { stp =>
+      {
+        Applicative[Option].map3(redBand, greenBand, blueBand) {
+          case (r, g, b) =>
+            MosaicDefinition(
+              stp.sceneId,
+              stp.colorCorrectParams.copy(
+                redBand = r,
+                greenBand = g,
+                blueBand = b
+              ),
+              stp.sceneType,
+              stp.ingestLocation,
+              stp.dataFootprint flatMap { _.geom.as[MultiPolygon] },
+              stp.isSingleBand,
+              stp.singleBandOptions
+            )
+        } getOrElse {
+          MosaicDefinition(
+            stp.sceneId,
+            stp.colorCorrectParams,
+            stp.sceneType,
+            stp.ingestLocation,
+            stp.dataFootprint flatMap { _.geom.as[MultiPolygon] },
+            stp.isSingleBand,
+            stp.singleBandOptions
+          )
+        }
+      }
+    }
   }
 }
