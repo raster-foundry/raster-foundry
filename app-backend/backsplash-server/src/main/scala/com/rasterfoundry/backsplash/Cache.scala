4c5160a6b0694d0bd7b3d10498506cb3cbcfd3e0
James Santucci <james.santucci@gmail.com>
2019-01-21 15:51:23 -0500

    Fix the ol' switcheroo

--- app-backend/backsplash-server/src/main/scala/com/rasterfoundry/backsplash/Cache.scala
+++ app-backend/backsplash-server/src/main/scala/com/rasterfoundry/backsplash/Cache.scala
@@ -10,14 +10,14 @@ import scalacache.CatsEffect.modes._
 
 object Cache extends LazyLogging {
 
-  val authorizationCacheFlags = Http4sUtilCache.authenticationCacheFlags
+  val authorizationCacheFlags = Flags(Config.cache.authorizationCacheEnable,
+                                      Config.cache.authorizationCacheEnable)
   val caffeineAuthorizationCache: Cache[Boolean] =
     CaffeineCache[Boolean]
   logger.info(
     s"Authorization Cache Status (read/write) ${authorizationCacheFlags}")
 
-  val authenticationCacheFlags = Flags(Config.cache.authenticationCacheEnable,
-                                       Config.cache.authenticationCacheEnable)
+  val authenticationCacheFlags = Http4sUtilCache.authenticationCacheFlags
   val caffeineAuthenticationCache: Cache[Option[User]] =
     CaffeineCache[Option[User]]
   logger.info(
