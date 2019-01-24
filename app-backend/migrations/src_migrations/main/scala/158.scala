46cf5c8d14cc0f5660906db85d58d959c33e5e6e
Chris Brown <cbrown@azavea.com>
2019-01-24 09:41:52 -0500

    ScalaFmt Fix

--- app-backend/migrations/src_migrations/main/scala/158.scala
+++ app-backend/migrations/src_migrations/main/scala/158.scala
@@ -2,8 +2,9 @@ import slick.jdbc.PostgresProfile.api._
 import com.liyaos.forklift.slick.SqlMigration
 
 object M158 {
-  RFMigrations.migrations = RFMigrations.migrations :+ SqlMigration(158)(List(
-    sqlu"""
+  RFMigrations.migrations = RFMigrations.migrations :+ SqlMigration(158)(
+    List(
+      sqlu"""
     -- Populate scenes_to_layers table based on the joined result of scenes_to_projects and projects tables
     -- This command adds relationship between scenes and current projects' default layers
     INSERT INTO scenes_to_layers (
@@ -14,5 +15,5 @@ object M158 {
     )
     ON CONFLICT ON CONSTRAINT scenes_to_layers_pkey DO NOTHING;
     """
-  ))
+    ))
 }
