3400f6f1a84ef9b6f50c507932ff7badd4c50b05
Aaron Su <xunzesu@outlook.com>
2019-02-01 16:57:30 -0500

    Add project layer create modal on layer list UI

--- app-frontend/src/app/components/projects/projectLayerCreateModal/projectLayerCreateModal.js
+++ app-frontend/src/app/components/projects/projectLayerCreateModal/projectLayerCreateModal.js
@@ -14,11 +14,61 @@ const ProjectLayerCreateModalComponent = {
 
 class ProjectLayerCreateModalController {
     constructor(
+        $rootScope, $log,
+        projectService
     ) {
         'ngInject';
+        $rootScope.autoInject(this, arguments);
     }
 
     $onInit() {
+        this.showSmartLayerOption = false;
+        this.setProjectLayerCreate();
+    }
+
+    setProjectLayerCreate() {
+        this.projectLayerCreate = {
+            name: '',
+            projectId: this.resolve.projectId,
+            colorGroupHex: '#4da687'
+        };
+        this.projectLayerCreateBuffer = Object.assign({}, this.projectLayerCreate);
+    }
+
+    resetModal() {
+        this.isCreatingLayer = false;
+        this.isCreatingLayerError = false;
+        this.projectLayerCreateBuffer = Object.assign({}, this.projectLayerCreate);
+    }
+
+    isCreateDisabled() {
+        return !(this.projectLayerCreateBuffer.name &&
+            this.projectLayerCreateBuffer.name.length) ||
+            this.isCreatingLayer ||
+            this.isCreatingLayerError;
+    }
+
+    createProjectLayer() {
+        this.isCreatingLayer = true;
+        this.isCreatingLayerError = false;
+
+        this.projectService
+            .createProjectLayer(this.resolve.projectId, this.projectLayerCreateBuffer)
+            .then(createdProjectLayer => {
+                this.isCreatingLayerError = false;
+                this.projectLayerCreate = Object.assign(
+                    this.projectLayerCreate,
+                    this.projectLayerCreateBuffer
+                );
+                this.close({$value: createdProjectLayer});
+            })
+            .catch(err => {
+                this.isCreatingLayerError = true;
+                this.$log.error(err);
+            })
+            .finally(() => {
+                this.isCreatingLayer = false;
+            });
     }
 }
 
