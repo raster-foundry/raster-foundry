83a5a7b862efbbfbba1c08e85bab32719f18ee02
James Santucci <james.santucci@gmail.com>
2019-02-05 10:57:53 -0500

    Put something back that I shouldn't have removed

--- app-backend/backsplash-server/src/main/scala/com/rasterfoundry/backsplash/ProjectStoreImplicits.scala
+++ app-backend/backsplash-server/src/main/scala/com/rasterfoundry/backsplash/ProjectStoreImplicits.scala
@@ -5,6 +5,7 @@ import com.rasterfoundry.backsplash.color._
 import com.rasterfoundry.backsplash.ProjectStore.ToProjectStoreOps
 import com.rasterfoundry.backsplash.error._
 import com.rasterfoundry.database.{SceneDao, SceneToLayerDao, SceneToProjectDao}
+import com.rasterfoundry.database.Implicits._
 import com.rasterfoundry.common.datamodel.MosaicDefinition
 import com.rasterfoundry.common.datamodel.color.{
   BandGamma => RFBandGamma,
