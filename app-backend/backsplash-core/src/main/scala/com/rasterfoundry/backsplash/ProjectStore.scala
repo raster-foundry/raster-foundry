12af9a53a65d7396336dfe68689e494be197ab46
James Santucci <james.santucci@gmail.com>
2019-02-12 15:05:10 -0500

    Tell tile server how to color scene previews based on datasource

--- app-backend/backsplash-core/src/main/scala/com/rasterfoundry/backsplash/ProjectStore.scala
+++ app-backend/backsplash-core/src/main/scala/com/rasterfoundry/backsplash/ProjectStore.scala
@@ -1,16 +1,11 @@
 package com.rasterfoundry.backsplash
 
 import cats.data.{NonEmptyList => NEL}
+import com.rasterfoundry.common.datamodel.BandOverride
 import geotrellis.vector.{Polygon, Projected}
 import simulacrum._
 
 import java.util.UUID
-
-final case class BandOverride(
-    red: Int,
-    green: Int,
-    blue: Int
-)
 
 @typeclass trait ProjectStore[A] {
   @op("read") def read(self: A,
