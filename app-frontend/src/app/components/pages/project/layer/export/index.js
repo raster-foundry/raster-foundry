0b260c6b78c690c7871bc594e249fe7cf7528aaf
Aaron Su <xunzesu@outlook.com>
2019-03-01 13:53:18 -0500

    Add export creare UI skeleton

--- app-frontend/src/app/components/pages/project/layer/export/index.js
+++ app-frontend/src/app/components/pages/project/layer/export/index.js
@@ -1,11 +1,112 @@
 import tpl from './index.html';
 
-class LayerExportCreateController {
+const mapName = 'project';
+const mapLayerName = 'Project Layer';
+const shapeLayerName = 'Export AOI';
+const geomDrawType = 'Polygon';
 
+class LayerExportCreateController {
+    constructor(
+        $rootScope,
+        $scope,
+        $state,
+        $log,
+        projectService,
+        mapService,
+        exportService,
+        authService,
+        modalService
+    ) {
+        'ngInject';
+        $rootScope.autoInject(this, arguments);
+    }
+
+    $onInit() {
+        this.initExportTarget();
+        this.initResolutions();
+        this.setMapLayers();
+        this.initAoiDrawToolbar();
+    }
+
+    $onDestroy() {
+        this.removeMapLayers();
+        this.removeExportAois();
+    }
+
+    initAoiDrawToolbar() {
+        this.mapId = mapName;
+        this.geomDrawType = geomDrawType;
+        this.isDrawAoiClicked = false;
+    }
+
+    initExportTarget() {
+        this.availableTargets = this.exportService.getAvailableTargets();
+        const defaultTarget = this.availableTargets.find(target => target.default);
+        if (defaultTarget) {
+            this.exportTarget = defaultTarget.value;
+        }
+    }
+
+    initResolutions() {
+        this.availableResolutions = this.exportService.getAvailableResolutions();
+        const defaultResolution = this.availableResolutions[0];
+        if (defaultResolution) {
+            this.resolution = defaultResolution.value.toString();
+        }
+    }
+
+    getMap() {
+        return this.mapService.getMap(mapName);
+    }
+
+    setMapLayers() {
+        let mapLayer = this.projectService.mapLayerFromLayer(this.project, this.layer);
+        return this.getMap().then(map => {
+            map.setLayer(mapLayerName, mapLayer, true);
+        });
+    }
+
+    removeMapLayers() {
+        this.getMap().then(map => {
+            map.deleteLayers(mapLayerName);
+        });
+    }
+
+    removeExportAois() {
+        this.getMap().then(map => {
+            map.deleteGeojson(shapeLayerName);
+        });
+    }
+
+    onCancelDrawAoi() {
+        this.$log.log('cancel');
+        this.isDrawAoiClicked = false;
+    }
+
+    onConfirmAoi(aoiGeojson, isSaveShape) {
+        this.$log.log('confirm', aoiGeojson, isSaveShape);
+    }
+
+    onClickDefineAoi() {
+        this.isDrawAoiClicked = true;
+    }
+
+    isValidExportDef() {
+        const isValidTarget = this.exportTarget.value;
+        const isValidResolution = this.resolution;
+        // const isValidGeom = this.
+        return false;
+    }
+
+    onShapeOp(isInProgress) {
+        this.isDrawing = isInProgress;
+    }
 }
 
 const component = {
     bindings: {
+        project: '<',
+        layer: '<'
     },
     templateUrl: tpl,
     controller: LayerExportCreateController.name
@@ -14,5 +115,4 @@ const component = {
 export default angular
     .module('components.pages.project.layer.export', [])
     .controller(LayerExportCreateController.name, LayerExportCreateController)
-    .component('rfProjectLayerExportPage', component)
-    .name;
+    .component('rfProjectLayerExportPage', component).name;
