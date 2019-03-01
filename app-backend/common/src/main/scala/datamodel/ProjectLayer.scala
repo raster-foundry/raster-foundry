fd0f858d09b626dbf4b7e6468090b490443a8f40
James Santucci <james.santucci@gmail.com>
2019-03-01 11:32:08 -0500

    Add explicit ProjectLayer.Create decoding

--- app-backend/common/src/main/scala/datamodel/ProjectLayer.scala
+++ app-backend/common/src/main/scala/datamodel/ProjectLayer.scala
@@ -1,8 +1,11 @@
 package com.rasterfoundry.common.datamodel
 
+import com.rasterfoundry.common.JsonCodecs
 import com.typesafe.scalalogging.LazyLogging
 import geotrellis.vector.{Geometry, Projected}
+import io.circe.{Decoder, Encoder}
 import io.circe.generic.JsonCodec
+import io.circe.generic.semiauto._
 
 import java.sql.Timestamp
 import java.util.UUID
@@ -53,10 +56,11 @@ final case class ProjectLayerProperties(
     isSingleBand: Boolean,
     singleBandOptions: Option[SingleBandOptions.Params])
 
-object ProjectLayer extends LazyLogging {
-  def create = Create.apply _
+object ProjectLayer extends LazyLogging with JsonCodecs {
 
-  @JsonCodec
+  implicit val projGeomODecoder: Decoder[Option[Projected[Geometry]]] =
+    implicitly
+
   final case class Create(name: String,
                           projectId: Option[UUID],
                           colorGroupHex: String,
@@ -84,6 +88,46 @@ object ProjectLayer extends LazyLogging {
       )
     }
   }
+
+  object Create {
+    implicit val createEncoder: Encoder[Create] = deriveEncoder
+    implicit val createDecoder: Decoder[Create] = Decoder.forProduct7(
+      "name",
+      "projectId",
+      "colorGroupHex",
+      "smartLayerId",
+      "rangeStart",
+      "rangeEnd",
+      "geometry"
+    )(
+      (name: String,
+       projectId: Option[UUID],
+       colorGroupHex: String,
+       smartLayerId: Option[UUID],
+       rangeStart: Option[Timestamp],
+       rangeEnd: Option[Timestamp],
+       geometry: Option[Projected[Geometry]]) =>
+        Create(name,
+               projectId,
+               colorGroupHex,
+               smartLayerId,
+               rangeStart,
+               rangeEnd,
+               geometry,
+               false,
+               None)) or Decoder.forProduct9(
+      "name",
+      "projectId",
+      "colorGroupHex",
+      "smartLayerId",
+      "rangeStart",
+      "rangeEnd",
+      "geometry",
+      "isSingleBand",
+      "singleBandOptions"
+    )(Create.apply _)
+  }
+
   final case class GeoJSON(id: UUID,
                            geometry: Option[Projected[Geometry]],
                            properties: ProjectLayerProperties,
