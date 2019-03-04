fd41a412d2c6e6dddc219941bdf7729a55135e03
Alex Kaminsky <akaminsky@azavea.com>
2019-03-04 12:03:35 -0500

    Project layer color correction UI

--- app-frontend/src/app/components/pages/project/layer/corrections/index.js
+++ app-frontend/src/app/components/pages/project/layer/corrections/index.js
@@ -1,5 +1,8 @@
 import { Set } from 'immutable';
+import _ from 'lodash';
 import tpl from './index.html';
+
+const mapLayerName = 'project-color-correction';
 
 const baseGammaOptions = {
     floor: 0,
@@ -46,7 +49,7 @@ const betaOptions = {
 class LayerCorrectionsController {
     constructor(
         $rootScope, $state, $timeout,
-        projectService, paginationService
+        projectService, paginationService, colorCorrectService, mapService
     ) {
         'ngInject';
         $rootScope.autoInject(this, arguments);
@@ -56,52 +59,118 @@ class LayerCorrectionsController {
         this.selections = new Set();
         this.fetchScenesPage();
         this.initCorrectionHandlers();
+        this.setMapLayers();
     }
 
     $onDestroy() {
+        this.removeMapLayers();
+    }
 
+    getMap() {
+        return this.mapService.getMap('project');
+    }
+
+    setMapLayers() {
+        let mapLayer = this.projectService.mapLayerFromLayer(this.project, this.layer);
+        return this.getMap().then(map => {
+            map.setLayer(mapLayerName, mapLayer, true);
+        });
+    }
+
+    removeMapLayers() {
+        return this.getMap().then(map => {
+            map.deleteLayers(mapLayerName);
+        });
     }
 
     initCorrectionHandlers() {
         this.redGammaOptions = {
             ...baseGammaOptions,
             id: 'redGamma',
-            onEnd: (id, val) => this.onGammaFilterChange(val)
+            onEnd: () => this.persistChanges()
         };
+
         this.greenGammaOptions = {
             ...baseGammaOptions,
             id: 'greenGamma',
-            onEnd: (id, val) => this.onGammaFilterChange(val)
+            onEnd: () => this.persistChanges()
         };
+
         this.blueGammaOptions = {
             ...baseGammaOptions,
             id: 'blueGamma',
-            onEnd: (id, val) => this.onGammaFilterChange(val)
+            onEnd: () => this.persistChanges()
+        };
+
+        this.singleGammaOptions = {
+            ...baseGammaOptions,
+            id: 'singleGamma',
+            onEnd: () => {
+                this.updateGammaFromProxy();
+                this.persistChanges();
+            }
         };
 
         this.saturationOptions = {
             ...baseSaturationOptions,
             id: 'saturation',
-            onEnd: () => this.onFilterChange()
+            onEnd: () => this.persistChanges()
         };
 
         this.alphaOptions = {
             ...alphaOptions,
             id: 'alpha',
-            onEnd: () => this.onFilterChange()
+            onEnd: () => this.persistChanges()
         };
 
         this.betaOptions = {
             ...betaOptions,
             id: 'beta',
-            onEnd: () => this.onFilterChange()
+            onEnd: () => this.persistChanges()
         };
 
-        this.gammaLinkToggle = true;
+        this.gammaLink = false;
     }
 
-    addSceneActions() {
+    ensureGammaValues() {
+        this.ensureValues('gamma', ['redGamma', 'greenGamma', 'blueGamma']);
+        this.updateSingleGammaProxy();
+    }
 
+
+    ensureSigmoidalContrastValues() {
+        this.ensureValues('sigmoidalContrast', ['alpha', 'beta']);
+    }
+
+    ensureSaturationValues() {
+        this.ensureValues('saturation', ['saturation']);
+    }
+
+    // avg(rgb channel values) -> single gamma value
+    updateSingleGammaProxy() {
+        this.singleGammaProxy = ((
+            this.correction.gamma.redGamma +
+            this.correction.gamma.greenGamma +
+            this.correction.gamma.blueGamma
+        ) / 3).toFixed(2);
+    }
+
+    // proxy -> rgb channel values
+    updateGammaFromProxy() {
+        this.correction.gamma.redGamma = this.singleGammaProxy;
+        this.correction.gamma.greenGamma = this.singleGammaProxy;
+        this.correction.gamma.blueGamma = this.singleGammaProxy;
+    }
+
+    ensureValues(type, keys) {
+        const defaults = this.colorCorrectService.getDefaultColorCorrection();
+        keys.forEach(k => {
+            // We use non-strict equality to match on undefined and null
+            // eslint-disable-next-line eqeqeq
+            if (this.correction[type][k] == null) {
+                this.correction[type][k] = defaults[type][k];
+            }
+        });
     }
 
     onSceneSelect(scene) {
@@ -111,6 +180,41 @@ class LayerCorrectionsController {
         } else {
             this.selections = this.selections.add(scene);
         }
+        if (this.selections.size === 1) {
+            this.isLoadingCorrections = true;
+            const baseScene = this.selections.first();
+            this.colorCorrectService
+                .getForLayer(baseScene.id, this.layer.id, this.project.id)
+                .then(correction => {
+                    this.originalCorrection = correction;
+                    this.correction = _.cloneDeep(correction);
+                    this.ensureGammaValues();
+                    this.ensureSaturationValues();
+                    this.ensureSigmoidalContrastValues();
+                    this.persistChanges();
+                })
+                .catch(() => {})
+                .finally(() => {
+                    this.isLoadingCorrections = false;
+                });
+        }
+    }
+
+    onToggleFilter(filterType, value = !this.correction[filterType].enabled) {
+        this.correction[filterType].enabled = value;
+        this.persistChanges();
+    }
+
+    onToggleGammaLink(value = !this.gammaLink) {
+        this.gammaLink = value;
+        if (value) {
+            // toggling the linked mode on
+            this.updateSingleGammaProxy();
+        } else {
+            // toggling the linked mode off
+            this.updateGammaFromProxy();
+        }
+        this.persistChanges();
     }
 
     onToggleSelection() {
@@ -118,6 +222,33 @@ class LayerCorrectionsController {
             this.selections = new Set();
         } else {
             this.selections = new Set(this.sceneList);
+        }
+    }
+
+    onToggleIsCorrecting(value = !this.isCorrecting) {
+        this.isCorrecting = value;
+    }
+
+    onResetAll() {
+        this.correction = this.colorCorrectService.getDefaultColorCorrection();
+        this.persistChanges(this.sceneList);
+    }
+
+    onResetSelection() {
+        this.correction = this.colorCorrectService.getDefaultColorCorrection();
+        this.persistChanges();
+    }
+
+    persistChanges(scenes = this.selections.toArray()) {
+        if (scenes.length) {
+            this.colorCorrectService.bulkUpdateForLayer(
+                this.project.id,
+                this.layer.id,
+                scenes,
+                this.correction
+            ).then(() => {
+                this.setMapLayers();
+            });
         }
     }
 
@@ -153,8 +284,15 @@ class LayerCorrectionsController {
         return currentQuery;
     }
 
-    toggleIsCorrecting(value = !this.isCorrecting) {
-        this.isCorrecting = value;
+    resetCorrection() {
+        const sceneIds = Array.from(this.selectedScenes.keys());
+        const promise = this.colorCorrectService.bulkUpdate(
+            this.projectEditService.currentProject.id,
+            sceneIds
+        );
+        const defaultCorrection = this.colorCorrectService.getDefaultColorCorrection();
+        this.setCorrection(defaultCorrection);
+        this.redrawMosaic(promise, defaultCorrection);
     }
 }
 
