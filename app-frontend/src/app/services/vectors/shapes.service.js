1f814e08bdad67eb47a866513625e661ebe360d7
Aaron Su <xunzes@upenn.edu>
2019-03-05 13:11:52 -0500

    Add Project Layer Export Create UI (#4718)

--- app-frontend/src/app/services/vectors/shapes.service.js
+++ app-frontend/src/app/services/vectors/shapes.service.js
@@ -1,26 +1,31 @@
 /* globals BUILDCONFIG */
-export default (app) => {
+export default app => {
     class ShapesService {
-        constructor($resource) {
-            this.shapeApi = $resource(`${BUILDCONFIG.API_HOST}/api/shapes/:id`, {
-                id: '@properties.id'
-            }, {
-                query: {
-                    method: 'GET',
-                    cache: false
+        constructor(uuid4, $resource) {
+            this.shapeApi = $resource(
+                `${BUILDCONFIG.API_HOST}/api/shapes/:id`,
+                {
+                    id: '@properties.id'
                 },
-                get: {
-                    method: 'GET',
-                    cache: false
-                },
-                delete: {
-                    method: 'DELETE'
-                },
-                create: {
-                    method: 'POST',
-                    isArray: true
+                {
+                    query: {
+                        method: 'GET',
+                        cache: false
+                    },
+                    get: {
+                        method: 'GET',
+                        cache: false
+                    },
+                    delete: {
+                        method: 'DELETE'
+                    },
+                    create: {
+                        method: 'POST',
+                        isArray: true
+                    }
                 }
-            });
+            );
+            this.uuid4 = uuid4;
         }
 
         query(params = {}) {
@@ -37,6 +42,28 @@ export default (app) => {
 
         createShape(shape) {
             return this.shapeApi.create(shape).$promise;
+        }
+
+        generateFeatureCollection(geometryArray, name = '') {
+            return {
+                type: 'FeatureCollection',
+                features: geometryArray.map(geom => this.generateFeature(geom, name))
+            };
+        }
+
+        generateFeature(geometry, name = '') {
+            let result = {
+                type: 'Feature',
+                properties: {},
+                geometry
+            };
+
+            if (name.length) {
+                result.id = this.uuid4.generate();
+                result.properties = { name };
+            }
+
+            return result;
         }
     }
 
