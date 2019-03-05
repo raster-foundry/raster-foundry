e383c6561a7b46a44a708b593c1a4502c6c66862
Lucien Knechtli <lknechtli@gmail.com>
2019-03-05 16:48:23 -0500

    Remove leftover comments

--- app-frontend/src/app/components/common/listItemWidgets/listItemActions/index.js
+++ app-frontend/src/app/components/common/listItemWidgets/listItemActions/index.js
@@ -1,17 +1,13 @@
 import tpl from './index.html';
 
-// class ListItemActionsController {
-// }
 const component = {
     bindings: {
         actions: '<'
     },
     templateUrl: tpl
-    // controller: ListItemActionsController.name
 };
 
 export default angular
     .module('components.common.listItemWidgets.listItemActions', [])
-    // .controller(ListItemActionsController.name, ListItemActionsController)
     .component('rfListItemActions', component)
     .name;
