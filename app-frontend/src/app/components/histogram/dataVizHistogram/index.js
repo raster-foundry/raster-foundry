dce96361b8aad3682fa421470c3d98f9802843b4
Aaron Su <xunzesu@outlook.com>
2019-03-11 19:37:52 -0400

    Update graph service with StatHistogram class

--- app-frontend/src/app/components/histogram/dataVizHistogram/index.js
+++ app-frontend/src/app/components/histogram/dataVizHistogram/index.js
@@ -1,10 +1,10 @@
-import _ from 'lodash';
+import { min, max, range, get } from 'lodash';
 import tpl from './index.html';
 import { Map } from 'immutable';
 
 class DataVizHistogramController {
     constructor(
-        $rootScope, $scope, $log, $q, $element,
+        $rootScope, $scope, $log, $q, $element, $window,
         graphService
     ) {
         'ngInject';
@@ -12,21 +12,52 @@ class DataVizHistogramController {
     }
 
     $postLink() {
+        this.isLoadingHistogram = true;
         this.$q.all({
-            histStat: this.histogram,
-            bounds: this.bounds
-        }).then(({histStat, bounds}) => {
-            const {histogram, statistics} = histStat;
-            this.$scope.$evalAsync(() => this.initGraph(histogram, bounds));
-            this.getGraph().then(graph => {
-                this.createPlotFromHistogram(histogram, bounds, statistics);
-                graph.setData({histogram: this.plot})
-                    .render();
-            });
+            histStat: this.histogramPromise,
+            histBounds: this.boundsPromise
+        }).then(({histStat, histBounds}) => {
+            const { histogram } = histStat;
+            this.histogramData = histogram;
+            this.histogramBounds = histBounds;
+            if (this.histogramData &&
+                this.histogramBounds &&
+                get(this.histogramData, 'buckets.length')
+            ) {
+                this.$scope.$evalAsync(() => {
+                    this.initGraphOptions(this.histogramBounds);
+                    this.visualizeHistogram();
+                });
+            }
+        });
+
+        this.$window.addEventListener('resize', this.onWindowResize.bind(this));
+    }
+
+    $onDestroy() {
+        if (this.onWindowResize) {
+            this.$window.removeEventListener('resize', this.onWindowResize);
+        }
+        this.graphService.deregister(this.id);
+    }
+
+    onWindowResize() {
+        this.getGraph().then(graph => {
+            graph.render();
         });
     }
 
-    initGraph(histogram, bounds) {
+    visualizeHistogram() {
+        this.initGraph();
+        this.getGraph().then(graph => {
+            const plot =
+                this.createPlotFromHistogram(this.histogramData, this.histogramBounds);
+            graph.setData({ histogram: plot }).render();
+            this.isLoadingHistogram = false;
+        });
+    }
+
+    initGraphOptions(bounds) {
         this.options = {
             dataRange: {
                 min: bounds.min,
@@ -44,7 +75,9 @@ class DataVizHistogramController {
                 left: 0
             }
         };
+    }
 
+    initGraph() {
         const elem = this.$element.find('.graph-container svg')[0];
         this.graph = this.graphService.register(elem, this.id, this.options);
     }
@@ -53,35 +86,34 @@ class DataVizHistogramController {
         return this.graphService.getGraph(this.id);
     }
 
-    createPlotFromHistogram(histogram, bounds, statistics) {
-        let diff = (bounds.max - bounds.min) / 100;
-        let magnitude = Math.round(Math.log10(diff));
+    createPlotFromHistogram(histogram, bounds) {
+        const diff = (bounds.max - bounds.min) / 100;
+        const magnitude = Math.round(Math.log10(diff));
         this.precision = Math.pow(10, magnitude);
 
-        let buckets = histogram.buckets;
+        const buckets = histogram.buckets;
         let plot;
         if (buckets.length < 100) {
             let valueMap = new Map();
             buckets.forEach(([bucket, value]) => {
-                let roundedBucket = Math.round(bucket / this.precision) * this.precision;
+                const roundedBucket = Math.round(bucket / this.precision) * this.precision;
                 valueMap = valueMap.set(roundedBucket, value);
             });
-            _.range(0, 100).forEach((mult) => {
-                let key = Math.round(
+            range(0, 100).forEach((mult) => {
+                const key = Math.round(
                     (mult * diff + this.options.dataRange.min) / this.precision
                 ) * this.precision;
-                let value = valueMap.get(key, 0);
+                const value = valueMap.get(key, 0);
                 valueMap = valueMap.set(key, value);
             });
 
             plot = valueMap.entrySeq()
                 .sort(([K1], [K2]) => K1 - K2)
                 .map(([key, value]) => ({
-                    x: _.min([_.max([key, histogram.minimum]), histogram.maximum]),
+                    x: min([max([key, histogram.minimum]), histogram.maximum]),
                     y: value}))
                 .toArray();
         } else {
-            // TODO fix this so it acts like above
             plot = buckets.map((bucket) => {
                 return {
                     x: Math.round(bucket[0]), y: bucket[1]
@@ -89,15 +121,15 @@ class DataVizHistogramController {
             });
         }
 
-        this.plot = plot;
+        return plot;
     }
 }
 
 const component = {
     bindings: {
         id: '<',
-        histogram: '<',
-        bounds: '<'
+        histogramPromise: '<',
+        boundsPromise: '<'
     },
     templateUrl: tpl,
     controller: DataVizHistogramController.name,
