28c19d451d52003e12d1d392eea6b311ca3d8145
James Santucci <james.santucci@gmail.com>
2019-03-18 16:17:13 -0400

    Add map token route parameter to ogc services

--- app-backend/backsplash-server/src/main/scala/com/rasterfoundry/backsplash/services/WcsService.scala
+++ app-backend/backsplash-server/src/main/scala/com/rasterfoundry/backsplash/services/WcsService.scala
@@ -14,21 +14,25 @@ import geotrellis.server.ogc.wcs.params.{
 }
 import geotrellis.server.ogc.wcs.ops.Operations
 import org.http4s._
-import org.http4s.scalaxml._
 import org.http4s.dsl.io._
+import org.http4s.scalaxml._
+
+import com.typesafe.scalalogging.LazyLogging
 
 class WcsService[LayerReader: OgcStore](layers: LayerReader)
-    extends ToOgcStoreOps {
+    extends ToOgcStoreOps
+    with LazyLogging {
 
-  private val urlPrefix = "https://tiles.staging.rasterfoundry.com/"
+  // TODO lookup from environment
+  private val urlPrefix = "https://tiles.staging.rasterfoundry.com"
 
   private def requestToServiceUrl(request: Request[IO]) = {
-    List(urlPrefix, request.scriptName, request.pathInfo).mkString("/")
+    List(urlPrefix, request.scriptName, request.pathInfo).mkString
   }
 
   // Authed so we can piggyback on magic public checks from existing authenticators,
   // and so that if something _can_ provide the params we want, we can still auth
-  val routes = AuthedService[User, IO] {
+  val routes: AuthedService[User, IO] = AuthedService[User, IO] {
     case authedReq @ GET -> Root / UUIDWrapper(projectId) as user =>
       val serviceUrl = requestToServiceUrl(authedReq.req)
       WcsParams(authedReq.req.multiParams) match {
@@ -47,5 +51,21 @@ class WcsService[LayerReader: OgcStore](layers: LayerReader)
               BadRequest("not yet implemented")
           }
       }
+
+    case r @ GET -> Root / UUIDWrapper(_) / "map-token" / UUIDWrapper(_) as user =>
+      logger.debug(s"Request path info to start: ${r.req.pathInfo}")
+      val rewritten = OgcMapTokenRewrite(r)
+      logger.debug(
+        s"Request path info after rewrite: ${rewritten.req.pathInfo}")
+      routes(rewritten).value flatMap {
+        case Some(resp) =>
+          IO.pure { resp }
+        case _ =>
+          NotFound()
+      }
+
+    case r @ _ =>
+      logger.debug(s"Unexpected request: ${r.req.pathInfo}, ${r.req.params}")
+      NotFound()
   }
 }
