9b3841c1d03d3a98858c06a2fd0b87bc5452f843
Alex Kaminsky <akaminsky@azavea.com>
2019-03-21 23:58:42 -0400

    Implement project embed page

--- app-frontend/src/app/components/pages/project/settings/embed/index.js
+++ app-frontend/src/app/components/pages/project/settings/embed/index.js
@@ -1,18 +1,23 @@
+/* global BUILDCONFIG  */
 import tpl from './index.html';
 import { Set } from 'immutable';
+import _ from 'lodash';
 
 class ProjectEmbedController {
     constructor(
-        $rootScope, $state,
-        projectService, paginationService
+        $rootScope, $state, $timeout,
+        projectService, analysisService, paginationService
     ) {
         'ngInject';
         $rootScope.autoInject(this, arguments);
     }
 
     $onInit() {
+        this.BUILDCONFIG = BUILDCONFIG;
         this.initEmbedParameters();
+        this.embedUrl = '';
         this.selectedLayers = new Set();
+        this.layerAnalyses = {};
         this.fetchPage();
     }
 
@@ -69,45 +74,138 @@ class ProjectEmbedController {
         return currentQuery;
     }
 
+    fetchAnalysesForLayer(layer, page = 1) {
+        this.layerAnalyses[layer.id] = {
+            isLoading: true,
+            items: [],
+            pagination: {}
+        };
+
+        const currentQuery = this.analysisService.fetchAnalyses(
+            {
+                pageSize: 30,
+                page: page - 1,
+                projectLayerId: layer.id
+            }
+        ).then((paginatedAnalyses) => {
+            this.layerAnalyses[layer.id] = {
+                isLoading: false,
+                items: paginatedAnalyses.results,
+                pagination: this.paginationService.buildPagination(paginatedAnalyses)
+            };
+        }).catch((e) => {
+            this.fetchError = e;
+        });
+    }
+
     onSelect(layer) {
         const without = this.selectedLayers.filter(i => i.id !== layer.id);
         if (without.size !== this.selectedLayers.size) {
             this.selectedLayers = without;
+            this.onEmbedLayerToggle(layer, false);
         } else {
             this.selectedLayers = this.selectedLayers.add(layer);
+            this.fetchAnalysesForLayer(layer);
         }
-        this.updateEmbedParams();
+        this.updateEmbedUrl();
     }
 
-    onEmbedLayerToggle(layer) {
-        const without = this.embedParameters.layers.filter(i => i.id !== layer.id);
-        if (without.size !== this.embedParameters.layers.size) {
+    onEmbedLayerToggle(layer, value = !this.embedParameters.layers.has(layer)) {
+        const { pane1, pane2, layers } = this.embedParameters;
+        const without = layers.filter(i => i.id !== layer.id);
+        if (!value) {
             this.embedParameters.layers = without;
+            if (pane1 === layer.id) {
+                this.embedParameters.pane1 = this.someLayerIdOrEmptyString();
+            }
+            if (pane2 === layer.id) {
+                this.embedParameters.pane2 = this.someLayerIdOrEmptyString();
+            }
         } else {
-            this.embedParameters.layers = this.embedParameters.layers.add(layer);
+            this.embedParameters.layers = layers.add(layer);
+            this.fillPanes(layer.id);
         }
-        this.updateEmbedParams();
+        this.updateEmbedUrl();
     }
 
-    onEmbedAnalysisToggle(analysis) {
+    onEmbedAnalysisToggle(analysis, value = !this.embedParameters.analyses.has(analysis)) {
+        const { pane1, pane2, analyses } = this.embedParameters;
         const without = this.embedParameters.analyses.filter(i => i.id !== analysis.id);
-        if (without.size !== this.embedParameters.analyses.size) {
+        if (!value) {
             this.embedParameters.analyses = without;
+            if (pane1 === analysis.id) {
+                this.embedParameters.pane1 = this.someLayerIdOrEmptyString();
+            }
+            if (pane2 === analysis.id) {
+                this.embedParameters.pane2 = this.someLayerIdOrEmptyString();
+            }
         } else {
             this.embedParameters.analyses = this.embedParameters.analyses.add(analysis);
+            this.fillPanes(analysis.id);
         }
-        this.updateEmbedParams();
+        this.updateEmbedUrl();
+    }
+
+    onEmbedTypeChange(type) {
+        this.embedParameters.type = type;
+        this.fillPanes();
+        this.updateEmbedUrl();
+    }
+
+    fillPanes(layerId = '') {
+        const { pane1, pane2 } = this.embedParameters;
+        if (!pane1) {
+            this.embedParameters.pane1 = layerId || this.someLayerIdOrEmptyString();
+        }
+        if (!pane2) {
+            this.embedParameters.pane2 = layerId || this.someLayerIdOrEmptyString();
+        }
     }
 
     isSelected(layer) {
         return this.selectedLayers.has(layer);
     }
 
-    updateEmbedParams(params) {
-        this.embedParameters = {
-            ...this.embedParameters,
-            ...params
-        };
+    hasValidParameters() {
+        return !!this.embedParameters.pane1 && (
+            this.embedParameters.type === 'single' || !!this.embedParameters.pane2
+        );
+    }
+
+    someLayerIdOrEmptyString() {
+        const { layers, analyses } = this.embedParameters;
+        if (layers.size) {
+            return layers.first().id;
+        } else if (analyses.size) {
+            return analyses.first().id;
+        }
+        return '';
+    }
+
+    updateEmbedUrl() {
+        const base = this.BUILDCONFIG.EMBED_URI;
+        if (this.hasValidParameters()) {
+            const src = `${base}?${this.embedParametersToString(this.embedParameters)}`;
+            // eslint-disable-next-line max-len
+            this.embedUrl = `<iframe width="480" height="270" src="${src}" frameborder="0" allow=“full-screen;”></iframe>`;
+        } else {
+            this.embedUrl = '';
+        }
+    }
+
+    embedParametersToString(params) {
+        const p = { ...params };
+        if (p.type === 'single') {
+            p.pane2 = '';
+        }
+        const kvTransform = (k, v) => `${k}=${v}`;
+        const defaultTransform = k => kvTransform(k, p[k]);
+        const setTransform = k => kvTransform(k, p[k].toJS().map(i => i.id).join(','));
+        const setParameters = ['layers', 'analyses'];
+        const stringifiedParams = Object.keys(p).map(k => setParameters.includes(k) ?
+            setTransform(k) :
+            defaultTransform(k)).join('&');
+        return stringifiedParams;
     }
 }
 
