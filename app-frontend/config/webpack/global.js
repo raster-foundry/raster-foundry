718dc4c326a0aa9e729a6f3828ed858dce61c1ff
Alex Kaminsky <akaminsky@azavea.com>
2019-03-22 09:59:39 -0400

    Remove test from global.js

--- app-frontend/config/webpack/global.js
+++ app-frontend/config/webpack/global.js
@@ -322,7 +322,7 @@ module.exports = function (_path) {
                     MAP_CENTER: JSON.stringify([-6.8, 39.2]),
                     MAP_ZOOM: 5,
                     PLATFORM_USERS: PLATFORM_USERS,
-                    EMBED_URI: JSON.stringify('testing')
+                    EMBED_URI: JSON.stringify('')
                 },
                 'HELPCONFIG': {
                     API_DOCS_URL: JSON.stringify('https://docs.rasterfoundry.com/'),
