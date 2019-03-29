d6e1b1b13688cf319491530967b63f9ed74001cb
Alex Kaminsky <akaminsky@azavea.com>
2019-03-29 16:08:58 -0400

    Update index.js

--- app-frontend/src/app/components/pages/project/layers/index.js
+++ app-frontend/src/app/components/pages/project/layers/index.js
@@ -384,8 +384,7 @@ class ProjectLayersPageController {
                         <p>
                             At least one selected layer does not have an AOI defined.
                             Click on the missing AOI warning icon
-                            (<i class="icon-warning color-danger"></i>)
-                            on selected layers to define an AOI.
+                            (<i class="icon-warning color-danger"></i>) to define an AOI.
                         </p>
                     `,
                     feedbackIconType: () => 'warning',
