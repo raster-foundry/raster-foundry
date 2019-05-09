4a637343e183fae0b453171a78039b127f4426c7
James Santucci <james.santucci@gmail.com>
2019-05-09 13:22:18 -0400

    Get project layer scene counts

--- app-backend/batch/src/main/scala/cogMetadata/OverviewBackfill.scala
+++ app-backend/batch/src/main/scala/cogMetadata/OverviewBackfill.scala
@@ -3,22 +3,31 @@ package com.rasterfoundry.batch.cogMetadata
 import com.rasterfoundry.batch.Job
 import com.rasterfoundry.common.RollbarNotifier
 import com.rasterfoundry.datamodel.ProjectLayer
+import com.rasterfoundry.database.ProjectLayerDao
 
 import cats.effect.IO
-import doobie.{ConnectionIO, Transactor}
+import doobie._
 import doobie.implicits._
+import doobie.postgres.implicits._
 import fs2.Stream
 
 object OverviewBackfill extends Job with RollbarNotifier {
   val name = "backfill-layer-overviews"
   val xa: Transactor[IO] = ???
 
-  def getProjectLayerSceneCount(projectLayer: ProjectLayer): Stream[ConnectionIO, (ProjectLayer, Int)] =
-    ???
+  def getProjectLayerSceneCount(
+      projectLayer: ProjectLayer
+  ): Stream[ConnectionIO, (ProjectLayer, Int)] =
+    fr"select count(1) from scenes_to_layers where project_layer_id = ${projectLayer.id}"
+      .query[Int]
+      .stream map { (projectLayer, _) }
 
   def kickoffOverviewGeneration(projectLayer: ProjectLayer): IO[Unit] = ???
 
-  val projectLayers: Stream[ConnectionIO, ProjectLayer] = ???
+  // Giant number inside listQ is because listQ needs a limit parameter, but we don't actually
+  // want to limit
+  val projectLayers: Stream[ConnectionIO, ProjectLayer] =
+    ProjectLayerDao.query.listQ(1000000).stream
   val projectLayersWithSceneCounts: Stream[ConnectionIO, (ProjectLayer, Int)] =
     projectLayers.flatMap(getProjectLayerSceneCount)
 
@@ -28,7 +37,7 @@ object OverviewBackfill extends Job with RollbarNotifier {
         case (_, n) => n <= 300
       })
       .transact(xa)
-      .parEvalMap(15)({
+      .parEvalMap(20)({
         case (projectLayer, _) =>
           kickoffOverviewGeneration(projectLayer)
       })
