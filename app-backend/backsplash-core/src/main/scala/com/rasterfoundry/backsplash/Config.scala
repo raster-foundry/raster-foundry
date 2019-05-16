53fdd83d50196939bc16a08533d740d408fca1cd
James Santucci <james.santucci@gmail.com>
2019-05-16 10:00:20 -0400

    Remove unused config

--- app-backend/backsplash-core/src/main/scala/com/rasterfoundry/backsplash/Config.scala
+++ app-backend/backsplash-core/src/main/scala/com/rasterfoundry/backsplash/Config.scala
@@ -43,11 +43,6 @@ object Config {
       cacheConfig.getInt("core.memcachedTimeoutMilliseconds")
   }
 
-  object s3 {
-    private val s3Config = config.getConfig("s3")
-    val dataBucket = s3Config.getString("dataBucket")
-  }
-
   object tuning {
     private val tuningConfig = config.getConfig("tuning")
     val overviewThreshold = tuningConfig.getInt("overviewThreshold")
