937ce792570105570a17068f97fe3e62e17d7c23
Alex Kaminsky <alexander.s.kaminsky@gmail.com>
2019-05-21 08:34:00 -0400

    Add all entities as object types (#4992)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,9 @@
 ## [Unreleased](https://github.com/raster-foundry/raster-foundry/tree/develop)
 
 ### Added
+
+- Added `ActionType` for `CREATE` [\#4992](https://github.com/raster-foundry/raster-foundry/pull/4992)
+- Added `ObjectType` for all entities [\#4992](https://github.com/raster-foundry/raster-foundry/pull/4992)
 - Added job to backfill low-zoom overviews for existing project layers [\#4970](https://github.com/raster-foundry/raster-foundry/pull/4970)
 - Convert BacksplashImage to a trait [\#4952](https://github.com/raster-foundry/raster-foundry/pull/4952)
 - Kickoff project layer overview generation reactively [\#4936](https://github.com/raster-foundry/raster-foundry/pull/4936)
--- app-backend/datamodel/src/main/scala/ObjectType.scala
+++ app-backend/datamodel/src/main/scala/ObjectType.scala
@@ -15,16 +15,40 @@ object ObjectType {
   case object Workspace extends ObjectType("WORKSPACE")
   case object Template extends ObjectType("TEMPLATE")
   case object Analysis extends ObjectType("ANALYSIS")
+  case object Platform extends ObjectType("PLATFORM")
+  case object Organization extends ObjectType("ORGANIZATION")
+  case object Team extends ObjectType("TEAM")
+  case object User extends ObjectType("USER")
+  case object Upload extends ObjectType("UPLOAD")
+  case object Export extends ObjectType("EXPORT")
+  case object Feed extends ObjectType("FEED")
+  case object MapToken extends ObjectType("MAPTOKEN")
+  case object License extends ObjectType("LICENSE")
+  case object ToolTag extends ObjectType("TOOLTAG")
+  case object ToolCategory extends ObjectType("TOOLCATEGORY")
+  case object AOI extends ObjectType("AOI")
 
   def fromString(s: String): ObjectType = s.toUpperCase match {
-    case "PROJECT"    => Project
-    case "SCENE"      => Scene
-    case "DATASOURCE" => Datasource
-    case "SHAPE"      => Shape
-    case "WORKSPACE"  => Workspace
-    case "TEMPLATE"   => Template
-    case "ANALYSIS"   => Analysis
-    case _            => throw new Exception(s"Invalid ObjectType: ${s}")
+    case "PROJECT"      => Project
+    case "SCENE"        => Scene
+    case "DATASOURCE"   => Datasource
+    case "SHAPE"        => Shape
+    case "WORKSPACE"    => Workspace
+    case "TEMPLATE"     => Template
+    case "ANALYSIS"     => Analysis
+    case "PLATFORM"     => Platform
+    case "ORGANIZATION" => Organization
+    case "TEAM"         => Team
+    case "USER"         => User
+    case "UPLOAD"       => Upload
+    case "EXPORT"       => Export
+    case "FEED"         => Feed
+    case "MAPTOKEN"     => MapToken
+    case "LICENSE"      => License
+    case "TOOLTAG"      => ToolTag
+    case "TOOLCATEGORY" => ToolCategory
+    case "AOI"          => AOI
+    case _              => throw new Exception(s"Invalid ObjectType: ${s}")
   }
 
   implicit val ObjectTypeEncoder: Encoder[ObjectType] =
