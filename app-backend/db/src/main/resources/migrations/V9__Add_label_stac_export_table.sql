d2d643057a0f8b80bc592c4ec5ef7523bdec1b42
Aaron Su <xunzesu@outlook.com>
2019-07-09 16:23:45 -0400

    Add a migration for stac_exports table

---
+++ app-backend/db/src/main/resources/migrations/V9__Add_label_stac_export_table.sql
@@ -0,0 +1,16 @@
+-- Add a table for label stac export
+
+CREATE TABLE public.label_stac_exports (
+    id uuid NOT NULL,
+    created_at timestamp without time zone NOT NULL,
+    created_by character varying(255) NOT NULL REFERENCES public.users(id) ON DELETE CASCADE,
+    modified_at timestamp without time zone NOT NULL,
+    modified_by character varying(255) REFERENCES public.users(id) ON DELETE SET NULL,
+    owner character varying(255) NOT NULL REFERENCES public.users(id) ON DELETE CASCADE,
+    name character varying(255) NOT NULL,
+    export_location text,
+    export_status public.export_status NOT NULL,
+    layer_definition jsonb NOT NULL,
+    is_union boolean NOT NULL DEFAULT false,
+    task_statuses text[] NOT NULL DEFAULT ARRAY['VALIDATED']::text[]
+);
