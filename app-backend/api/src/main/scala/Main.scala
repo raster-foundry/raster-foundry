8c6ab2222218c9aead7974616a231f0ffaad350e
Christopher Brown <cbrown@azavea.com>
2019-09-24 09:48:40 -0400

    Supply context shifts for doobie
    The previous commit now requires that an implicit context shift must
    be in scope when creating a transactor.

--- app-backend/api/src/main/scala/Main.scala
+++ app-backend/api/src/main/scala/Main.scala
@@ -1,12 +1,15 @@
 package com.rasterfoundry.api
 
+import java.util.concurrent.Executors
+
 import akka.actor.{ActorSystem, Terminated}
 import akka.http.scaladsl.Http
 import akka.stream.ActorMaterializer
+import cats.effect.{ContextShift, IO}
+import com.google.common.util.concurrent.ThreadFactoryBuilder
 import com.rasterfoundry.akkautil.RFRejectionHandler._
 import com.rasterfoundry.api.utils.Config
 import com.rasterfoundry.database.util.RFTransactor
-
 import doobie.implicits._
 
 import scala.concurrent.{ExecutionContext, Future}
@@ -20,6 +23,15 @@ object Main extends App with Config with Router {
 
   implicit val system = AkkaSystem.system
   implicit val materializer = AkkaSystem.materializer
+
+  implicit val contextShift: ContextShift[IO] =
+    IO.contextShift(
+      ExecutionContext.fromExecutor(
+        Executors.newCachedThreadPool(
+          new ThreadFactoryBuilder().setNameFormat("db-transactor-%d").build()
+        )
+      )
+    )
 
   val xa = RFTransactor.buildTransactor()
   implicit val ec = ExecutionContext.Implicits.global
