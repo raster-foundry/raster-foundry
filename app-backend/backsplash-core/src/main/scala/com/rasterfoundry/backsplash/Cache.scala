61aebba253ab203f4f9c5588541972e69cd280e2
Lucien Knechtli <lknechtli@gmail.com>
2019-09-24 14:54:44 -0400

    Remove caching for RasterSource and Tiles
    Remove references to tile and raster source cache configuration

--- app-backend/backsplash-core/src/main/scala/com/rasterfoundry/backsplash/Cache.scala
+++ app-backend/backsplash-core/src/main/scala/com/rasterfoundry/backsplash/Cache.scala
@@ -1,23 +1,11 @@
 package com.rasterfoundry.backsplash
 
 import com.typesafe.scalalogging.LazyLogging
-import com.rasterfoundry.common.{Config => CommonConfig}
-import geotrellis.contrib.vlm.RasterSource
-import geotrellis.raster.MultibandTile
 import geotrellis.raster.histogram.Histogram
 import geotrellis.vector.{MultiPolygon, Point}
 import org.locationtech.spatial4j.io.GeohashUtils
 import scalacache._
 import scalacache.caffeine._
-import scalacache.memcached._
-import scalacache.memoization._
-import scalacache.serialization.binary._
-import net.spy.memcached._
-import java.net.InetSocketAddress
-
-import com.rasterfoundry.common.BacksplashConnectionFactory
-
-import scala.collection.JavaConverters._
 
 object Cache extends LazyLogging {
 
@@ -42,44 +30,10 @@ object Cache extends LazyLogging {
     def stringToCacheKey(key: String) = key
   }
 
-  val tileCache: Cache[Option[MultibandTile]] = {
-
-    val address = new InetSocketAddress(CommonConfig.memcached.host,
-                                        CommonConfig.memcached.port)
-    val memcachedClient =
-      new MemcachedClient(new BacksplashConnectionFactory, List(address).asJava)
-
-    implicit val cacheConfig: CacheConfig = CacheConfig(
-      cacheKeyBuilder = new BacksplashCacheKeyBuilder,
-      memoization = MemoizationConfig(
-        MethodCallToStringConverter.includeClassConstructorParams)
-    )
-
-    MemcachedCache[Option[MultibandTile]](memcachedClient)
-  }
-
-  val tileCacheFlags =
-    Flags(Config.cache.tileCacheEnable, Config.cache.tileCacheEnable)
-  logger.info(s"Tile Cache Status: ${tileCacheFlags}")
-
   val histCache: Cache[Array[Histogram[Double]]] =
     CaffeineCache[Array[Histogram[Double]]]
 
   val histCacheFlags =
     Flags(Config.cache.histogramCacheEnable, Config.cache.histogramCacheEnable)
   logger.info(s"Histogram Cache Status: ${histCacheFlags}")
-
-  val rasterSourceCache: Cache[RasterSource] = {
-    implicit val cacheConfig: CacheConfig = CacheConfig(
-      memoization = MemoizationConfig(
-        MethodCallToStringConverter.includeClassConstructorParams)
-    )
-    CaffeineCache[RasterSource]
-  }
-
-  val rasterSourceCacheFlags: Flags = Flags(
-    Config.cache.rasterSourceCacheEnable,
-    Config.cache.rasterSourceCacheEnable)
-  logger.info(s"Raster Source Cache Status: ${rasterSourceCacheFlags}")
-
 }
