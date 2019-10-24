276086471a6dccb9ccf5e5cb2c0c0cda9e341b63
James Santucci <james.santucci@gmail.com>
2019-10-24 08:48:37 -0500

    Remove AOI batch jobs

--- app-backend/batch/src/main/scala/Main.scala
+++ app-backend/batch/src/main/scala/Main.scala
@@ -13,14 +13,12 @@ import com.rasterfoundry.batch.geojsonImport.ImportGeojsonFiles
 object Main {
   val modules = Map[String, Array[String] => Unit](
     CreateExportDef.name -> (CreateExportDef.main(_)),
-    FindAOIProjects.name -> (FindAOIProjects.main(_)),
     HealthCheck.name -> (HealthCheck.main(_)),
     HistogramBackfill.name -> (HistogramBackfill.main(_)),
     RasterSourceMetadataBackfill.name -> (RasterSourceMetadataBackfill.main(_)),
     NotifyIngestStatus.name -> (NotifyIngestStatus.main(_)),
     ReadStacFeature.name -> (ReadStacFeature.main(_)),
     WriteStacCatalog.name -> (WriteStacCatalog.main(_)),
-    UpdateAOIProject.name -> (UpdateAOIProject.main(_)),
     UpdateExportStatus.name -> (UpdateExportStatus.main(_)),
     ImportGeojsonFiles.name -> (ImportGeojsonFiles.main(_)),
   )
