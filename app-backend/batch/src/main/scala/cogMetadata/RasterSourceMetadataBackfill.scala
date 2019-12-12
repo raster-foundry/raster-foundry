3d4fd98fb1e01de6092ce1052b9275323685f7b0
Lucien Knechtli <lknechtli@gmail.com>
2019-12-12 08:59:03 -0500

    support uuid list in args

--- app-backend/batch/src/main/scala/cogMetadata/RasterSourceMetadataBackfill.scala
+++ app-backend/batch/src/main/scala/cogMetadata/RasterSourceMetadataBackfill.scala
@@ -26,7 +26,8 @@ object RasterSourceMetadataBackfill extends Job with RollbarNotifier {
   val name = "rastersource-metadata-backfill"
 
   def getScenesToBackfill(
-      implicit xa: Transactor[IO]): IO[List[(UUID, String)]] = {
+      implicit xa: Transactor[IO]
+  ): IO[List[(UUID, String)]] = {
     logger.info("Finding COG scenes without metadata in layer_attributes")
     fr"""select
            id, ingest_location
@@ -44,13 +45,37 @@ object RasterSourceMetadataBackfill extends Job with RollbarNotifier {
     }
   }
 
+  def getSceneLocations(ids: List[UUID])(
+      implicit xa: Transactor[IO]
+  ): IO[List[(UUID, String)]] = {
+    logger.info("Finding scenes in arg list")
+
+    ids traverse { id: UUID =>
+      {
+        SceneDao.unsafeGetSceneById(id).transact(xa) map { scene =>
+          scene.ingestLocation match {
+            case Some(ig) => Some((scene.id, ig))
+            case _        => None
+          }
+        }
+      }
+    } map (_.flatten) map { tuples =>
+      {
+        logger.info(s"Found ${tuples.length} scenes to get metadata for")
+        tuples
+      }
+    }
+  }
+
   def getBacksplashInfo(
       id: UUID,
-      path: String): IO[Either[Throwable, BacksplashGeoTiffInfo]] = {
+      path: String
+  ): IO[Either[Throwable, BacksplashGeoTiffInfo]] = {
     IO(BacksplashGeotiffReader.getBacksplashGeotiffInfo(path)).attempt map {
       case l @ Left(_) =>
         logger.error(
-          s"""Scene(id='$id'): Failed to fetch geotiff for path "$path".""")
+          s"""Scene(id='$id'): Failed to fetch geotiff for path "$path"."""
+        )
         l
       case r => r
     }
@@ -58,10 +83,11 @@ object RasterSourceMetadataBackfill extends Job with RollbarNotifier {
 
   // presence of the ingest location is guaranteed by the filter in the sql string
   @SuppressWarnings(Array("OptionGet"))
-  def insertRasterSourceMetadata(id: UUID,
-                                 path: String,
-                                 backsplashInfo: BacksplashGeoTiffInfo)(
-      implicit xa: Transactor[IO]): IO[Int] = {
+  def insertRasterSourceMetadata(
+      id: UUID,
+      path: String,
+      backsplashInfo: BacksplashGeoTiffInfo
+  )(implicit xa: Transactor[IO]): IO[Int] = {
     val rasterSourceMetadata = RasterSourceMetadata(
       GDALDataPath(path),
       backsplashInfo.crs,
@@ -79,17 +105,31 @@ object RasterSourceMetadataBackfill extends Job with RollbarNotifier {
   }
 
   def insertGeotiffInfo(id: UUID, backsplashInfo: BacksplashGeoTiffInfo)(
-      implicit xa: Transactor[IO]): IO[Int] = {
+      implicit xa: Transactor[IO]
+  ): IO[Int] = {
     logger.info(s"Getting Metadata: ${id}")
     SceneDao.updateSceneGeoTiffInfo(backsplashInfo, id).transact(xa)
   }
 
   def runJob(args: List[String]) = {
-    val threads = args.headOption.map(Integer.parseInt(_)).getOrElse(4)
+    // val threads = args.headOption.map(Integer.parseInt(_)).getOrElse(4)
+    val (threads: Int, ids: List[UUID]) = args match {
+      case Nil => (4, List.empty)
+      case (threads: String) :: ids =>
+        (threads.toInt, ids.map(UUID.fromString(_)))
+    }
 
     RFTransactor.xaResource.use { transactor =>
       implicit val xa = transactor
-      val scenesToUpdate = getScenesToBackfill(xa).unsafeRunSync()
+
+      val idsAndLocations: List[(UUID, String)] = for {
+        idLocationTuples <- ids match {
+          case Nil    => getScenesToBackfill(xa).unsafeRunSync
+          case idsNel => getSceneLocations(idsNel)(xa).unsafeRunSync
+        }
+      } yield idLocationTuples
+
+      // val scenesToUpdate = getScenesToBackfill(xa).unsafeRunSync()
 
       val rasterIO: ContextShift[IO] = IO.contextShift(
         ExecutionContext.fromExecutor(
@@ -97,16 +137,18 @@ object RasterSourceMetadataBackfill extends Job with RollbarNotifier {
             threads,
             new ThreadFactoryBuilder().setNameFormat("backfill-%d").build()
           )
-        ))
+        )
+      )
 
       implicit val contextShift: ContextShift[IO] = rasterIO
 
-      val metadataIOs = scenesToUpdate.parTraverse {
+      val metadataIOs = idsAndLocations.parTraverse {
         case (id, uri) =>
           for {
             backsplashInfo <- getBacksplashInfo(id, uri)
             rmdResult <- backsplashInfo.traverse(
-              insertRasterSourceMetadata(id, uri, _))
+              insertRasterSourceMetadata(id, uri, _)
+            )
             geotiffResult <- backsplashInfo.traverse(insertGeotiffInfo(id, _))
           } yield (rmdResult, geotiffResult)
       }
@@ -120,7 +162,8 @@ object RasterSourceMetadataBackfill extends Job with RollbarNotifier {
             }
           if (lefts.length > 0) {
             logger.error(
-              s"Failed to insert metadata for ${lefts.length} resources")
+              s"Failed to insert metadata for ${lefts.length} resources"
+            )
           }
           logger.info(s"Inserted metadata for ${rights.length} resources")
         })
