4742949bc468faf5e8891809f01b9f9e2e3d96d8
James Santucci <james.santucci@gmail.com>
2019-12-27 11:23:42 -0600

    Push limits down to actions

--- app-backend/datamodel/src/main/scala/Scope.scala
+++ app-backend/datamodel/src/main/scala/Scope.scala
@@ -2,23 +2,20 @@ package com.rasterfoundry.datamodel
 
 import cats.{Eq, Monoid}
 
+case class Action(domain: String, action: String, limit: Option[Long])
+
 sealed abstract class Scope {
-  val actions: Set[String]
+  val actions: Set[Action]
 }
 
-sealed abstract class QuantitativeLimit {
-  val limit: Option[Long]
-}
-
-sealed class SimpleScope(val actions: Set[String]) extends Scope
-object SimpleScope {
-  private[datamodel] def apply(actions: String*): SimpleScope =
-    new SimpleScope(actions.toSet)
-}
+sealed class SimpleScope(
+    val actions: Set[Action]
+) extends Scope
 
 sealed class ComplexScope(scopes: Set[Scope]) extends Scope {
   val actions = scopes flatMap { _.actions }
 }
+
 object ComplexScope {
   private[datamodel] def apply(scopes: Scope*): ComplexScope =
     new ComplexScope(scopes.toSet)
@@ -41,65 +38,87 @@ object Scope {
 
 object Scopes {
 
-  private def makeCRUDScopes(prefix: String): Set[String] =
-    Set(":read", ":create", ":update", ":delete") map { s =>
-      prefix ++ s
+  private def makeCRUDActions(domain: String): Set[Action] =
+    Set("read", "create", "update", "delete") map { s =>
+      makeAction(domain, s)
     }
+
+  private def makeAction(
+      domain: String,
+      action: String,
+      limit: Option[Long] = None
+  ): Action =
+    Action(domain, action, limit)
 
   case object NoAccess extends SimpleScope(Set.empty)
 
   case object Uploader
       extends SimpleScope(
-        Set("uploads:read", "uploads:create", "uploads:delete")
+        Set("read", "create", "delete") map { makeAction("uploads", _) }
       )
 
   case object UploadsCRUD
-      extends ComplexScope(Set(Uploader, SimpleScope("uploads:delete")))
+      extends ComplexScope(
+        Set(
+          Uploader,
+          new SimpleScope(Set(makeAction("uploads", "delete")))
+        )
+      )
 
-  case object ScenesCRUD extends SimpleScope(makeCRUDScopes("scenes"))
+  case object ScenesCRUD extends SimpleScope(makeCRUDActions("scenes"))
 
-  case object ScenesMultiPlayer extends SimpleScope(Set("scenes:share"))
+  case object ScenesMultiPlayer
+      extends SimpleScope(Set(makeAction("scenes", "share")))
 
   case object ScenesFullAccess
       extends ComplexScope(Set(ScenesCRUD, ScenesMultiPlayer))
 
-  case object ProjectsCRUD extends SimpleScope(makeCRUDScopes("projects"))
+  case object ProjectsCRUD extends SimpleScope(makeCRUDActions("projects"))
 
   case object ProjectExport
-      extends SimpleScope(Set("projects:listExports", "projects:createExport"))
+      extends SimpleScope(Set("listExports", "createExport") map {
+        makeAction("projects", _)
+      })
 
   case object ProjectAnnotate
       extends SimpleScope(
         Set(
-          "projects:createAnnotation",
-          "projects:deleteAnnotation",
-          "projects:editAnnotation"
-        )
+          "createAnnotation",
+          "deleteAnnotation",
+          "editAnnotation"
+        ) map {
+          makeAction("projects", _)
+        }
       )
 
-  case object DatasourcesCRUD extends SimpleScope(makeCRUDScopes("datasources"))
+  case object DatasourcesCRUD
+      extends SimpleScope(makeCRUDActions("datasources"))
 
-  case object ProjectsMultiPlayer extends SimpleScope(Set("projects:share"))
+  case object ProjectsMultiPlayer
+      extends SimpleScope(Set(makeAction("projects", "share")))
 
   case object ProjectsFullAccess
       extends ComplexScope(
         Set(ProjectsCRUD, ProjectsMultiPlayer, ProjectExport, ProjectAnnotate)
       )
 
-  case object ShapesCRUD extends SimpleScope(makeCRUDScopes("shapes"))
+  case object ShapesCRUD extends SimpleScope(makeCRUDActions("shapes"))
 
-  case object ShapesMultiPlayer extends SimpleScope(Set("shapes:share"))
+  case object ShapesMultiPlayer
+      extends SimpleScope(Set(makeAction("shapes", "share")))
 
   case object ShapesFullAccess
       extends ComplexScope(Set(ShapesCRUD, ShapesMultiPlayer))
 
-  case object TemplatesCRUD extends SimpleScope(makeCRUDScopes("templates"))
-  case object TemplatesMultiPlayer extends SimpleScope(Set("templates:share"))
+  case object TemplatesCRUD extends SimpleScope(makeCRUDActions("templates"))
+  case object TemplatesMultiPlayer
+      extends SimpleScope(Set(makeAction("templates", "share")))
   case object TemplatesFullAccess
       extends ComplexScope(Set(TemplatesCRUD, TemplatesMultiPlayer))
 
-  case object AnalysesCRUD extends SimpleScope(makeCRUDScopes("analyses"))
-  case object AnalysesMultiPlayer extends SimpleScope(Set("analyses:share"))
+  case object AnalysesCRUD extends SimpleScope(makeCRUDActions("analyses"))
+  case object AnalysesMultiPlayer
+      extends SimpleScope(Set(makeAction("analyses", "share")))
   case object AnalysesFullAccess
       extends ComplexScope(Set(AnalysesCRUD, AnalysesMultiPlayer))
 
@@ -115,16 +134,48 @@ object Scopes {
           Uploader,
           // Regular users can view their teams and organizations, but can do nothing
           // else in that domain
-          SimpleScope("teams:read", "organizations:read")
+          new SimpleScope(Set("team", "organizations") map {
+            makeAction(_, "read")
+          })
+        )
+      )
+
+  case object TeamsUserAdmin
+      extends SimpleScope(
+        Set("addUser", "removeUser", "promoteUser") map {
+          makeAction("teams", _)
+        }
+      )
+
+  case object TeamsEdit
+      extends SimpleScope(
+        Set("edit", "delete") map {
+          makeAction("teams", _)
+        }
+      )
+
+  case object RasterFoundryTeamAdmin
+      extends ComplexScope(
+        Set(
+          RasterFoundryUser,
+          TeamsUserAdmin,
+          TeamsEdit
         )
       )
 
   // TODO
-  case object RasterFoundryTeamAdmin extends ComplexScope(Set.empty)
+  case object RasterFoundryOrganizationAdmin
+      extends ComplexScope(
+        Set(
+          RasterFoundryTeamAdmin
+        )
+      )
 
   // TODO
-  case object RasterFoundryOrganizationAdmin extends ComplexScope(Set.empty)
-
-  // TODO
-  case object RasterFoundryPlatformAdmin extends ComplexScope(Set.empty)
+  case object RasterFoundryPlatformAdmin
+      extends ComplexScope(
+        Set(
+          RasterFoundryOrganizationAdmin
+        )
+      )
 }
--- app-backend/datamodel/src/test/scala/ScopeSpec.scala
+++ app-backend/datamodel/src/test/scala/ScopeSpec.scala
@@ -7,13 +7,21 @@ import org.typelevel.discipline.scalatest.Discipline
 
 class ScopeSpec extends FunSuite with Discipline {
 
+  implicit val arbAction: Arbitrary[Action] = Arbitrary[Action] {
+    for {
+      domain <- Arbitrary.arbitrary[String]
+      action <- Arbitrary.arbitrary[String]
+      limit <- Arbitrary.arbitrary[Option[Long]]
+    } yield Action(domain, action, limit)
+  }
+
   // Not separating out into a separate object until we have more than
   // one of these. I don't think for the most part we depend on laws holding,
   // but in this case, since it determines user powers, I wanted the extra
   // security
   implicit val arbScope: Arbitrary[Scope] = Arbitrary[Scope] {
     for {
-      actions <- Arbitrary.arbitrary[Set[String]]
+      actions <- Arbitrary.arbitrary[Set[Action]]
     } yield new SimpleScope(actions)
   }
 
