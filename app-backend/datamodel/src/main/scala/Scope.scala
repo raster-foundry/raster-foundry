3ce7fc338cf0b8d41515388ef1861f7af8b82603
James Santucci <james.santucci@gmail.com>
2019-12-27 13:59:27 -0600

    Add codecs + discipline-based codec tests

--- app-backend/datamodel/src/main/scala/Scope.scala
+++ app-backend/datamodel/src/main/scala/Scope.scala
@@ -1,8 +1,32 @@
 package com.rasterfoundry.datamodel
 
 import cats.{Eq, Monoid}
+import cats.implicits._
+import _root_.io.circe.{Decoder, Encoder, Json}
+import scala.util.{Failure, Success, Try}
 
-case class Action(domain: String, action: String, limit: Option[Long])
+case class Action(domain: String, action: String, limit: Option[Long]) {
+  def repr: String =
+    s"$domain:$action" ++ {
+      limit map { lim =>
+        s":$lim"
+      } getOrElse ""
+    }
+}
+
+object Action {
+  private[datamodel] def fromString(s: String): Try[Action] =
+    s.split(":").toList match {
+      case domain :: action :: Nil =>
+        Success(Action(domain, action, None))
+      case domain :: action :: lim :: Nil =>
+        Success(Action(domain, action, Some(lim.toLong)))
+      case result =>
+        Failure(
+          new Exception(s"Cannot build an action from split string: $result")
+        )
+    }
+}
 
 sealed abstract class Scope {
   val actions: Set[Action]
@@ -12,6 +36,11 @@ sealed class SimpleScope(
     val actions: Set[Action]
 ) extends Scope
 
+object SimpleScope {
+  def unapply(simpleScope: SimpleScope): Option[Set[Action]] =
+    Some(simpleScope.actions)
+}
+
 sealed class ComplexScope(scopes: Set[Scope]) extends Scope {
   val actions = scopes flatMap { _.actions }
 }
@@ -19,9 +48,41 @@ sealed class ComplexScope(scopes: Set[Scope]) extends Scope {
 object ComplexScope {
   private[datamodel] def apply(scopes: Scope*): ComplexScope =
     new ComplexScope(scopes.toSet)
+
+  def unapply(complexScope: ComplexScope): Option[Set[Action]] =
+    Some(complexScope.actions)
 }
 
 object Scope {
+
+  private[datamodel] def fromString(s: String): Try[Scope] = s match {
+    case "uploader"                  => Success(Scopes.Uploader)
+    case "analyses:crud"             => Success(Scopes.AnalysesCRUD)
+    case "analyses:multiplayer"      => Success(Scopes.AnalysesMultiPlayer)
+    case "datasources:crud"          => Success(Scopes.DatasourcesCRUD)
+    case "organizations:userAdmin"   => Success(Scopes.OrganizationsUserAdmin)
+    case "projects:exportFullAccess" => Success(Scopes.ProjectExport)
+    case "projects:fullAccess"       => Success(Scopes.ProjectsFullAccess)
+    case "organizations:admin"       => Success(Scopes.RasterFoundryOrganizationAdmin)
+    case "teams:admin"               => Success(Scopes.RasterFoundryTeamAdmin)
+    case "scenes:crud"               => Success(Scopes.ScenesCRUD)
+    case "scenes:multiplayer"        => Success(Scopes.ScenesMultiPlayer)
+    case "shapes:fullAccess"         => Success(Scopes.ShapesFullAccess)
+    case "teams:edit"                => Success(Scopes.TeamsEdit)
+    case "templates:crud"            => Success(Scopes.TemplatesCRUD)
+    case "templates:multiplayer"     => Success(Scopes.TemplatesMultiPlayer)
+    case "uploads:crud"              => Success(Scopes.UploadsCRUD)
+    case s =>
+      (s.split(";").toList match {
+        case List("") => Success(List.empty)
+        case actions =>
+          actions traverse { scopePart =>
+            Action.fromString(scopePart)
+          }
+      }).map { scopeParts =>
+        new SimpleScope(scopeParts.toSet)
+      }
+  }
 
   implicit val eqScope = new Eq[Scope] {
     def eqv(x: Scope, y: Scope): Boolean =
@@ -34,6 +95,42 @@ object Scope {
       ComplexScope(x, y)
   }
 
+  implicit val encScope: Encoder[Scope] = new Encoder[Scope] {
+    def apply(thing: Scope): Json = thing match {
+      case Scopes.Uploader            => Json.fromString("uploader")
+      case Scopes.AnalysesCRUD        => Json.fromString("analyses:crud")
+      case Scopes.AnalysesMultiPlayer => Json.fromString("analyses:multiplayer")
+      case Scopes.DatasourcesCRUD     => Json.fromString("datasources:crud")
+      case Scopes.OrganizationsUserAdmin =>
+        Json.fromString("organizations:userAdmin")
+      case Scopes.ProjectExport      => Json.fromString("projects:exportFullAccess")
+      case Scopes.ProjectsFullAccess => Json.fromString("projects:fullAccess")
+      case Scopes.RasterFoundryOrganizationAdmin =>
+        Json.fromString("organizations:admin")
+      case Scopes.RasterFoundryTeamAdmin => Json.fromString("teams:admin")
+      case Scopes.ScenesCRUD             => Json.fromString("scenes:crud")
+      case Scopes.ScenesMultiPlayer      => Json.fromString("scenes:multiplayer")
+      case Scopes.ShapesFullAccess       => Json.fromString("shapes:fullAccess")
+      case Scopes.TeamsEdit              => Json.fromString("teams:edit")
+      case Scopes.TemplatesCRUD          => Json.fromString("templates:crud")
+      case Scopes.TemplatesMultiPlayer =>
+        Json.fromString("templates:multiplayer")
+      case Scopes.UploadsCRUD => Json.fromString("uploads:crud")
+      case SimpleScope(actions) =>
+        Json.fromString((actions map { action =>
+          action.repr
+        }).mkString(";"))
+      case ComplexScope(actions) =>
+        Json.fromString((actions map { action =>
+          action.repr
+        }).mkString(";"))
+    }
+  }
+
+  implicit val decScope: Decoder[Scope] = Decoder.decodeString.emapTry {
+    (s: String) =>
+      Scope.fromString(s)
+  }
 }
 
 object Scopes {
@@ -142,8 +239,15 @@ object Scopes {
 
   case object TeamsUserAdmin
       extends SimpleScope(
-        Set("addUser", "removeUser", "promoteUser") map {
+        Set("addUser", "removeUser", "editUserRole") map {
           makeAction("teams", _)
+        }
+      )
+
+  case object OrganizationsUserAdmin
+      extends SimpleScope(
+        Set("addUser", "removeUser", "editUserRole") map {
+          makeAction("organizations", _)
         }
       )
 
@@ -163,19 +267,20 @@ object Scopes {
         )
       )
 
-  // TODO
   case object RasterFoundryOrganizationAdmin
       extends ComplexScope(
         Set(
-          RasterFoundryTeamAdmin
+          RasterFoundryTeamAdmin,
+          OrganizationsUserAdmin,
+          new SimpleScope(Set(makeAction("teams", "create")))
         )
       )
 
-  // TODO
   case object RasterFoundryPlatformAdmin
       extends ComplexScope(
         Set(
-          RasterFoundryOrganizationAdmin
+          RasterFoundryOrganizationAdmin,
+          new SimpleScope(Set(makeAction("organizations", "create")))
         )
       )
 }
--- app-backend/datamodel/src/test/scala/ScopeSpec.scala
+++ app-backend/datamodel/src/test/scala/ScopeSpec.scala
@@ -1,19 +1,57 @@
 package com.rasterfoundry.datamodel
 
 import cats.kernel.laws.discipline.MonoidTests
-import org.scalacheck.Arbitrary
+import io.circe.testing.{ArbitraryInstances, CodecTests}
+import org.scalacheck.{Arbitrary, Gen}
 import org.scalatest.FunSuite
+import org.scalatestplus.scalacheck.Checkers
 import org.typelevel.discipline.scalatest.Discipline
 
-class ScopeSpec extends FunSuite with Discipline {
+class ScopeSpec
+    extends FunSuite
+    with Discipline
+    with Checkers
+    with ArbitraryInstances {
 
   implicit val arbAction: Arbitrary[Action] = Arbitrary[Action] {
     for {
-      domain <- Arbitrary.arbitrary[String]
-      action <- Arbitrary.arbitrary[String]
+      domain <- Gen.oneOf(
+        "projects",
+        "scenes",
+        "shapes",
+        "templates",
+        "teams",
+        "organizations",
+        "datasources"
+      )
+      action <- Gen.oneOf(
+        "read",
+        "edit",
+        "create",
+        "delete"
+      )
       limit <- Arbitrary.arbitrary[Option[Long]]
     } yield Action(domain, action, limit)
   }
+
+  def cannedPolicyGen: Gen[Scope] = Gen.oneOf(
+    Scopes.Uploader,
+    Scopes.AnalysesCRUD,
+    Scopes.AnalysesMultiPlayer,
+    Scopes.DatasourcesCRUD,
+    Scopes.OrganizationsUserAdmin,
+    Scopes.ProjectExport,
+    Scopes.ProjectsFullAccess,
+    Scopes.RasterFoundryOrganizationAdmin,
+    Scopes.RasterFoundryTeamAdmin,
+    Scopes.ScenesCRUD,
+    Scopes.ScenesMultiPlayer,
+    Scopes.ShapesFullAccess,
+    Scopes.TeamsEdit,
+    Scopes.TemplatesCRUD,
+    Scopes.TemplatesMultiPlayer,
+    Scopes.UploadsCRUD
+  )
 
   // Not separating out into a separate object until we have more than
   // one of these. I don't think for the most part we depend on laws holding,
@@ -21,12 +59,15 @@ class ScopeSpec extends FunSuite with Discipline {
   // security
   implicit val arbScope: Arbitrary[Scope] = Arbitrary[Scope] {
     for {
-      actions <- Arbitrary.arbitrary[Set[Action]]
-    } yield new SimpleScope(actions)
+      scope <- Gen.oneOf(
+        Arbitrary.arbitrary[Set[Action]] map { new SimpleScope(_) },
+        cannedPolicyGen
+      )
+    } yield scope
   }
 
   checkAll("Scope.MonoidLaws", MonoidTests[Scope].monoid)
+  checkAll("Scope.CodecTests", CodecTests[Scope].codec)
 
-  // TODO test ser-de
   // TODO test some permissions relationships
 }
