94867d85b794ff349c407668040abd6b132c85ea
Aaron Su <xunzesu@outlook.com>
2020-01-02 16:47:46 -0500

    Add a migration to attach users with right scopes

---
+++ app-backend/db/src/main/resources/migrations/V20__update_user_scopes.sql
@@ -0,0 +1,85 @@
+-- updated user scopes in the users table with new JSONB strings
+
+ALTER TABLE public.users ALTER COLUMN scopes SET DEFAULT '""'::jsonb;
+
+
+UPDATE public.users SET scopes = '""'::jsonb;
+
+
+UPDATE
+   public.users 
+SET
+   scopes = 'platforms:admin'::jsonb 
+WHERE
+   id in 
+   (
+      SELECT
+         DISTINCT user_id 
+      FROM
+         user_group_roles 
+      WHERE
+         group_type = 'PLATFORM' 
+         AND group_role = 'ADMIN' 
+         AND is_active = true 
+         AND membership_status = 'APPROVED'
+   )
+   AND scopes = '""'::jsonb;
+
+
+UPDATE
+   public.users 
+SET
+   scopes = 'organizations:admin'::jsonb 
+WHERE
+   id in 
+   (
+      SELECT
+         DISTINCT user_id 
+      FROM
+         user_group_roles 
+      WHERE
+         group_type = 'ORGANIZATION' 
+         AND group_role = 'ADMIN' 
+         AND is_active = true 
+         AND membership_status = 'APPROVED'
+   )
+   AND scopes = '""'::jsonb;
+
+
+UPDATE
+   public.users 
+SET
+   scopes = 'teams:admin'::jsonb 
+WHERE
+   id in 
+   (
+      SELECT
+         DISTINCT user_id 
+      FROM
+         user_group_roles 
+      WHERE
+         group_type = 'TEAM' 
+         AND group_role = 'ADMIN' 
+         AND is_active = true 
+         AND membership_status = 'APPROVED'
+   )
+   AND scopes = '""'::jsonb;
+
+
+UPDATE
+   public.users 
+SET
+   scopes = 'users:member'::jsonb 
+WHERE
+   id in 
+   (
+      SELECT
+         DISTINCT user_id 
+      FROM
+         user_group_roles 
+      WHERE
+         group_role = 'MEMBER' 
+         AND is_active = true 
+         AND membership_status = 'APPROVED'
+   )
+   AND scopes = '""'::jsonb;
