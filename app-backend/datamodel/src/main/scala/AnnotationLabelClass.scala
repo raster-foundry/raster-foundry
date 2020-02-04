394a7e990d7e0e08af36d2e534f1fe924c1f27a4
James Santucci <james.santucci@gmail.com>
2020-02-04 16:10:07 -0600

    scalafmt

--- app-backend/datamodel/src/main/scala/AnnotationLabelClass.scala
+++ app-backend/datamodel/src/main/scala/AnnotationLabelClass.scala
@@ -6,25 +6,27 @@ import io.circe._
 import java.util.UUID
 
 final case class AnnotationLabelClass(
-  id: UUID,
-  name: String,
-  annotationLabelClassGroupId: UUID,
-  colorHexCode: String,
-  default: Option[Boolean],
-  determinant: Option[Boolean],
-  index: Int
-)
-
-object AnnotationLabelClass {
-  implicit val encAnnotationLabelClass: Encoder[AnnotationLabelClass] = deriveEncoder
-  implicit val decAnnotationLabelClass: Decoder[AnnotationLabelClass] = deriveDecoder
-
-  final case class Create(
+    id: UUID,
     name: String,
+    annotationLabelClassGroupId: UUID,
     colorHexCode: String,
     default: Option[Boolean],
     determinant: Option[Boolean],
     index: Int
+)
+
+object AnnotationLabelClass {
+  implicit val encAnnotationLabelClass: Encoder[AnnotationLabelClass] =
+    deriveEncoder
+  implicit val decAnnotationLabelClass: Decoder[AnnotationLabelClass] =
+    deriveDecoder
+
+  final case class Create(
+      name: String,
+      colorHexCode: String,
+      default: Option[Boolean],
+      determinant: Option[Boolean],
+      index: Int
   )
 
   object Create {
