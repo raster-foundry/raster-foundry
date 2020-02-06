59ed7e6cffbcfc92978041a8273f31135752088c
aaronxsu <xunzesu@outlook.com>
2020-02-06 16:35:25 -0500

    Do not remove the project id and project layer id

--- app-backend/db/src/main/resources/migrations/V27__update_task_annotation_project_id.sql
+++ app-backend/db/src/main/resources/migrations/V27__update_task_annotation_project_id.sql
@@ -10,7 +10,3 @@ FROM annotation_projects AS ap
 WHERE ap.project_id = t.project_id;
 
 ALTER TABLE public.tasks VALIDATE CONSTRAINT annotation_project_id_not_null;
-
-ALTER TABLE public.tasks 
-DROP COLUMN project_id,
-DROP COLUMN project_layer_id;
