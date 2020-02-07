63eb1299c21b2b8b422c79c4e6ba997768fb2c5a
James Santucci <james.santucci@gmail.com>
2020-02-07 10:46:20 -0600

    wip -- works for tiny table when i hard code a bearer token

--- app-backend/api-it/src/test/scala/ScopeSpec.scala
+++ app-backend/api-it/src/test/scala/ScopeSpec.scala
@@ -3,12 +3,25 @@ package com.rasterfoundry.api.it
 import com.rasterfoundry.datamodel._
 
 import cats.implicits._
+import com.softwaremill.sttp.{
+  DeserializationError,
+  Empty,
+  Id,
+  Request,
+  RequestT,
+  Response,
+  Uri
+}
+import com.softwaremill.sttp.circe._
+import com.softwaremill.sttp.quick._
+import com.typesafe.config.ConfigFactory
 import io.circe._
 import io.circe.generic.semiauto.deriveDecoder
 import io.circe.syntax._
 import org.scalatest.FunSpec
 import org.scalatest.prop.TableDrivenPropertyChecks._
 
+import java.net.URI
 import java.util.UUID
 
 sealed abstract class Verb
@@ -59,26 +72,105 @@ object SimResponse {
 
 class ScopeSpec extends FunSpec {
 
+  private val config = ConfigFactory.load()
+
+  val apiHost = config.getString("apiHost")
+  val csvPath = config.getString("scopeITCSVLocation")
+
   val bogusId = UUID.randomUUID
 
-  // FLOW / TODO
+  // match strings of alpha characters between braces
+  val idSegment = "\\{[aA-zZ]*\\}".r
+
+  def subUUID(path: String): String = idSegment.replaceAllIn(path, s"$bogusId")
+
+  def makeRoute(path: String): Uri = {
+    val cleaned = subUUID(path)
+    println(cleaned)
+    val unsafeUri = URI.create(s"$apiHost$cleaned")
+    val out = Uri(unsafeUri)
+    println(out)
+    out
+  }
+
+  def getAuthToken(refreshToken: String): String = ":("
+
+  def getBaseRequest(scope: Scope, expectSuccess: Boolean) = {
+    val root = sttp.header("X-PolicySim", "true").auth.bearer(getAuthToken(""))
+    val scopeStringNoQuotes = scope.asJson.noSpaces.replace("\"", "")
+    if (expectSuccess) {
+      root.header("X-PolicySim-Include", scopeStringNoQuotes)
+    } else {
+      root.header("X-PolicySim-Exclude", scopeStringNoQuotes)
+    }
+  }
+
+  def addMethod(
+      request: RequestT[Empty, String, Nothing],
+      path: Uri,
+      verb: Verb
+  ): Request[Either[DeserializationError[Error], SimResponse], Nothing] =
+    (verb match {
+      case Get    => request.get(path)
+      case Post   => request.post(path)
+      case Put    => request.put(path)
+      case Patch  => request.patch(path)
+      case Delete => request.delete(path)
+    }).response(asJson[SimResponse])
+
+  // TODO
   // - get the csv location from testing resources
   // - read the csv into a `Table` -- http://www.scalatest.org/user_guide/table_driven_property_checks
-  // - use table drive property checks to verify that with the scope present,
-  //   we don't get a 403
-  // - do the same for when the scope is absent
+  // - get a bearer token from the api
 
   // this will come from the csv in the real version, but just setting this up for now
   val routes = Table(
     ("Path", "Domain:Action", "Verb"),
     ("/datasources/", "datasources:read", "get"),
     ("/datasources/", "datasources:create", "post"),
-    ("/datasources/", "bogus:permission", "fakeVerb")
+    ("/datasources/{datasourceID}", "datasources:read", "get")
   )
 
-  forAll(routes) { (path: String, scope: String, verb: String) =>
-    CsvRow.fromStringsE(path, scope, verb) map { _ =>
-      assert(true)
-    } getOrElse { fail(s"Could not decode $path, $scope, and $verb into row types") }
+  def getSimResult(row: CsvRow, expectation: Boolean) = {
+    val base = getBaseRequest(row.scope, expectation)
+    val requestUri = makeRoute(row.path)
+    val response
+        : Id[Response[Either[DeserializationError[Error], SimResponse]]] =
+      addMethod(base, requestUri, row.verb).send()
+    println(response)
+    // for some reason I'm not allowed to bail on the Id wrapper in the previous step, though I'd really
+    // prefer to. this is a bit janky but I'm not sure what to do about it.
+    val resultBody: Either[String, SimResponse] = response map { resp =>
+      resp.body match {
+        case Right(Right(simResp)) => Right(simResp)
+        case Left(err)             => Left("body deserialization failed")
+      }
+    }
+    assert(
+      resultBody == Right(SimResponse(expectation)),
+      "Authorization expectation failed"
+    )
+  }
+
+  def expectAllowed(row: CsvRow) = getSimResult(row, true)
+
+  def expectForbidden(row: CsvRow): Unit = getSimResult(row, false)
+
+  describe("Policy simulation") {
+    it("reports expected failure when relevant scopes are excluded") {
+      forAll(routes) { (path: String, scope: String, verb: String) =>
+        CsvRow.fromStringsE(path, scope, verb) map { expectForbidden _ } getOrElse {
+          fail(s"Could not decode $path, $scope, and $verb into row types")
+        }
+      }
+    }
+
+    it("reports expected success when relevant scopes are included") {
+      forAll(routes) { (path: String, scope: String, verb: String) =>
+        CsvRow.fromStringsE(path, scope, verb) map { expectAllowed _ } getOrElse {
+          fail(s"Could not decode $path, $scope, and $verb into row types")
+        }
+      }
+    }
   }
 }
