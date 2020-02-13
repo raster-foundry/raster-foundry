d4b59cd901a97ecb5a0cb2e71b30b05899be8a42
Lucien Knechtli <lknechtli@gmail.com>
2020-02-13 13:35:19 -0500

    Add AnnotationLabel model and dao, fix Metals (#5303)
    * fix metals
    * Add AnnotationLabel model and dao

---
+++ app-backend/datamodel/src/main/scala/AnnotationLabel.scala
@@ -0,0 +1,168 @@
+package com.rasterfoundry.datamodel
+
+import java.sql.Timestamp
+import java.util.UUID
+
+import geotrellis.vector.{Geometry, Projected, io => _}
+import io.circe.generic.JsonCodec
+import io.circe.generic.extras._
+import io.circe.Encoder
+import io.circe._
+import io.circe.syntax._
+import io.circe.generic.semiauto._
+
+final case class AnnotationLabel(
+    id: UUID,
+    createdAt: Timestamp,
+    createdBy: String,
+    geometry: Option[Projected[Geometry]],
+    annotationProjectId: UUID,
+    annotationTaskId: UUID,
+)
+
+final case class AnnotationLabelProperties(
+    createdAt: Timestamp,
+    createdBy: String,
+    annotationProjectId: UUID,
+    annotationTaskId: UUID,
+)
+
+final case class AnnotationLabelPropertiesCreate(
+    annotationProjectId: UUID,
+    annotationTaskId: UUID,
+)
+
+@JsonCodec
+final case class AnnotationLabelWithClassesFeatureCollectionCreate(
+    features: Seq[AnnotationLabelWithClasses.GeoJSONFeatureCreate]
+)
+
+@JsonCodec
+final case class AnnotationLabelWithClasses(
+    id: UUID,
+    createdAt: Timestamp,
+    createdBy: String,
+    geometry: Option[Projected[Geometry]],
+    annotationProjectId: UUID,
+    annotationTaskId: UUID,
+    annotationLabelClasses: List[UUID]
+) extends GeoJSONSerializable[AnnotationLabelWithClasses.GeoJSON] {
+  def toGeoJSONFeature = AnnotationLabelWithClasses.GeoJSON(
+    this.id,
+    this.geometry,
+    AnnotationLabelWithClassesProperties(
+      this.createdAt,
+      this.createdBy,
+      this.annotationProjectId,
+      this.annotationTaskId,
+      this.annotationLabelClasses
+    )
+  )
+}
+
+object AnnotationLabelWithClasses {
+  implicit val config: Configuration =
+    Configuration.default.copy(transformMemberNames = {
+      case "_type" => "type"
+      case other   => other
+    })
+  def tupled = (AnnotationLabel.apply _).tupled
+
+  final case class Create(
+      geometry: Option[Projected[Geometry]],
+      annotationProjectId: UUID,
+      annotationTaskId: UUID,
+      annotationLabelClasses: List[UUID]
+  ) {
+    def toAnnotationLabelWithClasses(user: User): AnnotationLabelWithClasses = {
+      val now = new Timestamp(new java.util.Date().getTime)
+      AnnotationLabelWithClasses(
+        UUID.randomUUID,
+        now,
+        user.id,
+        geometry,
+        annotationProjectId,
+        annotationTaskId,
+        annotationLabelClasses
+      )
+    }
+  }
+
+  @ConfiguredJsonCodec
+  final case class GeoJSON(
+      id: UUID,
+      geometry: Option[Projected[Geometry]],
+      properties: AnnotationLabelWithClassesProperties,
+      _type: String = "Feature"
+  ) extends GeoJSONFeature
+
+  @JsonCodec
+  final case class GeoJSONFeatureCreate(
+      geometry: Option[Projected[Geometry]],
+      properties: AnnotationLabelWithClassesPropertiesCreate
+  ) {
+    def toAnnotationLabelWithClassesCreate
+      : AnnotationLabelWithClasses.Create = {
+      AnnotationLabelWithClasses.Create(
+        geometry,
+        properties.annotationProjectId,
+        properties.annotationTaskId,
+        properties.annotationLabelClasses
+      )
+    }
+  }
+
+  object GeoJSON {
+    implicit val annoLabelWithClassesGeojonEncoder: Encoder[GeoJSON] =
+      Encoder.forProduct4("id", "geometry", "properties", "type")(
+        geojson =>
+          (geojson.id, geojson.geometry, geojson.properties, geojson._type)
+      )
+  }
+}
+
+@JsonCodec
+final case class AnnotationLabelWithClassesPropertiesCreate(
+    annotationProjectId: UUID,
+    annotationTaskId: UUID,
+    annotationLabelClasses: List[UUID]
+)
+
+@JsonCodec
+final case class AnnotationLabelWithClassesProperties(
+    createdAt: Timestamp,
+    createdBy: String,
+    annotationProjectId: UUID,
+    annotationTaskId: UUID,
+    annotationLabelClasses: List[UUID]
+)
+
+object AnnotationLabelWithClassesProperties {
+  implicit val annotationLabelWithClassesPropertiesEncoder
+    : Encoder[AnnotationLabelWithClassesProperties] =
+    new Encoder[AnnotationLabelWithClassesProperties] {
+      final def apply(
+          properties: AnnotationLabelWithClassesProperties): Json = {
+        (
+          Map(
+            "createdAt" -> properties.createdAt.asJson,
+            "createdBy" -> properties.createdBy.asJson,
+            "annotationProjectId" -> properties.annotationProjectId.asJson,
+            "annotationTaskId" -> properties.annotationTaskId.asJson,
+            "annotationLabelClasses" -> properties.annotationLabelClasses.asJson
+          )
+        ).asJson
+      }
+    }
+}
+
+final case class AnnotationLabelWithClassesFeatureCollection(
+    features: List[AnnotationLabelWithClasses.GeoJSON],
+    `type`: String = "FeatureCollection"
+)
+
+object AnnotationLabelWithClassesFeatureCollection {
+  implicit val annoLabelWithClassesFCEncoder
+    : Encoder[AnnotationLabelWithClassesFeatureCollection] =
+    deriveEncoder[AnnotationLabelWithClassesFeatureCollection]
+}
