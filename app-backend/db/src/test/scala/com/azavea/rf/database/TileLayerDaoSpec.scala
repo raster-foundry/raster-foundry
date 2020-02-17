8ab3aa8f2d330e7124b6820111e0ed2a48b93417
James Santucci <james.santucci@gmail.com>
2020-02-17 16:48:23 -0600

    Add TileLayerDaoSpec dumb tests

---
+++ app-backend/db/src/test/scala/com/azavea/rf/database/TileLayerDaoSpec.scala
@@ -0,0 +1,36 @@
+package com.rasterfoundry.database
+
+import com.rasterfoundry.datamodel._
+import com.rasterfoundry.common.Generators.Implicits._
+import org.scalacheck.Prop.forAll
+import org.scalatest._
+import org.scalatestplus.scalacheck.Checkers
+
+class TileLayerDaoSpec
+    extends FunSuite
+    with Matchers
+    with Checkers
+    with DBTestConfig
+    with PropTestHelpers {
+  test("inserting a tile layer") {
+    check {
+      forAll(
+        (
+            annotationProjectCreate: AnnotationProject.Create,
+            tileLayerCreate: TileLayer.Create
+        ) => true
+      )
+    }
+  }
+
+  test("list tile layers") {
+    check {
+      forAll(
+        (
+            annotationProjectCreate: AnnotationProject.Create,
+            tileLayerCreate: TileLayer.Create
+        ) => true
+      )
+    }
+  }
+}
