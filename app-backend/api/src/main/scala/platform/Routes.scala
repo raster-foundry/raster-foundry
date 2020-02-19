e18f9e25d83d780cb43f22da9ec17910de5c862a
James Santucci <james.santucci@gmail.com>
2020-02-19 17:42:42 -0600

    `sbt fix` after develop merge

--- app-backend/api/src/main/scala/platform/Routes.scala
+++ app-backend/api/src/main/scala/platform/Routes.scala
@@ -18,13 +18,14 @@ import com.rasterfoundry.datamodel._
 
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.server._
 import cats.effect.IO
 import cats.implicits._
 import de.heikoseeberger.akkahttpcirce.ErrorAccumulatingCirceSupport._
 import doobie._
 import doobie.implicits._
-import akka.http.scaladsl.server._
 import doobie.postgres.implicits._
+
 import java.util.UUID
 
 trait PlatformRoutes
