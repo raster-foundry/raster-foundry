f161a17be4d1c79ceb071aac8bc912eda663b6aa
James Santucci <james.santucci@gmail.com>
2020-02-20 15:32:29 -0600

    Set nulls on annotation projects when optional fks are deleted

--- app-backend/db/src/main/resources/migrations/V31__add_cascade_delete_to_task_annotation_projects.sql
+++ app-backend/db/src/main/resources/migrations/V31__add_cascade_delete_to_task_annotation_projects.sql
@@ -16,6 +16,24 @@ ALTER TABLE annotation_projects ADD CONSTRAINT annotation_projects_owner_fkey
   REFERENCES users(id)
   ON DELETE CASCADE;
 
+ALTER TABLE annotation_projects DROP CONSTRAINT annotation_projects_labelers_team_id_fkey;
+ALTER TABLE annotation_projects ADD CONSTRAINT annotation_projects_labelers_team_id_fkey
+  FOREIGN KEY (labelers_team_id)
+  REFERENCES teams(id)
+  ON DELETE SET NULL;
+
+ALTER TABLE annotation_projects DROP CONSTRAINT annotation_projects_validators_team_id_fkey;
+ALTER TABLE annotation_projects ADD CONSTRAINT annotation_projects_validators_team_id_fkey
+  FOREIGN KEY (validators_team_id)
+  REFERENCES teams(id)
+  ON DELETE SET NULL;
+
+ALTER TABLE annotation_projects DROP CONSTRAINT annotation_projects_project_id_fkey;
+ALTER TABLE annotation_projects ADD CONSTRAINT annotation_projects_project_id_fkey
+  FOREIGN KEY (project_id)
+  REFERENCES projects(id)
+  ON DELETE SET NULL;
+
 ALTER TABLE annotation_labels DROP CONSTRAINT annotation_labels_created_by_fkey;
 ALTER TABLE annotation_labels ADD CONSTRAINT annotation_labels_created_by_fkey
   FOREIGN KEY (created_by)
