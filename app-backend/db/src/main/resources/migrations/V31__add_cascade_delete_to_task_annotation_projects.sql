ce8ef23f972831d6b87afaef97853db0286524a4
James Santucci <james.santucci@gmail.com>
2020-02-20 15:30:28 -0600

    Add more on delete cascades

--- app-backend/db/src/main/resources/migrations/V31__add_cascade_delete_to_task_annotation_projects.sql
+++ app-backend/db/src/main/resources/migrations/V31__add_cascade_delete_to_task_annotation_projects.sql
@@ -9,3 +9,15 @@ ALTER TABLE annotation_labels ADD CONSTRAINT annotation_labels_annotation_task_i
   FOREIGN KEY (annotation_task_id)
   REFERENCES tasks(id)
   ON DELETE CASCADE;
+
+ALTER TABLE annotation_projects DROP CONSTRAINT annotation_projects_created_by_fkey;
+ALTER TABLE annotation_projects ADD CONSTRAINT annotation_projects_owner_fkey
+  FOREIGN KEY (owner)
+  REFERENCES users(id)
+  ON DELETE CASCADE;
+
+ALTER TABLE annotation_labels DROP CONSTRAINT annotation_labels_created_by_fkey;
+ALTER TABLE annotation_labels ADD CONSTRAINT annotation_labels_created_by_fkey
+  FOREIGN KEY (created_by)
+  REFERENCES users(id)
+  ON DELETE CASCADE;
