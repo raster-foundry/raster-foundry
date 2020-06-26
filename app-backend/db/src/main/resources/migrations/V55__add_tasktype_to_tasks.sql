4ea31bfbc78a3e5efe0c1a6a80bc2c70702a88e3
Alex Kaminsky <akaminsky@azavea.com>
2020-06-26 03:25:18 +0000

    Fix migrations

--- app-backend/db/src/main/resources/migrations/V55__add_tasktype_to_tasks.sql
+++ app-backend/db/src/main/resources/migrations/V55__add_tasktype_to_tasks.sql
@@ -7,5 +7,3 @@ ALTER TABLE public.tasks
 ADD COLUMN task_type task_type NOT NULL DEFAULT 'LABEL',
 ADD COLUMN parent_task_id UUID REFERENCES tasks(id),
 ADD COLUMN reviews JSONB NOT NULL DEFAULT '{}';
-
-CREATE INDEX CONCURRENTLY idx_task_parent ON public.tasks(parent_task_id);
---
+++ app-backend/db/src/main/resources/migrations/V56__add_task_parent_index.sql
@@ -0,0 +1,1 @@
+CREATE INDEX CONCURRENTLY idx_task_parent ON public.tasks(parent_task_id);
