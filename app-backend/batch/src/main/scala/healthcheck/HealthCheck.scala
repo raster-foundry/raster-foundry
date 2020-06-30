a57f1ec78a39603157a68e20339a0a691f01ee96
James Santucci <james.santucci@gmail.com>
2020-06-30 11:31:13 -0500

    add reading from s3 to batch jar healthcheck

--- app-backend/batch/src/main/scala/healthcheck/HealthCheck.scala
+++ app-backend/batch/src/main/scala/healthcheck/HealthCheck.scala
@@ -1,12 +1,15 @@
 package com.rasterfoundry.batch.healthcheck
 
 import com.typesafe.scalalogging.LazyLogging
+import geotrellis.raster.geotiff.GeoTiffRasterSource
 
 object HealthCheck extends LazyLogging {
   val name = "healthcheck"
 
   def run(): Unit = {
-    logger.info("Batch containers and the batch jar are friends")
+    val rs = GeoTiffRasterSource("s3://rasterfoundry-development-data-us-east-1/test.tif")
+    rs.tiff
+    logger.info("The batch jar can run and can read tifs")
   }
 
   def main(args: Array[String]): Unit = {
