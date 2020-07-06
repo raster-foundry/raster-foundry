63d9d442a457cdae0e8d26d9fc20a0b5945e5de5
James Santucci <james.santucci@gmail.com>
2020-07-06 11:30:05 -0500

    index columns used for joins in history table

--- app-backend/db/src/main/resources/migrations/V57__add_breadcrumb_tables_for_project_copying.sql
+++ app-backend/db/src/main/resources/migrations/V57__add_breadcrumb_tables_for_project_copying.sql
@@ -2,3 +2,9 @@ CREATE TABLE public.label_class_history (
     parent_label_class_id uuid not null,
     child_label_class_id uuid not null
 );
+
+CREATE INDEX IF NOT EXISTS label_class_history_parent_label_class_id_idx
+ON public.label_class_history (parent_label_class_id);
+
+CREATE INDEX IF NOT EXISTS label_class_history_child_label_class_id_idx
+ON public.label_class_history (child_label_class_id);
