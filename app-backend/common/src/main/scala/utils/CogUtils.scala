fd2fce1893f73b639e1dbd43e46f9ab9ad3210aa
Chris Brown <cbrown@azavea.com>
2020-07-10 09:33:03 -0400

    Formatting

--- app-backend/common/src/main/scala/utils/CogUtils.scala
+++ app-backend/common/src/main/scala/utils/CogUtils.scala
@@ -24,17 +24,16 @@ object CogUtils extends LazyLogging {
       uri: String,
       buckets: Int = 80
   ): Option[Array[Histogram[Double]]] = {
-    // use the resolution that's closest to 100,000 pixels and not greater than 400,000 pixels
-    // this must fit in a request/response cycle. A 500x500 overview is reasonable, and that adds
-    // up to 250,000 pixels
-    // We may need to adjust this number depending on how fast our API is able to process it, these
-    // numbers are based off local testing
+    // Get the smallest overview and calculate histogrm from that
     val rasterSource = GeoTiffRasterSource(uri)
     logger.debug(s"Base cell size is: ${rasterSource.cellSize}")
-    val targetCellSize = TargetCellSize(rasterSource.resolutions.sortBy(_.height).max)
+    val targetCellSize = TargetCellSize(
+      rasterSource.resolutions.sortBy(_.height).max)
 
     rasterSource
-      .resample(targetCellSize, ResampleMethods.NearestNeighbor, OverviewStrategy.DEFAULT)
+      .resample(targetCellSize,
+                ResampleMethods.NearestNeighbor,
+                OverviewStrategy.DEFAULT)
       .read()
       .map(_.tile.histogramDouble(buckets))
   }
