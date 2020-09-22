7be1c35723659650e5f0d22d6a00c745dba17488
Aaron Su <xunzesu@outlook.com>
2020-09-22 13:50:53 -0400

    Run fix

--- app-backend/datamodel/src/main/scala/Task.scala
+++ app-backend/datamodel/src/main/scala/Task.scala
@@ -222,9 +222,9 @@ object Task {
 
   object TaskPropertiesWithCampaign {
     implicit val encTaskPropertiesWithCampaign
-        : Encoder[TaskPropertiesWithCampaign] = deriveEncoder
+      : Encoder[TaskPropertiesWithCampaign] = deriveEncoder
     implicit val decTaskPropertiesWithCamapgin
-        : Decoder[TaskPropertiesWithCampaign] = deriveDecoder
+      : Decoder[TaskPropertiesWithCampaign] = deriveDecoder
   }
 
   final case class TaskPropertiesCreate(
@@ -314,7 +314,7 @@ object Task {
           (
             tfc._type,
             tfc.features
-          )
+        )
       )
 
     implicit val decTaskFeatureCollection: Decoder[TaskFeatureCollection] =
@@ -331,12 +331,12 @@ object Task {
 
   object TaskFeatureCollectionCreate {
     implicit val decTaskFeatureCollectionCreate
-        : Decoder[TaskFeatureCollectionCreate] =
+      : Decoder[TaskFeatureCollectionCreate] =
       Decoder.forProduct2("type", "features")(
         TaskFeatureCollectionCreate.apply _
       )
     implicit val encTaskFeatureCollectionCreate
-        : Encoder[TaskFeatureCollectionCreate] =
+      : Encoder[TaskFeatureCollectionCreate] =
       Encoder.forProduct2("type", "features")(
         tfc => (tfc._type, tfc.features)
       )
@@ -348,10 +348,10 @@ object Task {
 
   object TaskGridCreateProperties {
     implicit val encTaskGridCreateProperties
-        : Encoder[TaskGridCreateProperties] =
+      : Encoder[TaskGridCreateProperties] =
       deriveEncoder
     implicit val decTaskGridCreateProperties
-        : Decoder[TaskGridCreateProperties] =
+      : Decoder[TaskGridCreateProperties] =
       deriveDecoder
   }
 
