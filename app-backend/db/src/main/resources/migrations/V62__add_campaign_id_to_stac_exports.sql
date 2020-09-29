2243511d08c3520bdbe75e048deb9740589552be
James Santucci <james.santucci@gmail.com>
2020-09-29 11:04:25 -0600

    add delete behavior and index to migration

--- app-backend/db/src/main/resources/migrations/V62__add_campaign_id_to_stac_exports.sql
+++ app-backend/db/src/main/resources/migrations/V62__add_campaign_id_to_stac_exports.sql
@@ -1,1 +1,3 @@
-ALTER TABLE stac_exports ADD COLUMN campaign_id uuid REFERENCES campaigns(id);
+ALTER TABLE stac_exports ADD COLUMN campaign_id uuid REFERENCES campaigns(id) ON DELETE CASCADE;
+
+CREATE INDEX IF NOT EXISTS stac_exports_campaign_id_idx ON stac_exports (campaign_id);
