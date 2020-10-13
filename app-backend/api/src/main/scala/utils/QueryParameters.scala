fb49f88d99b7e522686ef8f546c03fddb792f625
James Santucci <james.santucci@gmail.com>
2020-10-13 15:36:10 -0600

    remove summary endpoint in list tasks

--- app-backend/api/src/main/scala/utils/QueryParameters.scala
+++ app-backend/api/src/main/scala/utils/QueryParameters.scala
@@ -34,7 +34,7 @@ trait QueryParameterDeserializers {
     }
 
   implicit val deserializerAnnotationProjectType
-    : Unmarshaller[String, AnnotationProjectType] =
+      : Unmarshaller[String, AnnotationProjectType] =
     Unmarshaller.strict[String, AnnotationProjectType] { s =>
       AnnotationProjectType.fromString(s)
     }
@@ -187,7 +187,6 @@ trait QueryParametersCommon extends QueryParameterDeserializers {
       'actionEndTime.as(deserializerTimestamp).?,
       'actionMinCount.as[Int].?,
       'actionMaxCount.as[Int].?,
-      'format.as[String].?,
       'taskType.as(deserializerTaskType).?
     ).as(TaskQueryParameters.apply _)
 
