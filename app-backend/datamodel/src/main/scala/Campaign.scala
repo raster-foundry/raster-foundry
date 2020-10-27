35b3729863085dd4798b4de22d06fe8cb0aca5d6
moradology <npzimmerman@gmail.com>
2020-10-27 14:44:54 -0500

    Scalafmt

--- app-backend/datamodel/src/main/scala/Campaign.scala
+++ app-backend/datamodel/src/main/scala/Campaign.scala
@@ -25,14 +25,26 @@ final case class Campaign(
     resourceLink: Option[String] = None
 ) {
   def withRelated(
-    labelClassGroups: List[AnnotationLabelClassGroup.WithLabelClasses]
+      labelClassGroups: List[AnnotationLabelClassGroup.WithLabelClasses]
   ): Campaign.WithRelated = {
     Campaign.WithRelated(
-      id, createdAt, owner, name, campaignType,
-      description, videoLink, partnerName,
-      partnerLogo, parentCampaignId, continent,
-      tags, childrenCount, projectStatuses,
-      isActive, resourceLink, labelClassGroups
+      id,
+      createdAt,
+      owner,
+      name,
+      campaignType,
+      description,
+      videoLink,
+      partnerName,
+      partnerLogo,
+      parentCampaignId,
+      continent,
+      tags,
+      childrenCount,
+      projectStatuses,
+      isActive,
+      resourceLink,
+      labelClassGroups
     )
   }
 }
@@ -69,23 +81,23 @@ object Campaign {
   }
 
   final case class WithRelated(
-    id: UUID,
-    createdAt: Timestamp,
-    owner: String,
-    name: String,
-    campaignType: AnnotationProjectType,
-    description: Option[String] = None,
-    videoLink: Option[String] = None,
-    partnerName: Option[String] = None,
-    partnerLogo: Option[String] = None,
-    parentCampaignId: Option[UUID] = None,
-    continent: Option[Continent] = None,
-    tags: List[String] = List.empty,
-    childrenCount: Int,
-    projectStatuses: Map[String, Int],
-    isActive: Boolean,
-    resourceLink: Option[String] = None,
-    labelClassGroups: List[AnnotationLabelClassGroup.WithLabelClasses],
+      id: UUID,
+      createdAt: Timestamp,
+      owner: String,
+      name: String,
+      campaignType: AnnotationProjectType,
+      description: Option[String] = None,
+      videoLink: Option[String] = None,
+      partnerName: Option[String] = None,
+      partnerLogo: Option[String] = None,
+      parentCampaignId: Option[UUID] = None,
+      continent: Option[Continent] = None,
+      tags: List[String] = List.empty,
+      childrenCount: Int,
+      projectStatuses: Map[String, Int],
+      isActive: Boolean,
+      resourceLink: Option[String] = None,
+      labelClassGroups: List[AnnotationLabelClassGroup.WithLabelClasses],
   )
 
   object WithRelated {
