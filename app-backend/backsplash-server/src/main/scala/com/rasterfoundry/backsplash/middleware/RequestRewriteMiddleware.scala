93ccc22ca72b9562e9cabff86797a9898ee4a3dd
Chris Brown <cbrown@azavea.com>
2020-12-04 15:04:25 -0500

    Add rewrite for `/tiles/` prefixed tile requests (#5527)
    This used to be in nginx, but we stopped deploying the
    nginx sidecar container recently and this needs to happen
    in application code now.

--- app-backend/backsplash-server/src/main/scala/com/rasterfoundry/backsplash/middleware/RequestRewriteMiddleware.scala
+++ app-backend/backsplash-server/src/main/scala/com/rasterfoundry/backsplash/middleware/RequestRewriteMiddleware.scala
@@ -13,15 +13,38 @@ import org.http4s.dsl.io._
 import java.util.UUID
 
 object RequestRewriteMiddleware {
-  private def getDefaultLayerId(projectId: UUID,
-                                xa: Transactor[IO]): OptionT[IO, UUID] =
+  private def getDefaultLayerId(
+      projectId: UUID,
+      xa: Transactor[IO]
+  ): OptionT[IO, UUID] =
     OptionT { ProjectDao.getProjectById(projectId).transact(xa) } map {
       _.defaultLayerId
     }
 
+  private def rewrite(
+      req: Request[IO],
+      projectId: UUID,
+      z: Int,
+      x: Int,
+      y: Int,
+      service: HttpRoutes[IO],
+      xa: Transactor[IO]
+  ) = {
+    for {
+      defaultLayerId <- getDefaultLayerId(projectId, xa)
+      resp <- service(
+        req.withPathInfo(
+          s"/${projectId}/layers/${defaultLayerId}/${z}/${x}/${y}"
+        )
+      )
+    } yield resp
+  }
+
+  @SuppressWarnings(Array("RepeatedCaseBody"))
   def apply(
       service: HttpRoutes[IO],
-      xa: Transactor[IO]): Kleisli[OptionT[IO, ?], Request[IO], Response[IO]] =
+      xa: Transactor[IO]
+  ): Kleisli[OptionT[IO, ?], Request[IO], Response[IO]] =
     Kleisli { req: Request[IO] =>
       {
         // match on the request here and route to the service if it matches the deprecated route
@@ -29,19 +52,18 @@ object RequestRewriteMiddleware {
         // Due to middleware application order, we have to strip a trailing slash here
         if (req.pathInfo.charAt(req.pathInfo.length - 1) == '/') {
           apply(service, xa)(
-            req.withPathInfo(
-              req.pathInfo.substring(0, req.pathInfo.length - 1)))
+            req.withPathInfo(req.pathInfo.substring(0, req.pathInfo.length - 1))
+          )
         } else if (scriptName.isEmpty) {
           req match {
-            case GET -> Root / UUIDWrapper(projectId) / IntVar(z) / IntVar(x) / IntVar(
-                  y) =>
-              for {
-                defaultLayerId <- getDefaultLayerId(projectId, xa)
-                resp <- service(
-                  req.withPathInfo(
-                    s"/${projectId}/layers/${defaultLayerId}/${z}/${x}/${y}"))
-              } yield resp
-
+            case GET -> Root / "tiles" / UUIDWrapper(projectId) / IntVar(
+                  z
+                ) / IntVar(x) / IntVar(y) =>
+              rewrite(req, projectId, z, x, y, service, xa)
+            case GET -> Root / UUIDWrapper(projectId) / IntVar(z) / IntVar(
+                  x
+                ) / IntVar(y) =>
+              rewrite(req, projectId, z, x, y, service, xa)
             case GET -> Root / UUIDWrapper(projectId) / "histogram" =>
               for {
                 defaultLayerId <- getDefaultLayerId(projectId, xa)
