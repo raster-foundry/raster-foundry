da8ece589d34bf0480aa2b3492e5843e181b6f4a
James Santucci <james.santucci@gmail.com>
2020-12-22 11:35:06 -0700

    bake campaign auth into annotation project authorize method

--- app-backend/datamodel/src/main/scala/AuthResult.scala
+++ app-backend/datamodel/src/main/scala/AuthResult.scala
@@ -1,5 +1,9 @@
 package com.rasterfoundry.datamodel
 
+import cats.{Applicative, Functor}
+import cats.syntax.applicative._
+import cats.syntax.functor._
+
 sealed trait AuthResult[T] {
   def toBoolean: Boolean
 }
@@ -15,8 +19,31 @@ case class AuthFailure[T]() extends AuthResult[T] {
 }
 
 object AuthResult {
-  def fromOption[T](src: Option[T]): AuthResult[T] = src match {
-    case Some(v) => AuthSuccess(v)
-    case _       => AuthFailure()
-  }
+  def fromOption[T](src: Option[T]): AuthResult[T] =
+    src match {
+      case Some(v) => AuthSuccess(v)
+      case _       => AuthFailure()
+    }
+
+  def combine[T](x: AuthResult[T], y: AuthResult[T]): AuthResult[T] =
+    (x, y) match {
+      case (succ @ AuthSuccess(_), _) => succ
+      case (_, succ @ AuthSuccess(_)) => succ
+      case _                          => AuthFailure()
+    }
+
+  def success[F[_]: Applicative, T](v: T): F[AuthResult[T]] =
+    AuthSuccess(v).pure[F].widen
+
+  def failed[F[_]: Applicative, T]: F[AuthResult[T]] =
+    AuthFailure[T]().pure[F].widen
+
+  implicit val functorAuthResult: Functor[AuthResult] =
+    new Functor[AuthResult] {
+      def map[A, B](fa: AuthResult[A])(f: A => B): AuthResult[B] =
+        fa match {
+          case AuthSuccess(v) => AuthSuccess(f(v))
+          case AuthFailure()  => AuthFailure()
+        }
+    }
 }
