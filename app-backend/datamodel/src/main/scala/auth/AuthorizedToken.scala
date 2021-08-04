be87d1c19605d1a72967dc9c2149260d04915b52
James Santucci <james.santucci@gmail.com>
2021-08-04 14:45:10 -0600

    tokens from ROP flow in Auth0 don't have id_token

--- app-backend/datamodel/src/main/scala/auth/AuthorizedToken.scala
+++ app-backend/datamodel/src/main/scala/auth/AuthorizedToken.scala
@@ -1,9 +1,31 @@
 package com.rasterfoundry.datamodel.auth
 
-import io.circe.generic.JsonCodec
+import cats.syntax.all._
+import io.circe.{Decoder, Encoder, HCursor}
+import io.circe.generic.semiauto._
 
-@JsonCodec
 final case class AuthorizedToken(id_token: String,
                                  access_token: String,
                                  expires_in: Int,
                                  token_type: String)
+
+object AuthorizedToken {
+  implicit val encAuthorizedToken: Encoder[AuthorizedToken] = deriveEncoder
+
+  implicit val decAuthorizedToken: Decoder[AuthorizedToken] = { (c: HCursor) =>
+    (
+      c.get[Option[String]]("id_token"),
+      c.get[String]("access_token"),
+      c.get[Int]("expires_in"),
+      c.get[String]("token_type")
+    ).mapN {
+      case (idTokenO, accessToken, expiresIn, tokenType) =>
+        AuthorizedToken(
+          idTokenO getOrElse accessToken,
+          accessToken,
+          expiresIn,
+          tokenType
+        )
+    }
+  }
+}
