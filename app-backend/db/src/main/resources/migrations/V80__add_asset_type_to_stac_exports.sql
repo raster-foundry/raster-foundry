72d13025b772b496b94bdffda43863763ad63eb6
Peter Caisse <pcaisse@gmail.com>
2021-08-20 11:06:51 -0400

    Update migration to be an array of strings
    instead of an enum type which isn't supported by doobie. See:
    https://tpolecat.github.io/doobie/docs/15-Extensions-PostgreSQL.html#array-types

--- app-backend/db/src/main/resources/migrations/V80__add_asset_type_to_stac_exports.sql
+++ app-backend/db/src/main/resources/migrations/V80__add_asset_type_to_stac_exports.sql
@@ -1,4 +1,2 @@
-CREATE TYPE export_asset_type AS ENUM ('TILE_LAYER', 'SIGNED_URL', 'IMAGES');
-
 ALTER TABLE stac_exports
-    ADD COLUMN export_asset_types export_asset_type[] DEFAULT NULL;
+    ADD COLUMN export_asset_types varchar[] DEFAULT NULL;
