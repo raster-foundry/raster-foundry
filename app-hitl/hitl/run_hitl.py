78b00485f42bbb27171bc80a3167191c4ac4abe5
Simon Kassel <kassel.simon@gmail.com>
2022-07-18 11:06:23 -0400

    Use HITL specific image from dockerhub (#5647)
    * Update dev environment to use hitl-specific image
    
    * Add function for clipping labels to tasks
    
    * Remove placeholder code in entrypoint

--- app-hitl/hitl/run_hitl.py
+++ app-hitl/hitl/run_hitl.py
@@ -1,9 +1,13 @@
 import click
+import geopandas as gpd
+
+from utils import merge_labels_with_task_grid
+
 
 @click.command()
 @click.argument('hitl_project_id')
 def run_hitl(hitl_project_id):
-    print(f'processing project: {hitl_project_id}')
+    pass
 
 if __name__ == '__main__':
     run_hitl()
