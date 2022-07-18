f512b99b8e04044edbc38b200174675f6b29a088
Aaron Su <xunzesu@outlook.com>
2022-07-18 08:29:25 -0400

    Update HITL batch job: - pull data from api - persist data after hitl prediction - send message to intercom

---
+++ app-hitl/hitl/models/hitl_job.py
@@ -0,0 +1,60 @@
+from .base import BaseModel
+from hitl.utils.io import get_session
+
+
+class HITLJob(BaseModel):
+    URL_PATH = "/api/hitl-jobs/"
+
+    def __init__(
+        self,
+        id=None,
+        createdAt=None,
+        createdBy=None,
+        modifiedAt=None,
+        owner=None,
+        campaignId=None,
+        projectId=None,
+        status=None,
+        version=None
+    ):
+        self.id = id
+        self.createdAt = createdAt
+        self.createdBy = createdBy
+        self.modifiedAt = modifiedAt
+        self.owner = owner
+        self.campaignId = campaignId
+        self.projectId = projectId
+        self.status = status
+        self.version = version
+
+    def to_dict(self):
+        return dict(
+            id=self.id,
+            createdAt=self.createdAt,
+            createdBy=self.createdBy,
+            modifiedAt=self.modifiedAt,
+            owner=self.owner,
+            campaignId=self.campaignId,
+            projectId=self.projectId,
+            status=self.status,
+            version=self.version
+        )
+
+    def update_job_status(self, status):
+        self.status = status
+        return self.update()
+
+    @classmethod
+    def from_dict(cls, d):
+        return cls(
+            id=d.get("id"),
+            createdAt=d.get("createdAt"),
+            createdBy=d.get("createdBy"),
+            modifiedAt=d.get("modifiedAt"),
+            owner=d.get("owner"),
+            campaignId=d.get("campaignId"),
+            projectId=d.get("projectId"),
+            status=d.get("status"),
+            version=d.get("version"),
+        )
+
--- app-hitl/hitl/run_hitl.py
+++ app-hitl/hitl/run_hitl.py
@@ -1,13 +1,24 @@
+import logging
+import os
+
 import click
 import geopandas as gpd
 
-from utils import merge_labels_with_task_grid
+from utils.merge_labels import merge_labels_with_task_grid
 
+logger = logging.getLogger(__name__)
+HOST = os.getenv("RF_HOST")
+JOB_ATTEMPT = int(os.getenv("AWS_BATCH_JOB_ATTEMPT", -1))
 
-@click.command()
-@click.argument('hitl_project_id')
-def run_hitl(hitl_project_id):
+@click.command(name="run")
+@click.argument('job_id')
+def run(job_id):
+    """Run a HITL job to generate label predictions
+
+    Args:
+        job_id (str): ID of a HITL Job to run
+    """
     pass
 
 if __name__ == '__main__':
-    run_hitl()
+    run()
