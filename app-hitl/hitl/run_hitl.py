77a5ecda41eecf9fdb775926ddf1989d97d8b0c5
simonkassel <kassel.simon@gmail.com>
2022-07-18 10:06:41 -0400

    Remove placeholder code in entrypoint

--- app-hitl/hitl/run_hitl.py
+++ app-hitl/hitl/run_hitl.py
@@ -7,16 +7,7 @@ from utils import merge_labels_with_task_grid
 @click.command()
 @click.argument('hitl_project_id')
 def run_hitl(hitl_project_id):
-    print('PLACEHOLDER:')
-    print(f'Processing HITL project "{hitl_project_id}"')
-    tasks = gpd.read_file('/opt/src/data/hitl/hitl-tasks.json')
-    labels = gpd.read_file('/opt/src/data/hitl/hitl-predictions.geojson')
-    merged_labels = merge_labels_with_task_grid(labels, tasks)
-    print('merged labels (head):')
-    print(merged_labels.head())
-    print(
-        f'{labels.shape[0]} labels before intersection, {merged_labels.shape[0]} labels after')
-
+    pass
 
 if __name__ == '__main__':
     run_hitl()
